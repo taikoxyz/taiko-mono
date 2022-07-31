e652f47f95388ae2abf2633c9ad4c1559f161399
David <david@taikocha.in>
2022-07-31 21:42:02 +0800

    feat: add deploy_L1 task

--- packages/protocol/hardhat.config.ts
+++ packages/protocol/hardhat.config.ts
@@ -7,6 +7,7 @@ import "hardhat-abi-exporter"
 import "hardhat-gas-reporter"
 import { HardhatUserConfig, task } from "hardhat/config"
 import "solidity-coverage"
+import "./tasks/deploy_L1"
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
