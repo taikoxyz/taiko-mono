a51efc69e12947ca99f7b5d04e46c81f815a4e81
David <david@taikocha.in>
2022-07-31 20:39:25 +0800

    test: update test scripts

--- packages/protocol/test/l2_genesis/generate_l2_genesis.test.ts
+++ packages/protocol/test/l2_genesis/generate_l2_genesis.test.ts
@@ -1,6 +1,7 @@
 const hre = require("hardhat")
 const { expect } = require("chai")
 
+const ethers = hre.ethers
 const action = process.env.TEST_L2_GENESIS ? describe : describe.skip
 
 action("Generate L2 Genesis", function () {
@@ -14,14 +15,13 @@ action("Generate L2 Genesis", function () {
         "http://localhost:18545"
     )
 
-    // const signer = new hre.ethers.Wallet(
-    //     "2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200",
-    //     provider
-    // )
+    const signer = new hre.ethers.Wallet(
+        "2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200",
+        provider
+    )
 
     const testConfig = require("./test_config")
 
-    // const contractOwner = testConfig.contractOwner
     const premintEthAccounts = testConfig.premintEthAccounts
 
     before(async () => {
@@ -86,9 +86,47 @@ action("Generate L2 Genesis", function () {
         )
     })
 
-    describe("variables should be initialized", function () {})
+    describe("contracts can be called normally", function () {
+        it("LibTxList", async function () {
+            const LibTxListAlloc = getContractAlloc("LibTxList")
 
-    describe("tx can be sent normally", function () {})
+            const LibTxList = new hre.ethers.Contract(
+                LibTxListAlloc.address,
+                require("../../artifacts/contracts/libs/LibTxList.sol/LibTxList.json").abi,
+                signer
+            )
+
+            const txListHash = await LibTxList.hashTxList([])
+
+            expect(txListHash.length).to.be.equal(2 + 32 * 2)
+        })
+
+        it("TaikoL2", async function () {
+            const TaikoL2Alloc = getContractAlloc("TaikoL2")
+
+            const TaikoL2 = new hre.ethers.Contract(
+                TaikoL2Alloc.address,
+                require("../../artifacts/contracts/L2/TaikoL2.sol/TaikoL2.json").abi,
+                signer
+            )
+
+            const anchorHeight = 1
+            const anchorHash = ethers.utils.hexlify(
+                ethers.utils.randomBytes(32)
+            )
+
+            const tx = await TaikoL2.anchor(anchorHeight, anchorHash)
+
+            const { events } = await tx.wait()
+
+            const anchoredEvent = events.filter(
+                ({ event }: any) => event === "Anchored"
+            )[0]
+
+            expect(anchoredEvent.args[0]).to.be.equal(anchorHeight)
+            expect(anchoredEvent.args[1]).to.be.equal(anchorHash)
+        })
+    })
 
     function getContractAlloc(name: string): any {
         for (const address of Object.keys(alloc)) {
