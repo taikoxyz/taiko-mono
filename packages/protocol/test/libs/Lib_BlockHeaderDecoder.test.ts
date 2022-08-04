fd0e17a924171ea2fb2c1fe15c476142f3218287
RogerLamTd <bob.rogerbob1@gmail.com>
2022-08-04 14:50:45 -0400

    Structure of test set

--- packages/protocol/test/libs/Lib_BlockHeaderDecoder.test.ts
+++ packages/protocol/test/libs/Lib_BlockHeaderDecoder.test.ts
@@ -2,13 +2,41 @@
 import { expect } from "chai"
 const hre = require("hardhat")
 const ethers = hre.ethers
+// const EBN = ethers.BigNumber
 
 describe("Lib_BlockHeaderDecoder", async function () {
     // eslint-disable-next-line no-unused-vars
     let blockHeaderDecoder: any
+    let blockHash: any
+    let blockHeader: any
+    let headerStateRoot: any
+    let headerTimeStamp: any
+
     before(async function () {
-        blockHeaderDecoder = await (
-            await ethers.getContractFactory("TestLibBlockHeaderDecoder")
+        // Deploying Lib to Link
+        const blkHdrDcdrLib = await (
+            await ethers.getContractFactory("Lib_BlockHeaderDecoder")
         ).deploy()
+
+        // Deploying Library
+        blockHeaderDecoder = await (
+            await ethers.getContractFactory("TestLibBlockHeaderDecoder", {
+                libraries: {
+                    Lib_BlockHeaderDecoder: blkHdrDcdrLib.address,
+                },
+            })
+        ).deploy()
+
+        // Defining test block header and hash
+    })
+
+    it("Decode should return stateRoot and timeStamp", async function () {
+        // console.log(await ethers.utils.isBytesLike(blockHeader))
+        expect(
+            await blockHeaderDecoder.decodeBlockHeader(
+                await ethers.utils.RLP.encode(blockHeader),
+                blockHash
+            )
+        ).to.equal({ headerStateRoot, headerTimeStamp })
     })
 })
