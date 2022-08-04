f132ab6a7f837a4794a2d739a807ffb9edb1e7c4
RogerLamTd <bob.rogerbob1@gmail.com>
2022-08-04 11:14:09 -0400

    Implemented ConfigManager.sol and unit tests, also made variable private.

--- packages/protocol/contracts/common/ConfigManager.sol
+++ packages/protocol/contracts/common/ConfigManager.sol
@@ -10,15 +10,31 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-// TODO: implement this contract
 contract ConfigManager is OwnableUpgradeable {
+    /*************
+     * Variables *
+     *************/
+
+    mapping(bytes32 => bytes) private keys;
+
+    /*************
+     * Events *
+     *************/
+
+    event KeySet(string indexed _name, bytes _newKey, bytes _oldKey);
+
     function init() external initializer {
         OwnableUpgradeable.__Ownable_init();
     }
 
     function set(string calldata name, bytes calldata key) external onlyOwner {
-        // TODO: implement and emit event
+        bytes32 _nameHash = keccak256(abi.encodePacked(name));
+        bytes memory _oldKey = keys[_nameHash];
+        keys[_nameHash] = key;
+        emit KeySet(name, key, _oldKey);
     }
 
-    function get(string memory name) public view returns (bytes memory) {}
+    function get(string memory name) public view returns (bytes memory) {
+        return keys[keccak256(abi.encodePacked(name))];
+    }
 }
--- packages/protocol/test/ConfigManager.test.ts
+++ packages/protocol/test/ConfigManager.test.ts
@@ -2,27 +2,27 @@ import { expect } from "chai"
 const hre = require("hardhat")
 const ethers = hre.ethers
 
-describe("KeyManager tests", function () {
-    let keyManager: any
+describe("ConfigManager tests", function () {
+    let configManager: any
     before(async function () {
-        keyManager = await (
-            await ethers.getContractFactory("KeyManager")
+        configManager = await (
+            await ethers.getContractFactory("ConfigManager")
         ).deploy()
-        await keyManager.init()
+        await configManager.init()
     })
     describe("Testing set and get Key", async function () {
         const testKey = ethers.utils.hexlify(ethers.utils.randomBytes(32))
         const testName = "test"
 
         it("setKey should not revert & emit event KeySet", async function () {
-            await expect(keyManager.setKey(testName, testKey)).to.emit(
-                keyManager,
+            await expect(configManager.set(testName, testKey)).to.emit(
+                configManager,
                 "KeySet"
             )
         })
 
         it("getKey with testName should return testKey", async function () {
-            expect(await keyManager.getKey(testName)).to.equal(testKey)
+            expect(await configManager.get(testName)).to.equal(testKey)
         })
     })
 })
