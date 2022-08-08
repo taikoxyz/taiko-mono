c1ac41dc5ac20fb589a2c7baa75f54dd38f451bd
dantaik <99078276+dantaik@users.noreply.github.com>
2022-08-08 18:56:00 +0800

    Update TestLibBlockHeaderDecoder.sol

--- packages/protocol/contracts/test/TestLibBlockHeaderDecoder.sol
+++ packages/protocol/contracts/test/TestLibBlockHeaderDecoder.sol
@@ -11,11 +11,16 @@ pragma solidity ^0.8.9;
 import "../thirdparty/Lib_BlockHeaderDecoder.sol";
 
 contract TestLibBlockHeaderDecoder {
-    function decodeBlockHeader(bytes calldata blockHeader, bytes32 blockHash)
-        public
-        pure
-        returns (bytes32 _stateRoot, uint256 _timestamp)
-    {
-        return Lib_BlockHeaderDecoder.decodeBlockHeader(blockHeader, blockHash);
+    function decodeBlockHeader(
+        bytes calldata blockHeader,
+        bytes32 expectedBlockHash,
+        bool postEIP1559
+    ) public pure returns (bytes32 _stateRoot, uint256 _timestamp) {
+        return
+            Lib_BlockHeaderDecoder.decodeBlockHeader(
+                blockHeader,
+                expectedBlockHash,
+                postEIP1559
+            );
     }
 }
