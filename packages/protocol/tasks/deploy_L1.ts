c4c38cd9dc1fcefc151b8458d0c0741cd00a5ffe
David <104078303+davidtaikocha@users.noreply.github.com>
2022-08-10 19:25:42 +0800

    [deployment] update L1 deploying scripts (#76)
    * [deployment] update L1 deploying scripts
    
    * chore: update deploy_l1 script

--- packages/protocol/tasks/deploy_L1.ts
+++ packages/protocol/tasks/deploy_L1.ts
@@ -1,3 +1,4 @@
+import * as ethers from "ethers"
 import { task } from "hardhat/config"
 import * as types from "hardhat/internal/core/params/argumentTypes"
 import * as config from "./config"
@@ -5,6 +6,42 @@ import * as log from "./log"
 import * as utils from "./utils"
 
 task("deploy_L1")
+    .addParam("daoVault", "The DAO vault address")
+    .addParam("teamVault", "The team vault address")
+    .addOptionalParam(
+        "taikoL2",
+        "The TaikoL2 address",
+        ethers.constants.AddressZero
+    )
+    .addOptionalParam(
+        "l2GenesisBlockHash",
+        "L2 genesis block hash",
+        ethers.constants.HashZero
+    )
+    .addOptionalParam(
+        "gasPriceNow",
+        "The initialization parameter `_gasPriceNow` of TaikoProtoBroker",
+        0,
+        types.int
+    )
+    .addOptionalParam(
+        "amountToMintToDAOThreshold",
+        "The initialization parameter `_amountToMintToDAOThreshold` of TaikoProtoBroker",
+        1,
+        types.int
+    )
+    .addOptionalParam(
+        "amountMintToDAO",
+        "The initialization parameter `_amountMintToDAO` of TaikoProtoBroker",
+        0,
+        types.int
+    )
+    .addOptionalParam(
+        "amountMintToTeam",
+        "The initialization parameter `_amountMintToTeam` of TaikoProtoBroker",
+        0,
+        types.int
+    )
     .addOptionalParam(
         "confirmations",
         "Number of confirmations to wait for deploy transaction.",
@@ -29,42 +66,90 @@ task("deploy_L1")
     })
 
 export async function deployContracts(hre: any) {
-    const ethers = hre.ethers
     const network = hre.network.name
     const { chainId } = await hre.ethers.provider.getNetwork()
     const deployer = await utils.getDeployer(hre)
-    const l2GenesisBlockHash =
-        process.env.L2_GENESIS_BLOCK_HASH || ethers.constants.HashZero
-    const l2GenesisStateRoot =
-        process.env.L2_GENESIS_STATE_ROOT || ethers.constants.HashZero
+    const daoVault = hre.args.daoVault
+    const teamVault = hre.args.teamVault
+    const l2GenesisBlockHash = hre.args.l2GenesisBlockHash
+    const taikoL2Address = hre.args.taikoL2
+    // Initialization parameters of TaikoProtoBroker
+    const gasPriceNow = hre.args.gasPriceNow
+    const amountToMintToDAOThreshold = hre.args.amountToMintToDAOThreshold
+    const amountMintToDAO = hre.args.amountMintToDAO
+    const amountMintToTeam = hre.args.amountMintToTeam
 
     log.debug(`network: ${network}`)
     log.debug(`chainId: ${chainId}`)
     log.debug(`deployer: ${deployer}`)
-    log.debug(`L2_GENESIS_BLOCK_HASH: ${l2GenesisBlockHash}`)
-    log.debug(`L2_GENESIS_STATE_ROOT: ${l2GenesisStateRoot}`)
+    log.debug(`daoVault: ${daoVault}`)
+    log.debug(`l2GenesisBlockHash: ${l2GenesisBlockHash}`)
+    log.debug(`taikoL2Address: ${taikoL2Address}`)
+    log.debug(`gasPriceNow: ${gasPriceNow}`)
+    log.debug(`amountToMintToDAOThreshold: ${amountToMintToDAOThreshold}`)
+    log.debug(`amountMintToDAO: ${amountMintToDAO}`)
+    log.debug(`amountMintToTeam: ${amountMintToTeam}`)
     log.debug(`confirmations: ${hre.args.confirmations}`)
     log.debug()
 
-    const { KeyManager, LibZKP, LibMerkleProof, LibTxList } =
-        await deployBaseLibs(hre)
-
-    // deploy TaikoL1 and init
-    const taikoL1 = await utils.deployContract(hre, "TaikoL1", {
-        LibZKP,
-        LibMerkleProof,
-        LibTxList,
-    })
-
+    // AddressManager
+    const AddressManager = await utils.deployContract(hre, "AddressManager")
+    await utils.waitTx(hre, await AddressManager.init())
     await utils.waitTx(
         hre,
-        await taikoL1.init(
-            {
-                blockHash: l2GenesisBlockHash,
-                stateRoot: l2GenesisStateRoot,
-            },
-            KeyManager
+        await AddressManager.setAddress("dao_vault", daoVault)
+    )
+    await utils.waitTx(
+        hre,
+        await AddressManager.setAddress("team_vault", teamVault)
+    )
+    await utils.waitTx(
+        hre,
+        await AddressManager.setAddress("taiko_l2", taikoL2Address)
+    )
+
+    // TaiToken
+    const TaiToken = await utils.deployContract(hre, "TaiToken")
+    await utils.waitTx(hre, await TaiToken.init(AddressManager.address))
+    await utils.waitTx(
+        hre,
+        await AddressManager.setAddress("tai_token", TaiToken.address)
+    )
+
+    // Proto Broker
+    const ProtoBroker = await utils.deployContract(hre, "TaikoProtoBroker")
+    await utils.waitTx(
+        hre,
+        await AddressManager.setAddress("proto_broker", ProtoBroker.address)
+    )
+    await utils.waitTx(
+        hre,
+        await ProtoBroker.init(
+            AddressManager.address,
+            gasPriceNow,
+            amountToMintToDAOThreshold,
+            amountMintToDAO,
+            amountMintToTeam
         )
+    )
+
+    // Config manager
+    const ConfigManager = await utils.deployContract(hre, "ConfigManager")
+    await utils.waitTx(hre, await ConfigManager.init())
+    await utils.waitTx(
+        hre,
+        await AddressManager.setAddress("config_manager", ConfigManager.address)
+    )
+
+    // TaikoL1
+    const taikoL1 = await utils.deployContract(
+        hre,
+        "TaikoL1",
+        await deployBaseLibs(hre)
+    )
+    await utils.waitTx(
+        hre,
+        await taikoL1.init(AddressManager.address, l2GenesisBlockHash)
     )
 
     // save deployments
@@ -72,8 +157,13 @@ export async function deployContracts(hre: any) {
         network: network,
         chainId: chainId,
         deployer: deployer,
-        l2GenesisStateRoot,
-        contracts: Object.assign({ KeyManager }, { TaikoL1: taikoL1.address }),
+        l2GenesisBlockHash,
+        contracts: Object.assign(
+            { AddressManager: AddressManager.address },
+            { TaiToken: TaiToken.address },
+            { ProtoBroker: ProtoBroker.address },
+            { TaikoL1: taikoL1.address }
+        ),
     }
 
     utils.saveDeployments(`${network}_L1`, deployments)
@@ -82,17 +172,13 @@ export async function deployContracts(hre: any) {
 }
 
 async function deployBaseLibs(hre: any) {
-    const keyManager = await utils.deployContract(hre, "KeyManager")
-    await utils.waitTx(hre, await keyManager.init())
-
     const libZKP = await utils.deployContract(hre, "LibZKP")
     const libMerkleProof = await utils.deployContract(hre, "LibMerkleProof")
-    const libTxList = await utils.deployContract(hre, "LibTxList")
+    const libTxListDecoder = await utils.deployContract(hre, "LibTxListDecoder")
 
     return {
-        KeyManager: keyManager.address,
         LibZKP: libZKP.address,
         LibMerkleProof: libMerkleProof.address,
-        LibTxList: libTxList.address,
+        LibTxListDecoder: libTxListDecoder.address,
     }
 }
