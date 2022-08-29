9eb4e7749bc3e2e8d854bc8be2865f2f07bb7db4
dantaik <99078276+dantaik@users.noreply.github.com>
2022-08-29 17:53:00 +0800

    new way to handle anchor TX (#100)

--- packages/protocol/contracts/libs/LibTaikoConstants.sol
+++ packages/protocol/contracts/libs/LibTaikoConstants.sol
@@ -13,4 +13,9 @@ library LibTaikoConstants {
     uint256 public constant TAIKO_BLOCK_MAX_TXS = 20; // TODO
     uint256 public constant TAIKO_BLOCK_MAX_TXLIST_BYTES = 1000000; // TODO
     uint256 public constant TAIKO_TX_MIN_GAS_LIMIT = 10000; // TODO
+
+    address public constant GOLD_FINGER_ADDRESS =
+        0x00000040Bd11E9804ac180704A2aA76A750f4e02;
+    bytes public constant GOLD_FINGURE_PRIVATE_KEY =
+        "22789a6923ce63b50ff9f82879e404236d6fbc5a0db35b6fc72b98db0ec383a1";
 }
