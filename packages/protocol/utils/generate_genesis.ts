26ee9731906cdd95902134c89f6d2e6708a25bbd
David <david@taiko.xyz>
2022-09-19 13:18:55 +0800

    fix: fix workflow errors

--- packages/protocol/utils/generate_genesis.ts
+++ packages/protocol/utils/generate_genesis.ts
@@ -227,6 +227,16 @@ async function generateContractConfigs(
                 _status: 1, // _NOT_ENTERED
                 // AddressResolver
                 _addressManager: addressMap.AddressManager,
+                // V1TaikoL2
+                // keccak256(abi.encodePacked(block.chainid, basefee, ancestors))
+                publicInputHash: `${ethers.utils.solidityKeccak256(
+                    ["uint256", "uint256", "bytes32[255]"],
+                    [
+                        config.chainId,
+                        0,
+                        new Array(255).fill(ethers.constants.HashZero),
+                    ]
+                )}`,
             },
         },
     }
