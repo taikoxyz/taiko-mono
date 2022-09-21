134b7711e2a126f90e81ff3cf29b944ba1a38f54
dantaik <dan@taiko.xyz>
2022-09-21 13:25:40 +0800

    Default chain ID to 167

--- packages/protocol/tasks/config.ts
+++ packages/protocol/tasks/config.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_DEPLOY_CONFIRMATIONS = 12
-export const TAIKO_CHAINID = 1337 // TODO: change chainId when it get registered
+export const TAIKO_CHAINID = 167 // TODO: change chainId when it get registered
