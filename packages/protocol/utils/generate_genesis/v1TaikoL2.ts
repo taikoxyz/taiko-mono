94a7f9ad28fbab6ce7cd78e539a5e13bdc5aeabd
David <104078303+davidtaikocha@users.noreply.github.com>
2022-09-27 16:33:55 +0800

    [protocol] pre-deploy an ERC-20 token in L2 genesis (#162)

--- packages/protocol/utils/generate_genesis/v1TaikoL2.ts
+++ packages/protocol/utils/generate_genesis/v1TaikoL2.ts
@@ -1,4 +1,4 @@
-const fs = require("fs")
+import { Config, Result } from "./interface"
 const path = require("path")
 const { ethers } = require("ethers")
 // eslint-disable-next-line node/no-extraneous-require
@@ -7,71 +7,26 @@ const {
     computeStorageSlots,
     getStorageLayout,
 } = require("@defi-wonderland/smock/dist/src/utils")
+const ARTIFACTS_PATH = path.join(__dirname, "../../artifacts/contracts")
 
-const ARTIFACTS_PATH = path.join(__dirname, "../artifacts/contracts")
-
-if (process.argv.length < 3) {
-    throw new Error("missing config json")
-}
-
-const config = require(path.isAbsolute(process.argv[2])
-    ? process.argv[2]
-    : path.join(process.cwd(), process.argv[2]))
-
-const contractOwner = config.contractOwner
-const ethDepositor = config.ethDepositor
-const chainId = config.chainId
-const premintEthAccounts = config.premintEthAccounts
-
-if (
-    !ethers.utils.isAddress(contractOwner) ||
-    !ethers.utils.isAddress(ethDepositor) ||
-    !Number.isInteger(chainId) ||
-    !Array.isArray(premintEthAccounts) ||
-    !premintEthAccounts.every((premintEthAccount) => {
-        return (
-            Object.keys(premintEthAccount).length === 1 &&
-            ethers.utils.isAddress(Object.keys(premintEthAccount)[0]) &&
-            Number.isInteger(Object.values(premintEthAccount)[0])
-        )
-    })
-) {
-    throw new Error(
-        `invalid input: ${JSON.stringify({
-            contractOwner,
-            chainId,
-            premintEthAccounts,
-        })}`
-    )
-}
-
-generateL2Genesis(
-    contractOwner as string,
-    chainId as number,
-    premintEthAccounts as Array<any>
-).catch(console.error)
-
-// generateL2Genesis generates a L2 genesis `alloc`.
-// ref: https://docs.soliditylang.org/en/latest/internals/layout_in_storage.html
-async function generateL2Genesis(
-    contractOwner: string,
-    chainId: number,
-    premintEthAccounts: Array<any>
-) {
-    console.log(`contractOwner: ${contractOwner}`)
-    console.log(`chainId: ${chainId}`)
+// deployV1TaikoL2 generates a L2 genesis alloc of the V1TaikoL2 contract.
+export async function deployV1TaikoL2(
+    config: Config,
+    result: Result
+): Promise<Result> {
+    const { contractOwner, chainId, seedAccounts, ethDepositor } = config
 
     const alloc: any = {}
 
     let bridgeBalance = ethers.BigNumber.from("2").pow(128).sub(1) // MaxUint128
 
-    for (const premintEthAccount of premintEthAccounts) {
-        const accountAddress = Object.keys(premintEthAccount)[0]
+    for (const seedAccount of seedAccounts) {
+        const accountAddress = Object.keys(seedAccount)[0]
         const balance = ethers.utils.parseEther(
-            `${Object.values(premintEthAccount)[0]}`
+            `${Object.values(seedAccount)[0]}`
         )
 
-        console.log(`premintAccountAddress: ${accountAddress}`)
+        console.log(`seedAccountAddress: ${accountAddress}`)
         console.log(`premintBalance: ${balance}`)
 
         alloc[accountAddress] = { balance: balance.toHexString() }
@@ -84,7 +39,8 @@ async function generateL2Genesis(
 
     const contractConfigs: any = await generateContractConfigs(
         contractOwner,
-        chainId
+        chainId,
+        ethDepositor
     )
 
     const storageLayouts: any = {}
@@ -123,30 +79,18 @@ async function generateL2Genesis(
         }
     }
 
-    const allocSavedPath = path.join(
-        __dirname,
-        "../deployments/genesis_alloc.json"
-    )
+    result.alloc = Object.assign(result.alloc, alloc)
+    result.storageLayouts = Object.assign(result.storageLayouts, storageLayouts)
 
-    fs.writeFileSync(allocSavedPath, JSON.stringify(alloc, null, 2))
-
-    const layoutSavedPath = path.join(
-        __dirname,
-        "../deployments/genesis_storage_layout.json"
-    )
-
-    fs.writeFileSync(layoutSavedPath, JSON.stringify(storageLayouts, null, 2))
-
-    console.log("done")
-    console.log(`alloc JSON saved to ${allocSavedPath}`)
-    console.log(`layout JSON saved to ${layoutSavedPath}`)
+    return result
 }
 
 // generateContractConfigs returns all L2 contracts address, deployedBytecode,
 // and initialized variables.
 async function generateContractConfigs(
     contractOwner: string,
-    chainId: number
+    chainId: number,
+    ethDepositor: string
 ): Promise<any> {
     const contractArtifacts: any = {
         AddressManager: require(path.join(
@@ -204,7 +148,7 @@ async function generateContractConfigs(
                     [`${ethers.utils.solidityKeccak256(
                         ["string"],
                         ["eth_depositor"]
-                    )}`]: config.ethDepositor,
+                    )}`]: ethDepositor,
                 },
             },
         },
@@ -232,7 +176,7 @@ async function generateContractConfigs(
                 publicInputHash: `${ethers.utils.solidityKeccak256(
                     ["uint256", "uint256", "uint256", "bytes32[255]"],
                     [
-                        config.chainId,
+                        chainId,
                         0,
                         0,
                         new Array(255).fill(ethers.constants.HashZero),
