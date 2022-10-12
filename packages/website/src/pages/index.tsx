c9e1e97b6895caba6947a8afd83553a83fd9b75e
David Weisiger <dweisiger@gmail.com>
2022-10-12 10:47:44 -0500

    fix: meta description

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description={`${siteConfig.tagline}`}
     >
       <HomepageHeader />
       <main>
