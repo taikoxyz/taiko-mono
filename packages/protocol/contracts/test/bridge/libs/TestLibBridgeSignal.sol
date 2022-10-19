3f954438689b7eedbc577d948ac34998eceff800
RogerLamTd <bob.rogerbob1@gmail.com>
2022-10-19 12:35:20 -0400

    remove unused vars from LibBridgeData, add LibBridgeSignal stubs

---
+++ packages/protocol/contracts/test/bridge/libs/TestLibBridgeSignal.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+//
+// ╭━━━━╮╱╱╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮
+// ┃╭╮╭╮┃╱╱┃┃╱╱╱╱╱┃┃╱╱╱╱╱┃┃
+// ╰╯┃┃┣┻━┳┫┃╭┳━━╮┃┃╱╱╭━━┫╰━┳━━╮
+// ╱╱┃┃┃╭╮┣┫╰╯┫╭╮┃┃┃╱╭┫╭╮┃╭╮┃━━┫
+// ╱╱┃┃┃╭╮┃┃╭╮┫╰╯┃┃╰━╯┃╭╮┃╰╯┣━━┃
+// ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
+pragma solidity ^0.8.9;
+
+import "../../../bridge/libs/LibBridgeData.sol";
+import "../../../bridge/libs/LibBridgeSignal.sol";
+
+contract TestLibBridgeSignal {}
---
+++ packages/protocol/test/bridge/libs/LibBridgeSignal.test.ts
@@ -0,0 +1,60 @@
+// import { expect } from "chai"
+import { AddressManager } from "../../../typechain"
+import { ethers } from "hardhat"
+// import { TAIKO_BRIDGE_MESSAGE } from "../../constants/messages"
+
+describe("LibBridgeData", function () {
+    async function deployLibBridgeSignalFixture() {
+        const [owner, nonOwner] = await ethers.getSigners()
+        // deploy addressManager
+        const addressManager: AddressManager = await (
+            await ethers.getContractFactory("AddressManager")
+        ).deploy()
+        await addressManager.init()
+
+        const libSignal = await (
+            await ethers.getContractFactory("TestLibBridgeSignal")
+        ).deploy()
+        return { owner, nonOwner, addressManager, libSignal }
+    }
+
+    describe("LibBridgeSignal", async function () {
+        describe("sendSignal()", async function () {
+            it.only("stub", async function () {
+                const { owner, nonOwner, addressManager, libSignal } =
+                    await deployLibBridgeSignalFixture()
+                console.log(owner, nonOwner, addressManager, libSignal)
+            })
+            //     it.only("throws when sender is zero address", async function () {
+            //         const { owner, nonOwner, bridge, enabledDestChainId } =
+            //             await deployBridgeFixture()
+            //         const message: Message = {
+            //             id: 1,
+            //             sender: owner.address,
+            //             srcChainId: 1,
+            //             destChainId: enabledDestChainId,
+            //             owner: owner.address,
+            //             to: nonOwner.address,
+            //             refundAddress: owner.address,
+            //             depositValue: 1,
+            //             callValue: 1,
+            //             processingFee: 1,
+            //             gasLimit: 100,
+            //             data: ethers.constants.HashZero,
+            //             memo: "",
+            //         }
+            //         const expectedAmount =
+            //             message.depositValue + message.callValue + message.processingFee
+            //         const signal = await bridge.sendMessage(message, {
+            //             value: expectedAmount,
+            //         })
+            //         await signal.wait()
+            //         expect(
+            //             await bridge
+            //                 .connect(ethers.constants.AddressZero)
+            //                 .sendSignal(signal)
+            //         ).to.be.revertedWith("B:sender")
+            //     })
+        })
+    })
+})
