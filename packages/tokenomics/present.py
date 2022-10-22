2c4cf36f0ed17bd9833d9adfae5e41615ef239e7
dantaik <99078276+dantaik@users.noreply.github.com>
2022-10-23 00:01:42 +0800

    Update present.py

--- packages/tokenomics/present.py
+++ packages/tokenomics/present.py
@@ -1,10 +1,12 @@
 from typing import NamedTuple
 
+
 class Timing(NamedTuple):
     block_time_avg_second: int
     block_time_sd_pctg: int
     proof_time_avg_minute: int
-    proof_time_sd_pctg: int  
+    proof_time_sd_pctg: int
+
 
 class Config(NamedTuple):
     max_slots: int
@@ -16,9 +18,9 @@ class Config(NamedTuple):
     block_and_proof_time_maf: int
     timing: list[Timing]
 
+
 class Present(NamedTuple):
     title: str
     desc: str
     days: int
     config: Config
-
