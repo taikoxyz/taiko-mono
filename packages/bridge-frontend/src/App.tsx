5d307c1f9f553c5526a40cbb0a8e708f74961678
Shadab Khan <shadab@taiko.xyz>
2022-10-26 20:31:58 +0530

    [bridge] add rainbowkit

--- packages/bridge-frontend/src/App.tsx
+++ packages/bridge-frontend/src/App.tsx
@@ -1,36 +1,55 @@
 import React from "react";
 import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+// import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+import { mainnet, taiko } from "./config/chains";
 
 import Home from "./pages/Home";
+
+const { chains, provider } = configureChains(
+  [mainnet, taiko],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function App() {
   const [wallet, setWallet] = React.useState();
   return (
-    <Router>
-      <header className="bg-white h-12 p-2 flex items-center justify-between text-taiko-blue">
-        <Link to="/" className="flex items-center text-taiko-blue">
-          <img src="/taikologo.png" alt="" height={48} width={48} />
-          <span className="text-xl">Taiko</span>
-        </Link>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Router>
+          <header className="bg-white h-12 p-2 flex items-center justify-between text-taiko-blue">
+            <Link to="/" className="flex items-center text-taiko-blue">
+              <img src="/taikologo.png" alt="" height={48} width={48} />
+              <span className="text-xl">Taiko</span>
+            </Link>
 
-        <button
-          type="button"
-          className={`${
-            wallet
-              ? "bg-transparent border border-taiko-pink"
-              : "bg-taiko-pink text-white"
-          } w-[150px] rounded-md py-2`}
-        >
-          {wallet ? "0x1234...678" : "Connect Wallet"}
-        </button>
-      </header>
+            <ConnectButton />
+          </header>
 
-      <main className="bg-taiko-blue h-[calc(100vh-48px)] text-taiko-blue">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </main>
-    </Router>
+          <main className="bg-taiko-blue h-[calc(100vh-48px)] text-taiko-blue">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </main>
+        </Router>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
--- packages/bridge-frontend/src/config/chains.ts
+++ packages/bridge-frontend/src/config/chains.ts
@@ -1,4 +1,5 @@
-import { Chain } from "../types";
+import { Chain as WagmiChain } from "wagmi";
+import type { Chain } from "../types";
 
 export const chains: Record<string, Chain> = {
   31336: {
@@ -12,5 +13,51 @@ export const chains: Record<string, Chain> = {
     name: "Taiko",
     rpc: "http://rpc.a1.testnet.taiko.xyz",
     enabled: true,
+  },
+};
+
+export const mainnet: WagmiChain = {
+  id: 31336,
+  name: "Mainnet",
+  network: "",
+  nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: "https://34.132.67.34:8545",
+  },
+  blockExplorers: {
+    default: {
+      name: "Main",
+      url: "https://34.132.67.34:4000",
+    },
+  },
+  ens: {
+    address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+  },
+  multicall: {
+    address: "0xca11bde05977b3631167028862be2a173976ca11",
+    blockCreated: 14353601,
+  },
+};
+
+export const taiko: WagmiChain = {
+  id: 167001,
+  name: "Taiko",
+  network: "",
+  nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: "https://rpc.a1.testnet.taiko.xyz",
+  },
+  blockExplorers: {
+    default: {
+      name: "Main",
+      url: "https://a1.testnet.taiko.xyz",
+    },
+  },
+  ens: {
+    address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+  },
+  multicall: {
+    address: "0xca11bde05977b3631167028862be2a173976ca11",
+    blockCreated: 14353601,
   },
 };
