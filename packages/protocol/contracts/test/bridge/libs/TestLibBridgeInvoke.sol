0267753567d9fbf29be1b758c82cddf45d53654f
RogerLamTd <bob.rogerbob1@gmail.com>
2022-10-27 15:15:46 -0400

    LibBridgeInvoke tests

---
+++ packages/protocol/contracts/test/bridge/libs/TestLibBridgeInvoke.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+//
+// ╭━━━━╮╱╱╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮
+// ┃╭╮╭╮┃╱╱┃┃╱╱╱╱╱┃┃╱╱╱╱╱┃┃
+// ╰╯┃┃┣┻━┳┫┃╭┳━━╮┃┃╱╱╭━━┫╰━┳━━╮
+// ╱╱┃┃┃╭╮┣┫╰╯┫╭╮┃┃┃╱╭┫╭╮┃╭╮┃━━┫
+// ╱╱┃┃┃╭╮┃┃╭╮┫╰╯┃┃╰━╯┃╭╮┃╰╯┣━━┃
+// ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
+pragma solidity ^0.8.9;
+
+import "../../../bridge/libs/LibBridgeInvoke.sol";
+
+contract TestLibBridgeInvoke {
+    LibBridgeData.State public state;
+
+    function invokeMessageCall(
+        IBridge.Message memory message,
+        bytes32 signal,
+        uint256 gasLimit
+    ) public returns (bool) {
+        return
+            LibBridgeInvoke.invokeMessageCall(state, message, signal, gasLimit);
+    }
+}
---
+++ packages/protocol/test/bridge/libs/LibBridgeInvoke.test.ts
@@ -0,0 +1,97 @@
+// import { expect } from "chai"
+import { expect } from "chai"
+import { ethers } from "hardhat"
+import { Message } from "../../utils/message"
+
+describe("LibBridgeInvoke", function () {
+    async function deployLibBridgeDataFixture() {
+        const libData = await (
+            await ethers.getContractFactory("TestLibBridgeData")
+        ).deploy()
+        return { libData }
+    }
+
+    async function deployLibBridgeInvokeFixture() {
+        const [owner, nonOwner] = await ethers.getSigners()
+
+        const libInvoke = await (
+            await ethers.getContractFactory("TestLibBridgeInvoke")
+        ).deploy()
+
+        return { owner, nonOwner, libInvoke }
+    }
+
+    describe("invokeMessageCall()", async function () {
+        it("should throw when gasLimit <= 0", async function () {
+            const { owner, nonOwner, libInvoke } =
+                await deployLibBridgeInvokeFixture()
+
+            const { libData } = await deployLibBridgeDataFixture()
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: 1,
+                destChainId: 5,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 0,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            const signal = await libData.hashMessage(message)
+
+            await expect(
+                libInvoke.invokeMessageCall(message, signal, message.gasLimit)
+            ).to.be.revertedWith("B:gasLimit")
+        })
+
+        it("should return true if message has no issues", async function () {
+            const { owner, nonOwner, libInvoke } =
+                await deployLibBridgeInvokeFixture()
+
+            const { libData } = await deployLibBridgeDataFixture()
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: 1,
+                destChainId: 5,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            const signal = await libData.hashMessage(message)
+
+            console.log(
+                await libInvoke.invokeMessageCall(
+                    message,
+                    signal,
+                    message.gasLimit
+                )
+            )
+
+            // * Why does libInvoke.invokeMessageCall return a transaction rather than a bool?
+
+            // expect(
+            //     await libInvoke.invokeMessageCall(
+            //         message,
+            //         signal,
+            //         message.gasLimit
+            //     )
+            // ).to.be.eq(true)
+        })
+    })
+})
