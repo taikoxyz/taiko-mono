7cb66a5396fc5fcb555a6e9604cab853ed4d45da
RogerLamTd <bob.rogerbob1@gmail.com>
2022-10-27 17:57:00 -0400

    Finished tests

--- packages/protocol/contracts/test/bridge/libs/TestLibBridgeSend.sol
+++ packages/protocol/contracts/test/bridge/libs/TestLibBridgeSend.sol
@@ -8,28 +8,29 @@
 // ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
 pragma solidity ^0.8.9;
 
+import "../../../common/EssentialContract.sol";
 import "../../../bridge/libs/LibBridgeSend.sol";
 
-contract TestLibBridgeSend {
+contract TestLibBridgeSend is EssentialContract {
     LibBridgeData.State public state;
 
-    function sendMessage(
-        AddressResolver resolver,
-        IBridge.Message memory message
-    ) public returns (bytes32 signal) {
-        return LibBridgeSend.sendMessage(state, resolver, message);
+    function init(address _addressManager) external initializer {
+        EssentialContract._init(_addressManager);
     }
-    
-    function enableDestChain(
-        uint256 chainId,
-        bool enabled
-    ) public {
+
+    function sendMessage(IBridge.Message memory message)
+        public
+        payable
+        returns (bytes32 signal)
+    {
+        return LibBridgeSend.sendMessage(state, AddressResolver(this), message);
+    }
+
+    function enableDestChain(uint256 chainId, bool enabled) public {
         LibBridgeSend.enableDestChain(state, chainId, enabled);
     }
 
-    function getDestChainStatus(
-        uint256 chainId
-    ) public view returns (bool) {
+    function getDestChainStatus(uint256 chainId) public view returns (bool) {
         return state.destChains[chainId];
     }
 }
--- packages/protocol/test/bridge/libs/LibBridgeSend.test.ts
+++ packages/protocol/test/bridge/libs/LibBridgeSend.test.ts
@@ -1,29 +1,26 @@
 import { expect } from "chai"
 import hre, { ethers } from "hardhat"
-import { smock } from "@defi-wonderland/smock"
-import { AddressResolver } from "../../../typechain"
 import { Message } from "../../utils/message"
 
 describe("LibBridgeSend", function () {
     async function deployLibBridgeSendFixture() {
         const [owner, nonOwner, etherVault] = await ethers.getSigners()
 
-        const libSend = await (
-            await ethers.getContractFactory("TestLibBridgeSend")
-        ).deploy()
-
         const addressManager = await (
             await ethers.getContractFactory("AddressManager")
         ).deploy()
+        await addressManager.init()
+        await addressManager.setAddress("ether_vault", etherVault.address)
 
-        const addressResolver = await smock.fake<AddressResolver>(
-            "AddressResolver"
+        const libSend = await (
+            await ethers.getContractFactory("TestLibBridgeSend")
         )
-        addressResolver["resolve(string)"]
-            .whenCalledWith("ether_vault")
-            .returns(etherVault.address)
+            .connect(owner)
+            .deploy()
 
-        return { owner, nonOwner, libSend, addressResolver }
+        await libSend.init(addressManager.address)
+
+        return { owner, nonOwner, libSend }
     }
     describe("enableDestChain()", async function () {
         it("should throw when chainId <= 0", async function () {
@@ -59,7 +56,7 @@ describe("LibBridgeSend", function () {
 
     describe("sendMessage()", async function () {
         it("should throw when message.owner == address(0)", async function () {
-            const { owner, nonOwner, libSend, addressResolver } =
+            const { owner, nonOwner, libSend } =
                 await deployLibBridgeSendFixture()
 
             const message: Message = {
@@ -77,26 +74,121 @@ describe("LibBridgeSend", function () {
                 data: ethers.constants.HashZero,
                 memo: "",
             }
-            const signal = await libSend.sendMessage(addressResolver, message)
-            // expect(
-            //     await libSend.sendMessage(addressResolver, message)
-            // ).to.be.revertedWith("B:owner")
+
+            await expect(libSend.sendMessage(message)).to.be.revertedWith(
+                "B:owner"
+            )
         })
 
         it("should throw when destchainId == block.chainId", async function () {
-            deployLibBridgeSendFixture()
+            const { owner, nonOwner, libSend } =
+                await deployLibBridgeSendFixture()
+
+            const blockChainId = hre.network.config.chainId ?? 1
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: 1,
+                destChainId: blockChainId,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            await expect(libSend.sendMessage(message)).to.be.revertedWith(
+                "B:destChainId"
+            )
         })
 
         it("should throw when destChainId has not yet been enabled", async function () {
-            deployLibBridgeSendFixture()
+            const { owner, nonOwner, libSend } =
+                await deployLibBridgeSendFixture()
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: 1,
+                destChainId: 2,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            await expect(libSend.sendMessage(message)).to.be.revertedWith(
+                "B:destChainId"
+            )
         })
 
         it("should throw when expectedAmount != msg.value", async function () {
-            deployLibBridgeSendFixture()
+            const { owner, nonOwner, libSend } =
+                await deployLibBridgeSendFixture()
+
+            await libSend.enableDestChain(2, true)
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: 1,
+                destChainId: 2,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            await expect(libSend.sendMessage(message)).to.be.revertedWith(
+                "B:value"
+            )
         })
 
         it("should emit MessageSent() event and signal should be hashed correctly", async function () {
-            deployLibBridgeSendFixture()
+            const { owner, nonOwner, libSend } =
+                await deployLibBridgeSendFixture()
+
+            await libSend.enableDestChain(100, true)
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: 1,
+                destChainId: 100,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            const expectedAmount =
+                message.depositValue + message.callValue + message.processingFee
+
+            expect(
+                await libSend.sendMessage(message, {
+                    value: expectedAmount,
+                })
+            ).to.emit(libSend, "MessageSent")
         })
     })
 })
