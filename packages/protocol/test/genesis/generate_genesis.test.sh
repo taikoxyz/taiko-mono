c5b311be82fc14e0daa49642a416ef33e4b61e32
David <david@taiko.xyz>
2022-10-28 16:20:17 +0800

    [tests] fix occasional error when running LibTrieProof's tests

--- packages/protocol/test/genesis/generate_genesis.test.sh
+++ packages/protocol/test/genesis/generate_genesis.test.sh
@@ -22,7 +22,7 @@ touch $GENESIS_JSON
 echo '
 {
   "config": {
-    "chainId": 167,
+    "chainId": 167001,
     "homesteadBlock": 0,
     "eip150Block": 0,
     "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
