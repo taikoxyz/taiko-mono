e21244537f369c37cd25aa3fe5385310b5859af7
dantaik <99078276+dantaik@users.noreply.github.com>
2022-10-30 15:09:07 +0800

    Step 5: Apply a fixed proof reward burn of 1% (#206)
    This PR burns 1% of proof reward.

--- packages/protocol/contracts/libs/LibConstants.sol
+++ packages/protocol/contracts/libs/LibConstants.sol
@@ -28,6 +28,7 @@ library LibConstants {
 
     uint256 public constant TAIKO_TXLIST_MAX_BYTES = 10240; // TODO
     uint256 public constant TAIKO_TX_MIN_GAS_LIMIT = 21000; // TODO
+    uint256 public constant TAIKO_REWARD_BURN_POINTS = 100; // 1%
 
     // Taiko L2 releated constants
     uint256 public constant V1_ANCHOR_TX_GAS_LIMIT = 250000;
