0e3ea5467ee73395bbb53471a6a53b3c90c947c4
RogerLamTd <bob.rogerbob1@gmail.com>
2022-10-30 22:35:01 -0400

    test stubs

--- packages/protocol/test/bridge/libs/LibBridgeProcess.test.ts
+++ packages/protocol/test/bridge/libs/LibBridgeProcess.test.ts
@@ -22,8 +22,28 @@ describe("LibBridgeProcess", function () {
         return { owner, nonOwner, libProcess }
     }
 
-    describe("stub", async function () {
-        it("stub", async function () {
+    describe("processMessage()", async function () {
+        it("should throw if gaslimit == 0 & msg.sender != message.owner", async function () {
+            await deployLibBridgeProcessFixture()
+        })
+
+        it("should throw if message.destChain != block.chainId", async function () {
+            await deployLibBridgeProcessFixture()
+        })
+
+        it("should throw if message's status is not NEW", async function () {
+            await deployLibBridgeProcessFixture()
+        })
+
+        it("should throw if signal has not been received", async function () {
+            await deployLibBridgeProcessFixture()
+        })
+
+        it("if message fails, refund should go to the intended account and amount", async function () {
+            await deployLibBridgeProcessFixture()
+        })
+
+        it("should pass properly, message should be processed and marked DONE", async function () {
             await deployLibBridgeProcessFixture()
         })
     })
