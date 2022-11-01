91a62b16a63129b81d9414147281d2c948568539
dantaik <99078276+dantaik@users.noreply.github.com>
2022-11-01 14:28:34 +0800

    Update V1Utils.sol

--- packages/protocol/contracts/L1/v1/V1Utils.sol
+++ packages/protocol/contracts/L1/v1/V1Utils.sol
@@ -52,6 +52,6 @@ library V1Utils {
         uint256 a = tLast + tGrace;
         uint256 b = tNow > a ? tNow - a : 0;
         uint256 tRel = (b.min(tMax) * 10000) / tMax;
-        return 10000 + (LibConstants.TAIKO_FEE_ALPHA - 1) * tRel;
+        return 10000 + ((LibConstants.TAIKO_FEE_ALPHA - 100) * tRel) / 100;
     }
 }
