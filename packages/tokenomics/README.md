6a649d3bffe7d218b583398c9bc5864e08aaa539
dantaik <99078276+dantaik@users.noreply.github.com>
2022-11-11 22:06:08 +0800

    chore(tokenomics): simulate tokenomics v1.  (#180)

---
+++ packages/tokenomics/README.md
@@ -0,0 +1,5 @@
+# README
+
+You need to install python, **salabim**, and **streamlit**. 
+Then in this directory, simply run `streamlit run main.py`,
+then visit [http://localhost:8501](http://localhost:8501/) to see simulation results.
---
+++ packages/tokenomics/presents/vbcp1.py
@@ -0,0 +1,66 @@
+from present import Config, Timing, Present
+
+present = Present(
+    title="vbcp1: constant proof time, block time goes down, up, then restores",
+    desc="""
+
+**About this config**
+
+- the proof time average set to a constant.
+- the block time average varies but eventually changes back to the initial value.
+
+**What to verify**
+- fee_base will become smaller if block time becomes larger.
+- fee_base remains the same if block time becomes smaller.
+
+""",
+    days=21,
+    config=Config(
+        max_blocks=2048, 
+        lamda=590,
+        fee_base=100.0,
+        fee_maf=1024,
+        reward_multiplier=4.0,
+        time_avg_maf=1024,
+        block_time_sd_pctg=0,
+        proof_time_sd_pctg=0,
+        timing=[
+            Timing(
+                block_time_avg_second=15,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15/1.3,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15/1.3/1.3,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15/1.3,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15*1.3,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15*1.3*1.3,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15*1.3,
+                proof_time_avg_minute=45,
+            ),
+            Timing(
+                block_time_avg_second=15,
+                proof_time_avg_minute=45,
+            ),
+        ],
+    ),
+)
