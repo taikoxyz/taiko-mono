a75031708b760ff730f3750a607b2b46ec1493b2
RogerLamTd <bob.rogerbob1@gmail.com>
2022-11-11 01:40:58 -0500

    Finished first 3 tests, 4th test is blocked

--- packages/protocol/test/bridge/libs/LibBridgeProcess.test.ts
+++ packages/protocol/test/bridge/libs/LibBridgeProcess.test.ts
@@ -1,11 +1,14 @@
-// import { expect } from "chai"
+import { expect } from "chai"
 import hre, { ethers } from "hardhat"
-// import { getSlot, decode } from "../../../tasks/utils"
+import { Message } from "../../utils/message"
+import { AddressManager, Bridge } from "../../../typechain"
+import { getSlot } from "../../../tasks/utils"
+const helpers = require("@nomicfoundation/hardhat-network-helpers")
 // ethers.provider.getBalance(address)
 
 describe("LibBridgeProcess", function () {
     async function deployLibBridgeProcessFixture() {
-        const [owner, etherVaultOwner] = await ethers.getSigners()
+        const [owner, nonOwner, etherVaultOwner] = await ethers.getSigners()
 
         // slot number of IBridge.State for TestLibBridgeProcess.
         // mapping destChains is at position 0 (201)
@@ -14,9 +17,11 @@ describe("LibBridgeProcess", function () {
         // Context takes up 3 slots, starts at position 3 (204)
         const stateSlot = 201
 
+        const srcChainId = 1
+
         const messageOwner = ethers.Wallet.createRandom()
 
-        const addressManager = await (
+        const addressManager: AddressManager = await (
             await ethers.getContractFactory("AddressManager")
         ).deploy()
         await addressManager.init()
@@ -43,7 +48,9 @@ describe("LibBridgeProcess", function () {
 
         const libTrieLink = await (
             await ethers.getContractFactory("LibTrieProof")
-        ).deploy()
+        )
+            .connect(owner)
+            .deploy()
         await libTrieLink.deployed()
 
         const libProcessLink = await (
@@ -52,7 +59,9 @@ describe("LibBridgeProcess", function () {
                     LibTrieProof: libTrieLink.address,
                 },
             })
-        ).deploy()
+        )
+            .connect(owner)
+            .deploy()
         await libProcessLink.deployed()
 
         const libProcess = await (
@@ -67,6 +76,10 @@ describe("LibBridgeProcess", function () {
 
         await libProcess.init(addressManager.address)
 
+        const testLibData = await (
+            await ethers.getContractFactory("TestLibBridgeData")
+        ).deploy()
+
         await etherVault
             .connect(etherVaultOwner)
             .authorize(libProcess.address, true)
@@ -77,24 +90,192 @@ describe("LibBridgeProcess", function () {
             DONE: 2,
         }
 
-        return { owner, messageOwner, libProcess, MessageStatus, stateSlot }
+        return {
+            owner,
+            srcChainId,
+            messageOwner,
+            libProcess,
+            MessageStatus,
+            stateSlot,
+            blockChainId,
+            nonOwner,
+            testLibData,
+            addressManager,
+        }
+    }
+    async function deployBridgeFixture() {
+        const [owner, etherVault] = await ethers.getSigners()
+
+        const addressManager: AddressManager = await (
+            await ethers.getContractFactory("AddressManager")
+        ).deploy()
+        await addressManager.init()
+
+        const chainId = hre.network.config.chainId ?? 0
+
+        await addressManager.setAddress(
+            `${chainId}.ether_vault`,
+            etherVault.address
+        )
+
+        const libTrieProof = await (
+            await ethers.getContractFactory("LibTrieProof")
+        ).deploy()
+
+        const libBridgeProcess = await (
+            await ethers.getContractFactory("LibBridgeProcess", {
+                libraries: {
+                    LibTrieProof: libTrieProof.address,
+                },
+            })
+        ).deploy()
+
+        const libBridgeRetry = await (
+            await ethers.getContractFactory("LibBridgeRetry")
+        ).deploy()
+
+        const BridgeFactory = await ethers.getContractFactory("Bridge", {
+            libraries: {
+                LibBridgeProcess: libBridgeProcess.address,
+                LibBridgeRetry: libBridgeRetry.address,
+                LibTrieProof: libTrieProof.address,
+            },
+        })
+
+        const bridge: Bridge = await BridgeFactory.connect(owner).deploy()
+
+        await bridge.init(addressManager.address)
+
+        return {
+            bridge,
+        }
     }
 
     describe("processMessage()", async function () {
         it("should throw if gaslimit == 0 & msg.sender != message.owner", async function () {
-            await deployLibBridgeProcessFixture()
+            const { owner, srcChainId, nonOwner, libProcess, blockChainId } =
+                await deployLibBridgeProcessFixture()
+            const message: Message = {
+                id: 1,
+                sender: nonOwner.address,
+                srcChainId: srcChainId,
+                destChainId: blockChainId,
+                owner: nonOwner.address,
+                to: owner.address,
+                refundAddress: nonOwner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 0,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+            await expect(
+                libProcess.processMessage(message, ethers.constants.HashZero)
+            ).to.be.revertedWith("B:forbidden")
         })
 
         it("should throw if message.destChain != block.chainId", async function () {
-            await deployLibBridgeProcessFixture()
+            const { owner, srcChainId, nonOwner, libProcess, blockChainId } =
+                await deployLibBridgeProcessFixture()
+            const badBlockChainId = blockChainId + 1
+            const message: Message = {
+                id: 1,
+                sender: nonOwner.address,
+                srcChainId: srcChainId,
+                destChainId: badBlockChainId,
+                owner: nonOwner.address,
+                to: owner.address,
+                refundAddress: nonOwner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100000000,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+            await expect(
+                libProcess.processMessage(message, ethers.constants.HashZero)
+            ).to.be.revertedWith("B:destChainId")
         })
 
         it("should throw if message's status is not NEW", async function () {
-            await deployLibBridgeProcessFixture()
+            const {
+                owner,
+                srcChainId,
+                nonOwner,
+                libProcess,
+                testLibData,
+                blockChainId,
+                MessageStatus,
+                stateSlot,
+            } = await deployLibBridgeProcessFixture()
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: srcChainId,
+                destChainId: blockChainId,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100000000,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+
+            const signal = await testLibData.hashMessage(message)
+
+            await helpers.setStorageAt(
+                libProcess.address,
+                await getSlot(hre, signal, stateSlot + 1),
+                MessageStatus.RETRIABLE
+            )
+
+            await expect(
+                libProcess.processMessage(message, ethers.constants.HashZero)
+            ).to.be.revertedWith("B:status")
         })
 
         it("should throw if signal has not been received", async function () {
-            await deployLibBridgeProcessFixture()
+            const {
+                owner,
+                srcChainId,
+                nonOwner,
+                libProcess,
+                blockChainId,
+                addressManager,
+            } = await deployLibBridgeProcessFixture()
+            const { bridge } = await deployBridgeFixture()
+
+            await addressManager.setAddress(
+                `${srcChainId}.bridge`,
+                bridge.address
+            )
+
+            const message: Message = {
+                id: 1,
+                sender: owner.address,
+                srcChainId: srcChainId,
+                destChainId: blockChainId,
+                owner: owner.address,
+                to: nonOwner.address,
+                refundAddress: owner.address,
+                depositValue: 1,
+                callValue: 1,
+                processingFee: 1,
+                gasLimit: 100000000,
+                data: ethers.constants.HashZero,
+                memo: "",
+            }
+            await expect(
+                libProcess.processMessage(message, ethers.constants.HashZero)
+            ).to.be.reverted
+
+            // Reverts because there is no HeaderSync, can't test without
         })
 
         it("if message fails, refund should go to the intended account and amount", async function () {
