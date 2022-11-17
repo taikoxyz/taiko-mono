67fb25c8bf0d47e87db825c2ac905ab7041d2034
dantaik <99078276+dantaik@users.noreply.github.com>
2022-11-17 18:17:26 +0800

    more

--- packages/whitepaper/build.sh
+++ packages/whitepaper/build.sh
@@ -16,8 +16,7 @@ fi
 rm -rf temp.*
 rm -rf main.pdf
 cp main.tex temp.tex
-pdflatex  -interaction=errorstopmode -halt-on-error temp.tex
-bibtex temp || true
+pdflatex  -interaction=errorstopmode -halt-on-error temp.tex && (bibtex temp || true)
 pdflatex  -interaction=errorstopmode -halt-on-error temp.tex
 mv temp.pdf main.pdf
 rm -rf temp.*
