2a5a072baa220a1257046887163f31301c94da56
shadab-taiko <108871478+shadab-taiko@users.noreply.github.com>
2022-11-18 13:07:41 +0530

    [bridge] implement a bridge UI (#176)
    * [bridge] add bridge backend API spec
    
    * [bridge] create react app init
    
    * [bridge] remove api.spec.yml
    
    * [bridge] add graphql schema
    
    * [bridge] review suggestions
    
    * [bridge] remove BridgeContext type
    
    * [bridge] add mocked bridge UI
    
    * [bridge] add chains config and default chains
    
    * [bridge] create chain selector
    
    * [bridge] add token selector
    
    * [bridge] add rainbowkit
    
    * [bridge] add switch network button if on wrong network
    
    * [bridge] add advanced data and memo inputs
    
    * [bridge] fire sendEther transaction
    
    * [bridge] remove unused files
    
    * [bridge] fix lint errors
    
    * [bridge] add husky prettier and eslint for pre-commit hook
    
    * test
    
    Co-authored-by: cyberhorsey <113397187+cyberhorsey@users.noreply.github.com>
    Co-authored-by: dantaik <99078276+dantaik@users.noreply.github.com>

---
+++ packages/bridge-frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
