ca6181c477f21fe386003745a5c8ae7cd6c4210b
Shadab Khan <shadab@taiko.xyz>
2022-11-23 21:26:16 +0530

    [bridge] change neutral color

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
                 "primary": "#242424",
                 "secondary": "#181818",
                 "accent": "#FC0FC0",
-                "neutral": "#272626",
+                "neutral": "#242424",
                 "base-100": "#0f0f0f",
                 "info": "#373737",
                 "success": "#008000",
