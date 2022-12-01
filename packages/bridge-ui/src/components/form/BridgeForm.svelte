0652595af38f17ed4bfc6e72bca64d2a6fb5616c
jeff <113397187+cyberhorsey@users.noreply.github.com>
2022-12-01 10:49:55 -0800

    feat(bridge-ui): ERC20 Bridge + approval (#353)
    * erc20 bridge + approve + requires allowance
    
    * bridge form checks allowance, disables button
    
    * if allowance is required, show approve button instead
    
    * set allowance required to false after successful approval
    
    * requiresallowance should take approveopts, not bridgeopts
    
    * show Approve text appropriatley
    
    * Update packages/bridge-ui/src/components/form/BridgeForm.svelte
    
    Co-authored-by: David <104078303+davidtaikocha@users.noreply.github.com>
    
    * Update packages/bridge-ui/src/eth/bridge.ts
    
    Co-authored-by: David <104078303+davidtaikocha@users.noreply.github.com>
    
    * Update packages/bridge-ui/src/components/form/BridgeForm.svelte
    
    Co-authored-by: David <104078303+davidtaikocha@users.noreply.github.com>
    
    * Update packages/bridge-ui/src/components/form/BridgeForm.svelte
    
    Co-authored-by: David <104078303+davidtaikocha@users.noreply.github.com>
    
    * inverse approve logic
    
    Co-authored-by: David <104078303+davidtaikocha@users.noreply.github.com>

--- packages/bridge-ui/src/components/form/BridgeForm.svelte
+++ packages/bridge-ui/src/components/form/BridgeForm.svelte
@@ -2,21 +2,53 @@
   import { _ } from "svelte-i18n";
   import { token } from "../../store/token";
   import { fromChain, toChain } from "../../store/chain";
-  import { activeBridge, chainIdToBridgeAddress } from "../../store/bridge";
+  import {
+    activeBridge,
+    chainIdToBridgeAddress,
+    bridgeType,
+  } from "../../store/bridge";
   import { signer } from "../../store/signer";
   import { BigNumber, ethers, Signer } from "ethers";
   import { toast } from "@zerodevx/svelte-toast";
+  import type { Token } from "../../domain/token";
+  import type { BridgeType } from "../../domain/bridge";
+  import type { Chain } from "../../domain/chain";
 
   let amount: string;
+  let requiresAllowance: boolean = true;
   let btnDisabled: boolean = true;
 
   $: isBtnDisabled($signer, amount)
     .then((d) => (btnDisabled = d))
     .catch((e) => console.log(e));
 
+  $: checkAllowance(amount, $token, $bridgeType, $fromChain, $signer)
+    .then((a) => (requiresAllowance = a))
+    .catch((e) => console.log(e));
+
+  async function checkAllowance(
+    amt: string,
+    token: Token,
+    bridgeType: BridgeType,
+    fromChain: Chain,
+    signer: Signer
+  ) {
+    if (!signer || !amt || !token || !fromChain) return true;
+
+    return await $activeBridge.RequiresAllowance({
+      amountInWei: amt
+        ? ethers.utils.parseUnits(amt, token.decimals)
+        : BigNumber.from(0),
+      signer: signer,
+      contractAddress: token.address,
+      spenderAddress: $chainIdToBridgeAddress.get(fromChain.id),
+    });
+  }
+
   async function isBtnDisabled(signer: Signer, amount: string) {
     if (!signer) return true;
     if (!amount) return true;
+    if (requiresAllowance) return true;
     const balance = await signer.getBalance("latest");
     if (balance.lt(ethers.utils.parseUnits(amount, $token.decimals)))
       return true;
@@ -24,8 +56,33 @@
     return false;
   }
 
+  async function approve() {
+    try {
+      if (!requiresAllowance)
+        throw Error("does not require additional allowance");
+
+      const tx = await $activeBridge.Approve({
+        amountInWei: ethers.utils.parseUnits(amount, $token.decimals),
+        signer: $signer,
+        contractAddress: $token.address,
+        spenderAddress: $chainIdToBridgeAddress.get($fromChain.id),
+      });
+      console.log("approved, waiting for confirmations ", tx);
+      await $signer.provider.waitForTransaction(tx.hash, 3);
+
+      requiresAllowance = false;
+
+      toast.push($_("toast.transactionSent"));
+    } catch (e) {
+      console.log(e);
+      toast.push($_("toast.errorSendingTransaction"));
+    }
+  }
+
   async function bridge() {
     try {
+      if (requiresAllowance) throw Error("requires additional allowance");
+
       const tx = await $activeBridge.Bridge({
         amountInWei: ethers.utils.parseUnits(amount, $token.decimals),
         signer: $signer,
@@ -76,10 +133,20 @@
   </label>
 </div>
 
-<button
-  class="btn btn-accent"
-  on:click={async () => await bridge()}
-  disabled={btnDisabled}
->
-  {$_("home.bridge")}
-</button>
+{#if !requiresAllowance}
+  <button
+    class="btn btn-accent"
+    on:click={bridge}
+    disabled={btnDisabled}
+  >
+    {$_("home.bridge")}
+  </button>
+{:else}
+  <button
+    class="btn btn-accent"
+    on:click={approve}
+    disabled={btnDisabled}
+  >
+    {$_("home.approve")}
+  </button>
+{/if}
