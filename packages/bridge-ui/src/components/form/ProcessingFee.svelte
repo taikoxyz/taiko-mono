d7f668378e4e87de75648a1628429c79f49527ee
Jeffery Walsh <cyberhorsey@gmail.com>
2022-12-07 16:30:02 -0800

    processing fee amounts + inputs

--- packages/bridge-ui/src/components/form/ProcessingFee.svelte
+++ packages/bridge-ui/src/components/form/ProcessingFee.svelte
@@ -1,10 +1,16 @@
-<script>
+<script lang="ts">
   import { _ } from "svelte-i18n";
   import { processingFee } from "../../store/fee";
-  import { PROCESSING_FEE_META } from "../../domain/fee";
+  import { ProcessingFeeMethod, PROCESSING_FEE_META } from "../../domain/fee";
+
+  export let customFee: string;
 
   function selectProcessingFee(fee) {
     $processingFee = fee;
+  }
+
+  function updateAmount(e: any) {
+    customFee = (e.data as number).toString();
   }
 </script>
 
@@ -15,10 +21,32 @@
   <div class="flex items-center justify-between">
     {#each Array.from(PROCESSING_FEE_META) as fee}
       <button
-        class="{$processingFee === fee[0] ? 'border-accent hover:border-accent' : ''} btn btn-md"
+        class="{$processingFee === fee[0]
+          ? 'border-accent hover:border-accent'
+          : ''} btn btn-md"
         on:click={() => selectProcessingFee(fee[0])}
         >{fee[1].displayText}</button
       >
     {/each}
   </div>
+
+  {#if $processingFee === ProcessingFeeMethod.CUSTOM}
+    <label
+      class="mt-2 input-group relative rounded-lg bg-dark-4 justify-between items-center pr-4"
+    >
+      <input
+        type="number"
+        step="0.01"
+        placeholder="0.01"
+        min="0"
+        on:input={updateAmount}
+        class="input input-primary bg-dark-4 input-lg flex-1"
+        name="amount"
+      />
+    </label>
+  {:else if $processingFee === ProcessingFeeMethod.RECOMMENDED}
+    <div class="flex items-left justify-between">
+      <span class="mt-2 text-sm">0.01 ETH </span>
+    </div>
+  {/if}
 </div>
