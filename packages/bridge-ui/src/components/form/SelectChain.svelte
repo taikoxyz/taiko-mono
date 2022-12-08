ab3db3520bb0605788dc82ed53ecb91b713ca9b9
Jeffery Walsh <cyberhorsey@gmail.com>
2022-12-08 11:39:40 -0800

    toggle chain fix

--- packages/bridge-ui/src/components/form/SelectChain.svelte
+++ packages/bridge-ui/src/components/form/SelectChain.svelte
@@ -2,15 +2,25 @@
   import ArrowRight from "../icons/ArrowRight.svelte";
   import { fromChain, toChain } from "../../store/chain";
   import { CHAIN_MAINNET, CHAIN_TKO } from "../../domain/chain";
+  import { ethereum } from "../../store/ethereum";
+  import { signer } from "../../store/signer";
+  import { switchEthereumChain } from "../../utils/switchEthereumChain";
+  import { ethers } from "ethers";
 
-  function toggleChains() {
-    fromChain.update((val) =>
-      val === CHAIN_MAINNET ? CHAIN_TKO : CHAIN_MAINNET
-    );
-    toChain.update((val) =>
-      val === CHAIN_MAINNET ? CHAIN_TKO : CHAIN_MAINNET
-    );
-  }
+  const toggleChains = async () => {
+    const chain = $fromChain === CHAIN_MAINNET ? CHAIN_TKO : CHAIN_MAINNET;
+    await switchEthereumChain($ethereum, chain);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+
+    fromChain.set(chain);
+    if (chain === CHAIN_MAINNET) {
+      toChain.set(CHAIN_TKO);
+    } else {
+      toChain.set(CHAIN_MAINNET);
+    }
+    signer.set(provider.getSigner());
+  };
 </script>
 
 <div class="flex items-center justify-between w-full p-8 py-6 text-lg">
