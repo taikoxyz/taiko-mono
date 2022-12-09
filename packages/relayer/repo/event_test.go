1ec395f197b578c01364485549a5929a05b9824e
Jeffery Walsh <cyberhorsey@gmail.com>
2022-12-09 10:35:22 -0800

    repo tests

--- packages/relayer/repo/event_test.go
+++ packages/relayer/repo/event_test.go
@@ -182,3 +182,58 @@ func TestIntegration_Event_FindAllByAddressAndChainID(t *testing.T) {
 		})
 	}
 }
+
+func TestIntegration_Event_FindAllByAddress(t *testing.T) {
+	db, close, err := testMysql(t)
+	assert.Equal(t, nil, err)
+
+	defer close()
+
+	eventRepo, err := NewEventRepository(db)
+	assert.Equal(t, nil, err)
+
+	addr := common.HexToAddress("0x71C7656EC7ab88b098defB751B7401B5f6d8976F")
+
+	_, err = eventRepo.Save(context.Background(), relayer.SaveEventOpts{
+		Name:    "name",
+		Data:    fmt.Sprintf(`{"Owner":"%s"}`, addr.Hex()),
+		ChainID: big.NewInt(1),
+		Status:  relayer.EventStatusDone,
+	})
+	assert.Equal(t, nil, err)
+	tests := []struct {
+		name     string
+		address  common.Address
+		wantResp []*relayer.Event
+		wantErr  error
+	}{
+		{
+			"success",
+			addr,
+			[]*relayer.Event{
+				{
+					ID:      1,
+					Name:    "name",
+					Data:    datatypes.JSON([]byte(fmt.Sprintf(`{"Owner": "%s"}`, addr.Hex()))),
+					ChainID: 1,
+					Status:  relayer.EventStatusDone,
+				},
+			},
+			nil,
+		},
+		{
+			"noneByAddr",
+			common.HexToAddress("0x165CD37b4C644C2921454429E7F9358d18A45e14"),
+			[]*relayer.Event{},
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp, err := eventRepo.FindAllByAddress(context.Background(), tt.address)
+			assert.Equal(t, tt.wantResp, resp)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
