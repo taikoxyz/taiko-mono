86204f7c71694ad705f0232d43a7fc77552cf924
Rishav Raj <rishavraj1707music@gmail.com>
2022-12-09 15:53:44 +0530

    Homepage hero for mobile view has been changed

--- packages/website/src/components/Hero/index.tsx
+++ packages/website/src/components/Hero/index.tsx
@@ -4,19 +4,19 @@ export default function Hero() {
   return (
     <div className="relative bg-neutral-50 dark:bg-neutral-800">
       <main className="lg:relative">
-        <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:py-48 lg:text-left">
-          <div className="px-4 sm:px-8 lg:w-1/2 xl:pr-16">
+        <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-left lg:py-48">
+          <div className="px-4 sm:px-8 lg:w-1/2 xl:pr-16 ">
             <h1 className="font-oxanium text-4xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
               A <span className="text-[#e30ead]">Type 1</span> ZK-EVM
             </h1>
-            <p className="font-oxanium mx-auto mt-3 max-w-md text-lg text-neutral-600 sm:text-xl md:mt-5 md:max-w-3xl dark:text-neutral-100">
+            <p className="font-oxanium mx-auto mt-3 text-lg text-neutral-600 sm:text-xl md:mt-5 dark:text-neutral-100">
               Fully decentralized, Ethereum-equivalent ZK-Rollup.
             </p>
-            <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
-              <div className="inline-flex rounded-md shadow">
+            <div className="mt-10">
+              <div className="rounded-md shadow">
                 <a
                   href="./docs/intro"
-                  className="inline-flex items-center justify-center rounded-md border border-transparent bg-[#e30ead] px-5 py-3 text-base font-semibold text-white dark:text-neutral-100 hover:bg-[#bd0b90] hover:no-underline hover:text-white"
+                  className=" rounded-md border border-transparent bg-[#e30ead] px-5 py-3 text-base font-semibold text-white dark:text-neutral-100 hover:bg-[#bd0b90] hover:no-underline hover:text-white"
                 >
                   Get started
                 </a>
@@ -35,3 +35,4 @@ export default function Hero() {
     </div>
   );
 }
+
