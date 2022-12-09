0358addf37186294fa2a452321d6f642762abde7
shadab-taiko <108871478+shadab-taiko@users.noreply.github.com>
2022-12-10 01:49:58 +0530

    fix(bridge): minor responsiveness fixes (#407)
    * fix: minor responsiveness fixes
    
    * fix(bridge): remove console logs
    
    Co-authored-by: Roger <50648015+RogerLamTd@users.noreply.github.com>
    Co-authored-by: jeff <113397187+cyberhorsey@users.noreply.github.com>

--- packages/bridge-ui/src/components/Navbar.svelte
+++ packages/bridge-ui/src/components/Navbar.svelte
@@ -14,7 +14,6 @@
 
   export let transactioner: Transactioner;
   let transactions: BridgeTransaction[];
-
   $: getTransactions($signer, $fromChain);
 
   async function getTransactions(signer: Signer, chain: Chain) {
@@ -26,7 +25,7 @@
   }
 </script>
 
-<nav class="navbar mb-4 md:h-[125px] pt-4 md:pt-0 md:px-4">
+<nav class="navbar mb-4 md:h-[125px] pt-4 md:pt-0 md:px-4 w-full">
   <div class="navbar-end justify-start">
     <Logo />
   </div>
