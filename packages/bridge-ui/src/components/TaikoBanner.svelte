e46c04f79b1621e680a13221277c6fc203aceb64
Jeffery Walsh <cyberhorsey@gmail.com>
2022-12-15 11:46:32 -0800

    header removed in bridge form + margin on tabs

--- packages/bridge-ui/src/components/TaikoBanner.svelte
+++ packages/bridge-ui/src/components/TaikoBanner.svelte
@@ -3,9 +3,8 @@
   import SelectChain from "./form/SelectChain.svelte";
 </script>
 
-<div class="taiko-banner bg-cover bg-center bg-no-repeat h-36 rounded-lg py-4 flex flex-col items-center justify-center md:w-[450px]">
-  <h1 class="text-2xl font-bold">
-    {$_("home.title")}
-  </h1>
+<div
+  class="taiko-banner bg-cover bg-center bg-no-repeat h-36 rounded-lg py-4 flex flex-col items-center justify-center md:w-[450px]"
+>
   <SelectChain />
 </div>
