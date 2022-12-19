bc7b29534503260c4a399cbf85a03a23a4e6efb6
dave | d1onys1us <dave@taiko.xyz>
2022-12-18 22:35:52 -0500

    feat(bridge-ui): add announcement for bridge ui testnet

---
+++ packages/bridge-ui/src/components/HeaderAnnouncement.svelte
@@ -0,0 +1,23 @@
+<div class="relative bg-[#fc0fc0]">
+    <div class="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div class="pr-16 sm:px-16 sm:text-center">
+        <p class="font-medium text-white">
+            <span class="hidden md:inline">Hello! At Taiko, we develop in open-source, so this URL, and others, are
+                publicly available. However, it is not ready to be used. We will make an
+                announcement when our public testnet is ready!</span>
+            <span class="block sm:ml-2 sm:inline-block">
+            </span>
+        </p>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:items-start sm:pt-1 sm:pr-2">
+        <button type="button" class="flex rounded-md p-2 hover:bg-[#dc009b] focus:outline-none focus:ring-2 focus:ring-white">
+            <span class="sr-only">Dismiss</span>
+            <!-- Heroicon name: outline/x-mark -->
+            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
+        </div>
+    </div>
+</div>
+  
