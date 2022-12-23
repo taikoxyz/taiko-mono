15edba6084058b0632dbef00fcdf2b3f5e9179de
Daniel Wang <dan@taiko.xyz>
2022-12-23 16:30:02 +0800

    Update LibSharedConfig.sol

--- packages/protocol/contracts/libs/LibSharedConfig.sol
+++ packages/protocol/contracts/libs/LibSharedConfig.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.8.9;
 import "../L1/LibData.sol";
 
 library LibSharedConfig {
-    function getConfig() public pure returns (LibData.Config memory config) {
+    function getConfig() internal pure returns (LibData.Config memory config) {
         config.chainId = 167;
         // up to 2048 pending blocks
         config.maxNumBlocks = 2049;
