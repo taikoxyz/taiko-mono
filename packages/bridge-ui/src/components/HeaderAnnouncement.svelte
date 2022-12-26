896862892133263e6f9fa4d028b846d535b023c1
Shadab Khan <shadab@taiko.xyz>
2022-12-26 20:17:25 +0530

    feat(bridge): add faucet link to announcement

--- packages/bridge-ui/src/components/HeaderAnnouncement.svelte
+++ packages/bridge-ui/src/components/HeaderAnnouncement.svelte
@@ -3,19 +3,10 @@
     <div class="sm:px-16 text-center">
       <p class="font-medium text-white">
         <span class="md:inline"
-          >Hello! At Taiko, we develop in open-source, so this URL, and others,
-          are publicly available. However, it is not ready to be used. We will
-          make an announcement when our public testnet is ready!</span
-        >
-        <span class="block sm:inline-block">
-          <a
-            href="https://twitter.com/taikoxyz"
-            class="font-bold text-white underline"
-          >
-            Follow @taikoxyz on Twitter
-            <span aria-hidden="true"> &rarr;</span>
-          </a>
-        </span>
+          >Receive some tokens for bridging with our <a
+          href="https://twitter.com/taikoxyz"
+          class="font-bold text-white underline"
+        >faucet</a>.</span>
       </p>
     </div>
   </div>
