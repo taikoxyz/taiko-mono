4cafe8b2d4193b58589c90cea116750d3998d129
Daniel Wang <dan@taiko.xyz>
2023-01-04 18:39:32 +0800

    add signal service

---
+++ packages/protocol/contracts/signal/ISignalService.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+//
+// ╭━━━━╮╱╱╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮
+// ┃╭╮╭╮┃╱╱┃┃╱╱╱╱╱┃┃╱╱╱╱╱┃┃
+// ╰╯┃┃┣┻━┳┫┃╭┳━━╮┃┃╱╱╭━━┫╰━┳━━╮
+// ╱╱┃┃┃╭╮┣┫╰╯┫╭╮┃┃┃╱╭┫╭╮┃╭╮┃━━┫
+// ╱╱┃┃┃╭╮┃┃╭╮┫╰╯┃┃╰━╯┃╭╮┃╰╯┣━━┃
+// ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
+pragma solidity ^0.8.9;
+
+interface ISignalService {
+    /**
+     * Send a signal by storing the key with a value of 1.
+     *
+     * @param user The user address sending the signal.
+     * @param signal The signal to send.
+     */
+    function sendSignal(address user, bytes32 signal) external;
+
+    /**
+     * Check if a signal has been sent (key stored with a value of 1).
+     *
+     * @param user The sender address of the signal.
+     * @param signal The signal to check.
+     */
+    function isSignalSent(
+        address user,
+        bytes32 signal
+    ) external view returns (bool);
+
+    /**
+     * Check if signal has been received on the destination chain (current).
+     *
+     * @param srcApp Address of the source bridge where the bridge
+     *                  was initiated.
+     * @param user Address of the sender of the signal
+     *               (also should be srcApp).
+     * @param signal The signal to check.
+     * @param proof The proof of the signal being sent on the source chain.
+     */
+    function isSignalReceived(
+        address srcApp,
+        address user,
+        bytes32 signal,
+        bytes calldata proof
+    ) external view returns (bool);
+}
---
+++ packages/protocol/contracts/signal/SignalService.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+//
+// ╭━━━━╮╱╱╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮
+// ┃╭╮╭╮┃╱╱┃┃╱╱╱╱╱┃┃╱╱╱╱╱┃┃
+// ╰╯┃┃┣┻━┳┫┃╭┳━━╮┃┃╱╱╭━━┫╰━┳━━╮
+// ╱╱┃┃┃╭╮┣┫╰╯┫╭╮┃┃┃╱╭┫╭╮┃╭╮┃━━┫
+// ╱╱┃┃┃╭╮┃┃╭╮┫╰╯┃┃╰━╯┃╭╮┃╰╯┣━━┃
+// ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
+pragma solidity ^0.8.9;
+
+import "../common/EssentialContract.sol";
+import "../common/IHeaderSync.sol";
+import "../libs/LibBlockHeader.sol";
+import "../libs/LibTrieProof.sol";
+import "./ISignalService.sol";
+
+contract SignalService is ISignalService, EssentialContract {
+    using LibBlockHeader for BlockHeader;
+
+    struct SignalProof {
+        BlockHeader header;
+        bytes proof;
+    }
+
+    /// @dev Initializer to be called after being deployed behind a proxy.
+    function init(address _addressManager) external initializer {
+        EssentialContract._init(_addressManager);
+    }
+
+    function sendSignal(address user, bytes32 signal) public {
+        require(signal != 0, "B:signal");
+
+        bytes32 k = getSignalSlot(msg.sender, user, signal);
+        assembly {
+            sstore(k, 1)
+        }
+    }
+
+    function isSignalSent(
+        address user,
+        bytes32 signal
+    ) public view returns (bool) {
+        require(signal != 0, "B:signal");
+
+        bytes32 k = getSignalSlot(msg.sender, user, signal);
+        uint256 v;
+        assembly {
+            v := sload(k)
+        }
+        return v == 1;
+    }
+
+    function isSignalReceived(
+        address srcApp,
+        address user,
+        bytes32 signal,
+        bytes calldata proof
+    ) public view returns (bool) {
+        require(signal != 0, "B:signal");
+        require(srcApp != address(0), "B:srcApp");
+
+        SignalProof memory sp = abi.decode(proof, (SignalProof));
+        LibTrieProof.verify({
+            stateRoot: sp.header.stateRoot,
+            addr: srcApp,
+            key: getSignalSlot(srcApp, user, signal),
+            value: bytes32(uint256(1)),
+            mkproof: sp.proof
+        });
+        // get synced header hash of the header height specified in the proof
+        bytes32 syncedHeaderHash = IHeaderSync(resolve("taiko"))
+            .getSyncedHeader(sp.header.height);
+
+        // check header hash specified in the proof matches the current chain
+        return
+            syncedHeaderHash != 0 &&
+            syncedHeaderHash == sp.header.hashBlockHeader();
+    }
+
+    function getSignalSlot(
+        address app,
+        address user,
+        bytes32 signal
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(app, user, signal));
+    }
+}
