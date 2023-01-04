0e5ae27bafb9aa2c3b469549de8d5583463e79cb
David <david@taiko.xyz>
2023-01-04 20:00:30 +0800

    fix `signal_service` address

--- packages/protocol/tasks/deploy_L1.ts
+++ packages/protocol/tasks/deploy_L1.ts
@@ -192,7 +192,7 @@ export async function deployContracts(hre: any) {
         hre,
         await AddressManager.setAddress(
             `${chainId}.signal_service`,
-            Bridge.address
+            SignalService.address
         )
     );
 
