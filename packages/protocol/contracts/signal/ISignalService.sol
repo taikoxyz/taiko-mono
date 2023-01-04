cf739bc679d2f6af42fae7c9fe13977fcf5c9500
Daniel Wang <dan@taiko.xyz>
2023-01-04 19:27:37 +0800

    Update ISignalService.sol

--- packages/protocol/contracts/signal/ISignalService.sol
+++ packages/protocol/contracts/signal/ISignalService.sol
@@ -20,7 +20,8 @@ interface ISignalService {
     /**
      * Check if a signal has been sent (key stored with a value of 1).
      *
-     * @param user The sender address of the signal.
+     * @param app The address that sent this message.
+     * @param user The logical owner of the signal.
      * @param signal The signal to check.
      */
     function isSignalSent(
@@ -32,10 +33,8 @@ interface ISignalService {
     /**
      * Check if signal has been received on the destination chain (current).
      *
-     * @param app Address of the source bridge where the bridge
-     *                  was initiated.
-     * @param user Address of the sender of the signal
-     *               (also should be app).
+     * @param app The address that sent this message.
+     * @param @param user The logical owner of the signal.
      * @param signal The signal to check.
      * @param proof The proof of the signal being sent on the source chain.
      */
