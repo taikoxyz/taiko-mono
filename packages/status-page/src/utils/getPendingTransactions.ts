17bc55d45abc7a13f85c3b84ca2481712397caba
Jeffery Walsh <cyberhorsey@gmail.com>
2023-01-06 17:55:01 -0800

    faster mempool

--- packages/status-page/src/utils/getPendingTransactions.ts
+++ packages/status-page/src/utils/getPendingTransactions.ts
@@ -1,13 +1,9 @@
-import type { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 
 export const getPendingTransactions = async (
   provider: ethers.providers.JsonRpcProvider,
   contractAddress: string
 ): Promise<number> => {
-  const mempool = await provider.send("txpool_content", []);
-  let len = 0;
-  Object.values(mempool.pending).forEach((p) => {
-    len += Object.entries(p).length;
-  });
-  return len;
+  const mempool = await provider.send("txpool_status", []);
+  return BigNumber.from(mempool.pending).toNumber();
 };
