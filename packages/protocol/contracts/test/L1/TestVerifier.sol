9f4173a186cdc1dad580c35c91165cdbfffd53a5
kongliangzhong <kongliangzhong@gmail.com>
2023-01-13 04:21:33 +0000

    update

---
+++ packages/protocol/contracts/test/L1/TestVerifier.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+//
+// ╭━━━━╮╱╱╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮
+// ┃╭╮╭╮┃╱╱┃┃╱╱╱╱╱┃┃╱╱╱╱╱┃┃
+// ╰╯┃┃┣┻━┳┫┃╭┳━━╮┃┃╱╱╭━━┫╰━┳━━╮
+// ╱╱┃┃┃╭╮┣┫╰╯┫╭╮┃┃┃╱╭┫╭╮┃╭╮┃━━┫
+// ╱╱┃┃┃╭╮┃┃╭╮┫╰╯┃┃╰━╯┃╭╮┃╰╯┣━━┃
+// ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
+pragma solidity ^0.8.9;
+
+contract TestVerifier {
+    function verifyZKP(
+        address prover,
+        bytes memory zkproof
+    ) external view returns (bool verified) {
+        (verified, ) = prover.staticcall(zkproof);
+    }
+}
--- packages/protocol/hardhat.config.ts
+++ packages/protocol/hardhat.config.ts
@@ -1,7 +1,6 @@
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
-import "@tovarishfin/hardhat-yul";
 import "@typechain/hardhat";
 import "hardhat-abi-exporter";
 import "hardhat-gas-reporter";
