eebda796df5ff3a9a478a0a3341d65c316e487ec
kongliangzhong <kongliangzhong@gmail.com>
2023-01-16 12:00:58 +0000

    update

--- packages/protocol/contracts/test/L1/TestVerifier.sol
+++ packages/protocol/contracts/test/L1/TestVerifier.sol
@@ -8,11 +8,33 @@
 // ╱╱╰╯╰╯╰┻┻╯╰┻━━╯╰━━━┻╯╰┻━━┻━━╯
 pragma solidity ^0.8.9;
 
+import "../../thirdparty/LibBytesUtils.sol";
+import "hardhat/console.sol";
+
 contract TestVerifier {
+    using LibBytesUtils for bytes;
+
+    event ProofData(bytes data);
+
     function verifyZKP(
         address prover,
         bytes memory zkproof
-    ) external view returns (bool verified) {
+    ) external returns (bool verified) {
+        // bytes memory sliced = zkproof.slice(64);
+        // console.logBytes(zkproof);
+        // console.logBytes(sliced);
+
+        // emit ProofData(zkproof);
+
         (verified, ) = prover.staticcall(zkproof);
+
+        console.logBool(verified);
+
+        // assembly {
+        //    let success := staticcall(gas(), prover, add(zkproof, 64), mload(zkproof), 0, 0)
+        //    if eq(success, 1) {
+        //         verified := true
+        //    }
+        // }
     }
 }
