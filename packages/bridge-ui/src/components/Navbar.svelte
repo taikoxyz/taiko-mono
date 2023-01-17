099f8a5e0e983247f11a025f9a4026318f41d7c2
Shadab Khan <shadab@taiko.xyz>
2023-01-17 12:25:01 +0530

    feat(bridge): add mobile logo

--- packages/bridge-ui/src/components/Navbar.svelte
+++ packages/bridge-ui/src/components/Navbar.svelte
@@ -5,15 +5,19 @@
   import { signer } from "../store/signer";
   import AddressDropdown from "./AddressDropdown.svelte";
   import ChainDropdown from "./ChainDropdown.svelte";
+  import TaikoLogoFluo from "./icons/TaikoLogoFluo.svelte";
 </script>
 
 <div class="navbar bg-base-100">
   <div class="flex-1">
-    <span class="inline dark:hidden">
+    <span class="taiko-light-logo">
       <TaikoLight width={120} />
     </span>
-    <span class="hidden dark:inline">
+    <span class="taiko-logo">
       <TaikoLogo width={120} />
+    </span>
+    <span class="md:hidden">
+      <TaikoLogoFluo width={50} />
     </span>
   </div>
   <div class="flex-none">
