1c573f9c99c8cbe685d520c511206f6b8c6e2002
Daniel Wang <dan@taiko.xyz>
2023-01-19 12:37:12 +0800

    Update LibSharedConfig.sol

--- packages/protocol/contracts/libs/LibSharedConfig.sol
+++ packages/protocol/contracts/libs/LibSharedConfig.sol
@@ -16,20 +16,25 @@ library LibSharedConfig {
         return
             TaikoData.Config({
                 chainId: 167,
-                maxNumBlocks: 2049, // up to 2048 pending blocks
-                blockHashHistory: 100000,
-                // This number is calculated from maxNumBlocks to make
-                // the 'the maximum value of the multiplier' close to 20.0
+                // Assuming proof time 1 hour, block time 10 seconds
+                // Level of parallelization is 60*60/10 = 360
+                // We give it extra 50% more slots, then we have 360*1.5+1=
+                maxNumBlocks: 541,
+                // Changed it to a smaller value
+                // Assuming block time 10 seconds, we support cross-chain tx
+                // to have 1 confirmation within 600 seconds (10 minutes).
+                // Then we have: 600/10 = 60.
+                blockHashHistory: 60,
                 zkProofsPerBlock: 1,
                 maxVerificationsPerTx: 20,
                 commitConfirmations: 0,
-                maxProofsPerForkChoice: 5,
+                maxProofsPerForkChoice: 3,
                 blockMaxGasLimit: 5000000, // TODO
-                maxTransactionsPerBlock: 20, // TODO
-                maxBytesPerTxList: 10240, // TODO
-                minTxGasLimit: 21000, // TODO
+                maxTransactionsPerBlock: 200, // TODO
+                maxBytesPerTxList: 1500000, // TODO(david): verify this
+                minTxGasLimit: 21000, // TODO(david): verify this
                 anchorTxGasLimit: 250000,
-                feePremiumLamda: 590,
+                feePremiumLamda: 590, // TODO(daniel): how is this calculated
                 rewardBurnBips: 100, // 100 basis points or 1%
                 proposerDepositPctg: 25, // 25%
                 // Moving average factors
@@ -40,7 +45,7 @@ library LibSharedConfig {
                 feeGracePeriodPctg: 125, // 125%
                 feeMaxPeriodPctg: 375, // 375%
                 blockTimeCap: 48 seconds,
-                proofTimeCap: 60 minutes,
+                proofTimeCap: 90 minutes,
                 bootstrapDiscountHalvingPeriod: 180 days,
                 initialUncleDelay: 60 minutes,
                 enableTokenomics: false,
