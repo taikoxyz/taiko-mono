7096922b5c6cd896a2fbb163f4592db274bbb6a7
David <david@taiko.xyz>
2023-01-28 23:22:41 +0800

    feat: use batch provider

--- packages/protocol/scripts/snaefellsjokull-poap-list.js
+++ packages/protocol/scripts/snaefellsjokull-poap-list.js
@@ -1,15 +1,20 @@
 "use strict";
 const ethers = require("ethers");
+const fs = require("fs");
 
-const L2_RPC_ENDPOINT = "http://localhost:8080";
+const L2_RPC_ENDPOINT = "http://l2rpc.a1.taiko.xyz";
 const L2_BLOCK_START_HEIGHT = 0;
-const L2_BLOCK_END_HEIGHT = 1000; // TODO: change this value
+// 512862 timestamp: Thu, 05 Jan 2023 23:59:52 GMT
+// 512863 timestamp: Fri, 06 Jan 2023 00:00:16 GMT
+const L2_BLOCK_END_HEIGHT = 512862;
+
 const GOLDEN_TOUCH_ADDRESS = "0x0000777735367b36bC9B61C50022d9D0700dB4Ec";
 const L2_BRIDGE_ADDRESS = "0x0000777700000000000000000000000000000004";
 
 // Any user with a bridging transaction + one other type of transaction (transfer, dapp tx)
 const REASON_USED_BRIDGE = "REASON_USED_BRIDGE";
 const REASON_USED_OTHER_TYPES_OF_TXS = "REASON_USED_OTHER_TYPES_OF_TXS";
+const BATCH_SIZE = 5000;
 
 class Account {
   constructor(address, reason) {
@@ -44,7 +49,7 @@ class AccountsList {
 }
 
 async function main() {
-  const l2Provider = new ethers.providers.JsonRpcProvider(L2_RPC_ENDPOINT);
+  const l2Provider = new ethers.providers.JsonRpcBatchProvider(L2_RPC_ENDPOINT);
   const accountsList = new AccountsList();
   const bridgeInterface = new ethers.utils.Interface(
     require("../artifacts/contracts/bridge/Bridge.sol/Bridge.json").abi
@@ -52,40 +57,74 @@ async function main() {
 
   const processMessageSelector = bridgeInterface.getSighash("processMessage");
 
-  for (let i = L2_BLOCK_START_HEIGHT; i <= L2_BLOCK_END_HEIGHT; i++) {
-    const block = await l2Provider.getBlockWithTransactions(i);
+  for (
+    let i = L2_BLOCK_START_HEIGHT;
+    i <= L2_BLOCK_END_HEIGHT;
+    i += BATCH_SIZE
+  ) {
+    let batchEnd = i + BATCH_SIZE - 1;
+    if (batchEnd > L2_BLOCK_END_HEIGHT) batchEnd = L2_BLOCK_END_HEIGHT;
 
-    for (const tx of block.transactions) {
-      if (tx.from === GOLDEN_TOUCH_ADDRESS) continue; // ignore those `TaikoL2.anchor` transactions
-      const receipt = await l2Provider.getTransactionReceipt(tx.hash);
-      if (receipt.status !== 1) continue;
+    const batchBlockHeights = new Array(batchEnd - i + 1)
+      .fill(0)
+      .map((_, j) => i + j);
 
-      // Bridging transaction
-      if (
-        tx.data &&
-        tx.to === L2_BRIDGE_ADDRESS &&
-        tx.data.startsWith(processMessageSelector)
-      ) {
-        const { message } = bridgeInterface.decodeFunctionData(
-          "processMessage",
-          tx.data
-        );
+    await Promise.all(
+      batchBlockHeights.map(async function (height) {
+        console.log(`Block: ${height}`);
 
-        accountsList.addAccountWithReason(message.to, REASON_USED_BRIDGE);
-        continue;
-      }
+        const block = await l2Provider.getBlockWithTransactions(height);
 
-      // Other types of transaction (transfer, dapp tx)
-      accountsList.addAccountWithReason(
-        tx.from,
-        REASON_USED_OTHER_TYPES_OF_TXS
-      );
-    }
+        for (const tx of block.transactions) {
+          if (tx.from === GOLDEN_TOUCH_ADDRESS) continue; // ignore those `TaikoL2.anchor` transactions
+          const receipt = await l2Provider.getTransactionReceipt(tx.hash);
+          if (receipt.status !== 1) continue;
+
+          // Bridging transaction
+          // we use `message.to` to identify the actual user account, since `tx.from` is always the relayer account.
+          if (
+            tx.data &&
+            tx.to === L2_BRIDGE_ADDRESS &&
+            tx.data.startsWith(processMessageSelector)
+          ) {
+            const { message } = bridgeInterface.decodeFunctionData(
+              "processMessage",
+              tx.data
+            );
+
+            accountsList.addAccountWithReason(message.to, REASON_USED_BRIDGE);
+            continue;
+          }
+
+          // Other types of transaction (transfer, dapp tx)
+          accountsList.addAccountWithReason(
+            tx.from,
+            REASON_USED_OTHER_TYPES_OF_TXS
+          );
+        }
+      })
+    );
   }
 
+  console.log(accountsList.accountList.length);
   console.log(
     `Eligible accounts: ${accountsList.getEligibleAccounts().length}`
   );
+
+  const wallets = accountsList
+    .getEligibleAccounts()
+    .map((account) => account.address);
+
+  fs.writeFileSync("./wallets.json", JSON.stringify({ wallets }));
+
+  // eslint-disable-next-line no-process-exit
+  process.exit(0);
 }
 
 main().catch(console.error);
+
+// function sleep(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, ms);
+//   });
+// }
