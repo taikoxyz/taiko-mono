03da36098b17c585f04b3de199f5f40048109f2c
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-02-05 22:09:46 +0200

    Update AddressManager.sol
    Here are a few things to consider to improve the contract:
    
    It would be a good idea to include a require statement in the setAddress() function to check that the _address is not null, to avoid unexpected behavior.
    The contract does not include any logic to handle the scenario when the same name is assigned to multiple addresses, so it might be a good idea to add such logic, if desired.
    Consider adding a way to remove an address from the mapping if needed.
    Consider adding a maximum length for the _name parameter to prevent accidental storage of long strings.

--- packages/protocol/contracts/thirdparty/AddressManager.sol
+++ packages/protocol/contracts/thirdparty/AddressManager.sol
@@ -35,73 +35,80 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /**
- * @title AddressManager
- */
+
+@title AddressManager
+/
 contract AddressManager is OwnableUpgradeable {
-    /*************
-     * Variables *
-     *************/
+/************
 
-    mapping(bytes32 => address) private addresses;
+Variables *
+*************/
+mapping(bytes32 => address) private addresses;
 
-    /**********
-     * Events *
-     **********/
+/**********
 
-    event AddressSet(
-        string indexed _name,
-        address _newAddress,
-        address _oldAddress
-    );
+Events *
+**********/
+event AddressSet(
+string indexed _name,
+address _newAddress,
+address _oldAddress
+);
 
-    /********************
-     * External Functions*
-     ********************/
+/********************
 
-    /// @dev Initializer to be called after being deployed behind a proxy.
-    function init() external initializer {
-        OwnableUpgradeable.__Ownable_init();
-    }
+External Functions*
+********************/
+/// @dev Initializer to be called after being deployed behind a proxy.
+function init() external initializer {
+OwnableUpgradeable.__Ownable_init();
+}
 
-    /**
-     * Changes the address associated with a particular name.
-     * @param _name String name to associate an address with.
-     * @param _address Address to associate with the name.
-     */
-    function setAddress(
-        string memory _name,
-        address _address
-    ) external onlyOwner {
-        bytes32 nameHash = _getNameHash(_name);
-        address oldAddress = addresses[nameHash];
-        addresses[nameHash] = _address;
+/**
 
-        emit AddressSet(_name, _address, oldAddress);
-    }
+Changes the address associated with a particular name.
 
-    /********************
-     * Public Functions *
-     ********************/
+@param _name String name to associate an address with.
 
-    /**
-     * Retrieves the address associated with a given name.
-     * @param _name Name to retrieve an address for.
-     * @return Address associated with the given name.
-     */
-    function getAddress(string memory _name) external view returns (address) {
-        return addresses[_getNameHash(_name)];
-    }
+@param _address Address to associate with the name.
+*/
+function setAddress(
+string memory _name,
+address _address
+) external onlyOwner {
+require(_address != address(0), "Address cannot be null");
 
-    /**********************
-     * Internal Functions *
-     **********************/
+bytes32 nameHash = _getNameHash(_name);
+address oldAddress = addresses[nameHash];
+addresses[nameHash] = _address;
 
-    /**
-     * Computes the hash of a name.
-     * @param _name Name to compute a hash for.
-     * @return Hash of the given name.
-     */
-    function _getNameHash(string memory _name) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_name));
-    }
+emit AddressSet(_name, _address, oldAddress);
+}
+
+/********************
+
+Public Functions *
+********************/
+/**
+
+Retrieves the address associated with a given name.
+@param _name Name to retrieve an address for.
+@return Address associated with the given name.
+*/
+function getAddress(string memory _name) external view returns (address) {
+return addresses[_getNameHash(_name)];
+}
+/**********************
+
+Internal Functions *
+**********************/
+/**
+
+Computes the hash of a name.
+@param _name Name to compute a hash for.
+@return Hash of the given name.
+*/
+function _getNameHash(string memory _name) internal pure returns (bytes32) {
+return keccak256(abi.encodePacked(_name));
+}
 }
