e184ab251536bfb70af80ddb05d7951b8d9d6fd1
Daniel Wang <dan@taiko.xyz>
2023-02-10 13:29:10 +0800

    Update Bridge.sol

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -50,9 +50,9 @@ contract Bridge is EssentialContract, IBridge {
     /// Allow Bridge to receive ETH from the TokenVault or EtherVault.
     receive() external payable {
         require(
-            msg.sender == owner() ||
-                msg.sender == resolve("token_vault", true) ||
-                msg.sender == resolve("ether_valut", true),
+            msg.sender == resolve("token_vault", true) ||
+                msg.sender == resolve("ether_valut", true) ||
+                msg.sender == owner(),
             "B:receive"
         );
     }
