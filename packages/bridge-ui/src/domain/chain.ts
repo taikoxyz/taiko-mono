9d95b304a39908fad77e60aa9fd17cd69e288e92
Shadab Khan <shadab@taiko.xyz>
2023-02-15 09:19:34 +0530

    fix(bridge-ui): refactor

--- packages/bridge-ui/src/domain/chain.ts
+++ packages/bridge-ui/src/domain/chain.ts
@@ -13,45 +13,25 @@ export const CHAIN_ID_TAIKO = import.meta.env
   ? BigNumber.from(import.meta.env.VITE_TAIKO_CHAIN_ID).toNumber()
   : 167001;
 
-const L1_RPC = import.meta.env
-  ? import.meta.env.VITE_L1_RPC_URL
-  : "https://l1rpc.internal.taiko.xyz/";
+const L1_RPC = import.meta?.env?.VITE_L1_RPC_URL ?? "https://l1rpc.internal.taiko.xyz/";
 
-const L2_RPC = import.meta.env
-  ? import.meta.env.VITE_L2_RPC_URL
-  : "https://l2rpc.internal.taiko.xyz/";
+const L2_RPC = import.meta?.env?.VITE_L2_RPC_URL ?? "https://l2rpc.internal.taiko.xyz/";
 
-const L1_BRIDGE_ADDRESS = import.meta.env
-  ? import.meta.env.VITE_MAINNET_BRIDGE_ADDRESS
-  : "0x0237443359aB0b11EcDC41A7aF1C90226a88c70f";
+const L1_BRIDGE_ADDRESS = import.meta?.env?.VITE_MAINNET_BRIDGE_ADDRESS ?? "0x0237443359aB0b11EcDC41A7aF1C90226a88c70f";
 
-const L2_BRIDGE_ADDRESS = import.meta.env
-  ? import.meta.env.VITE_TAIKO_BRIDGE_ADDRESS
-  : "0x0000777700000000000000000000000000000004";
+const L2_BRIDGE_ADDRESS = import.meta?.env?.VITE_TAIKO_BRIDGE_ADDRESS ?? "0x0000777700000000000000000000000000000004";
 
-const L1_HEADER_SYNC_ADDRESS = import.meta.env
-? import.meta.env.VITE_MAINNET_HEADER_SYNC_ADDRESS
-: "0xa6421A7f48498cee3aEb6428a8A2DD5fAA3AcE2f";
+const L1_HEADER_SYNC_ADDRESS = import.meta?.env?.VITE_MAINNET_HEADER_SYNC_ADDRESS ?? "0xa6421A7f48498cee3aEb6428a8A2DD5fAA3AcE2f";
 
-const L2_HEADER_SYNC_ADDRESS = import.meta.env
-  ? import.meta.env.VITE_TAIKO_HEADER_SYNC_ADDRESS
-  : "0x0000777700000000000000000000000000000001";
+const L2_HEADER_SYNC_ADDRESS = import.meta?.env?.VITE_TAIKO_HEADER_SYNC_ADDRESS ?? "0x0000777700000000000000000000000000000001";
 
-const L1_SIGNAL_SERVICE_ADDRESS = import.meta.env
-  ? import.meta.env.VITE_MAINNET_SIGNAL_SERVICE_ADDRESS
-  : "0x403cc7802725928652a3d116Bb1781005e2e76d3";
+const L1_SIGNAL_SERVICE_ADDRESS = import.meta?.env?.VITE_MAINNET_SIGNAL_SERVICE_ADDRESS ?? "0x403cc7802725928652a3d116Bb1781005e2e76d3";
 
-const L2_SIGNAL_SERVICE_ADDRESS = import.meta.env
-  ? import.meta.env.VITE_TAIKO_SIGNAL_SERVICE_ADDRESS
-  : "0x0000777700000000000000000000000000000007";
+const L2_SIGNAL_SERVICE_ADDRESS = import.meta?.env?.VITE_TAIKO_SIGNAL_SERVICE_ADDRESS ?? "0x0000777700000000000000000000000000000007";
 
-const L1_EXPLORER_URL = import.meta.env
-  ? import.meta.env.VITE_L1_EXPLORER_URL
-  : "https://l1explorer.internal.taiko.xyz/";
+const L1_EXPLORER_URL = import.meta?.env?.VITE_L1_EXPLORER_URL ?? "https://l1explorer.internal.taiko.xyz/";
 
-const L2_EXPLORER_URL = import.meta.env
-  ? import.meta.env.VITE_L2_EXPLORER_URL
-  : "https://l2explorer.internal.taiko.xyz/";
+const L2_EXPLORER_URL = import.meta?.env?.VITE_L2_EXPLORER_URL ?? "https://l2explorer.internal.taiko.xyz/";
 
 export type Chain = {
   id: number;
