09999df4699da49dc1c3784e16be9f52a70b4a97
Daniel Wang <dan@taiko.xyz>
2023-02-23 15:00:24 +0800

    more

--- packages/protocol/contracts/L1/TkoToken.sol
+++ packages/protocol/contracts/L1/TkoToken.sol
@@ -44,7 +44,10 @@ contract TkoToken is EssentialContract, ERC20Upgradeable, IMintableERC20 {
     /// @dev Initializer to be called after being deployed behind a proxy.
     ///      Based on our simulation in simulate/tokenomics/index.js, both
     ///      amountMintToDAO and amountMintToDev shall be set to ~150,000,000.
-    function init(string memory _symbol, address _addressManager) external initializer {
+    function init(
+        string memory _symbol,
+        address _addressManager
+    ) external initializer {
         EssentialContract._init(_addressManager);
         ERC20Upgradeable.__ERC20_init({
             name_: "Taiko Token",
