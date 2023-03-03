c9299d54d0148bf6d957e651b5373c37ba6e51e2
Francisco <jscriptcoder@gmail.com>
2023-03-03 23:07:48 +0100

    Add prettier

--- packages/bridge-ui/src/components/buttons/SelectToken.svelte
+++ packages/bridge-ui/src/components/buttons/SelectToken.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import { getProvider } from '@wagmi/core'
-  import { token } from "../../store/token";
-  import { bridgeType } from "../../store/bridge";
-  import { ETH, tokens } from "../../domain/token";
-  import type { Token } from "../../domain/token";
-  import { BridgeType } from "../../domain/bridge";
-  import { ChevronDown, PlusCircle } from "svelte-heros-v2";
-  import { errorToast, successToast } from "../../utils/toast";
-  import { ethers } from "ethers";
-  import ERC20 from "../../constants/abi/ERC20";
+  import { getProvider } from '@wagmi/core';
+  import { token } from '../../store/token';
+  import { bridgeType } from '../../store/bridge';
+  import { ETH, tokens } from '../../domain/token';
+  import type { Token } from '../../domain/token';
+  import { BridgeType } from '../../domain/bridge';
+  import { ChevronDown, PlusCircle } from 'svelte-heros-v2';
+  import { errorToast, successToast } from '../../utils/toast';
+  import { ethers } from 'ethers';
+  import ERC20_ABI from '../../constants/abi/ERC20';
   import { signer } from '../../store/signer';
   import { userTokens, tokenService } from '../../store/userToken';
   import { fromChain, toChain } from '../../store/chain';
@@ -16,6 +16,13 @@
   import AddCustomErc20 from '../form/AddCustomERC20.svelte';
 
   let dropdownElement: HTMLDivElement;
+  let showAddressField = false;
+  let loading = false;
+  let customTokens = [];
+
+  userTokens.subscribe((value) => {
+    if (value) customTokens = value;
+  });
 
   async function select(t: Token) {
     if (t === $token) return;
@@ -34,29 +41,25 @@
     }
   }
 
-  let showAddressField = false;
-
-  let loading = false;
-
   async function addERC20(event: SubmitEvent) {
     try {
       loading = true;
       const eventTarget = event.target as HTMLFormElement;
       const { customTokenAddress } = eventTarget;
       const tokenAddress = customTokenAddress.value;
-      if(!ethers.utils.isAddress(tokenAddress)) {
+      if (!ethers.utils.isAddress(tokenAddress)) {
         throw new Error();
       }
       const provider = getProvider();
-      const contract = new ethers.Contract(tokenAddress, ERC20, provider);
-      
+      const contract = new ethers.Contract(tokenAddress, ERC20_ABI, provider);
+
       const userAddress = await $signer.getAddress();
       const erc20Check = await contract.balanceOf(userAddress);
       const [tokenName, decimals, symbol] = await Promise.all([
         contract.name(),
         contract.decimals(),
         contract.symbol(),
-      ])
+      ]);
 
       const token = {
         name: tokenName,
@@ -67,35 +70,31 @@
           },
           {
             chainId: $toChain.id,
-            address: "0x00",
-          }
+            address: '0x00',
+          },
         ],
         decimals: decimals,
         symbol: symbol,
         logoComponent: null,
-      }
+      };
       const updateTokensList = await $tokenService.StoreToken(token, userAddress);
       select(token);
       userTokens.set(updateTokensList);
       eventTarget.reset();
       showAddressField = false;
-    } catch(error) {
-      errorToast("Not a valid ERC20 address");
+    } catch (error) {
+      errorToast('Not a valid ERC20 address');
       console.error(error);
     } finally {
       loading = false;
     }
   }
-  let customTokens = [];
-  userTokens.subscribe(value => {
-    if(value)
-      customTokens = value;
-  })
 </script>
 
 <div class="dropdown dropdown-bottom" bind:this={dropdownElement}>
   <button
     id="tokensDropdown"
+    aria-haspopup="listbox"
     class="flex items-center justify-center hover:cursor-pointer"
   >
     {#if $token.logoComponent}
@@ -104,43 +103,44 @@
       <Erc20 />
     {/if}
     <p class="px-2 text-sm">{$token.symbol.toUpperCase()}</p>
-    <ChevronDown size='20' />
+    <ChevronDown size="20" />
   </button>
+
   <ul
     class="token-dropdown dropdown-content menu my-2 shadow-xl bg-dark-2 rounded-box p-2"
+    tabindex="-1"
+    role="listbox"
     aria-labelledby="tokensDropdown"
   >
     {#each tokens as t}
-      <li class="cursor-pointer w-full hover:bg-dark-5 px-4 py-4 rounded-none">
-        <button on:click={async () => await select(t)} class="flex hover:bg-dark-5">
+      <li class="cursor-pointer w-full hover:bg-dark-5 rounded-none">
+        <button on:click={async () => await select(t)} class="flex hover:bg-dark-5 p-4">
           <svelte:component this={t.logoComponent} height={22} width={22} />
-          <span class="text-sm font-medium bg-transparent px-2"
-            >{t.symbol.toUpperCase()}</span
-          >
+          <span class="text-sm font-medium bg-transparent px-2">{t.symbol.toUpperCase()}</span>
         </button>
       </li>
     {/each}
     {#each customTokens as t}
-      <li class="cursor-pointer w-full hover:bg-dark-5 px-4 py-4">
-        <button on:click={async () => await select(t)} class="flex hover:bg-dark-5">
+      <li class="cursor-pointer w-full hover:bg-dark-5">
+        <button on:click={async () => await select(t)} class="flex hover:bg-dark-5 p-4">
           <Erc20 height={22} width={22} />
-          <span class="text-sm font-medium bg-transparent px-2"
-            >{t.symbol.toUpperCase()}</span
-          >
+          <span class="text-sm font-medium bg-transparent px-2">{t.symbol.toUpperCase()}</span>
         </button>
       </li>
     {/each}
-    <li class="cursor-pointer hover:bg-dark-5 px-4 py-4">
-      <button on:click={() => showAddressField = true} class="flex hover:bg-dark-5 justify-between items-center">
-        <PlusCircle size='25' />
-        <span class="text-sm font-medium bg-transparent flex-1 w-[100px] px-0 pl-2"
-          >Add Custom</span
+    <li class="cursor-pointer hover:bg-dark-5">
+      <button
+        on:click={() => (showAddressField = true)}
+        class="flex hover:bg-dark-5 justify-between items-center p-4"
+      >
+        <PlusCircle size="25" />
+        <span class="text-sm font-medium bg-transparent flex-1 w-[100px] px-0 pl-2">Add Custom</span
         >
       </button>
     </li>
   </ul>
 
   {#if showAddressField}
-    <AddCustomErc20 bind:showAddressField addERC20={addERC20} bind:loading />
+    <AddCustomErc20 bind:showAddressField {addERC20} bind:loading />
   {/if}
 </div>
--- packages/bridge-ui/src/components/modals/FaucetModal.svelte
+++ packages/bridge-ui/src/components/modals/FaucetModal.svelte
@@ -1,27 +1,21 @@
 <script lang="ts">
-  import { BigNumber, ethers, logger, Signer } from "ethers";
-  import { pendingTransactions } from "../../store/transactions";
-  import { signer } from "../../store/signer";
-  import { errorToast, successToast } from "../../utils/toast";
-  import { _ } from "svelte-i18n";
-  import MintableERC20 from "../../constants/abi/MintableERC20";
-  import { fromChain } from "../../store/chain";
-  import { fetchSigner, switchNetwork } from "@wagmi/core";
-  import { CHAIN_MAINNET } from "../../domain/chain";
-  import Modal from "./Modal.svelte";
-  import { onMount } from "svelte";
-  import { token } from "../../store/token";
+  import { BigNumber, ethers, Signer } from 'ethers';
+  import { pendingTransactions } from '../../store/transactions';
+  import { signer } from '../../store/signer';
+  import { errorToast, successToast } from '../../utils/toast';
+  import { _ } from 'svelte-i18n';
+  import MintableERC20 from '../../constants/abi/MintableERC20';
+  import { fromChain } from '../../store/chain';
+  import { fetchSigner, switchNetwork } from '@wagmi/core';
+  import { CHAIN_MAINNET } from '../../domain/chain';
+  import Modal from './Modal.svelte';
+  import { onMount } from 'svelte';
+  import { token } from '../../store/token';
 
   export let isOpen: boolean = false;
   export let onMint: () => Promise<void>;
 
   let disabled: boolean = true;
-
-  onMount(async () => {
-    isBtnDisabled($signer);
-  });
-
-  $: isBtnDisabled($signer).catch((e) => console.error(e));
 
   async function isBtnDisabled(signer: Signer) {
     if (!signer) return;
@@ -29,11 +23,7 @@
     const balance = await signer.getBalance();
     const address = await signer.getAddress();
 
-    const contract = new ethers.Contract(
-      $token.addresses[0].address,
-      MintableERC20,
-      signer
-    );
+    const contract = new ethers.Contract($token.addresses[0].address, MintableERC20, signer);
 
     const gas = await contract.estimateGas.mint(address);
     const gasPrice = await signer.getGasPrice();
@@ -55,11 +45,7 @@
 
         signer.set(wagmiSigner);
       }
-      const contract = new ethers.Contract(
-        $token.addresses[0].address,
-        MintableERC20,
-        $signer
-      );
+      const contract = new ethers.Contract($token.addresses[0].address, MintableERC20, $signer);
 
       const address = await $signer.getAddress();
       const tx = await contract.mint(address);
@@ -69,26 +55,30 @@
         return store;
       });
 
-      successToast($_("toast.transactionSent"));
+      successToast($_('toast.transactionSent'));
       isOpen = false;
       await $signer.provider.waitForTransaction(tx.hash, 1);
 
       await onMint();
     } catch (e) {
       console.log(e);
-      errorToast($_("toast.errorSendingTransaction"));
+      errorToast($_('toast.errorSendingTransaction'));
     }
   }
+
+  $: isBtnDisabled($signer).catch((e) => console.error(e));
+  $: mainnetName = import.meta.env ? import.meta.env.VITE_MAINNET_CHAIN_NAME : 'Ethereum A2';
+  $: taikonetName = import.meta.env ? import.meta.env.VITE_TAIKO_CHAIN_NAME : 'Taiko A2';
+
+  onMount(async () => {
+    isBtnDisabled($signer);
+  });
 </script>
 
-<Modal title={"ERC20 Faucet"} bind:isOpen>
-  You can request 1000 {$token.symbol}. {$token.symbol} is only available to be minted
-  on {import.meta.env
-    ? import.meta.env.VITE_MAINNET_CHAIN_NAME
-    : "Ethereum A2"}. If you are on {import.meta.env ? import.meta.env.VITE_TAIKO_CHAIN_NAME : "Taiko A2"}, your network will be changed first. You
-  must have a small amount of ETH in your {import.meta.env
-    ? import.meta.env.VITE_MAINNET_CHAIN_NAME
-    : "Ethereum A2"} wallet to send the transaction.
+<Modal title={'ERC20 Faucet'} bind:isOpen>
+  You can request 5 {$token.symbol}. {$token.symbol} is only available to be minted on {mainnetName}.
+  If you are on {taikonetName}, your network will be changed first. You must have a small amount of
+  ETH in your {mainnetName} wallet to send the transaction.
   <br />
   <button
     class="btn btn-dark-5 h-[60px] text-base"
