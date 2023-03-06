75a0a427512752e4e20d419be2e4c9ae0331e49c
Daniel Wang <dan@taiko.xyz>
2023-03-06 17:23:23 +0800

    fix

--- packages/protocol/test2/TaikoL1.t.sol
+++ packages/protocol/test2/TaikoL1.t.sol
@@ -4,12 +4,38 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../contracts/thirdparty/AddressManager.sol";
 import "../contracts/L1/TaikoL1.sol";
+import "../contracts/L1/TaikoData.sol";
 
 contract TaikoL1Test is Test {
     TaikoL1 public L1;
 
     AddressManager public addressManager;
     bytes32 public genesisBlockHash;
+
+    function propose(address proposer, uint256 txListSize) internal {
+        bytes[] memory inputs = new bytes[](3);
+        inputs[1] = bytes("txList");
+        inputs[2] = bytes("txListProof");
+
+        TaikoData.BlockMetadata memory meta = TaikoData.BlockMetadata({
+            id: 0,
+            l1Height: 0,
+            l1Hash: 0,
+            beneficiary: proposer,
+            txListHash: keccak256(inputs[1]),
+            txListProofHash: keccak256(inputs[2]),
+            mixHash: 0,
+            extraData: new bytes(10),
+            gasLimit: 1000000,
+            timestamp: 0
+        });
+
+        inputs[0] = abi.encode(meta);
+
+        vm.prank(proposer, proposer);
+        vm.deal(proposer, 1 ether);
+        L1.proposeBlock(inputs);
+    }
 
     function setUp() public {
         addressManager = new AddressManager();
@@ -20,10 +46,8 @@ contract TaikoL1Test is Test {
         L1.init(address(addressManager), genesisBlockHash, feeBase);
     }
 
-    function testIncrement() public {
-    }
-
-    function testSetNumber(uint256 x) public {
-        // assertEq(counter.number(), x);
+    function testProposeSingleBlock() public {
+        address alice = 0xc8885E210E59Dba0164Ba7CDa25f607e6d586B7A;
+        propose(alice, 1024);
     }
 }
