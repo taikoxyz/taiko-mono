447b5bf673d73d87361bebc87386da8ad07076cd
Shadab Khan <shadab@taiko.xyz>
2023-03-21 18:05:31 +0530

    fix(bridge-ui): move styles to component

--- packages/bridge-ui/src/components/form/BridgeForm.svelte
+++ packages/bridge-ui/src/components/form/BridgeForm.svelte
@@ -472,4 +472,14 @@
     margin: 0;
     -moz-appearance: textfield !important;
   }
+
+  .btn.btn-accent.approve-btn {
+    background-color: #4c1d95;
+    border-color: #4c1d95;
+  }
+
+  .btn.btn-accent.approve-btn:hover {
+    background-color: #5b21b6;
+    border-color: #5b21b6;
+  }
 </style>
