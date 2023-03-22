16eb81ac02c5df772783b766fad2f9f44c468c31
Daniel Wang <dan@taiko.xyz>
2023-03-22 13:06:40 +0800

    more

--- packages/protocol/test2/GasComparison.t.sol
+++ packages/protocol/test2/GasComparison.t.sol
@@ -8,13 +8,6 @@ import "../contracts/libs/LibAddress.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 library LibAddress2 {
-    /**
-     * Sends Ether to an address. Zero-value will also be sent.
-     * See more information at:
-     * https://consensys.net/diligence/blog/2019/09/stop-using-soliditys-transfer-now.
-     * @param to The target address.
-     * @param amount The amount of Ether to send.
-     */
     function sendEther(address to, uint256 amount) internal {
         (bool success, ) = payable(to).call{value: amount}("");
         require(success, "ETH transfer failed");
