08b26d21577ed8ecd14beed5a600108fe7a0f765
jeff <113397187+cyberhorsey@users.noreply.github.com>
2023-03-27 13:51:01 -0700

    feat(eventindexer): Event indexer (#13439)
    Co-authored-by: David <david@taiko.xyz>
    Co-authored-by: dave | d1onys1us <13951458+d1onys1us@users.noreply.github.com>

---
+++ packages/eventindexer/prometheus.go
@@ -0,0 +1,25 @@
+package eventindexer
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	EventsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "events_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+	BlocksProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "blocks_processed_ops_total",
+		Help: "The total number of processed blocks",
+	})
+	BlocksScannedError = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "blocks_scanned_error_ops_total",
+		Help: "The total number of scanned block errors.",
+	})
+	ErrorsEncounteredDuringSubscription = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "errors_encountered_during_subscription_opts_total",
+		Help: "The total number of errors that occured during active subscription",
+	})
+)
---
+++ packages/eventindexer/repo/event.go
@@ -0,0 +1,53 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/taikoxyz/taiko-mono/packages/eventindexer"
+	"gorm.io/datatypes"
+)
+
+type EventRepository struct {
+	db eventindexer.DB
+}
+
+func NewEventRepository(db eventindexer.DB) (*EventRepository, error) {
+	if db == nil {
+		return nil, eventindexer.ErrNoDB
+	}
+
+	return &EventRepository{
+		db: db,
+	}, nil
+}
+
+func (r *EventRepository) Save(ctx context.Context, opts eventindexer.SaveEventOpts) (*eventindexer.Event, error) {
+	e := &eventindexer.Event{
+		Data:    datatypes.JSON(opts.Data),
+		ChainID: opts.ChainID.Int64(),
+		Name:    opts.Name,
+		Event:   opts.Event,
+		Address: opts.Address,
+	}
+
+	if err := r.db.GormDB().Create(e).Error; err != nil {
+		return nil, errors.Wrap(err, "r.db.Create")
+	}
+
+	return e, nil
+}
+
+func (r *EventRepository) FindUniqueProvers(
+	ctx context.Context,
+) ([]eventindexer.UniqueProversResponse, error) {
+	addrs := make([]eventindexer.UniqueProversResponse, 0)
+
+	if err := r.db.GormDB().
+		Raw("SELECT address, count(*) AS count FROM events GROUP BY address").
+		FirstOrInit(&addrs).Error; err != nil {
+		return nil, errors.Wrap(err, "r.db.FirstOrInit")
+	}
+
+	return addrs, nil
+}
