cb3f7b9529addc25fe4d3067f2e2c3da3ae1b2bf
jeff <113397187+cyberhorsey@users.noreply.github.com>
2023-03-28 10:11:33 -0700

    feat(relayer): big Gas price (#13492)

--- packages/bridge-ui/src/components/InsufficientBalanceTooltip.svelte
+++ packages/bridge-ui/src/components/InsufficientBalanceTooltip.svelte
@@ -4,13 +4,15 @@
   export let show: boolean;
 </script>
 
-<TooltipModal title="Insufficient Balance" bind:isOpen={show}>
+<TooltipModal title="Waiting for Relayer" bind:isOpen={show}>
   <span slot="body">
     <div class="text-left">
-      You have insufficient balance to claim this transaction.
       <strong>
         Please wait for the relayer to claim the transaction for you.
       </strong>
+
+      This will happen automatically. Depending on network congestion, this can
+      take up to several hours.
     </div>
   </span>
 </TooltipModal>
