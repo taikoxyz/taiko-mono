cb58e5e529ec8cb3077af82316ad6f827255e29e
Francisco Ramos <jscriptcoder@gmail.com>
2023-04-04 17:29:59 +0200

    Update packages/bridge-ui/src/store/signer.ts
    Co-authored-by: dave | d1onys1us <13951458+d1onys1us@users.noreply.github.com>

--- packages/bridge-ui/src/store/signer.ts
+++ packages/bridge-ui/src/store/signer.ts
@@ -5,6 +5,6 @@ import { subscribeToSigner } from '../signer/subscriber';
 export const signer = writable<Signer>();
 
 // Everytime the signer changes, we need to run the subscribeToSigner,
-// which will take care of makind some API calls and update other stores
+// which will take care of making some API calls and update other stores
 // such as transactions and userTokens.
 signer.subscribe(subscribeToSigner);
