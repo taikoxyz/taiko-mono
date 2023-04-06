a4bfe4a4f97546f9adef3e7519fda27f37ae0018
Francisco <jscriptcoder@gmail.com>
2023-04-06 09:52:24 +0200

    Extract Router

---
+++ packages/bridge-ui/src/components/Router.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import SvelteRouter from 'svelte-spa-router';
+  import { wrap } from 'svelte-spa-router/wrap';
+  import Home from '../pages/home/Home.svelte';
+
+  const routes = {
+    '/:tab?': wrap({
+      component: Home,
+      props: {},
+      userData: {},
+    }),
+  };
+</script>
+
+<SvelteRouter {routes} />
