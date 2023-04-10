cb03d2886a2604d5b915928a49d2c7f192b10378
Daniel Wang <dan@taiko.xyz>
2023-04-10 13:11:20 +0800

    more

--- packages/protocol/contracts/L1/TaikoL1.sol
+++ packages/protocol/contracts/L1/TaikoL1.sol
@@ -155,10 +155,12 @@ contract TaikoL1 is EssentialContract, IXchainSync, TaikoEvents, TaikoErrors {
         LibEthDepositing.cancelEtherDepositToL2(state, depositId);
     }
 
+    // TODO: rename to `depositTaikoToken`
     function deposit(uint256 amount) external nonReentrant {
         LibTokenomics.deposit(state, AddressResolver(this), amount);
     }
 
+    // TODO: rename to `withdrawTaikoToken`
     function withdraw(uint256 amount) external nonReentrant {
         LibTokenomics.withdraw(state, AddressResolver(this), amount);
     }
