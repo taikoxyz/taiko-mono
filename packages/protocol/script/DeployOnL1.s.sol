e90370cb64d8e4499f5b86cd24de99a3af136664
Daniel Wang <dan@taiko.xyz>
2023-04-16 03:07:09 +0800

    fix

--- packages/protocol/script/DeployOnL1.s.sol
+++ packages/protocol/script/DeployOnL1.s.sol
@@ -185,7 +185,7 @@ contract DeployOnL1 is Script, AddressResolver {
 
         for (uint16 i = 0; i < plonkVerifiers.length; ++i) {
             setAddress(
-                string(abi.encodePacked("verifier_", i)),
+                string(bytes.concat(bytes("verifier_"), bytes2(uint16(2)))),
                 plonkVerifiers[i]
             );
         }
