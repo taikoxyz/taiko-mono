190c4529eebdbbe064c80d9d0ea3180016c4335c
Francisco <jscriptcoder@gmail.com>
2023-04-15 20:48:14 +0900

    Change test

--- packages/bridge-ui/src/utils/Deferred.spec.ts
+++ packages/bridge-ui/src/utils/Deferred.spec.ts
@@ -4,12 +4,22 @@ describe('Deferred', () => {
   it('should resolve', async () => {
     const deferred = new Deferred();
     deferred.resolve('test');
-    expect(await deferred.promise).toBe('test');
+
+    try {
+      await expect(deferred.promise).resolves.toBe('test');
+    } catch (e) {
+      throw Error('This should never happen');
+    }
   });
 
   it('should reject', async () => {
     const deferred = new Deferred();
     deferred.reject('test');
-    await expect(deferred.promise).rejects.toBe('test');
+
+    try {
+      await deferred.promise;
+    } catch (err) {
+      expect(err).toBe('test');
+    }
   });
 });
