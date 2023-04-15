c5254108256c3c8a3d155d94d89a29119fff8d67
Francisco <jscriptcoder@gmail.com>
2023-04-15 12:28:38 +0900

    More setup

---
+++ packages/bridge-ui-new/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import '../app.css'
+  import Header from '../components/Header'
+  import Footer from '../components/Footer'
+</script>
+
+<div class="layout">
+  <Header />
+  <main>
+    <slot />
+  </main>
+  <Footer />
+</div>
