76ef2a5343c7a423e51a5a9e9a44d56298b39051
adaki2004 <keszeydani@gmail.com>
2023-04-16 09:35:04 +0200

    Add more possible mainnet mock simulation

--- packages/protocol/test2/LibTokenomicsMainnetMock.t.sol
+++ packages/protocol/test2/LibTokenomicsMainnetMock.t.sol
@@ -50,12 +50,14 @@ contract LibL1TokenomicsTest is TaikoL1TestBase {
 
     /// @dev A possible (close to) mainnet scenarios is the following:
     //// - Blocks ever 10 seconds proposed
-    //// - Proofs coming shifted 30 min / proposed block afterwards
-    //// Foundry has some issues with the iterations (EVM revert) but the calculation mechnism can be seen from this
-    function test_possible_mainnet_scenarios() external {
+    //// - Proofs coming shifted slightly below 30 min / proposed block afterwards
+    //// Expected result: Withdrawals and deposits are in balance but keep shrinking since quicker proofTime
+    function xtest_possible_mainnet_scenario_proof_time_below_target()
+        external
+    {
         vm.pauseGasMetering();
         mine(1);
-        //Needs lot of token here - because there is lots of time elapsed between 2 'propose' blocks, which will raise the fee
+
         _depositTaikoToken(Alice, 1E8 * 1E8, 1000 ether);
         _depositTaikoToken(Bob, 1E8 * 1E8, 1000 ether);
         _depositTaikoToken(Carol, 1E8 * 1E8, 1000 ether);
@@ -66,8 +68,8 @@ contract LibL1TokenomicsTest is TaikoL1TestBase {
 
         // Can play to adjust
         uint32 iterationCnt = 5000;
+        uint8 proofTime = 179; // When proofs are coming, 179 means 1790 sec
 
-        /// 1.step: mine 180 blocks without proofs - then start prooving with a -180 offset
         TaikoData.BlockMetadata[] memory meta = new TaikoData.BlockMetadata[](
             iterationCnt
         );
@@ -87,20 +89,175 @@ contract LibL1TokenomicsTest is TaikoL1TestBase {
             blockHashes[blockId] = bytes32(1E10 + blockId);
             signalRoots[blockId] = bytes32(1E9 + blockId);
 
-            if (blockId > 179) {
+            if (blockId > proofTime) {
                 //Start proving with an offset
                 proveBlock(
                     Bob,
-                    meta[blockId - 179],
-                    parentHashes[blockId - 179],
-                    blockHashes[blockId - 179],
-                    signalRoots[blockId - 179]
+                    meta[blockId - proofTime],
+                    parentHashes[blockId - proofTime],
+                    blockHashes[blockId - proofTime],
+                    signalRoots[blockId - proofTime]
                 );
 
                 uint64 provenAt = uint64(block.timestamp);
                 console2.log(
                     "Proof reward is:",
-                    L1.getProofReward(provenAt, proposedAt[blockId - 179])
+                    L1.getProofReward(provenAt, proposedAt[blockId - proofTime])
+                );
+                verifyBlock(Carol, 1);
+            }
+
+            mine_every_10_sec();
+
+            parentHashes[blockId] = parentHash;
+            parentHash = blockHashes[blockId];
+        }
+
+        //Check end balances
+        uint256 deposits = Alice_start_balance - L1.getBalance(Alice);
+        uint256 withdrawals = L1.getBalance(Bob) - Bob_start_balance;
+
+        //Check end balances
+        deposits = Alice_start_balance - L1.getBalance(Alice);
+        withdrawals = L1.getBalance(Bob) - Bob_start_balance;
+
+        console2.log("Deposits:", deposits);
+        console2.log("withdrawals:", withdrawals);
+        // Assert their balance changed relatively the same way
+        // 1e18 == within 100 % delta -> 1e17 10%, let's see if this is within that range
+        assertApproxEqRel(deposits, withdrawals, 1e17);
+    }
+
+    /// @dev A possible (close to) mainnet scenarios is the following:
+    //// - Blocks ever 10 seconds proposed
+    //// - Proofs coming shifted 30 min / proposed block afterwards
+    //// Expected result: Withdrawals and deposits are in balance and since it is coming at proofTimeTarget stays the same
+    function xtest_possible_mainnet_scenario_proof_time_at_target() external {
+        vm.pauseGasMetering();
+        mine(1);
+
+        _depositTaikoToken(Alice, 1E8 * 1E8, 1000 ether);
+        _depositTaikoToken(Bob, 1E8 * 1E8, 1000 ether);
+        _depositTaikoToken(Carol, 1E8 * 1E8, 1000 ether);
+
+        // Check balances
+        uint256 Alice_start_balance = L1.getBalance(Alice);
+        uint256 Bob_start_balance = L1.getBalance(Bob);
+
+        // Can play to adjust
+        uint32 iterationCnt = 5000;
+        uint32 proofTime = 180; // When proofs are coming, 180 means 1800 sec
+
+        /// 1.step: mine 180 blocks without proofs - then start prooving with a -180 offset
+        TaikoData.BlockMetadata[] memory meta = new TaikoData.BlockMetadata[](
+            iterationCnt
+        );
+        uint64[] memory proposedAt = new uint64[](iterationCnt);
+        bytes32[] memory parentHashes = new bytes32[](iterationCnt);
+        bytes32[] memory blockHashes = new bytes32[](iterationCnt);
+        bytes32[] memory signalRoots = new bytes32[](iterationCnt);
+
+        bytes32 parentHash = GENESIS_BLOCK_HASH;
+        console2.logBytes32(parentHash);
+
+        for (uint256 blockId = 1; blockId < iterationCnt; blockId++) {
+            meta[blockId] = proposeBlock(Alice, 1024);
+            proposedAt[blockId] = (uint64(block.timestamp));
+            printVariables("after propose");
+            blockHashes[blockId] = bytes32(1E10 + blockId);
+            signalRoots[blockId] = bytes32(1E9 + blockId);
+
+            if (blockId > proofTime) {
+                //Start proving with an offset
+                proveBlock(
+                    Bob,
+                    meta[blockId - proofTime],
+                    parentHashes[blockId - proofTime],
+                    blockHashes[blockId - proofTime],
+                    signalRoots[blockId - proofTime]
+                );
+
+                uint64 provenAt = uint64(block.timestamp);
+                console2.log(
+                    "Proof reward is:",
+                    L1.getProofReward(provenAt, proposedAt[blockId - proofTime])
+                );
+                verifyBlock(Carol, 1);
+            }
+
+            mine_every_10_sec();
+
+            parentHashes[blockId] = parentHash;
+            parentHash = blockHashes[blockId];
+        }
+
+        //Check end balances
+        uint256 deposits = Alice_start_balance - L1.getBalance(Alice);
+        uint256 withdrawals = L1.getBalance(Bob) - Bob_start_balance;
+
+        //Check end balances
+        deposits = Alice_start_balance - L1.getBalance(Alice);
+        withdrawals = L1.getBalance(Bob) - Bob_start_balance;
+
+        console2.log("Deposits:", deposits);
+        console2.log("withdrawals:", withdrawals);
+        // Assert their balance changed relatively the same way
+        // 1e18 == within 100 % delta -> 1e17 10%, let's see if this is within that range
+        assertApproxEqRel(deposits, withdrawals, 1e17);
+    }
+
+    /// @dev A possible (close to) mainnet scenarios is the following:
+    //// - Blocks ever 10 seconds proposed
+    //// - Proofs coming shifted slightly above 30 min / proposed block afterwards
+    //// Expected result: Withdrawals and deposits are in balance but fees keep growing
+    function test_possible_mainnet_scenario_proof_time_above_target() external {
+        vm.pauseGasMetering();
+        mine(1);
+
+        _depositTaikoToken(Alice, 1E8 * 1E8, 1000 ether);
+        _depositTaikoToken(Bob, 1E8 * 1E8, 1000 ether);
+        _depositTaikoToken(Carol, 1E8 * 1E8, 1000 ether);
+
+        // Check balances
+        uint256 Alice_start_balance = L1.getBalance(Alice);
+        uint256 Bob_start_balance = L1.getBalance(Bob);
+
+        // Can play to adjust
+        uint32 iterationCnt = 5000;
+        uint8 proofTime = 181; // When proofs are coming, 181 means 1810 sec
+
+        TaikoData.BlockMetadata[] memory meta = new TaikoData.BlockMetadata[](
+            iterationCnt
+        );
+        uint64[] memory proposedAt = new uint64[](iterationCnt);
+        bytes32[] memory parentHashes = new bytes32[](iterationCnt);
+        bytes32[] memory blockHashes = new bytes32[](iterationCnt);
+        bytes32[] memory signalRoots = new bytes32[](iterationCnt);
+
+        bytes32 parentHash = GENESIS_BLOCK_HASH;
+        console2.logBytes32(parentHash);
+
+        for (uint256 blockId = 1; blockId < iterationCnt; blockId++) {
+            meta[blockId] = proposeBlock(Alice, 1024);
+            proposedAt[blockId] = (uint64(block.timestamp));
+            printVariables("after propose");
+            blockHashes[blockId] = bytes32(1E10 + blockId);
+            signalRoots[blockId] = bytes32(1E9 + blockId);
+
+            if (blockId > proofTime) {
+                //Start proving with an offset
+                proveBlock(
+                    Bob,
+                    meta[blockId - proofTime],
+                    parentHashes[blockId - proofTime],
+                    blockHashes[blockId - proofTime],
+                    signalRoots[blockId - proofTime]
+                );
+
+                uint64 provenAt = uint64(block.timestamp);
+                console2.log(
+                    "Proof reward is:",
+                    L1.getProofReward(provenAt, proposedAt[blockId - proofTime])
                 );
                 verifyBlock(Carol, 1);
             }
