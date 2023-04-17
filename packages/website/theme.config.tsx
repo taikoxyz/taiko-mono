6a1dbb9fd91dc0d82af4db4b952ef73cfa16cfdb
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-04-17 12:55:34 +0900

    Revert "docs(website): add alerts for hackathon network config (#13601)"
    This reverts commit 7e2de09770f933bda7e9d2cd22501337dc24535d.

--- packages/website/theme.config.tsx
+++ packages/website/theme.config.tsx
@@ -8,10 +8,10 @@ export default {
     key: "banner",
     text: (
       <a
-        href="https://taiko.xyz/docs/reference/ethglobal-tokyo-reference"
+        href="https://mirror.xyz/labs.taiko.eth/A6G6TNN-CXDAhl42k_bNHg_20fyGcT0xH-LBBSOPNzU"
         target="_blank"
       >
-        📌 Please use the <u>ETHTokyo network config</u> for the hackathon →
+        🌋 Askja testnet is here! Learn more →
       </a>
     ),
   },
