394a9d188da5a6bc8e2ffdd80121cb18471b3f08
Francisco Ramos <jscriptcoder@gmail.com>
2023-04-19 15:35:03 +0200

    fix(bridge-ui): pending transactions custom store with better error handling (#13581)
    Co-authored-by: jeff <113397187+cyberhorsey@users.noreply.github.com>

--- packages/bridge-ui/src/store/transactions.ts
+++ packages/bridge-ui/src/store/transactions.ts
@@ -1,10 +1,69 @@
 import { writable } from 'svelte/store';
-
-import type { Transaction } from 'ethers';
+import type { Signer, Transaction, ethers } from 'ethers';
 import type { BridgeTransaction, Transactioner } from '../domain/transactions';
+import { Deferred } from '../utils/Deferred';
 
-const pendingTransactions = writable<Transaction[]>([]);
-const transactions = writable<BridgeTransaction[]>([]);
-const transactioner = writable<Transactioner>();
+export const transactions = writable<BridgeTransaction[]>([]);
+export const transactioner = writable<Transactioner>();
 
-export { pendingTransactions, transactions, transactioner };
+// Custom store: pendingTransactions
+const { subscribe, set, update } = writable<Transaction[]>([]);
+export const pendingTransactions = {
+  /**
+   * We're creating here a custom store, which is a writable store.
+   * We must stick to the store contract, which is:
+   */
+  set,
+  subscribe,
+  // update, // this method is optional.
+
+  /**
+   * Custom method, which will help us add a new transaction to the store
+   * and get it removed onces the transaction is mined.
+   */
+  add: (tx: Transaction, signer: Signer) => {
+    const deferred = new Deferred<ethers.providers.TransactionReceipt>();
+
+    update((txs: Transaction[]) => {
+      // New array with the new transaction appended
+      const newPendingTransactions = [...txs, tx];
+
+      // Save the index of the new transaction to later on remove it
+      // from the list of pending transactions.
+      const idxAppendedTransaction = newPendingTransactions.length - 1;
+
+      // Next step is to wait for the transaction to be mined
+      // before removing it from the store.
+
+      /**
+       * Returns a Promise which will not resolve until transactionHash is mined.
+       * If confirms is 0, this method is non-blocking and if the transaction
+       * has not been mined returns null. Otherwise, this method will block until
+       * the transaction has confirms blocks mined on top of the block in which
+       * is was mined.
+       * See https://docs.ethers.org/v5/api/providers/provider/#Provider-waitForTransaction
+       */
+      signer.provider.waitForTransaction(tx.hash, 1).then((receipt) => {
+        // The transaction has been mined.
+
+        // Removes the transaction from the store
+        update((txs: Transaction[]) => {
+          const copyPendingTransactions = [...txs];
+          copyPendingTransactions.splice(idxAppendedTransaction, 1);
+          return copyPendingTransactions;
+        });
+
+        // Resolves or rejects the promise depending on the transaction status.
+        if (receipt.status === 1) {
+          deferred.resolve(receipt);
+        } else {
+          deferred.reject(receipt);
+        }
+      });
+
+      return newPendingTransactions;
+    });
+
+    return deferred.promise;
+  },
+};
