bf5d617e3e58ffdf646645d545aebb39670a4e1c
Francisco <jscriptcoder@gmail.com>
2023-04-20 15:43:37 +0200

    wip

---
+++ packages/bridge-ui-v2/src/libs/storage/jsonParseOrEmptyArray.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect,it } from 'vitest'
+
+import { jsonParseOrEmptyArray } from './jsonParseOrEmptyArray'
+
+describe('jsonParseOrEmptyArray', () => {
+  it('should return empty array when input is null or empty string', () => {
+    expect(jsonParseOrEmptyArray(null)).toEqual([])
+    expect(jsonParseOrEmptyArray('')).toEqual([])
+  })
+
+  it('should return empty array when input is not a valid JSON', () => {
+    expect(jsonParseOrEmptyArray(undefined)).toEqual([])
+    expect(jsonParseOrEmptyArray('not a valid JSON')).toEqual([])
+  })
+
+  it('should return parsed JSON when input is a valid JSON', () => {
+    const strJson = '{"person": "Fran", "age": "Unknown"}'
+    expect(jsonParseOrEmptyArray(strJson)).toEqual(JSON.parse(strJson))
+  })
+})
