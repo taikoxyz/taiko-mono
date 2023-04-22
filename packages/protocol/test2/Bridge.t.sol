a4116b7ce6d9247d0f34caff6e4490f92d76d573
Jeffery Walsh <cyberhorsey@gmail.com>
2023-04-21 21:16:05 -0700

    bridge tests

--- packages/protocol/test2/Bridge.t.sol
+++ packages/protocol/test2/Bridge.t.sol
@@ -11,6 +11,20 @@ import {SignalService} from "../contracts/signal/SignalService.sol";
 import {Test} from "forge-std/Test.sol";
 import {IXchainSync} from "../contracts/common/IXchainSync.sol";
 
+contract BadReceiver {
+    receive() external payable {
+        revert("can not send to this contract");
+    }
+
+    fallback() external payable {
+        revert("can not send to this contract");
+    }
+
+    function transfer() public pure {
+        revert("this fails");
+    }
+}
+
 contract PrankXchainSync is IXchainSync {
     bytes32 private _blockHash;
     bytes32 private _signalRoot;
@@ -34,6 +48,7 @@ contract PrankXchainSync is IXchainSync {
 
 contract BridgeTest is Test {
     AddressManager addressManager;
+    BadReceiver badReceiver;
     Bridge bridge;
     Bridge destChainBridge;
     EtherVault etherVault;
@@ -42,6 +57,8 @@ contract BridgeTest is Test {
     uint256 destChainId = 19389;
 
     address public constant Alice = 0x10020FCb72e27650651B05eD2CEcA493bC807Ba4;
+
+    address public constant Bob = 0x50081b12838240B1bA02b3177153Bca678a86078;
 
     function setUp() public {
         vm.startPrank(Alice);
@@ -228,6 +245,110 @@ contract BridgeTest is Test {
     // in foundry
     function test_process_message() public {
         vm.startPrank(Alice);
+        (
+            IBridge.Message memory message,
+            bytes memory proof
+        ) = setUpPredefinedSuccessfulProcessMessageCall();
+
+        bytes32 msgHash = destChainBridge.hashMessage(message);
+
+        bool isMessageReceived = destChainBridge.isMessageReceived(
+            msgHash,
+            1336,
+            proof
+        );
+
+        assertEq(isMessageReceived, true);
+
+        destChainBridge.processMessage(message, proof);
+
+        LibBridgeStatus.MessageStatus status = destChainBridge.getMessageStatus(
+            msgHash
+        );
+
+        assertEq(status == LibBridgeStatus.MessageStatus.DONE, true);
+    }
+
+    // test with a known good merkle proof / message since we cant generate proofs via rpc
+    // in foundry
+    function test_retry_message_and_end_up_in_failed_status() public {
+        vm.startPrank(Alice);
+        (
+            IBridge.Message memory message,
+            bytes memory proof
+        ) = setUpPredefinedSuccessfulProcessMessageCall();
+
+        // etch bad receiver at the to address, so it fails.
+        vm.etch(message.to, address(badReceiver).code);
+
+        bytes32 msgHash = destChainBridge.hashMessage(message);
+
+        bool isMessageReceived = destChainBridge.isMessageReceived(
+            msgHash,
+            1336,
+            proof
+        );
+
+        assertEq(isMessageReceived, true);
+
+        destChainBridge.processMessage(message, proof);
+
+        LibBridgeStatus.MessageStatus status = destChainBridge.getMessageStatus(
+            msgHash
+        );
+
+        assertEq(status == LibBridgeStatus.MessageStatus.RETRIABLE, true);
+
+        vm.stopPrank();
+        vm.prank(message.owner);
+
+        destChainBridge.retryMessage(message, true);
+
+        LibBridgeStatus.MessageStatus postRetryStatus = destChainBridge
+            .getMessageStatus(msgHash);
+
+        assertEq(postRetryStatus == LibBridgeStatus.MessageStatus.FAILED, true);
+    }
+
+    function retry_message_reverts_when_status_non_retriable() public {
+        IBridge.Message memory message = newMessage({
+            owner: Alice,
+            to: Alice,
+            depositValue: 1,
+            callValue: 0,
+            gasLimit: 10000,
+            processingFee: 1,
+            destChain: destChainId
+        });
+
+        vm.expectRevert(BridgeErrors.B_MSG_NON_RETRIABLE.selector);
+        destChainBridge.retryMessage(message, true);
+    }
+
+    function retry_message_reverts_when_last_attempt_and_message_is_not_owner()
+        public
+    {
+        vm.startPrank(Alice);
+        IBridge.Message memory message = newMessage({
+            owner: Bob,
+            to: Alice,
+            depositValue: 1,
+            callValue: 0,
+            gasLimit: 10000,
+            processingFee: 1,
+            destChain: destChainId
+        });
+
+        vm.expectRevert(BridgeErrors.B_DENIED.selector);
+        destChainBridge.retryMessage(message, true);
+    }
+
+    function setUpPredefinedSuccessfulProcessMessageCall()
+        internal
+        returns (IBridge.Message memory, bytes memory)
+    {
+        badReceiver = new BadReceiver();
+
         uint256 dest = 1337;
         addressManager.setAddress(
             string(bytes.concat(bytes32(dest), bytes("taiko"))),
@@ -259,11 +380,11 @@ contract BridgeTest is Test {
         );
 
         xChainSync.setXchainBlockHeader(
-            0x2f0658a880e7e1af9df5b00e67ff83613b97f6a8e58b33eab0a113441362b58c
+            0xd5f5d8ac6bc37139c97389b00e9cf53e89c153ad8a5fc765ffe9f44ea9f3d31e
         );
 
         xChainSync.setXchainSignalRoot(
-            0x09e1101bda6748ec95ef9c9d8ec487dc87beea5ffd326d0899f18f81dadae581
+            0x631b214fb030d82847224f0b3d3b906a6764dded176ad3c7262630204867ba85
         );
 
         vm.deal(address(destChainBridge), 1 ether);
@@ -277,8 +398,8 @@ contract BridgeTest is Test {
             srcChainId: 1336,
             destChainId: dest,
             owner: 0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39,
-            to: 0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39,
-            refundAddress: 0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39,
+            to: 0x200708D76eB1B69761c23821809d53F65049939e,
+            refundAddress: 0x10020FCb72e27650651B05eD2CEcA493bC807Ba4,
             depositValue: 1000,
             callValue: 1000,
             processingFee: 1000,
@@ -288,25 +409,9 @@ contract BridgeTest is Test {
         });
 
         bytes
-            memory proof = hex"0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000003e099947564d975df220911a6c6beb31446915da1d84ce3fc27bacbf8363feb45b71dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493470000000000000000000000000000000000000000000000000000000000000000c4e85c67809eb78115a206d1bf1d7e0ed6db3a6d945682b3da1ed67b82346fa332f43e008aa70ea5b0408ea1b5de805d742a7a14b3634b92e947b899017cf137dcac3b5f5ee1380b22eecc9c0567455fb63db11d7c468935a15ae478d7415d09800000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000001000040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000800000000400000000000000000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000001500000000000000000000000000000000000000000000000000000000009bbf55000000000000000000000000000000000000000000000000000000000001d4fb000000000000000000000000000000000000000000000000000000006443530c0000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004d2e85500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000061d883010a1a846765746888676f312e31382e38856c696e757800000000000000de2acd1f67a1c084d8a3c4d8b0176fb6cfee96d4656f8fafc5230f74490f36a32540f0f5ce89e486dcf49aff7e6d11a7877cc49297a34a837ebe53dbcbc418d1010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000dbf8d9b8b3f8b180a0ec28972d98f2fc53a982dae8f72427ed74dfba2d3383cf2c7b18bafeeacc96b0a04fc5f13ab2f9ba0c2da88b0151ab0e7cf4d85d08cca45ccd923c6ab76323eb28a02b70a98baa2507beffe8c266006cae52064dccf4fd1998af774ab3399029b38380808080a07394a09684ef3b2c87e9e2a753eb4ac78e2047b980e16d2e2133aee78946370d8080a0f4984a11f61a2921456141df88de6e1a710d28681b91af794c5a721e47839cd78080808080a3e2a03030d8e8a13da56640e5b3267b60970c536c784b94d2014ac7ed4d6c0df88e34010000000000";
+            memory proof = hex"0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000003e0f7ff3b519ec113138509a5b1b6f54761cebc6891bc0ba4f904b89688b1ef8e051dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d493470000000000000000000000000000000000000000000000000000000000000000a85358ff57974db8c9ce2ecabe743d44133f9d11e5da97e386111073f1a2f92c345bd00c2ef9db5726d84c184af67fdbad0be00921eb1dcbca674c427abb5c3ebda7d1e94e5b2b3d5e6a54c9a42423b1746afa4b264e7139877c0523c3397ec4000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000002000800002000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000001000040000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000001500000000000000000000000000000000000000000000000000000000009bbf55000000000000000000000000000000000000000000000000000000000001d4fb0000000000000000000000000000000000000000000000000000000064435d130000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004d2e85500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000061d883010a1a846765746888676f312e31382e38856c696e75780000000000000015b1ca61fbe1aa968ab60a461913aa40046b5357162466a4134d195647c14dd7488dd438abb39d6574e7d9d752fa2381bbd9dc780efc3fcc66af5285ebcb117b010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000dbf8d9b8b3f8b18080a04fc5f13ab2f9ba0c2da88b0151ab0e7cf4d85d08cca45ccd923c6ab76323eb28a02b70a98baa2507beffe8c266006cae52064dccf4fd1998af774ab3399029b38380808080a07394a09684ef3b2c87e9e2a753eb4ac78e2047b980e16d2e2133aee78946370d8080a0f4984a11f61a2921456141df88de6e1a710d28681b91af794c5a721e47839cd78080a09248167635e6f0eb40f782a6bbd237174104259b6af88b3c52086214098f0e2c8080a3e2a03ecd5e1f251bf1676a367f6b16e92ffe6b2638b4a27b3d31870d25442bd59ef4010000000000";
 
-        bytes32 msgHash = destChainBridge.hashMessage(message);
-
-        bool isMessageReceived = destChainBridge.isMessageReceived(
-            msgHash,
-            1336,
-            proof
-        );
-
-        assertEq(isMessageReceived, true);
-
-        destChainBridge.processMessage(message, proof);
-
-        LibBridgeStatus.MessageStatus status = destChainBridge.getMessageStatus(
-            msgHash
-        );
-
-        assertEq(status == LibBridgeStatus.MessageStatus.DONE, true);
+        return (message, proof);
     }
 
     function newMessage(
