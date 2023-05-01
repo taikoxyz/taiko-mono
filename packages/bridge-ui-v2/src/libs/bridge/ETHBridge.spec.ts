01a11a264c9beaa751f216f697eaacec0d928b3b
Francisco <jscriptcoder@gmail.com>
2023-05-01 10:34:02 +0200

    Add ETHBridge.bridge test

--- packages/bridge-ui-v2/src/libs/bridge/ETHBridge.spec.ts
+++ packages/bridge-ui-v2/src/libs/bridge/ETHBridge.spec.ts
@@ -12,6 +12,7 @@ vi.mock('ethers', async () => {
 
   const MockContract = vi.fn()
   MockContract.prototype = {
+    sendMessage: vi.fn(),
     estimateGas: {
       sendMessage: vi.fn().mockResolvedValue(10),
     },
@@ -44,13 +45,26 @@ const mockArgs: ETHBridgeArgs = {
 
 // TODO
 describe('ETHBridge', () => {
+  beforeEach(() => {
+    vi.mocked(Contract.prototype.sendMessage).mockClear()
+    vi.mocked(Contract.prototype.estimateGas.sendMessage).mockClear()
+  })
+
   it('should estimate gas to bridge ETH', async () => {
     const gasEstimate = await ETHBridge.estimateGas(mockArgs)
 
+    // This test is not that important but we make sure
+    // that the fake gas estimate is at least getting returned
     expect(gasEstimate).toEqual(10)
 
-    const mockedSendMessage = vi.mocked(Contract.prototype.estimateGas.sendMessage)
-    expect(mockedSendMessage).toHaveBeenCalledWith(
+    const mockedEstimateGasSendMessage = vi.mocked(Contract.prototype.estimateGas.sendMessage)
+
+    const depositValue = BigNumber.from(123)
+    const callValue = BigNumber.from(0)
+    const processingFee = BigNumber.from(123)
+    const gasLimit = BigNumber.from(140000)
+
+    expect(mockedEstimateGasSendMessage).toHaveBeenCalledWith(
       {
         sender: '0x123',
         srcChainId: PUBLIC_L1_CHAIN_ID,
@@ -58,16 +72,65 @@ describe('ETHBridge', () => {
         owner: '0x123',
         to: '0x123',
         refundAddress: '0x123',
-        depositValue: BigNumber.from(123),
-        callValue: BigNumber.from(0),
-        processingFee: BigNumber.from(123),
-        gasLimit: BigNumber.from(140000),
+        depositValue,
+        callValue,
+        processingFee,
+        gasLimit,
         memo: '',
         id: 1,
         data: '0x',
       },
       {
-        value: BigNumber.from(123).add(BigNumber.from(123)).add(BigNumber.from(0)),
+        value: depositValue.add(processingFee).add(callValue),
+      },
+    )
+  })
+
+  it('should bridge ETH', async () => {
+    // Let's make some changes here to execute other branches
+    const mockedArgsWithDifferentToAddress = {
+      ...mockArgs,
+
+      to: '0x321', // to !== owner address
+      // then:
+      // depositValue: BigNumber.from(0)
+      // callValue: amountInWei
+
+      processingFeeInWei: undefined,
+      // then:
+      // processingFee: BigNumber.from(0)
+      // gasLimit: BigNumber.from(0)
+
+      memo: 'This is a test',
+    }
+
+    await ETHBridge.bridge(mockedArgsWithDifferentToAddress)
+
+    const mockedSendMessage = vi.mocked(Contract.prototype.sendMessage)
+
+    const depositValue = BigNumber.from(0)
+    const callValue = BigNumber.from(123)
+    const processingFee = BigNumber.from(0)
+    const gasLimit = BigNumber.from(0)
+
+    expect(mockedSendMessage).toHaveBeenCalledWith(
+      {
+        sender: '0x123',
+        srcChainId: PUBLIC_L1_CHAIN_ID,
+        destChainId: PUBLIC_L2_CHAIN_ID,
+        owner: '0x123',
+        to: '0x321',
+        refundAddress: '0x123',
+        depositValue,
+        callValue,
+        processingFee,
+        gasLimit,
+        memo: 'This is a test',
+        id: 1,
+        data: '0x',
+      },
+      {
+        value: depositValue.add(processingFee).add(callValue),
       },
     )
   })
