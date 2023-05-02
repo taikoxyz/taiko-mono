37df1b8bc806384f9d4b1c81fc37520c27823a7b
Francisco <jscriptcoder@gmail.com>
2023-05-02 14:55:18 +0200

    wip: ETHBridge unit tests

--- packages/bridge-ui-v2/src/libs/bridge/ETHBridge.spec.ts
+++ packages/bridge-ui-v2/src/libs/bridge/ETHBridge.spec.ts
@@ -2,8 +2,14 @@ import { BigNumber, Contract, ethers, type Signer } from 'ethers'
 
 import { PUBLIC_L1_CHAIN_ID, PUBLIC_L2_CHAIN_ID } from '$env/static/public'
 
+import { chains } from '../chain'
+import { MessageOwnerError } from '../message/MessageOwnerError'
+import { MessageStatusError } from '../message/MessageStatusError'
+import { type Message, MessageStatus } from '../message/types'
+import { Prover } from '../prover'
+import { providers } from '../provider'
 import { ETHBridge } from './ETHBridge'
-import type { ETHBridgeArgs } from './types'
+import type { ClaimArgs, ETHBridgeArgs } from './types'
 
 vi.mock('$env/static/public')
 
@@ -12,6 +18,7 @@ vi.mock('ethers', async () => {
 
   const MockContract = vi.fn()
   MockContract.prototype = {
+    getMessageStatus: vi.fn(),
     sendMessage: vi.fn(),
     estimateGas: {
       sendMessage: vi.fn().mockResolvedValue(10),
@@ -43,11 +50,45 @@ const mockArgs: ETHBridgeArgs = {
   bridgeAddress: '0x123',
 }
 
+const mockMessage: Message = {
+  id: 123,
+  sender: '0x123',
+  srcChainId: PUBLIC_L1_CHAIN_ID,
+  destChainId: PUBLIC_L2_CHAIN_ID,
+  owner: '0x123',
+  to: '0x123',
+  refundAddress: '0x123',
+  depositValue: BigNumber.from(123),
+  callValue: BigNumber.from(0),
+  processingFee: BigNumber.from(123),
+  gasLimit: BigNumber.from(123),
+  memo: '',
+}
+
+const mockClaimArgs: ClaimArgs = {
+  message: mockMessage,
+  msgHash: '0x123',
+  signer: mockSigner,
+  destBridgeAddress: '0x123',
+  srcBridgeAddress: '0x345',
+}
+
+const mockProvider = { send: vi.fn() }
+
+providers[PUBLIC_L1_CHAIN_ID] = mockProvider as any
+providers[PUBLIC_L2_CHAIN_ID] = mockProvider as any
+
+const mockChain = {}
+
+chains[PUBLIC_L1_CHAIN_ID] = mockChain as any
+chains[PUBLIC_L2_CHAIN_ID] = mockChain as any
+
 // TODO
 describe('ETHBridge', () => {
   beforeEach(() => {
     vi.mocked(Contract.prototype.sendMessage).mockClear()
     vi.mocked(Contract.prototype.estimateGas.sendMessage).mockClear()
+    vi.mocked(Contract.prototype.getMessageStatus).mockResolvedValue(() => MessageStatus.New)
   })
 
   it('should estimate gas to bridge ETH', async () => {
@@ -133,5 +174,53 @@ describe('ETHBridge', () => {
         value: depositValue.add(processingFee).add(callValue),
       },
     )
+  })
+
+  it('should fail claiming if not the owner of the message', async () => {
+    const prover = new Prover(providers)
+    const bridge = new ETHBridge(prover, chains)
+
+    const mockClaimArgsWithDifferentOwner = {
+      ...mockClaimArgs,
+      message: {
+        ...mockClaimArgs.message,
+        owner: '0x321', // different from signer address 0x123
+      },
+    }
+
+    try {
+      await bridge.claim(mockClaimArgsWithDifferentOwner)
+      expect(true).toBe(false) // should not reach here
+    } catch (error) {
+      expect(error).instanceOf(MessageOwnerError)
+    }
+  })
+
+  it('should fail claiming if the message status is Done', async () => {
+    vi.mocked(Contract.prototype.getMessageStatus).mockResolvedValue(() => MessageStatus.Done)
+
+    const prover = new Prover(providers)
+    const bridge = new ETHBridge(prover, chains)
+
+    try {
+      await bridge.claim(mockClaimArgs)
+      expect(true).toBeFalsy()
+    } catch (error) {
+      expect(error).instanceOf(MessageStatusError)
+    }
+  })
+
+  it('should fail claiming if the message status is Failed', async () => {
+    vi.mocked(Contract.prototype.getMessageStatus).mockResolvedValue(() => MessageStatus.Failed)
+
+    const prover = new Prover(providers)
+    const bridge = new ETHBridge(prover, chains)
+
+    try {
+      await bridge.claim(mockClaimArgs)
+      expect(true).toBeFalsy()
+    } catch (error) {
+      expect(error).instanceOf(MessageStatusError)
+    }
   })
 })
