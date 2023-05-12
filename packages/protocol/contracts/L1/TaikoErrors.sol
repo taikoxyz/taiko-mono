feeca2b6eec5ec12d27dd4b314fceb18b33b0558
adaki2004 <keszeydani@gmail.com>
2023-05-12 14:43:11 +0200

    Rename custom error

--- packages/protocol/contracts/L1/TaikoErrors.sol
+++ packages/protocol/contracts/L1/TaikoErrors.sol
@@ -19,7 +19,7 @@ abstract contract TaikoErrors {
     error L1_INVALID_METADATA();
     error L1_INVALID_PARAM();
     error L1_INVALID_PROOF();
-    error L1_NOT_VALID_SGX_SIGNATURE();
+    error L1_INVALID_SGX_SIGNATURE();
     error L1_TOO_MANY_BLOCKS();
     error L1_TX_LIST_NOT_EXIST();
     error L1_TX_LIST_HASH();
