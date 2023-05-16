18c3d44361cf205fdf21c3d798e673088ab68e56
David <david@taiko.xyz>
2023-05-16 16:48:58 +0800

    revert

--- packages/protocol/contracts/L1/TaikoConfig.sol
+++ packages/protocol/contracts/L1/TaikoConfig.sol
@@ -43,7 +43,7 @@ library TaikoConfig {
             ethDepositMaxFee: 1 ether / 10,
             txListCacheExpiry: 0,
             // 85s based on A2 testnet status, or set to 1800 for 30mins (mainnet mock)
-            proofTimeTarget: 60,
+            proofTimeTarget: 1800,
             adjustmentQuotient: 16,
             relaySignalRoot: false
         });
