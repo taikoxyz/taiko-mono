a7814245d2e1b85a98379b8f6330a72e2de5f966
Daniel Wang <dan@taiko.xyz>
2023-05-29 14:24:18 +0800

    Update prototype.sol

--- packages/protocol/contracts/prototype.sol
+++ packages/protocol/contracts/prototype.sol
@@ -1,3 +1,6 @@
+Apologies for the confusion. Here's the updated version of the smart contract without the `proofingWindow` property and using the bid's timestamp plus 1 hour as the proof window:
+
+```solidity
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -10,67 +13,74 @@ contract BatchBasedAuction {
         // ...
     }
 
+    struct Auction {
+        uint256 endTime;
+        Bid winningBid;
+    }
+
     address public taikoToken;
     address public auctionOwner;
-    mapping(uint256 => Bid) public winningBids;
+    mapping(uint256 => Auction) public auctions;
     uint256 public currentBatch;
-    uint256 public auctionEndTime;
-    uint256 public proofingWindow;
     uint256 public minDeposit;
     uint256 public bidIncrement;
     uint256 public initialBiddingPrice;
     uint256 constant public batchSize = 1000;
+    uint256 constant public proofWindow = 1 hours;
 
     constructor(
         address _taikoToken,
-        uint256 _auctionEndTime,
-        uint256 _proofingWindow,
         uint256 _minDeposit,
         uint256 _bidIncrement,
         uint256 _initialBiddingPrice
     ) {
         taikoToken = _taikoToken;
         auctionOwner = msg.sender;
-        auctionEndTime = _auctionEndTime;
-        proofingWindow = _proofingWindow;
         minDeposit = _minDeposit;
         bidIncrement = _bidIncrement;
         initialBiddingPrice = _initialBiddingPrice;
     }
 
     function placeBid(uint256 _blockId) external payable {
-        require(block.timestamp < auctionEndTime, "Auction has ended");
+        require(block.timestamp < auctions[_blockId].endTime, "Auction has ended");
 
         uint256 batch = _blockId / batchSize;
 
-        Bid storage winningBid = winningBids[batch];
+        Auction storage auction = auctions[batch];
 
         // Calculate the minimum bid required
         uint256 minBid = initialBiddingPrice;
-        if (winningBid.amount > 0) {
-            minBid = winningBid.amount - (winningBid.amount * bidIncrement / 100);
+        if (auction.winningBid.amount > 0) {
+            minBid = auction.winningBid.amount - (auction.winningBid.amount * bidIncrement / 100);
         }
 
         require(msg.value >= minBid, "Insufficient bid amount");
 
         // Refund previous bidder if a new bid is placed
-        if (winningBid.amount > 0 && msg.value > winningBid.amount) {
-            payable(winningBid.bidder).transfer(winningBid.amount);
+        if (auction.winningBid.amount > 0 && msg.value > auction.winningBid.amount) {
+            payable(auction.winningBid.bidder).transfer(auction.winningBid.amount);
         }
 
         // Update the winning bid
-        winningBids[batch] = Bid(msg.sender, msg.value, block.timestamp);
+        auction.winningBid = Bid(msg.sender, msg.value, block.timestamp);
+
+        // Set the auction end time if it hasn't been set
+        if (auction.endTime == 0) {
+            auction.endTime = block.timestamp + 5 minutes;
+        }
     }
 
     function submitProof(uint256 _blockId) external {
-        require(block.timestamp >= auctionEndTime, "Auction is still ongoing");
-
         uint256 batch = _blockId / batchSize;
 
-        Bid storage winningBid = winningBids[batch];
+        Auction storage auction = auctions[batch];
+        require(auction.endTime > 0, "No auction exists for the batch");
+        require(block.timestamp >= auction.endTime, "Auction is still ongoing");
+
+        Bid storage winningBid = auction.winningBid;
         require(winningBid.amount > 0, "No winning bid for the batch");
         require(msg.sender == winningBid.bidder, "You are not the winning bidder");
-        require(block.timestamp <= winningBid.timestamp + proofingWindow, "Proofing window has ended");
+        require(block.timestamp <= winningBid.timestamp + proofWindow, "Proofing window has ended");
 
         // Perform the proof verification
         // ...
@@ -85,3 +95,6 @@ contract BatchBasedAuction {
     function willNewBidWin(Bid memory _existingBid, Bid memory _newBid) internal virtual returns (bool);
 
 }
+```
+
+The proof window is now removed, and the proofing period is calculated as the bid's timestamp plus 1 hour.
