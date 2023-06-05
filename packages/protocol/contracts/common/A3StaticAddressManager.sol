f69410b855536c21918068f154e705563f2f53b3
David <david@taiko.xyz>
2023-06-05 16:09:09 +0800

    feat(protocol): update A3 static address manager after re-genesis (#13873)

--- packages/protocol/contracts/common/A3StaticAddressManager.sol
+++ packages/protocol/contracts/common/A3StaticAddressManager.sol
@@ -35,16 +35,16 @@ contract A3StaticAddressManager is AddressManager {
         if (domain == 11155111) {
             if (name == "oracle_prover") addr = address(0x1567CDAb5F7a69154e61A16D8Ff5eE6A3e991b39);
             if (name == "system_prover") addr = address(0xE09e4fF4353fbf984F99fa824524277F704e7475);
-            if (name == "taiko_token") addr = address(0x1c1141c6D94895b38c9472E8469aa56483b646BB);
-            if (name == "taiko") addr = address(0x487C41e565DA0d96f5C4b7D8F2e1122B466ef348);
-            if (name == "proto_broker") addr = address(0x487C41e565DA0d96f5C4b7D8F2e1122B466ef348);
-            if (name == "bridge") addr = address(0xb5b16B80d9426021852dA4D19418b9a4d43CE490);
-            if (name == "token_vault") addr = address(0x7A20a9d727280db9f059A15D551E4964c41EA487);
+            if (name == "taiko_token") addr = address(0xE52952B8063d0AE6Bd35E894866d8148976ce645);
+            if (name == "taiko") addr = address(0x6375394335f34848b850114b66A49D6F47f2cdA8);
+            if (name == "proto_broker") addr = address(0x6375394335f34848b850114b66A49D6F47f2cdA8);
+            if (name == "bridge") addr = address(0x7D992599E1B8b4508Ba6E2Ba97893b4C36C23A28);
+            if (name == "token_vault") addr = address(0xD70506580B5F65e68ed0dbA7B4Ae507641C48197);
             if (name == "signal_service") {
-                addr = address(0x8DcE72C7f9548FFd2225FEA9b2d56e84d1Da1258);
+                addr = address(0x23baAc3892a823e9E59B85d6c90068474fe60086);
             }
             if (name == bytes32(uint256(0x1000000))) {
-                addr = address(0x23CD36D723D1ecD3c4F8d53A0CD4BE7e67759778);
+                addr = address(0xd46eb8cF2b47cd99bdb1dD8C76EEc55ac6eb930E);
             }
         } else if (domain == 167005) {
             if (name == "taiko") addr = address(0x0000777700000000000000000000000000000001);
