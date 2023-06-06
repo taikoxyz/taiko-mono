8d5dd0a24b1ac0b334c4fbb3b35e81dda336f00f
Jeffery Walsh <cyberhorsey@gmail.com>
2023-06-06 13:47:53 -0700

    mm mobile

--- packages/bridge-ui/src/components/AddTokenToWallet.svelte
+++ packages/bridge-ui/src/components/AddTokenToWallet.svelte
@@ -27,7 +27,7 @@
         },
       });
     } catch (e) {
-      if (e.code === 4001) {
+      if (e.code === 4001 || e?.data?.originalError?.code === 4001) {
         warningToast('Adding token has been rejected.');
       } else {
         errorToast('Failed to add token to wallet');
