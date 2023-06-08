6efb4400b3d9e7449f34a89572ce7036a81def6f
Francisco <jscriptcoder@gmail.com>
2023-06-08 23:19:19 +0200

    add tests

--- packages/bridge-ui/src/utils/injectedProvider.spec.ts
+++ packages/bridge-ui/src/utils/injectedProvider.spec.ts
@@ -1,12 +1,17 @@
 import { ethers } from 'ethers';
 
-import { rpcCall } from './injectedProvider';
+import {
+  getInjectedSigner,
+  hasInjectedProvider,
+  rpcCall,
+} from './injectedProvider';
 
 jest.mock('../constants/envVars');
 
 jest.mock('ethers', () => {
   const Web3Provider = jest.fn();
   Web3Provider.prototype = {
+    getSigner: jest.fn(),
     send: jest.fn(),
   };
 
@@ -17,6 +22,15 @@ jest.mock('ethers', () => {
       },
     },
   };
+});
+
+beforeEach(() => {
+  globalThis.ethereum = {
+    isMetaMask: true,
+    request: jest.fn(),
+  };
+
+  jest.clearAllMocks();
 });
 
 describe('injectedProvider - rpcCall', () => {
@@ -42,5 +56,33 @@ describe('injectedProvider - rpcCall', () => {
     await expect(rpcCall('eth_requestAccounts')).rejects.toThrowError(
       'RPC call "eth_requestAccounts" failed',
     );
+  });
+});
+
+describe('injectedProvider - getInjectedSigner', () => {
+  it('should return signer', () => {
+    const mockSigner = {} as ethers.providers.JsonRpcSigner;
+    jest
+      .mocked(ethers.providers.Web3Provider.prototype.getSigner)
+      .mockReturnValue(mockSigner);
+
+    expect(getInjectedSigner()).toEqual(mockSigner);
+
+    expect(ethers.providers.Web3Provider).toHaveBeenCalledWith(
+      globalThis.ethereum,
+      'any',
+    );
+  });
+});
+
+describe('injectedProvider - hasInjectedProvider', () => {
+  it('should return true if injected provider is available', () => {
+    expect(hasInjectedProvider()).toBeTruthy();
+  });
+
+  it('should return false if injected provider is not available', () => {
+    globalThis.ethereum = undefined;
+
+    expect(hasInjectedProvider()).toBeFalsy();
   });
 });
