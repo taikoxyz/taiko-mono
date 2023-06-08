48ef7affc971e5b64828cf820ca6b0bc925b328f
Kenk <kenghin_lim@hotmail.com>
2023-06-08 22:06:56 +0800

    docs(protocol): update bridge docs

--- packages/protocol/contracts/bridge/libs/LibBridgeSend.sol
+++ packages/protocol/contracts/bridge/libs/LibBridgeSend.sol
@@ -14,8 +14,6 @@ import { LibBridgeData } from "./LibBridgeData.sol";
 
 /**
  * Entry point for starting a bridge transaction.
- *
- * @title LibBridgeSend
  */
 library LibBridgeSend {
     using LibAddress for address;
@@ -27,18 +25,20 @@ library LibBridgeSend {
     error B_WRONG_TO_ADDRESS();
 
     /**
-     * Send a message to the Bridge with the details of the request. The Bridge
-     * takes custody of the funds, unless the source chain is Taiko, in which
-     * the funds are sent to and managed by the EtherVault.
-     *
-     * @param message Specifies the `depositValue`, `callValue`,
-     * and `processingFee`. These must sum to `msg.value`. It also specifies the
-     * `destChainId` which must have a `bridge` address set on the
-     * AddressResolver and differ from the current chain ID.
-     *
-     * @return msgHash The hash of message sent.
-     * This is picked up by an off-chain relayer which indicates a
-     * bridge message has been sent and is ready to be processed on the
+     * Send a message to the Bridge with the details of the request.
+     * @dev The Bridge takes custody of the funds, unless the source chain is
+     * Taiko,
+     * in which case the funds are sent to and managed by the EtherVault.
+     * @param state The current state of the Bridge
+     * @param resolver The address resolver
+     * @param message Specifies the `depositValue`, `callValue`, and
+     * `processingFee`.
+     * These must sum to `msg.value`. It also specifies the `destChainId`
+     * which must have a `bridge` address set on the AddressResolver and
+     * differ from the current chain ID.
+     * @return msgHash The hash of the sent message.
+     * This is picked up by an off-chain relayer which indicates
+     * a bridge message has been sent and is ready to be processed on the
      * destination chain.
      */
     function sendMessage(
@@ -89,6 +89,13 @@ library LibBridgeSend {
         emit LibBridgeData.MessageSent(msgHash, message);
     }
 
+    /**
+     * Check if the destination chain is enabled.
+     * @param resolver The address resolver
+     * @param chainId The destination chain id
+     * @return enabled True if the destination chain is enabled
+     * @return destBridge The bridge of the destination chain
+     */
     function isDestChainEnabled(
         AddressResolver resolver,
         uint256 chainId
@@ -101,6 +108,12 @@ library LibBridgeSend {
         enabled = destBridge != address(0);
     }
 
+    /**
+     * Check if the message was sent.
+     * @param resolver The address resolver
+     * @param msgHash The hash of the sent message
+     * @return True if the message was sent
+     */
     function isMessageSent(
         AddressResolver resolver,
         bytes32 msgHash
@@ -113,6 +126,14 @@ library LibBridgeSend {
             .isSignalSent({ app: address(this), signal: msgHash });
     }
 
+    /**
+     * Check if the message was received.
+     * @param resolver The address resolver
+     * @param msgHash The hash of the received message
+     * @param srcChainId The id of the source chain
+     * @param proof The proof of message receipt
+     * @return True if the message was received
+     */
     function isMessageReceived(
         AddressResolver resolver,
         bytes32 msgHash,
