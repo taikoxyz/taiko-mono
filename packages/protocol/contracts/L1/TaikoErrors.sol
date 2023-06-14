8e042a4ac3bfb0d3141c645765ffaba311309820
Daniel Wang <dan@taiko.xyz>
2023-06-14 14:37:07 +0800

    Update TaikoErrors.sol

--- packages/protocol/contracts/L1/TaikoErrors.sol
+++ packages/protocol/contracts/L1/TaikoErrors.sol
@@ -25,6 +25,7 @@ abstract contract TaikoErrors {
     error L1_INVALID_PROOF_OVERWRITE();
     error L1_NOT_PROVEABLE();
     error L1_NOT_SPECIAL_PROVER();
+    error L1_NOT_THE_BEST_BID();
     error L1_SAME_PROOF();
     error L1_TOO_MANY_BLOCKS();
     error L1_TX_LIST_NOT_EXIST();
