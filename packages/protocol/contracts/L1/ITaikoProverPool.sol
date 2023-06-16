4b0c4a8b30fdcc030219719bb76e328692b37618
adaki2004 <keszeydani@gmail.com>
2023-06-16 14:19:43 +0200

    Add all functions to interface

--- packages/protocol/contracts/L1/ITaikoProverPool.sol
+++ packages/protocol/contracts/L1/ITaikoProverPool.sol
@@ -7,7 +7,22 @@
 pragma solidity ^0.8.20;
 
 interface ITaikoProverPool {
-  function pickRandomProver(uint256 randomNumber) external view returns (address);
-  function getProver(uint256 blockId) external view returns (address);
-  function slash(address prover) external;
+
+    function enterProverPool(uint256 amount, uint256 feeMultiplier, uint32 capacity) external;
+
+    function stakeMoreTokens(uint256 amount) external;
+
+    function adjustFeeMultiplier(uint8 newFeeMultiplier) external;
+
+    function adjustCapacity(uint32 newCapacity) external;
+
+    function withdrawRewards(uint64 amount) external;
+   
+    function exit() external;
+
+    function pickRandomProver(uint256 randomNumber, uint256 blockId) external returns (address);
+
+    function getProver(uint256 blockId) external view returns (address);
+
+    function slash(address prover) external;
 }
