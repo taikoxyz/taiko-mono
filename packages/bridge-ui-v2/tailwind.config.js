b00b0518c0ea4586651a4d25470f527e33877786
Francisco <jscriptcoder@gmail.com>
2023-06-19 14:56:10 +0200

    postcss config

--- packages/bridge-ui-v2/tailwind.config.js
+++ packages/bridge-ui-v2/tailwind.config.js
@@ -1,8 +1,10 @@
+import daisyuiPlugin from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyuiPlugin],
 };
