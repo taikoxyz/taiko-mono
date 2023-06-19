664747fe8ea52e78c0563e3b5ff7b8cf9fdd3b21
Francisco <jscriptcoder@gmail.com>
2023-06-19 13:41:42 +0200

    prettier

--- packages/bridge-ui-v2/src/routes/+layout.ts
+++ packages/bridge-ui-v2/src/routes/+layout.ts
@@ -4,4 +4,4 @@
 
 // Setting this to false turns your app into an SPA
 // See https://kit.svelte.dev/docs/page-options#ssr
-export const ssr = false
+export const ssr = false;
--- packages/bridge-ui-v2/tailwind.config.js
+++ packages/bridge-ui-v2/tailwind.config.js
@@ -5,5 +5,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
-
+};
