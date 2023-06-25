3332e70bb3b821ab4efbcfe4aed4dbc3ed614850
Francisco Ramos <jscriptcoder@gmail.com>
2023-06-25 22:01:59 +0200

    fix(bridge-ui-v2): fixing vercel build (#14052)

--- packages/bridge-ui-v2/src/routes/+layout.svelte
+++ packages/bridge-ui-v2/src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import '../app.css';
 
+  import { onDestroy, onMount } from 'svelte';
+
   import Header from '$components/Header';
   import SideNavigation from '$components/SideNavigation';
+  import { startWatching, stopWatching } from '$libs/wagmi';
+
+  onMount(startWatching);
+  onDestroy(stopWatching);
 </script>
 
 <SideNavigation>
