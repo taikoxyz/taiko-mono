3ba06803cbd9e60d256cf54470def4529747aeb8
Francisco <jscriptcoder@gmail.com>
2023-06-28 14:01:32 +0200

    mobile version

--- packages/bridge-ui-v2/src/components/Card/Card.svelte
+++ packages/bridge-ui-v2/src/components/Card/Card.svelte
@@ -3,8 +3,8 @@
   export let text = '';
 </script>
 
-<div class="card w-full md:w-[524px] bg-elevated-background rounded-[20px] border border-neutral-background">
-  <div class="card-body p-12 body-regular">
+<div class="card w-full md:w-[524px] md:bg-elevated-background rounded-[20px] md:border md:border-neutral-background">
+  <div class="card-body p-4 md:p-12 body-regular">
     {#if title}
       <h2 class="card-title title-screen-bold">{title}</h2>
     {/if}
