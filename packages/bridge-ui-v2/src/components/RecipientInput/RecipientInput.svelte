bbacbd46018fbc60cc0e6cdbbcd01d4de4c501b6
Francisco <jscriptcoder@gmail.com>
2023-06-28 13:56:56 +0200

    add unit test

--- packages/bridge-ui-v2/src/components/RecipientInput/RecipientInput.svelte
+++ packages/bridge-ui-v2/src/components/RecipientInput/RecipientInput.svelte
@@ -11,5 +11,5 @@
     class="w-full input-box outline-none py-3 px-6 pr-[52px] body-regular placeholder:text-tertiary-content"
     pattern="^0x[a-fA-F0-9]{40}$"
     placeholder={$t('custom_recipient.placeholder')} />
-  <Tooltip class="absolute right-6" position="right">TODO: add description</Tooltip>
+  <Tooltip class="absolute right-6" position="top">TODO: add description</Tooltip>
 </div>
