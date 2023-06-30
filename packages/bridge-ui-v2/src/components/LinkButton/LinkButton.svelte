c59d8143b568c20ccef66f030dba27eaa6f42557
Francisco <jscriptcoder@gmail.com>
2023-06-30 17:12:39 +0200

    align to design

--- packages/bridge-ui-v2/src/components/LinkButton/LinkButton.svelte
+++ packages/bridge-ui-v2/src/components/LinkButton/LinkButton.svelte
@@ -5,15 +5,14 @@
   export let href = '/';
   export let external = false;
 
-  $: activeClass = active ? 'bg-primary-interactive hover:bg-primary-interactive-hover' : '';
+  $: activeClass = active
+    ? 'body-bold bg-primary-interactive hover:bg-primary-interactive-hover'
+    : 'body-regular hover:bg-secondary-interactive-hover';
 </script>
 
 <a
   {href}
   target={external ? '_blank' : null}
-  class={classNames(
-    'btn btn-sm md:btn-md btn-ghost p-3 rounded-full body-bold flex justify-start content-center',
-    activeClass,
-  )}>
+  class={classNames('p-3 rounded-full flex justify-start content-center', activeClass)}>
   <slot />
 </a>
