c6966f17821c7f65fa8238ef37eca1e6f2f26a62
Francisco <jscriptcoder@gmail.com>
2023-07-05 15:38:02 +0200

    wip

--- packages/bridge-ui-v2/src/libs/connect/web3modal.ts
+++ packages/bridge-ui-v2/src/libs/connect/web3modal.ts
@@ -1,7 +1,7 @@
 import { EthereumClient } from '@web3modal/ethereum';
 import { Web3Modal } from '@web3modal/html';
 
-import { PUBLIC_WALLETCONNECT_PROJECT_ID } from '$env/static/public';
+import { PUBLIC_L1_CHAIN_ID, PUBLIC_L2_CHAIN_ID, PUBLIC_WALLETCONNECT_PROJECT_ID } from '$env/static/public';
 import { chains } from '$libs/chain';
 import { wagmiConfig } from '$libs/wagmi';
 
@@ -12,6 +12,10 @@ const ethereumClient = new EthereumClient(wagmiConfig, chains);
 export const web3modal = new Web3Modal(
   {
     projectId,
+    chainImages: {
+      [PUBLIC_L1_CHAIN_ID]: '/ethereum-chain.svg',
+      [PUBLIC_L2_CHAIN_ID]: '/taiko-chain.svg',
+    },
     // TODO: can we bring these vars into Tailwind theme?
     themeVariables: {
       '--w3m-font-family': '"Public Sans", sans-serif',
@@ -23,23 +27,24 @@ export const web3modal = new Web3Modal(
       '--w3m-button-border-radius': '9999px',
 
       // Body small regular
-      '--w3m-text-small-regular-size': '0.875rem',
+      '--w3m-text-small-regular-size': '14px',
       '--w3m-text-small-regular-weight': '400',
-      '--w3m-text-small-regular-line-height': '1.25rem',
+      '--w3m-text-small-regular-line-height': '20px',
 
       // Body regular
-      '--w3m-text-medium-regular-size': '1rem',
+      '--w3m-text-medium-regular-size': '16px',
       '--w3m-text-medium-regular-weight': '400',
-      '--w3m-text-medium-regular-line-height': '1.5rem',
+      '--w3m-text-medium-regular-line-height': '24px',
+      '--w3m-text-medium-regular-letter-spacing': 'normal',
 
       // Title body bold
-      '--w3m-text-big-bold-size': '1.125rem',
+      '--w3m-text-big-bold-size': '18px',
       '--w3m-text-big-bold-weight': '700',
-      '--w3m-text-big-bold-line-height': '1.125rem',
+      '--w3m-text-big-bold-line-height': '24px',
 
       '--w3m-background-color': 'var(--neutral-background)',
-      '--w3m-background-border-radius': '1.25rem',
-      '--w3m-container-border-radius': '0rem',
+      '--w3m-background-border-radius': '20px',
+      '--w3m-container-border-radius': '0',
 
       // TODO: customize the rest of the theme variables
 
