a6b825a9adad10df27a2b6973d4bac2f2db5d604
Francisco <jscriptcoder@gmail.com>
2023-07-13 15:44:29 +0200

    small comment

--- packages/bridge-ui-v2/src/libs/token/getBalance.ts
+++ packages/bridge-ui-v2/src/libs/token/getBalance.ts
@@ -11,10 +11,13 @@ export async function getBalance(token: Token, userAddress: Address, srcChainId?
     return fetchBalance({ address: userAddress });
   }
 
+  // We are dealing with an ERC20 token. We need to first find out its address
+  // in order to fetch the balance.
   const tokenAddress = await getAddress(token, srcChainId, destChainId);
 
   if (!tokenAddress || tokenAddress === zeroAddress) return null;
 
+  // Wagmi is an excellent library 😊
   return fetchBalance({
     address: userAddress,
     chainId: srcChainId,
