8b5d82e3f5f25944535f8427e11192bd77a0dc88
David <david@taiko.xyz>
2023-07-14 18:57:04 +0800

    feat(protocol): update Alpha-4 network configs

--- packages/protocol/script/DeployOnL1.s.sol
+++ packages/protocol/script/DeployOnL1.s.sol
@@ -104,8 +104,8 @@ contract DeployOnL1 is Script {
                 taikoToken.init.selector,
                 abi.encode(
                     addressManagerProxy,
-                    "Taiko Token",
-                    "TKO",
+                    "Taiko Token Eldfell",
+                    "TTKOe",
                     taikoTokenPremintRecipients,
                     taikoTokenPremintAmounts
                 )
