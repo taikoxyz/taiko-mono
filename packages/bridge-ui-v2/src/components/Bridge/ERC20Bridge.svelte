f173c9854965eb50c31af4966c302d37b1cec210
Korbinian Kasberger <korbi@taiko.xyz>
2023-07-20 11:45:24 +0200

    chore(nft-bridge): code cleanup

---
+++ packages/bridge-ui-v2/src/components/Bridge/ERC20Bridge.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { t } from 'svelte-i18n';
+
+  import { Button } from '$components/Button';
+  import { Card } from '$components/Card';
+  import { ChainSelector } from '$components/ChainSelector';
+  import { TokenDropdown } from '$components/TokenDropdown';
+  import { chains } from '$libs/chain';
+  import { ETHToken, tokens } from '$libs/token';
+  import type { Account } from '$stores/account';
+  import { type Network, network } from '$stores/network';
+
+  import { AmountInput } from './AmountInput';
+  import { ProcessingFee } from './ProcessingFee';
+  import { RecipientInput } from './RecipientInput';
+  import { destNetwork,selectedToken } from './state';
+  import SwitchChainsButton from './SwitchChainsButton.svelte';
+
+  function onNetworkChange(network: Network) {
+    if (network && chains.length === 2) {
+      // If there are only two chains, the destination chain will be the other one
+      const otherChain = chains.find((chain) => chain.id !== network.id);
+
+      if (otherChain) destNetwork.set(otherChain);
+    }
+  }
+
+  function onAccountChange(account: Account) {
+    if (account && account.isConnected && !$selectedToken) {
+      $selectedToken = ETHToken;
+    } else if (account && account.isDisconnected) {
+      $selectedToken = null;
+      $destNetwork = null;
+    }
+  }
+</script>
+
+<Card class="md:w-[524px]" title={$t('bridge.erc20.title')} text={$t('bridge.subtitle')}>
+  <div class="space-y-[35px]">
+    <div class="space-y-4">
+      <div class="space-y-2">
+        <ChainSelector label={$t('chain.from')} value={$network} switchWallet />
+        <TokenDropdown {tokens} bind:value={$selectedToken} />
+      </div>
+
+      <AmountInput />
+
+      <div class="f-justify-center">
+        <SwitchChainsButton />
+      </div>
+
+      <div class="space-y-2">
+        <ChainSelector label={$t('chain.to')} value={$destNetwork} readOnly />
+        <RecipientInput />
+      </div>
+    </div>
+
+    <ProcessingFee />
+
+    <div class="h-sep" />
+
+    <Button type="primary" class="px-[28px] py-[14px]">
+      <span class="body-bold">{$t('bridge.button.bridge')}</span>
+    </Button>
+  </div>
+</Card>
