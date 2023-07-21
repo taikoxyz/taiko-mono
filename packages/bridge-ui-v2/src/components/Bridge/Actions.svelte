114eeaa5bfb81dacb7d245f28c9a39a58a8155d9
Francisco <jscriptcoder@gmail.com>
2023-07-21 14:43:49 +0200

    wip

--- packages/bridge-ui-v2/src/components/Bridge/Actions.svelte
+++ packages/bridge-ui-v2/src/components/Bridge/Actions.svelte
@@ -3,6 +3,8 @@
   import { t } from 'svelte-i18n';
 
   import { Button } from '$components/Button';
+  import { Icon } from '$components/Icon';
+  import { insufficientAllowance } from './state';
 
   export let approve = noop;
   export let bridge = noop;
@@ -24,11 +26,45 @@
     });
   }
 
-  $: showSteps = false;
+  $: showSteps = true;
+
+  $: cannotBridge = true;
 
   $: loading = approving || bridging;
 </script>
 
-<Button type="primary" class="px-[28px] py-[14px] rounded-full w-full" on:click={onBridgeClick}>
-  <span class="body-bold">{$t('bridge.button.bridge')}</span>
-</Button>
+<div class="f-between-center w-full gap-4">
+  {#if showSteps}
+    <Button
+      type="primary"
+      class="px-[28px] py-[14px] rounded-full flex-1"
+      disabled={!$insufficientAllowance || approving}
+      loading={approving}
+      on:click={onApproveClick}>
+      {#if approving}
+        <span class="body-bold">{$t('bridge.button.approving')}</span>
+      {:else if !$insufficientAllowance}
+        <div class="f-items-center">
+          <Icon type="check" />
+          <span class="body-bold">{$t('bridge.button.approved')}</span>
+        </div>
+      {:else}
+        <span class="body-bold">{$t('bridge.button.approve')}</span>
+      {/if}
+    </Button>
+    <Icon type="arrow-right" />
+  {/if}
+
+  <Button
+    type="primary"
+    class="px-[28px] py-[14px] rounded-full flex-1"
+    disabled={cannotBridge || bridging}
+    loading={bridging}
+    on:click={onBridgeClick}>
+    {#if bridging}
+      <span class="body-bold">{$t('bridge.button.briding')}</span>
+    {:else}
+      <span class="body-bold">{$t('bridge.button.bridge')}</span>
+    {/if}
+  </Button>
+</div>
