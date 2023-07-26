bf6cf08f0a60851cebd0378e6619391a947040ef
David <david@taiko.xyz>
2023-07-26 13:16:10 +0800

    feat: cherry-pick #14065

--- packages/protocol/contracts/L1/ProverPool.sol
+++ packages/protocol/contracts/L1/ProverPool.sol
@@ -372,6 +372,13 @@ contract ProverPool is EssentialContract, IProverPool {
         if (staker.proverId == 0) return;
 
         Prover memory prover = provers[staker.proverId];
+
+        delete proverIdToAddress[staker.proverId];
+
+        // Delete the prover but make it non-zero for cheaper rewrites
+        // by keep rewardPerGas = 1
+        provers[staker.proverId] = Prover(0, 1, 0);
+
         if (prover.stakedAmount > 0) {
             if (
                 checkExitTimestamp
@@ -384,12 +391,6 @@ contract ProverPool is EssentialContract, IProverPool {
             staker.exitRequestedAt = uint64(block.timestamp);
             staker.proverId = 0;
         }
-
-        // Delete the prover but make it non-zero for cheaper rewrites
-        // by keep rewardPerGas = 1
-        provers[staker.proverId] = Prover(0, 1, 0);
-
-        delete proverIdToAddress[staker.proverId];
 
         emit Exited(addr, staker.exitAmount);
     }
