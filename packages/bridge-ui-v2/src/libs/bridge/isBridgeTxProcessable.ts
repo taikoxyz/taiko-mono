e149d6666d6f25f74c1ddc87d9888bbfc029a0ad
Francisco <jscriptcoder@gmail.com>
2023-07-27 16:34:22 +0200

    minor change

--- packages/bridge-ui-v2/src/libs/bridge/isBridgeTxProcessable.ts
+++ packages/bridge-ui-v2/src/libs/bridge/isBridgeTxProcessable.ts
@@ -1,15 +1,21 @@
-import { chainContractsMap } from "$libs/chain";
-import { getContract } from "@wagmi/core";
-import { MessageStatus, type BridgeTransaction } from "./types";
-import { crossChainSyncABI } from "$abi";
-import { publicClient } from "$libs/wagmi";
+import { getContract } from '@wagmi/core';
+
+import { crossChainSyncABI } from '$abi';
+import { chainContractsMap } from '$libs/chain';
+import { publicClient } from '$libs/wagmi';
+
+import { type BridgeTransaction,MessageStatus } from './types';
 
 export async function isBridgeTxProcessable(bridgeTx: BridgeTransaction) {
   const { receipt, message, status, srcChainId, destChainId } = bridgeTx;
+
+  // Without these guys there is no way we can process this
+  // bridge transaction. The receipt is needed in order to compare
+  // the block number with the cross chain block number.
   if (!receipt || !message) return false;
 
-  // TODO: Not sure this could ever happens. When we add the 
-  // transaction to the local storage, we don't set the status, 
+  // TODO: Not sure this could ever happens. When we add the
+  // transaction to the local storage, we don't set the status,
   // but when we fetch them, then we query the contract for this status.
   if (status !== MessageStatus.NEW) return true;
 
@@ -22,7 +28,7 @@ export async function isBridgeTxProcessable(bridgeTx: BridgeTransaction) {
       chainId: Number(destChainId),
     });
 
-    const blockHash = await destCrossChainSyncContract.read.getCrossChainBlockHash([0]);
+    const blockHash = await destCrossChainSyncContract.read.getCrossChainBlockHash([BigInt(0)]);
 
     const srcBlock = await publicClient({ chainId: Number(srcChainId) }).getBlock({
       blockHash,
