ee458fa6a69ad432403df48f736db59627fb9441
Korbinian Kasberger <korbi@taiko.xyz>
2023-08-03 09:33:22 +0200

    feat(bridge-ui-v2): use responsive tailwind tags

--- packages/bridge-ui-v2/src/components/Activities/ChainSymbolName.svelte
+++ packages/bridge-ui-v2/src/components/Activities/ChainSymbolName.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-  import { DesktopOrLarger } from '$components/DesktopOrLarger';
   import { chainIcons, type ChainID, getChainName } from '$libs/chain';
 
   export let chainId: ChainID;
-  let isDesktopOrLarger: boolean;
   const chainName = getChainName(Number(chainId));
   const icon = chainIcons[Number(chainId)];
 </script>
 
-<div class="flex {isDesktopOrLarger ? 'items-stretch self-center' : 'justify-items-start'}">
+<div class="flex md:items-stretch self-center justify-items-start'}">
   <img src={icon} alt="chain-logo" class="rounded-full w-5 h-5 hidden md:block mr-2" />
-  <span class={isDesktopOrLarger ? 'font-bold' : ''}>{chainName}</span>
+  <span class="md:font-bold font-normal">{chainName}</span>
 </div>
-<DesktopOrLarger bind:is={isDesktopOrLarger} />
--- packages/bridge-ui-v2/src/components/Activities/Transaction.svelte
+++ packages/bridge-ui-v2/src/components/Activities/Transaction.svelte
@@ -46,7 +46,6 @@
     <div class="w-1/4 md-w-1/5 px-4 py-2 flex flex-row justify-left items-stretch">
       <ChainSymbolName chainId={item.srcChainId} />
     </div>
-
     <div class="w-1/4 md-w-1/5 px-4 py-2 flex flex-row justify-left items-stretch">
       <ChainSymbolName chainId={item.destChainId} />
     </div>
