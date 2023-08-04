cab52190dc2b764641c7152f8f817154680c9c58
adaki2004 <keszeydani@gmail.com>
2023-08-04 09:33:13 +0200

    Added natspec documentation

--- packages/protocol/contracts/test/erc20/FailBridgeBackERC20.sol
+++ packages/protocol/contracts/test/erc20/FailBridgeBackERC20.sol
@@ -8,7 +8,10 @@ pragma solidity ^0.8.20;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// An ERC20 token for creating a 'failed' status
+/// @notice An ERC20 token for creating a 'failed' status (testing only!)
+/// @dev Need to create failed messages on the destination chain, and
+/// @dev by getting a storage proof (eth_getProof) of the message
+/// @dev (that it failed) so that can perform some actions.
 contract FailWhenBridgeBackCanonical is ERC20 {
     mapping(address minter => bool hasMinted) public minters;
 
@@ -16,6 +19,11 @@ contract FailWhenBridgeBackCanonical is ERC20 {
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) { }
 
+    /**
+     * Public, open mint function
+     * @dev It is a one-time mint function / address
+     * @param to The address to mint to
+     */
     function mint(address to) public {
         if (minters[msg.sender]) {
             revert HasMinted();
@@ -25,6 +33,11 @@ contract FailWhenBridgeBackCanonical is ERC20 {
         _mint(to, 50 * (10 ** decimals()));
     }
 
+    /**
+     * transfer() function to fail when Bridge calls it.
+     * @dev This will create failed when bridges tries to
+     * recall it
+     */
     function transfer(
         address,
         uint256
