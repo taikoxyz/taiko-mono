b4be68917b86356bc5f3ae71b29a2ad4e8748262
Francisco <jscriptcoder@gmail.com>
2023-08-04 12:26:31 +0200

    fix bridge tabs

--- packages/bridge-ui-v2/src/components/Header/Header.svelte
+++ packages/bridge-ui-v2/src/components/Header/Header.svelte
@@ -27,9 +27,9 @@
  ">
   <LogoWithText width={77} height={21} class="md:hidden" />
 
-  <div class="flex justify-end md:f-between-center w-full">
+  <div class="flex justify-end w-full">
     {#if isBridgePage}
-      <BridgeTabs class="hidden md:flex" />
+      <BridgeTabs class="hidden md:flex md:flex-1" />
     {/if}
 
     <ConnectButton connected={$account?.isConnected} />
