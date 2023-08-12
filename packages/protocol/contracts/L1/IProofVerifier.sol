19a29907063a3fc08cee395886471c40ee55cb14
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-08-11 20:01:21 -0400

    refactor all to new spec

--- packages/protocol/contracts/L1/IProofVerifier.sol
+++ packages/protocol/contracts/L1/IProofVerifier.sol
@@ -9,8 +9,8 @@ pragma solidity ^0.8.20;
 /// @title IProofVerifier
 /// @notice Contract that is responsible for verifying proofs.
 interface IProofVerifier {
-    /// Verify the given proof(s) for the given blockId. This function should
-    /// revert if the verification fails.
+    /// @notice Verify the given proof(s) for the given blockId. This function
+    /// should revert if the verification fails.
     /// @param blockId Unique identifier for the block.
     /// @param blockProofs Raw bytes representing the proof(s).
     /// @param instance Hashed evidence & config data. If set to zero, proof is
