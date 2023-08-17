122f0dcd2a6fa1acf035071bd6a770e839dcee52
Daniel Wang <dan@taiko.xyz>
2023-08-17 12:23:04 +0800

    Update IProver.sol

--- packages/protocol/contracts/L1/IProver.sol
+++ packages/protocol/contracts/L1/IProver.sol
@@ -14,13 +14,13 @@ interface IProver {
     /// Burns the specified `bond` amount of Taiko token from this contract.
     /// @param proposer The address of the block proposer.
     /// @param blockId The ID of the block.
-    /// @param maxProverFee Maximum fee per gas the proposer is willing to pay.
+    /// @param maxProverFee Maximum prover fee the proposer is willing to pay.
     /// @param proofWindow Time window for a valid proof to be submitted by the
     /// prover.
     /// @param params Additional function parameters.
     /// @return actualProver The address of the prover to submit a proof. Usually
     /// this should simply be `address(this)`.
-    /// @return proverFee The fee per gas that will be paid to the actual
+    /// @return proverFee The prover fee that will be paid to the actual
     /// prover.
     function onBlockAssigned(
         address proposer,
