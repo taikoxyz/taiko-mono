3f3bdccdc7b1680a249d0dc8f3439071d941f317
Daniel Wang <dan@taiko.xyz>
2023-08-18 10:31:42 +0800

    Update IProver.sol

--- packages/protocol/contracts/L1/IProver.sol
+++ packages/protocol/contracts/L1/IProver.sol
@@ -7,12 +7,12 @@
 pragma solidity ^0.8.20;
 
 import { TaikoData } from "./TaikoData.sol";
+
 /// @title IProver Interface
 /// @notice Defines the function that handle prover assignment.
-
 interface IProver {
-    /// @notice Assigns a prover to a specific block or reverts if none is
-    /// available.
+    /// @notice Assigns a prover to a specific block or reverts if this prover
+    /// is not available.
     /// @param proposer The address of the block proposer.
     /// @param input The block's BlockMetadataInput data.
     function onBlockAssigned(
