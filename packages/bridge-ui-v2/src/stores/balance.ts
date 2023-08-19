9608e97402b28c6b1859df1ac8536f68628fc8f3
xiaodino <ruby@taiko.xyz>
2023-08-19 14:20:17 -0700

    Update

--- packages/bridge-ui-v2/src/stores/balance.ts
+++ packages/bridge-ui-v2/src/stores/balance.ts
@@ -1,3 +1,4 @@
+import type { FetchBalanceResult } from '@wagmi/core';
 import { writable } from 'svelte/store';
 
-export const ethBalance = writable<FetchBalanceResult>();
+export const ethBalance = writable<Maybe<FetchBalanceResult>>(null);
