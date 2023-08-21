f444fbc5183b06317694961798bf24293f46ebf7
xiaodino <ruby@taiko.xyz>
2023-08-21 15:15:31 -0700

    Update UI styles

--- packages/bridge-ui-v2/src/libs/connect/web3modal.ts
+++ packages/bridge-ui-v2/src/libs/connect/web3modal.ts
@@ -21,7 +21,8 @@ export const web3modal = new Web3Modal(
       '--w3m-font-family': '"Public Sans", sans-serif',
       '--w3m-font-feature-settings': 'normal',
 
-      '--w3m-accent-color': 'var(--primary-interactive)',
+      // button background-color
+      '--w3m-accent-color': 'var(--neutral-background)',
       '--w3m-accent-fill-color': '#F3F3F3', // grey-10
 
       '--w3m-button-border-radius': '9999px',
@@ -57,7 +58,7 @@ export const web3modal = new Web3Modal(
       '--w3m-color-bg-1': 'var(--primary-background)',
       '--w3m-color-bg-2': 'var(--neutral-background)',
       // '--w3m-color-bg-3': '',
-      '--w3m-color-overlay': 'var(--neutral-background)',
+      // '--w3m-color-overlay': 'none',
     },
     themeMode: (localStorage.getItem('theme') as 'dark' | 'light') ?? 'dark',
   },
