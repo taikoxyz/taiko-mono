f12bd6075ecfbc3717a7f5b1db8d1a8b41f6c809
xiaodino <ruby@taiko.xyz>
2023-08-21 23:06:43 -0700

    Update

---
+++ packages/bridge-ui-v2/src/components/ThemeButton/ThemeButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  import { Button } from '$components/Button';
+  import { Icon } from '$components/Icon';
+
+  function switchTheme() {
+    let darkTheme = localStorage.getItem('theme')?.toLocaleLowerCase() === 'dark';
+    if (typeof darkTheme === 'undefined') return;
+
+    const newTheme = darkTheme ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  }
+
+  onMount(() => {
+    darkTheme = localStorage.getItem('theme')?.toLocaleLowerCase() === 'dark';
+  });
+
+</script>
+
+<div class="flex items-center">
+  <Button class="bg-transparent hover:bg-transparent bg-primary-base-background rounded-none ml-[8px] pl-[8px] pr-[0px] border-l-2 border-gray-800/10 hover:border-gray-800/10" on:click={switchTheme}>
+    <Icon type="heroicons" />
+  </Button>
+</div>
