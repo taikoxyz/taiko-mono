5e1c89a4993d1c683ad0ab849358924f4ae2c8d7
RogerLamTd <bob.rogerbob1@gmail.com>
2023-09-13 12:47:50 -0700

    add nftbalance tests

--- packages/eventindexer/repo/nft_balance_test.go
+++ packages/eventindexer/repo/nft_balance_test.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -56,6 +57,61 @@ func TestIntegration_NFTBalance_Increase(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_, err := nftBalanceRepo.IncreaseBalance(context.Background(), tt.opts)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+func TestIntegration_NFTBalance_Decrease(t *testing.T) {
+	db, close, err := testMysql(t)
+	assert.Equal(t, nil, err)
+
+	defer close()
+
+	nftBalanceRepo, err := NewNFTBalanceRepository(db)
+	assert.Equal(t, nil, err)
+
+	tests := []struct {
+		name    string
+		opts    eventindexer.UpdateNFTBalanceOpts
+		wantErr error
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := nftBalanceRepo.SubtractBalance(context.Background(), tt.opts)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+func TestIntegration_NFTBalance_FindByAddress(t *testing.T) {
+	db, close, err := testMysql(t)
+	assert.Equal(t, nil, err)
+
+	defer close()
+
+	nftBalanceRepo, err := NewNFTBalanceRepository(db)
+	assert.Equal(t, nil, err)
+
+	tests := []struct {
+		name    string
+		address string
+		chainID string
+		wantErr error
+	}{
+		{},
+	}
+
+	get, err := http.NewRequest("GET", "", nil)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := nftBalanceRepo.FindByAddress(
+				context.Background(),
+				get,
+				tt.address,
+				tt.chainID)
 			assert.Equal(t, tt.wantErr, err)
 		})
 	}
