6fd943496cc958db4b0dc2db9bb465e873f26ddf
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-09-15 20:14:09 +0800

    convert all build on taiko guides

--- packages/website/domain/chain/jolnir/addEthereumChain.ts
+++ packages/website/domain/chain/jolnir/addEthereumChain.ts
@@ -1,7 +1,7 @@
 import { JOLNIR_CONFIG } from "./config";
 import { AddEthereumChainParameter } from "../baseTypes";
 
-export const GRIMSVOTN_ADD_ETHEREUM_CHAIN: AddEthereumChainParameter = {
+export const JOLNIR_ADD_ETHEREUM_CHAIN: AddEthereumChainParameter = {
   chainId: JOLNIR_CONFIG.chainId.hex,
   chainName: JOLNIR_CONFIG.names.mediumName,
   nativeCurrency: JOLNIR_CONFIG.nativeCurrency,
