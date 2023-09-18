39ba59475008b9a3e31958281a896320e62f4506
Francisco <jscriptcoder@gmail.com>
2023-09-18 10:55:14 +0200

    fix svelte-check

--- packages/bridge-ui-v2/src/types.d.ts
+++ packages/bridge-ui-v2/src/types.d.ts
@@ -2,3 +2,19 @@ declare module 'ethereum-address' {
   export function isAddress(address: string): boolean;
   export function isChecksumAddress(address: string): boolean;
 }
+
+declare module '$bridgeConfig' {
+  export const routingContractsMap: RoutingMap
+}
+
+declare module '$chainConfig' {
+  export const chainConfig: ChainConfigMap
+}
+
+declare module '$relayerConfi' {
+  export const customToken: Token[]
+}
+
+declare module '$customToken' {
+  export const configuredRelayer: RelayerConfig[]
+}
