d02d06e4811db17319d251044225b30d9b11562d
Korbinian <KorbinianK@users.noreply.github.com>
2023-09-18 17:04:13 +0200

    chore(bridge-ui-v2): rename activities to transactions (#14708)

--- packages/bridge-ui-v2/src/routes/transactions/+page.svelte
+++ packages/bridge-ui-v2/src/routes/transactions/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { Activities } from '$components/Activities';
   import { Page } from '$components/Page';
+  import { Transactions } from '$components/Transactions';
 </script>
 
 <svelte:head>
   <title>Taiko Bridge | Activities</title>
 </svelte:head>
 <Page>
-  <Activities />
+  <Transactions />
 </Page>
