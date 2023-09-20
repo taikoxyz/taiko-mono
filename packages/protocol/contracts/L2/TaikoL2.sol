a715d05ebe6a761416d59bb5910bdf049dd2ea6b
Daniel Wang <dong77@gmail.com>
2023-09-20 10:21:28 +0800

    Update TaikoL2.sol

--- packages/protocol/contracts/L2/TaikoL2.sol
+++ packages/protocol/contracts/L2/TaikoL2.sol
@@ -122,21 +122,13 @@ contract TaikoL2 is EssentialContract, TaikoL2Signer, ICrossChainSync {
         emit CrossChainSynced(l1Height, l1Hash, l1SignalRoot);
 
         // Check EIP-1559 basefee
-        uint256 basefee;
-        EIP1559Config memory config = getEIP1559Config();
-        if (config.gasIssuedPerSecond != 0) {
-            (basefee, gasExcess) = _calcBasefee({
-                config: config,
-                timeSinceParent: block.timestamp - parentTimestamp,
-                parentGasUsed: parentGasUsed
-            });
-        }
+        uint64 basefee;
+        (basefee, gasExcess) = _calcBasefee({
+            config: getEIP1559Config(),
+            timeSinceParent: block.timestamp - parentTimestamp,
+            parentGasUsed: parentGasUsed
+        });
 
-        // To make sure when EIP-1559 is enabled, the basefee is non-zero
-        // (Geth never uses 0 values for basefee)
-        if (basefee == 0) {
-            basefee = 1;
-        }
         // On L2, basefee is not burnt, but sent to a treasury instead.
         // The circuits will need to verify the basefee recipient is the
         // designated address.
@@ -152,7 +144,7 @@ contract TaikoL2 is EssentialContract, TaikoL2Signer, ICrossChainSync {
         // this event for debugging purpose.
         emit Anchored({
             number: uint64(block.number),
-            basefee: uint64(basefee),
+            basefee: basefee,
             gaslimit: uint32(block.gaslimit),
             timestamp: uint64(block.timestamp),
             parentHash: parentHash,
@@ -173,7 +165,7 @@ contract TaikoL2 is EssentialContract, TaikoL2Signer, ICrossChainSync {
     )
         public
         view
-        returns (uint256 _basefee)
+        returns (uint64 _basefee)
     {
         (_basefee,) = _calcBasefee({
             config: getEIP1559Config(),
@@ -298,25 +290,39 @@ contract TaikoL2 is EssentialContract, TaikoL2Signer, ICrossChainSync {
     )
         private
         view
-        returns (uint256 _basefee, uint64 _gasExcess)
+        returns (uint64 _basefee, uint64 _gasExcess)
     {
-        // Unchecked is safe because:
-        // - gasExcess is capped at uint64 max ever, so multiplying with a
-        // uint32 value is safe
-        // - 'excess' is bigger than 'issued'
-        unchecked {
-            uint256 issued = timeSinceParent * config.gasIssuedPerSecond;
-            uint256 excess = (uint256(gasExcess) + parentGasUsed).max(issued);
-            // Very important to cap _gasExcess uint64
-            _gasExcess = uint64((excess - issued).min(type(uint64).max));
-        }
+        if (config.gasIssuedPerSecond == 0) {
+            _basefee = 1;
+            _gasExcess = gasExcess; // Keep as is.
+        } else {
+            // Unchecked is safe because:
+            // - gasExcess is capped at uint64 max ever, so multiplying with a
+            // uint32 value is safe
+            // - 'excess' is bigger than 'issued'
+            unchecked {
+                uint256 issued = timeSinceParent * config.gasIssuedPerSecond;
+                uint256 excess =
+                    (uint256(gasExcess) + parentGasUsed).max(issued);
+                // Very important to cap _gasExcess uint64
+                _gasExcess = uint64((excess - issued).min(type(uint64).max));
+            }
 
-        _basefee = Lib1559Math.calculatePrice({
-            xscale: config.xscale,
-            yscale: config.yscale,
-            xExcess: _gasExcess,
-            xPurchase: 0
-        });
+            _basefee = uint64(
+                Lib1559Math.calculatePrice({
+                    xscale: config.xscale,
+                    yscale: config.yscale,
+                    xExcess: _gasExcess,
+                    xPurchase: 0
+                }).min(type(uint64).max)
+            );
+
+            // To make sure when EIP-1559 is enabled, the basefee is non-zero
+            // (Geth never uses 0 values for basefee)
+            if (_basefee == 0) {
+                _basefee = 1;
+            }
+        }
     }
 }
 
