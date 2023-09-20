2e681994fbf603932c974d5bb517a62ab20ca7ff
Kenk <kenghin_lim@hotmail.com>
2023-09-20 10:09:00 +0700

    feat(website): fix contract addresses

--- packages/website/domain/chain/jolnir/config.ts
+++ packages/website/domain/chain/jolnir/config.ts
@@ -31,43 +31,50 @@ export const JOLNIR_CONFIG = {
     taikoL2: {
       name: "TaikoL2",
       address: {
-        impl: "0x1000777700000000000000000000000000000001",
+        proxy: "0x1000777700000000000000000000000000000001",
+        impl: "0x1c7a1688604a79616826993A1AC27e802cC8b17D",
       },
     },
     etherVault: {
       name: "EtherVault",
       address: {
-        impl: "0x1000777700000000000000000000000000000003",
+        proxy: "0x1000777700000000000000000000000000000003",
+        impl: "0x0000777700000000000000000000000000000003",
       },
     },
     erc20Vault: {
       name: "ERC20Vault",
       address: {
-        impl: "0x1000777700000000000000000000000000000002",
+        proxy: "0x1000777700000000000000000000000000000002",
+        impl: "0x0000777700000000000000000000000000000002",
       },
     },
     erc721Vault: {
       name: "ERC721Vault",
       address: {
-        impl: "0x1000777700000000000000000000000000000008",
+        proxy: "0x1000777700000000000000000000000000000008",
+        impl: "0x0000777700000000000000000000000000000008",
       },
     },
     erc1155Vault: {
       name: "ERC1155Vault",
       address: {
-        impl: "0x1000777700000000000000000000000000000009",
+        proxy: "0x1000777700000000000000000000000000000009",
+        impl: "0x0000777700000000000000000000000000000009",
       },
     },
     bridge: {
       name: "Bridge",
       address: {
-        impl: "0x1000777700000000000000000000000000000004",
+        proxy: "0x1000777700000000000000000000000000000004",
+        impl: "0x0000777700000000000000000000000000000004",
       },
     },
     signalService: {
       name: "SignalService",
       address: {
-        impl: "0x1000777700000000000000000000000000000007",
+        proxy: "0x1000777700000000000000000000000000000007",
+        impl: "0x0000777700000000000000000000000000000007",
       },
     },
     // Bridged ERC-20 contracts
@@ -75,7 +82,8 @@ export const JOLNIR_CONFIG = {
       bridgedTaikoToken: {
         name: "Bridged Taiko Token Jolnir",
         address: {
-          impl: "0xe705498492D0aE94CA9365D395D2C6924F24F445",
+          proxy: "0xe705498492D0aE94CA9365D395D2C6924F24F445",
+          impl: "0x29E2e05dcE30299A90Fd212F6c680692BF7f1dC5",
         },
         decimals: 18,
         symbol: "TTKOj",
@@ -83,7 +91,8 @@ export const JOLNIR_CONFIG = {
       bridgedHorseToken: {
         name: "Bridged Horse Token",
         address: {
-          impl: "0x9833DcA11f178dbaF2b88da42557DA2970534430",
+          proxy: "0x9833DcA11f178dbaF2b88da42557DA2970534430",
+          impl: "0x5B9F2597FF055bfC304F1276048AB65fFde8FbEA",
         },
         decimals: 18,
         symbol: "HORSE",
@@ -91,7 +100,8 @@ export const JOLNIR_CONFIG = {
       bridgedBullToken: {
         name: "Bridged Bull Token",
         address: {
-          impl: "0xc02D1fE3aA2134D2d7561e99f0A699C5Ca7B44ED",
+          proxy: "0xc02D1fE3aA2134D2d7561e99f0A699C5Ca7B44ED",
+          impl: "0x073C9b4Ed7059eEdd2ad6F8a8bFd48AAB2413eC3",
         },
         decimals: 18,
         symbol: "BLL",
