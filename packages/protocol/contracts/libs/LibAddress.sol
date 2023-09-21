2dba5da45b6cf2c58195c50c3c39299c0c24a3ee
Daniel Wang <dong77@gmail.com>
2023-09-21 20:52:50 +0800

    Update LibAddress.sol

--- packages/protocol/contracts/libs/LibAddress.sol
+++ packages/protocol/contracts/libs/LibAddress.sol
@@ -6,27 +6,15 @@
 
 pragma solidity ^0.8.20;
 
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { IERC165Upgradeable } from
     "@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol";
 
 /// @title LibAddress
 /// @dev Provides utilities for address-related operations.
 library LibAddress {
-    /// @dev Sends Ether to the specified address. It is recommended to avoid
-    /// using `.transfer()` due to potential reentrancy issues.
-    /// Reference:
-    /// https://consensys.net/diligence/blog/2019/09/stop-using-soliditys-transfer-now
-    /// @param to The recipient address.
-    /// @param amount The amount of Ether to send in wei.
     function sendEther(address to, uint256 amount) internal {
-        // Check for zero-value or zero-address transactions
-        if (amount == 0 || to == address(0)) return;
-
-        // Attempt to send Ether to the recipient address
-        (bool success,) = payable(to).call{ value: amount }("");
-
-        // Ensure the transfer was successful
-        require(success, "ETH transfer failed");
+        Address.sendValue(payable(to), amount);
     }
 
     function supportsInterface(
