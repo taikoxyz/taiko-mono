dc5e098e320e932fd736a1746c3a81f950855545
dave | d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-09-26 17:39:25 +0900

    Update packages/website/components/Ecosystem/EcosystemSection.tsx

--- packages/website/components/Ecosystem/EcosystemSection.tsx
+++ packages/website/components/Ecosystem/EcosystemSection.tsx
@@ -340,15 +340,6 @@ const ecosystemData: EcosystemData[] = [
     isLive: true,
   },
   {
-    icon: "/images/ecosystem/gourds.jpeg",
-    name: "Gourds",
-    link: "https://gourds.studio",
-    description:
-      "Gourds aims to achieve crypto market efficiency by introducing novel trading instruments for mass adoption.",
-    filters: [],
-    isLive: true,
-  },
-  {
     icon: "/images/ecosystem/stakeme.png",
     name: "STAKEME",
     link: "https://stakeme.pro/",
