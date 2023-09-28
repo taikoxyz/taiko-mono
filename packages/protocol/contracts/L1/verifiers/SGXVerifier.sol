d559ef0652e3118dcebe500b6e8bc50946b6b4f4
adaki2004 <keszeydani@gmail.com>
2023-09-28 20:40:15 +0200

    Addressing Brechts comments

--- packages/protocol/contracts/L1/verifiers/SGXVerifier.sol
+++ packages/protocol/contracts/L1/verifiers/SGXVerifier.sol
@@ -7,7 +7,7 @@
 pragma solidity ^0.8.20;
 
 import { EssentialContract } from "../../common/EssentialContract.sol";
-import { LibAddress } from "../../libs/LibAddress.sol";
+import { ECDSAUpgradeable } from "@ozu/utils/cryptography/ECDSAUpgradeable.sol";
 import { Proxied } from "../../common/Proxied.sol";
 
 import { TaikoData } from "../TaikoData.sol";
@@ -16,21 +16,30 @@ import { IVerifier } from "./IVerifier.sol";
 
 /// @title GuardianVerifier
 contract SGXVerifier is EssentialContract, IVerifier {
-    using LibAddress for address;
+    event InstanceAdded(
+        uint256 indexed instanceId, address indexed instanceInitPubKey
+    );
 
     struct ProofData {
-        address currentPubKey;
+        uint256 id;
         address newPubKey;
         bytes signature;
     }
-    
-    /// @dev One SGX instance is uniquely identified (on-chain) by it's ECDSA public key. Once that public key is used (by proof verification) it has to be overwritten by a new one (representing the same instance). This is due to side-channel protection.
-    mapping(address sgxInstance => bool truested) sgxRegistry;
-    uint256[49] private __gap;
 
-    error SGX_NOT_TRUSTED();
-    error SGX_INVALID_PROOF_DATA();
-    error SGX_INVALID_PROOF_SIGNATURE();
+    /// @dev For gas savings, we shall assign each SGX instance with an id
+    /// so that when we need to set a new pub key, just write storage once.
+    uint256 public uniqueVerifiers;
+
+    /// @dev One SGX instance is uniquely identified (on-chain) by it's ECDSA
+    /// public key. Once that public key is used (by proof verification) it has
+    /// to be overwritten by a new one (representing the same instance). This is
+    /// due to side-channel protection.
+    mapping(uint256 instanceId => address sgxInstance) public sgxRegistry;
+
+    uint256[48] private __gap;
+
+    error SGX_NOT_VALID_SIGNER_OR_ID_MISMATCH();
+    error SGX_INVALID_PROOF_SIZE();
 
     /// @notice Initializes the contract with the provided address manager.
     /// @param _addressManager The address of the address manager contract.
@@ -40,9 +49,16 @@ contract SGXVerifier is EssentialContract, IVerifier {
 
     /// @notice Adds trusted SGX instances to the registry.
     /// @param trustedInstances The address array of trusted SGX instances.
-    function addToRegistry(address[] memory trustedInstances) external onlyOwner {
+    function addToRegistry(address[] memory trustedInstances)
+        external
+        onlyOwner
+    {
         for (uint256 i; i < trustedInstances.length; i++) {
-            sgxRegistry[trustedInstances[i]] = true;
+            sgxRegistry[uniqueVerifiers] = trustedInstances[i];
+
+            emit InstanceAdded(uniqueVerifiers, trustedInstances[i]);
+
+            uniqueVerifiers++;
         }
     }
 
@@ -51,7 +67,7 @@ contract SGXVerifier is EssentialContract, IVerifier {
         // blockId is unused now, but can be used later when supporting
         // different types of proofs.
         uint64,
-        address,
+        address prover,
         bool isContesting,
         TaikoData.BlockEvidence calldata evidence
     )
@@ -60,31 +76,32 @@ contract SGXVerifier is EssentialContract, IVerifier {
         // Do not run proof verification to contest an existing proof
         if (isContesting) return;
 
-        // At least (worst case) the 2 x 32 bytes shall be encoded (current and new pubKey)
-        if(evidence.proof.length < 64) {
-            revert SGX_INVALID_PROOF_DATA();
-        }
-        ProofData memory proofData =
-            abi.decode(evidence.proof, (ProofData));
-
-        if(!sgxRegistry[proofData.currentPubKey]) {
-            revert SGX_NOT_TRUSTED();
+        // Size shall be 2x32 bytes (id + address on a word) + 65 bytes (r,s,v)
+        if (evidence.proof.length != 129) {
+            revert SGX_INVALID_PROOF_SIZE();
         }
 
-        bytes32 signedInstance = getSignedHash(evidence,proofData.currentPubKey, proofData.newPubKey);
+        ProofData memory proofData = abi.decode(evidence.proof, (ProofData));
 
-        if(!proofData.currentPubKey.isValidSignature(signedInstance, proofData.signature)) {
-            revert SGX_INVALID_PROOF_SIGNATURE();
+        bytes32 signedInstance =
+            getSignedHash(evidence, prover, proofData.newPubKey);
+
+        // Would throw in case invalid
+        address signer =
+            ECDSAUpgradeable.recover(signedInstance, proofData.signature);
+
+        if (sgxRegistry[proofData.id] != signer) {
+            revert SGX_NOT_VALID_SIGNER_OR_ID_MISMATCH();
         }
 
-        // Invalidate current key, because it cannot be used again (side-channel attacks).
-        sgxRegistry[proofData.currentPubKey] = false;
-        sgxRegistry[proofData.newPubKey] = true;
+        // Invalidate current key, because it cannot be used again (side-channel
+        // attacks).
+        sgxRegistry[proofData.id] = proofData.newPubKey;
     }
 
     function getSignedHash(
         TaikoData.BlockEvidence memory evidence,
-        address currentInstancePubKey,
+        address assignedProver,
         address newPubKey
     )
         public
@@ -98,7 +115,7 @@ contract SGXVerifier is EssentialContract, IVerifier {
                 evidence.blockHash,
                 evidence.signalRoot,
                 evidence.graffiti,
-                currentInstancePubKey,
+                assignedProver,
                 newPubKey
             )
         );
