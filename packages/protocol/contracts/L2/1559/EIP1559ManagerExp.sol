bff146646590b8ae49d5f4ec4f7f806863cb41c1
Daniel Wang <dong77@gmail.com>
2023-10-01 21:14:53 +0800

    EIP1559ManagerExp

--- packages/protocol/contracts/L2/1559/EIP1559ManagerExp.sol
+++ packages/protocol/contracts/L2/1559/EIP1559ManagerExp.sol
@@ -16,6 +16,7 @@ library Lib1559Exp {
     using LibMath for uint256;
 
     error EIP1559_OUT_OF_GAS();
+    error EIP1559_UNEXPECTED_CHANGE(uint256, uint256);
 
     function calcBaseFeePerGas(
         uint256 gasIssuePerSecond,
@@ -36,15 +37,61 @@ library Lib1559Exp {
 
         if (_gasExcess > gasExcessMax) revert EIP1559_OUT_OF_GAS();
 
-        uint256 _gasToBuy = gasToBuy == 0 ? 1 : gasToBuy;
-        uint256 _before = _calcY(_gasExcessOld, xscale);
-        uint256 _after = _calcY(_gasExcess, xscale);
-        _baseFeePerGas = (_after - _before) / _gasToBuy / yscale;
+        _baseFeePerGas = _calculatePrice(xscale, yscale, _gasExcess, gasToBuy);
     }
-    /// @dev Internal function to calculate y value based on x value and scale.
-    /// @param x The x value.
-    /// @param xscale The x scale value.
-    /// @return The calculated y value.
+
+    /// @dev Calculates xscale and yscale values used for pricing.
+    /// @param xExcessMax The maximum excess value.
+    /// @param price The current price.
+    /// @param target The target gas value.
+    /// @param ratio2x1x Expected ratio of gas price for two blocks.
+    /// @return xscale Calculated x scale value.
+    /// @return yscale Calculated y scale value.
+    function calculateScales(
+        uint256 xExcessMax,
+        uint256 price,
+        uint256 target,
+        uint256 ratio2x1x
+    )
+        internal
+        pure
+        returns (uint256 xscale, uint256 yscale)
+    {
+        assert(xExcessMax != 0);
+        uint256 x = xExcessMax / 2;
+
+        // Calculate xscale
+        xscale = LibFixedPointMath.MAX_EXP_INPUT / xExcessMax;
+
+        // Calculate yscale
+        yscale = _calculatePrice(xscale, price, x, target);
+
+        // Verify the gas price ratio between two blocks, one has
+        // target * 2 gas and the other one has target gas.
+        uint256 price1x = _calculatePrice(xscale, yscale, x, target);
+        uint256 price2x = _calculatePrice(xscale, yscale, x, target * 2);
+        uint256 ratio = price2x * 10_000 / price1x;
+
+        if (ratio2x1x != ratio) {
+            revert EIP1559_UNEXPECTED_CHANGE(ratio2x1x, ratio);
+        }
+    }
+
+    function _calculatePrice(
+        uint256 xscale,
+        uint256 yscale,
+        uint256 gasExcess,
+        uint256 gasToBuy
+    )
+        private
+        pure
+        returns (uint256)
+    {
+        uint256 _gasToBuy = gasToBuy == 0 ? 1 : gasToBuy;
+        uint256 _before = _calcY(gasExcess, xscale);
+        uint256 _after = _calcY(gasExcess + _gasToBuy, xscale);
+        return (_after - _before) / _gasToBuy / yscale;
+    }
 
     function _calcY(uint256 x, uint256 xscale) private pure returns (uint256) {
         uint256 _x = x * xscale;
@@ -56,13 +103,14 @@ library Lib1559Exp {
 }
 
 /// @title EIP1559ManagerExp
-/// @notice Contract that implements EIP-1559 using Uinswap Exp math.
+/// @notice Contract that implements EIP-1559 using
+/// https://ethresear.ch/t/make-eip-1559-more-like-an-amm-curve/9082
 contract EIP1559ManagerExp is EssentialContract, EIP1559Manager {
     using LibMath for uint256;
 
-    uint128 public constant X_SCALE = 1_488_514_844;
-    uint128 public constant Y_SCALE = 358_298_803_609_133_338_138_868_404_779;
-    uint32 public constant GAS_ISSUE_PER_SECOND = 12_500_000;
+    uint256 public constant X_SCALE = 1_488_514_844;
+    uint256 public constant Y_SCALE = 358_298_803_609_133_338_138_868_404_779;
+    uint256 public constant GAS_ISSUE_PER_SECOND = 12_500_000;
     uint64 public constant MAX_GAS_EXCESS = 90_900_000_000;
 
     uint128 public gasExcess;
