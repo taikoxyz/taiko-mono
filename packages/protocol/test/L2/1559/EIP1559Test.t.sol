0073e5915cfec3bca350605984e8c5d218e6e824
Daniel Wang <dong77@gmail.com>
2023-10-01 21:44:23 +0800

    more

--- packages/protocol/test/L2/1559/EIP1559Test.t.sol
+++ packages/protocol/test/L2/1559/EIP1559Test.t.sol
@@ -6,6 +6,7 @@ import { StringsUpgradeable as Strings } from
 import { console2 } from "forge-std/console2.sol";
 
 import { Lib1559AMM } from "../../../contracts/L2/1559/EIP1559ManagerAMM.sol";
+import { Lib1559Exp } from "../../../contracts/L2/1559/EIP1559ManagerExp.sol";
 import { Lib1559Standard } from
     "../../../contracts/L2/1559/EIP1559ManagerStandard.sol";
 import { TestBase } from "../../TestBase.sol";
@@ -25,10 +26,48 @@ contract EIP1559Test is TestBase {
     uint256 public constant GAS_ISSUE_PER_SECOND =
         BLOCK_GAS_TARGET / AVG_BLOCK_TIME;
 
+    uint64 public constant RAITO_2X_1X = 11_250; // ~12.5% increase
+
+    function test_1559ParamCalculation() external {
+        // Tune this number manually so RAITO_2X_1X is ~112.5%.
+        uint64 _multiplier_to_figure_out = 1818;
+        uint256 gasExcessMax = GAS_ISSUE_PER_SECOND * _multiplier_to_figure_out;
+
+        console2.log("INIT_BASEFEE_PER_GAS.    : ", INIT_BASEFEE_PER_GAS);
+        console2.log("GAS_ISSUE_PER_SECOND.    : ", GAS_ISSUE_PER_SECOND);
+        console2.log("BLOCK_GAS_TARGET         : ", BLOCK_GAS_TARGET);
+        console2.log("RAITO_2X_1X              : ", RAITO_2X_1X);
+        console2.log("gasExcessMax             : ", gasExcessMax);
+
+        (uint256 xscale, uint256 yscale, uint256 actualRatio) = Lib1559Exp
+            .calculateScales(
+            gasExcessMax, INIT_BASEFEE_PER_GAS, BLOCK_GAS_TARGET, RAITO_2X_1X
+        );
+
+        console2.log("xscale                    : ", xscale);
+        console2.log("yscale                    : ", yscale);
+        console2.log("_multiplier_to_figure_out :", _multiplier_to_figure_out);
+        assertEq(actualRatio, RAITO_2X_1X);
+
+        // INIT_BASEFEE_PER_GAS.    :  10000000000
+        // GAS_ISSUE_PER_SECOND.    :  1666666
+        // BLOCK_GAS_TARGET         :  5000000
+        // RAITO_2X_1X              :  11250
+        // gasExcessMax             :  3029998788
+        // xscale                    :  44655463198
+        // yscale                    :  1203884546759353210212365284652
+        // _multiplier_to_figure_out : 1818
+    }
+
     function test_1559_compare() public view {
+        // Variable for standard 1559
         uint256 baseFeePerGasVanilla = INIT_BASEFEE_PER_GAS;
+
+        // Variables for AMM-based 1559
         uint256 gasInPool = INIT_GAS_IN_POOL;
         uint256 maxGasInPool = type(uint256).max; // INIT_GAS_IN_POOL * 100000;
+
+        // Variables for Exp 1559
 
         uint256 time;
 
