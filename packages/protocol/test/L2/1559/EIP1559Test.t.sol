c8e2c61bfbdeb770ca7199578bef478f6ea6e68f
Daniel Wang <dong77@gmail.com>
2023-10-01 22:54:24 +0800

    EIP1559ManagerExp

--- packages/protocol/test/L2/1559/EIP1559Test.t.sol
+++ packages/protocol/test/L2/1559/EIP1559Test.t.sol
@@ -26,39 +26,8 @@ contract EIP1559Test is TestBase {
     uint256 public constant GAS_ISSUE_PER_SECOND =
         BLOCK_GAS_TARGET / AVG_BLOCK_TIME;
 
-    uint256 public constant X_SCALE = 44_655_463_198;
-    uint256 public constant Y_SCALE = 1_203_884_546_759_353_210_212_365_284_652;
-    uint256 public constant GAS_EXCESS_MAX = 3_029_998_788;
-
-    function test_1559ParamCalculation() external {
-        // Tune this number manually so expectedRatio2X1X is ~112.5%.
-        uint64 _multiplier_to_figure_out = 1818;
-        uint64 expectedRatio2X1X = 11_250; // ~12.5% increase
-        uint256 gasExcessMax = GAS_ISSUE_PER_SECOND * _multiplier_to_figure_out;
-
-        console2.log("INIT_BASEFEE_PER_GAS.    : ", INIT_BASEFEE_PER_GAS);
-        console2.log("GAS_ISSUE_PER_SECOND.    : ", GAS_ISSUE_PER_SECOND);
-        console2.log("BLOCK_GAS_TARGET         : ", BLOCK_GAS_TARGET);
-        console2.log("expectedRatio2X1X              : ", expectedRatio2X1X);
-        console2.log("gasExcessMax             : ", gasExcessMax);
-
-        (uint256 xscale, uint256 yscale, uint256 actualRatio) = Lib1559Exp
-            .calculateScales(gasExcessMax, INIT_BASEFEE_PER_GAS, BLOCK_GAS_TARGET);
-
-        console2.log("xscale                    : ", xscale);
-        console2.log("yscale                    : ", yscale);
-        console2.log("_multiplier_to_figure_out :", _multiplier_to_figure_out);
-        assertEq(actualRatio, expectedRatio2X1X);
-
-        // INIT_BASEFEE_PER_GAS.    :  10000000000
-        // GAS_ISSUE_PER_SECOND.    :  1666666
-        // BLOCK_GAS_TARGET         :  5000000
-        // expectedRatio2X1X              :  11250
-        // gasExcessMax             :  3029998788
-        // xscale                    :  44655463198
-        // yscale                    :  1203884546759353210212365284652
-        // _multiplier_to_figure_out : 1818
-    }
+    uint256 public constant MAX_UNSOLD_GAS =
+        BLOCK_GAS_TARGET * 30 / AVG_BLOCK_TIME;
 
     function test_1559_compare() public view {
         // Variable for standard 1559
@@ -66,12 +35,23 @@ contract EIP1559Test is TestBase {
 
         // Variables for AMM-based 1559
         uint256 gasInPool = INIT_GAS_IN_POOL;
-        uint256 maxGasInPool =
-            INIT_GAS_IN_POOL + BLOCK_GAS_TARGET * 60 / AVG_BLOCK_TIME; // INIT_GAS_IN_POOL
-            // * 100000;
 
         // Variables for Exp 1559
-        uint256 gasExcess = GAS_EXCESS_MAX / 2;
+        uint256 gasExcess = GAS_ISSUE_PER_SECOND * 1_000_000;
+
+        (uint256 xscale, uint256 yscale) = Lib1559Exp.calculateScales(
+            gasExcess + MAX_UNSOLD_GAS,
+            gasExcess,
+            INIT_BASEFEE_PER_GAS,
+            BLOCK_GAS_TARGET
+        );
+
+        console2.log("xscale      : ", xscale);
+        console2.log("yscale      : ", yscale);
+        console2.log("gasExcess   : ", gasExcess);
+        console2.log(
+            "gasExcess + MAX_UNSOLD_GAS   : ", gasExcess + MAX_UNSOLD_GAS
+        );
 
         // Other variables
         uint256 time;
@@ -97,7 +77,7 @@ contract EIP1559Test is TestBase {
             (baseFeePerGasAMM, gasInPool) = Lib1559AMM.calcBaseFeePerGas(
                 POOL_AMM_PRODUCT,
                 GAS_ISSUE_PER_SECOND,
-                maxGasInPool,
+                INIT_GAS_IN_POOL + MAX_UNSOLD_GAS,
                 gasInPool,
                 delay,
                 blocks[i][1]
@@ -106,9 +86,9 @@ contract EIP1559Test is TestBase {
             uint256 baseFeePerGasExp;
             (baseFeePerGasExp, gasExcess) = Lib1559Exp.calcBaseFeePerGas(
                 GAS_ISSUE_PER_SECOND,
-                X_SCALE,
-                Y_SCALE,
-                GAS_EXCESS_MAX,
+                xscale,
+                yscale,
+                gasExcess - MAX_UNSOLD_GAS,
                 gasExcess,
                 delay,
                 blocks[i][1]
@@ -120,7 +100,8 @@ contract EIP1559Test is TestBase {
                 blocks[i][1],
                 baseFeePerGasVanilla,
                 baseFeePerGasAMM,
-                baseFeePerGasExp
+                baseFeePerGasExp,
+                gasExcess
             );
         }
     }
@@ -131,7 +112,8 @@ contract EIP1559Test is TestBase {
         uint256 gasUsed,
         uint256 baseFeePerGasVanilla,
         uint256 baseFeePerGasAMM,
-        uint256 baseFeePerGasExp
+        uint256 baseFeePerGasExp,
+        uint256 gasExcess
     )
         private
         view
@@ -148,6 +130,8 @@ contract EIP1559Test is TestBase {
             Strings.toString(baseFeePerGasVanilla),
             ", ",
             Strings.toString(baseFeePerGasExp),
+            ", ",
+            Strings.toString(gasExcess),
             ""
         );
 
