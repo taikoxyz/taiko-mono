383ff1516ddc7aca4905d70951a00224749f6840
Daniel Wang <dong77@gmail.com>
2023-10-03 13:07:56 +0800

    Update Lib1559Math.sol

--- packages/protocol/contracts/L2/Lib1559Math.sol
+++ packages/protocol/contracts/L2/Lib1559Math.sol
@@ -9,7 +9,6 @@ pragma solidity ^0.8.20;
 import { LibFixedPointMath } from "../thirdparty/LibFixedPointMath.sol";
 
 library Lib1559Math {
-    error EIP1559_INPUT_TOO_BIG();
     error EIP1559_INVALID_PARAMS();
 
     function basefee(
@@ -40,8 +39,8 @@ library Lib1559Math {
     {
         uint256 input = gasQuantity * LibFixedPointMath.SCALING_FACTOR_1E18
             / gasTarget / adjustmentQuotient;
-        if (input >= LibFixedPointMath.MAX_EXP_INPUT) {
-            revert EIP1559_INPUT_TOO_BIG();
+        if (input > LibFixedPointMath.MAX_EXP_INPUT) {
+            input = LibFixedPointMath.MAX_EXP_INPUT;
         }
         return LibFixedPointMath.exp(int256(input));
     }
