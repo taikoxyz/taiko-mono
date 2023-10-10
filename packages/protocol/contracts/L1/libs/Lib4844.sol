a42abf222a9452ab5b73e837b6e7b885b8fec54d
Daniel Wang <dong77@gmail.com>
2023-10-10 16:15:24 +0800

    Update Lib4844.sol

--- packages/protocol/contracts/L1/libs/Lib4844.sol
+++ packages/protocol/contracts/L1/libs/Lib4844.sol
@@ -12,15 +12,15 @@ library Lib4844 {
     address public constant POINT_EVALUATION_PRECOMPILE_ADDRESS = address(10);
     uint256 public constant BLS_MODULUS =
         52_435_875_175_126_190_479_447_740_508_185_965_837_690_552_500_527_637_822_603_658_699_938_581_184_513;
-    // versioned hash: first 32 bytes
-    // Evaluation point: next 32 bytes
-    // Expected output: next 32 bytes
-    // input kzg point: next 48 bytes
-    // Quotient kzg: next 48 bytes
 
     error EVAL_POINT_TOO_LARGE();
     error EVAL_FAILED();
 
+    /// @param versionHash The versioned hash
+    // @param x The evaluation point
+    // @param y: The expected output
+    // @param commitment The input kzg point
+    // @param proof The quotient kzg
     function point_evaluation_precompile(
         bytes32 versionHash,
         uint256 x,
