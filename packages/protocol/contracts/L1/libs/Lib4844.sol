76c573e3fae74aab4eb667e590d2fc728eda95c8
Daniel Wang <dong77@gmail.com>
2023-10-11 11:23:14 +0800

    Update Lib4844.sol

--- packages/protocol/contracts/L1/libs/Lib4844.sol
+++ packages/protocol/contracts/L1/libs/Lib4844.sol
@@ -9,10 +9,12 @@ pragma solidity ^0.8.20;
 /// @title Lib4844
 /// @notice A library for handling EIP-4844 blobs
 library Lib4844 {
-    address public constant POINT_EVALUATION_PRECOMPILE_ADDRESS = address(10);
+    address public constant POINT_EVALUATION_PRECOMPILE_ADDRESS = address(0x0A);
     uint32 public constant FIELD_ELEMENTS_PERBLOB = 4096;
     uint256 public constant BLS_MODULUS =
         52_435_875_175_126_190_479_447_740_508_185_965_837_690_552_500_527_637_822_603_658_699_938_581_184_513;
+
+    bytes1 public constant BLOBHASH = bytes1(0x49);
 
     error POINT_X_TOO_LARGE();
     error POINT_Y_TOO_LARGE();
@@ -40,5 +42,20 @@ library Lib4844 {
             abi.encodePacked(versionHash, x, y, commitment, proof)
         );
         if (!ok) revert EVAL_FAILED();
+    }
+
+    function blobhash(uint256 index) internal pure returns (bytes32) {
+        // bytes32 result;
+        // assembly {
+        //       let idx := index
+        //     // Opcode for BLOBHASH (replace HASH_OPCODE_BYTE with the actual
+        // opcode)
+        //     let opcode := 0x49
+        //     push(0x49)
+        //     // push(opcode)
+        //     // call
+        //     result := mload(add(0x20, 0))
+        // }
+        // return result;
     }
 }
