daea10f51e586998f7fae85e477d9a9aa0bce04d
Daniel Wang <dong77@gmail.com>
2023-10-15 11:45:08 +0800

    fix

--- packages/protocol/contracts/L1/verifiers/SgxAndZkVerifier.sol
+++ packages/protocol/contracts/L1/verifiers/SgxAndZkVerifier.sol
@@ -31,6 +31,7 @@ contract SgxAndZkVerifier is EssentialContract, IVerifier {
         uint64 blockId,
         address prover,
         bool isContesting,
+        bytes32 blobVersionHash,
         TaikoData.BlockEvidence calldata evidence
     )
         external
@@ -40,7 +41,7 @@ contract SgxAndZkVerifier is EssentialContract, IVerifier {
         // Verify the SGX part
         _evidence.proof = LibBytesUtils.slice(evidence.proof, 0, SGX_PROOF_SIZE);
         IVerifier(resolve("tier_sgx", false)).verifyProof(
-            blockId, prover, isContesting, _evidence
+            blockId, prover, isContesting, blobVersionHash, _evidence
         );
 
         // Verify the ZK part
@@ -50,7 +51,7 @@ contract SgxAndZkVerifier is EssentialContract, IVerifier {
             (evidence.proof.length - SGX_PROOF_SIZE)
         );
         IVerifier(resolve("tier_pse_zkevm", false)).verifyProof(
-            blockId, prover, isContesting, _evidence
+            blockId, prover, isContesting, blobVersionHash, _evidence
         );
     }
 }
--- packages/protocol/contracts/L1/verifiers/SgxVerifier.sol
+++ packages/protocol/contracts/L1/verifiers/SgxVerifier.sol
@@ -6,19 +6,51 @@
 
 pragma solidity ^0.8.20;
 
+import { ECDSAUpgradeable } from "@ozu/utils/cryptography/ECDSAUpgradeable.sol";
+
 import { EssentialContract } from "../../common/EssentialContract.sol";
 import { Proxied } from "../../common/Proxied.sol";
+import { LibBytesUtils } from "../../thirdparty/LibBytesUtils.sol";
 
 import { TaikoData } from "../TaikoData.sol";
 
 import { IVerifier } from "./IVerifier.sol";
 
-/// @title GuardianVerifier
-// TODO(dani): implement this verifier.
-contract SGXVerifier is EssentialContract, IVerifier {
-    uint256[50] private __gap;
+/// @title SgxVerifier
+contract SgxVerifier is EssentialContract, IVerifier {
+    using ECDSAUpgradeable for bytes32;
 
-    error UNIMPLEMENTED();
+    struct Instance {
+        address addr;
+        uint64 addedAt; // We can calculate if expired
+    }
+
+    uint256 public constant INSTANCE_EXPIRY = 180 days;
+
+    /// @dev For gas savings, we shall assign each SGX instance with an id
+    /// so that when we need to set a new pub key, just write storage once.
+    uint256 public nextInstanceId; // slot 1
+
+    /// @dev One SGX instance is uniquely identified (on-chain) by it's ECDSA
+    /// public key (or rather ethereum address). Once that address is used (by
+    /// proof verification) it has to be overwritten by a new one (representing
+    /// the same instance). This is due to side-channel protection. Also this
+    /// public key shall expire after some time. (For now it is a long enough 6
+    /// months setting.)
+    mapping(uint256 instanceId => Instance) public instances; // slot 2
+
+    uint256[48] private __gap;
+
+    event InstanceAdded(
+        uint256 indexed id,
+        address indexed instance,
+        address replaced,
+        uint256 timstamp
+    );
+
+    error SGX_INVALID_INSTANCE();
+    error SGX_INVALID_INSTANCES();
+    error SGX_INVALID_PROOF();
 
     /// @notice Initializes the contract with the provided address manager.
     /// @param _addressManager The address of the address manager contract.
@@ -26,21 +58,122 @@ contract SGXVerifier is EssentialContract, IVerifier {
         EssentialContract._init(_addressManager);
     }
 
+    /// @notice Adds trusted SGX instances to the registry.
+    /// @param _instances The address array of trusted SGX instances.
+    function addInstances(address[] calldata _instances) external onlyOwner {
+        if (_instances.length == 0) revert SGX_INVALID_INSTANCES();
+
+        for (uint256 i; i < _instances.length; ++i) {
+            _addInstance(_instances[i]);
+        }
+    }
+
+    /// @notice Adds SGX instances to the registry by another SGX instance.
+    /// @param id The id of the SGX instance who is adding new members.
+    /// @param _instances The address array of SGX instances.
+    /// @param signature The signature proving authenticity.
+    function addInstances(
+        uint256 id,
+        address[] calldata _instances,
+        bytes calldata signature
+    )
+        external
+    {
+        if (_instances.length == 0) revert SGX_INVALID_INSTANCES();
+
+        bytes32 signedHash = keccak256(abi.encode("ADD_INSTANCES", _instances));
+        address oldInstance = signedHash.recover(signature);
+        if (!_isInstanceValid(id, oldInstance)) revert SGX_INVALID_INSTANCE();
+
+        _replaceInstance(id, oldInstance, _instances[0]);
+        for (uint256 i = 1; i < _instances.length; ++i) {
+            _addInstance(_instances[i]);
+        }
+    }
+
     /// @inheritdoc IVerifier
     function verifyProof(
         uint64, /*blockId*/
-        address, /*prover*/
-        bool, /*isContesting*/
+        address prover,
+        bool isContesting,
         bytes32, /*blobVersionHash*/
-        TaikoData.BlockEvidence calldata /*evidence*/
+        TaikoData.BlockEvidence calldata evidence
     )
         external
-        pure
     {
-        revert UNIMPLEMENTED();
+        // Do not run proof verification to contest an existing proof
+        if (isContesting) return;
+
+        // Size is: 87 bytes
+        // 2 bytes + 20 bytes + 65 bytes (signature) = 87
+        if (evidence.proof.length != 87) revert SGX_INVALID_PROOF();
+
+        uint16 id = uint16(bytes2(LibBytesUtils.slice(evidence.proof, 0, 2)));
+        address newInstance =
+            address(bytes20(LibBytesUtils.slice(evidence.proof, 2, 20)));
+        bytes memory signature = LibBytesUtils.slice(evidence.proof, 22);
+        address oldInstance =
+            getSignedHash(evidence, prover, newInstance).recover(signature);
+
+        if (!_isInstanceValid(id, oldInstance)) revert SGX_INVALID_INSTANCE();
+        _replaceInstance(id, oldInstance, newInstance);
+    }
+
+    function getSignedHash(
+        TaikoData.BlockEvidence memory evidence,
+        address prover,
+        address newAddress
+    )
+        public
+        pure
+        returns (bytes32 signedHash)
+    {
+        return keccak256(
+            abi.encode(
+                evidence.metaHash,
+                evidence.parentHash,
+                evidence.blockHash,
+                evidence.signalRoot,
+                evidence.graffiti,
+                prover,
+                newAddress
+            )
+        );
+    }
+
+    function _addInstance(address instance) private {
+        if (instance == address(0)) revert SGX_INVALID_INSTANCE();
+
+        uint256 id = nextInstanceId++;
+        instances[id] = Instance(instance, uint64(block.timestamp));
+        emit InstanceAdded(id, instance, address(0), block.timestamp);
+    }
+
+    function _replaceInstance(
+        uint256 id,
+        address oldInstance,
+        address newInstance
+    )
+        private
+    {
+        instances[id] = Instance(newInstance, uint64(block.timestamp));
+        emit InstanceAdded(id, newInstance, oldInstance, block.timestamp);
+    }
+
+    function _isInstanceValid(
+        uint256 id,
+        address instance
+    )
+        private
+        view
+        returns (bool)
+    {
+        if (instance == address(0)) return false;
+        if (instance != instances[id].addr) return false;
+        return instances[id].addedAt + INSTANCE_EXPIRY > block.timestamp;
     }
 }
 
-/// @title ProxiedSGXVerifier
+/// @title ProxiedSgxVerifier
 /// @notice Proxied version of the parent contract.
-contract ProxiedSGXVerifier is Proxied, SGXVerifier { }
+contract ProxiedSgxVerifier is Proxied, SgxVerifier { }
