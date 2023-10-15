6fc023998e23afbcdc776ac372df03e3c7512b17
Daniel Wang <dong77@gmail.com>
2023-10-15 15:25:28 +0800

    add TODOs

--- packages/protocol/test/L1/TaikoL1TestBase.sol
+++ packages/protocol/test/L1/TaikoL1TestBase.sol
@@ -31,6 +31,8 @@ contract MockVerifier {
     }
 }
 
+// TODO (dani): remove some code to sub-contracts, this one shall only contain
+// shared logics and data.
 abstract contract TaikoL1TestBase is TestBase {
     AddressManager public addressManager;
     TaikoToken public tko;
--- packages/protocol/test/TestBase.sol
+++ packages/protocol/test/TestBase.sol
@@ -5,6 +5,8 @@ import { Test } from "forge-std/Test.sol";
 import { Bridge } from "../contracts/bridge/Bridge.sol";
 import { ICrossChainSync } from "../contracts/common/ICrossChainSync.sol";
 
+// TODO (dani): remove some code to sub-contracts, this one shall only contain
+// shared logics and data.
 abstract contract TestBase is Test {
     uint256 private _seed = 0x12345678;
     uint256 internal variablePKey = 0x6;
