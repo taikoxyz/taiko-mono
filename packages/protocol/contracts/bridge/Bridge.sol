d419512ff8227226a6d79e1301e936bd020be521
Daniel Wang <dong77@gmail.com>
2023-10-20 21:56:26 +0800

    Update Bridge.sol

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -64,6 +64,8 @@ contract Bridge is EssentialContract, IBridge {
         _;
     }
 
+    /// @dev Vaults will transfer Ether to this Bridge before these Ether are
+    /// sent to users.
     receive() external payable { }
 
     /// @notice Initializes the contract.
