2a4c400c5307e9f6ce5a73adb3b91d502d9bbc5b
Daniel Wang <dong77@gmail.com>
2023-10-20 14:52:10 +0800

    Update BridgedERC20.sol

--- packages/protocol/contracts/tokenvault/BridgedERC20.sol
+++ packages/protocol/contracts/tokenvault/BridgedERC20.sol
@@ -86,7 +86,7 @@ contract BridgedERC20 is
         uint256 amount
     )
         public
-        onlyFromNamed2("erc20_vault")
+        onlyFromNamed("erc20_vault")
     {
         _mint(account, amount);
         emit Transfer(address(0), account, amount);
