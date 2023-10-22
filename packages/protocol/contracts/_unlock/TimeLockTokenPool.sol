7ab3a37a486a8671146b7b744a729952a6191b86
Daniel Wang <dong77@gmail.com>
2023-10-22 20:38:13 +0800

    more

--- packages/protocol/contracts/_unlock/TimeLockTokenPool.sol
+++ packages/protocol/contracts/_unlock/TimeLockTokenPool.sol
@@ -20,27 +20,38 @@ contract TimeLockTokenPool is OwnableUpgradeable {
     using SafeERC20Upgradeable for ERC20Upgradeable;
 
     struct Grant {
-        uint128 amount;
+        // If `grantStart` or `grantPeriod` is 0, `amount` indicates the total
+        // tokens already owned by the recipient.
+        // Otherwise, `amount` represents the total grant amount, governed by
+        // the schedule set by `grantStart` and `grantPeriod`.
+        uint256 amount;
+        // If non-zero, indicates the start time for the recipient to receive
+        // tokens, subject to an unlocking schedule.
         uint64 grantStart;
-        uint64 grantWindow;
+        // If non-zero, specifies the total seconds required for the recipient
+        // to fully own all granted tokens.
+        uint64 grantPeriod;
+        // If non-zero, indicates the start time for the recipient to unlock
+        // tokens.
         uint64 unlockStart;
-        uint64 unlockWindow;
+        // If non-zero, specifies the total seconds required for the recipient
+        // to fully unlock all owned tokens.
+        uint64 unlockPeriod;
     }
 
     struct Recipient {
-        uint128 unlockedAmount;
-        uint128 totalWithdrawn;
+        uint256 amountWithdrawn;
         Grant[] grants;
     }
 
     address public taikoToken;
-    uint256 public totalWithdrawn;
+    uint256 public totalAmountWithdrawn;
     mapping(address recipient => Recipient) public recipients;
     uint256[47] private __gap;
 
     event Granted(address indexed user, Grant grant);
-    event Settled(address indexed user, uint128 newlySettled);
-    event Withdrawn(address indexed user, uint128 amount);
+    event Settled(address indexed user, uint256 newlySettled);
+    event Withdrawn(address indexed user, uint256 amount);
 
     error INVALID_PARAM();
     error NOTHING_TO_SETTLE();
@@ -55,98 +66,84 @@ contract TimeLockTokenPool is OwnableUpgradeable {
 
     /// @notice Gives a new grant to a address with its own unlock schedule.
     /// This transaction should happen on a regular basis, e.g., quarterly.
-    function grant(address recipient, Grant calldata g) external onlyOwner {
-        // if (g.recipient == address(0)) revert INVALID_PARAM();
-        // if (g.grantStart < block.timestamp - 30 days) revert INVALID_PARAM();
-        // if (g.amount == 0) revert INVALID_PARAM();
-
-        // uint64 _unlockWindow;
-        // if (unlockWindow != 0) {
-        //     if (unlockWindow < 365 days) revert INVALID_PARAM();
-        //     if (unlockWindow > 4 * 365 days) revert INVALID_PARAM();
-        //     _unlockWindow = unlockWindow;
-        // } else {
-        //     _unlockWindow = 4 * 365 days;
-        // }
-
-        // Grant memory g = Grant(grantStart, _unlockWindow, amount);
-        recipients[recipient].grants.push(g);
-        emit Granted(recipient, g);
-    }
+    function grant(address recipient, Grant calldata g) external onlyOwner { }
 
     /// @notice Puts a stop to all grants given to an address and settles
     /// withdrawal tokens. The recipient will still be able to get what he/she
     /// deserves to receive. This transaction simply invalidates all future
     /// unlocks.
-    function settle(address recipient) external onlyOwner {
-        Recipient storage r = recipients[recipient];
-        if (r.grants.length == 0) revert NOTHING_TO_SETTLE();
-
-        uint128 newlySettled;
-        for (uint256 i; i < r.grants.length; ++i) {
-            newlySettled += _getGrantUnlocked(r.grants[i]);
-        }
-
-        r.unlockedAmount += newlySettled;
-        delete r.grants;
-
-        emit Settled(recipient, newlySettled);
-    }
+    function settle(address recipient) external onlyOwner { }
 
     /// @notice Withdraws all withdrawal tokens.
-    function withdraw() external {
-        uint128 amount = getWithdrawable(msg.sender);
-        if (amount == 0) revert NOTHING_TO_WITHDRAW();
+    function withdraw() external { }
 
-        recipients[msg.sender].totalWithdrawn += amount;
-        totalWithdrawn += totalWithdrawn;
-        ERC20Upgradeable(taikoToken).transfer(msg.sender, amount);
-
-        emit Withdrawn(msg.sender, amount);
-    }
-
-    /// @notice Returns the amount of withdrawable tokens for a given recipient.
-    function getWithdrawable(address recipient) public view returns (uint128) {
-        return
-            getTotalUnlocked(recipient) - recipients[recipient].totalWithdrawn;
-    }
-
-    /// @notice Returns the amount of tokens ever unlocked for a given
-    /// recipient.
-    function getTotalUnlocked(address recipient)
+    function getGrantStatus(address recipient)
         public
         view
-        returns (uint128 totalUnlocked)
+        returns (
+            uint256 amountOwned,
+            uint256 amountUnlocked,
+            uint256 amountWithdrawn,
+            uint256 amountWithdrawable
+        )
+    {
+        amountOwned = _getAmountOwned(recipient);
+        amountUnlocked = _getAmountUnlocked(recipient);
+        amountWithdrawn = recipients[recipient].amountWithdrawn;
+        amountWithdrawable = amountUnlocked - amountWithdrawn;
+    }
+
+    function getGrants(address recipient)
+        public
+        view
+        returns (Grant[] memory)
+    {
+        return recipients[recipient].grants;
+    }
+
+    function _getAmountOwned(address recipient)
+        private
+        view
+        returns (uint256 amount)
     {
         Recipient storage r = recipients[recipient];
-        totalUnlocked = r.unlockedAmount;
-
         for (uint256 i; i < r.grants.length; ++i) {
-            totalUnlocked += _getGrantUnlocked(r.grants[i]);
+            amount += _getAmountOwned(r.grants[i]);
         }
     }
 
-    /// @notice Returns the amount of tokens ever unlocked from a single grant.
-    function _getGrantUnlocked(Grant memory g) private view returns (uint128) {
-        // if (g.amount == 0) return 0;
+    function _getAmountUnlocked(address recipient)
+        private
+        view
+        returns (uint256 amount)
+    {
+        Recipient storage r = recipients[recipient];
+        for (uint256 i; i < r.grants.length; ++i) {
+            amount += _getAmountUnlocked(r.grants[i]);
+        }
+    }
 
-        // uint128 grantedAmount;
+    function _getAmountOwned(Grant memory g) private view returns (uint256) {
+        if (g.amount == 0) return 0;
+        if (g.grantStart == 0 || g.grantPeriod == 0) return g.amount;
+        if (block.timestamp <= g.grantStart) return 0;
+        if (block.timestamp >= g.grantStart + g.grantPeriod) return g.amount;
 
-        // if (g.grantStart == 0 || g.unlockWindow == 0) {
-        //     grantedAmount = g.amount;
-        // } else {
-        //     if (block.timestamp <= g.grantStart) grantedAmount = 0;
-        //     if (block.timestamp >= g.grantStart + g.grantWindow) return
-        // g.amount;
-        // }
+        return g.amount * uint64(block.timestamp - g.grantStart) / g.grantPeriod;
+    }
 
-        // if (block.timestamp <= g.grantStart) return 0;
-        // if (block.timestamp >= g.grantStart + g.unlockWindow) return
-        // g.amount;
+    function _getAmountUnlocked(Grant memory g)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 amount = _getAmountOwned(g);
+        if (amount == 0) return 0;
+        if (g.unlockStart == 0 || g.unlockPeriod == 0) return amount;
+        if (block.timestamp <= g.unlockStart) return 0;
+        if (block.timestamp >= g.unlockStart + g.unlockPeriod) return amount;
 
-        // return
-        //     g.amount / g.unlockWindow * uint64(block.timestamp -
-        // g.grantStart);
+        return amount * uint64(block.timestamp - g.unlockStart) / g.unlockPeriod;
     }
 }
 
