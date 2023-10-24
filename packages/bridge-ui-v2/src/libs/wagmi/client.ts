c0d20b0f1d7d738d06e587f0ddfc9b3586e9cea4
xiaodino <ruby@taiko.xyz>
2023-10-23 23:11:05 -0700

    Fix lint error

--- packages/bridge-ui-v2/src/libs/wagmi/client.ts
+++ packages/bridge-ui-v2/src/libs/wagmi/client.ts
@@ -1,6 +1,6 @@
 import { configureChains } from '@wagmi/core';
 import { publicProvider } from '@wagmi/core/providers/public';
-import { defaultWagmiConfig,walletConnectProvider } from '@web3modal/wagmi';
+import { defaultWagmiConfig, walletConnectProvider } from '@web3modal/wagmi';
 
 import { PUBLIC_WALLETCONNECT_PROJECT_ID } from '$env/static/public';
 import { chains as chainsFromEnv } from '$libs/chain';
