2ee6871105f1ce6147e40b5603852fe710aa41be
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2023-10-27 21:19:40 +0800

    refactor(protocol): move depositsProcessed out of Metadata (#15055)
    Co-authored-by: Daniel Wang <dong77@gmail.com>

--- packages/protocol/contracts/L1/TaikoData.sol
+++ packages/protocol/contracts/L1/TaikoData.sol
@@ -85,6 +85,11 @@ library TaikoData {
         bytes signature;
     }
 
+    struct BlockParams {
+        ProverAssignment assignment;
+        bytes32 extraData;
+    }
+
     /// @dev Struct containing data only required for proving a block
     /// Warning: changing this struct requires changing
     /// {LibProposing.hashMetadata} accordingly.
@@ -97,14 +102,14 @@ library TaikoData {
         bytes32 difficulty; // slot 2
         bytes32 blobHash; //or txListHash (if Blob not yet supported), // slot 3
         bytes32 extraData; // slot 4
-        address coinbase; // L2 coinbase, // slot 5
+        bytes32 depositsHash; // slot 5
+        address coinbase; // L2 coinbase, // slot 6
         uint64 id;
         uint32 gasLimit;
-        uint64 timestamp; // slot 6
+        uint64 timestamp; // slot 7
         uint64 l1Height;
         uint16 minTier;
         bool blobUsed;
-        TaikoData.EthDeposit[] depositsProcessed; // slot 7
     }
 
     /// @dev Struct representing transition to be proven.
