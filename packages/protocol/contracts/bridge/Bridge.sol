385f823f3273cbdd4a76e0bb0a3cbe70d7c823d4
Daniel Wang <dong77@gmail.com>
2023-10-27 17:00:09 +0800

    fix Bug reported by Victor

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -100,11 +100,14 @@ contract Bridge is EssentialContract, IBridge {
         if (message.to == address(0)) revert B_INVALID_TO();
         if (message.to == destBridge) revert B_INVALID_TO();
 
+        address etherVault = resolve("ether_vault", false);
+        if (message.to == etherVault) revert B_INVALID_TO();
+
         // Ensure the sent value matches the expected amount.
         uint256 expectedAmount = message.value + message.fee;
         if (expectedAmount != msg.value) revert B_INVALID_VALUE();
 
-        resolve("ether_vault", false).sendEther(expectedAmount);
+        etherVault.sendEther(expectedAmount);
 
         _message = message;
         // Configure message details and send signal to indicate message
@@ -202,7 +205,10 @@ contract Bridge is EssentialContract, IBridge {
         uint256 refundAmount;
 
         // Process message differently based on the target address
-        if (message.to == address(this) || message.to == address(0)) {
+        if (
+            message.to == address(this) || message.to == address(0)
+                || message.to == ethVault
+        ) {
             // Handle special addresses that don't require actual invocation but
             // mark message as DONE
             status = Status.DONE;
