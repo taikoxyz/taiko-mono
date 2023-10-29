5e60e268f6dcc9e794e9275076e6729c305b123b
Daniel Wang <dong77@gmail.com>
2023-10-29 16:53:31 +0800

    Revert "more"
    This reverts commit 3b19743ed6006262384e8765d8ef5e51bab8a849.

--- packages/protocol/test/tokenvault/EtherVault.t.sol
+++ packages/protocol/test/tokenvault/EtherVault.t.sol
@@ -9,77 +9,53 @@ contract TestEtherVault is TestBase {
     AddressManager addressManager;
     EtherVault etherVault;
 
-    // function setUp() public {
-    //     vm.deal(Alice, 1 ether);
-    //     vm.deal(Bob, 1 ether);
-    //     addressManager = new AddressManager();
-    //     addressManager.init();
-    //     etherVault = new EtherVault();
-    //     etherVault.init(address(addressManager));
-    //     addressManager.setAddress(block.chainid, "bridge", Alice)
-    // }
+    function setUp() public {
+        vm.deal(Alice, 1 ether);
+        vm.deal(Bob, 1 ether);
+        addressManager = new AddressManager();
+        addressManager.init();
+        etherVault = new EtherVault();
+        vm.prank(Alice);
+        etherVault.init(address(addressManager));
+        addressManager.setAddress(block.chainid, "bridge", Alice);
+    }
 
-    // function test_EtherVault_authorize_revert() public {
-    //     vm.prank(Bob);
-    //     vm.expectRevert("Ownable: caller is not the owner");
-    //     etherVault.authorize(Bob, true);
+    function test_EtherVault_releaseEther_reverts_when_zero_address() public {
+        vm.startPrank(Alice);
+        _seedEtherVault();
 
-    //     vm.startPrank(Alice);
-    //     etherVault.authorize(Bob, true);
-    //     vm.expectRevert(EtherVault.VAULT_INVALID_PARAMS.selector);
-    //     etherVault.authorize(Bob, true);
-    //     assertTrue(etherVault.isAuthorized(Bob));
-    // }
+        vm.expectRevert(EtherVault.VAULT_INVALID_RECIPIENT.selector);
+        etherVault.releaseEther(address(0), 1 ether);
+    }
 
-    // function test_EtherVault_authorize_authorizes_when_owner_authorizing()
-    //     public
-    // {
-    //     vm.prank(Alice);
-    //     etherVault.authorize(Bob, true);
-    //     assertTrue(etherVault.isAuthorized(Bob));
-    // }
+    function test_EtherVault_releaseEther_releases_to_authorized_sender()
+        public
+    {
+        vm.startPrank(Alice);
+        _seedEtherVault();
 
-    // function test_EtherVault_releaseEther_reverts_when_zero_address() public
-    // {
-    //     vm.startPrank(Alice);
-    //     etherVault.authorize(Alice, true);
-    //     _seedEtherVault();
+        uint256 aliceBalanceBefore = Alice.balance;
+        etherVault.releaseEther(Alice, 1 ether);
+        uint256 aliceBalanceAfter = Alice.balance;
+        assertEq(aliceBalanceAfter - aliceBalanceBefore, 1 ether);
+        vm.stopPrank();
+    }
 
-    //     vm.expectRevert(EtherVault.VAULT_INVALID_RECIPIENT.selector);
-    //     etherVault.releaseEther(address(0), 1 ether);
-    // }
+    function test_EtherVault_releaseEther_releases_to_receipient_via_authorized_sender(
+    )
+        public
+    {
+        vm.startPrank(Alice);
+        _seedEtherVault();
 
-    // function test_EtherVault_releaseEther_releases_to_authorized_sender()
-    //     public
-    // {
-    //     vm.startPrank(Alice);
-    //     etherVault.authorize(Alice, true);
-    //     _seedEtherVault();
+        uint256 bobBalanceBefore = Bob.balance;
+        etherVault.releaseEther(Bob, 1 ether);
+        uint256 bobBalanceAfter = Bob.balance;
+        assertEq(bobBalanceAfter - bobBalanceBefore, 1 ether);
+        vm.stopPrank();
+    }
 
-    //     uint256 aliceBalanceBefore = Alice.balance;
-    //     etherVault.releaseEther(Alice, 1 ether);
-    //     uint256 aliceBalanceAfter = Alice.balance;
-    //     assertEq(aliceBalanceAfter - aliceBalanceBefore, 1 ether);
-    //     vm.stopPrank();
-    // }
-
-    // function
-    // test_EtherVault_releaseEther_releases_to_receipient_via_authorized_sender(
-    // )
-    //     public
-    // {
-    //     vm.startPrank(Alice);
-    //     etherVault.authorize(Alice, true);
-    //     _seedEtherVault();
-
-    //     uint256 bobBalanceBefore = Bob.balance;
-    //     etherVault.releaseEther(Bob, 1 ether);
-    //     uint256 bobBalanceAfter = Bob.balance;
-    //     assertEq(bobBalanceAfter - bobBalanceBefore, 1 ether);
-    //     vm.stopPrank();
-    // }
-
-    // function _seedEtherVault() private {
-    //     vm.deal(address(etherVault), 100 ether);
-    // }
+    function _seedEtherVault() private {
+        vm.deal(address(etherVault), 100 ether);
+    }
 }
