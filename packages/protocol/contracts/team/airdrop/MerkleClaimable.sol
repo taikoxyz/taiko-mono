15cfcaf8436d512f314229c9abc2f1ae6aeafc2b
Daniel Wang <dong77@gmail.com>
2023-11-04 12:52:24 +0800

    Update MerkleClaimable.sol

--- packages/protocol/contracts/team/airdrop/MerkleClaimable.sol
+++ packages/protocol/contracts/team/airdrop/MerkleClaimable.sol
@@ -15,6 +15,10 @@ import { Proxied } from "../../common/Proxied.sol";
 
 /// @title MerkleClaimable
 /// Contract for managing Taiko token airdrop for eligible users
+// TODO(dani): add claimStart and claimEnd timestamp so claim() can only be
+// called between these two timestamps.
+// Better to add non-reentrance guard to claim().
+
 abstract contract MerkleClaimable is OwnableUpgradeable {
     mapping(bytes32 => bool) public isClaimed;
     bytes32 public merkleRoot;
