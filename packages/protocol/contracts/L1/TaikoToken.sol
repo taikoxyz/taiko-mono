31c5b9546c349c6a64e38c07a8e151132447b8c4
Daniel Wang <dong77@gmail.com>
2023-11-16 20:57:41 +0300

    Update TaikoToken.sol

--- packages/protocol/contracts/L1/TaikoToken.sol
+++ packages/protocol/contracts/L1/TaikoToken.sol
@@ -6,12 +6,15 @@
 
 pragma solidity ^0.8.20;
 
+import { NoncesUpgradeable } from
+    "lib/openzeppelin-contracts-upgradeable/contracts/utils/NoncesUpgradeable.sol";
 import { ERC20Upgradeable } from
     "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
 import { ERC20VotesUpgradeable } from
     "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 import { ERC20PermitUpgradeable } from
     "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+
 import { EssentialContract } from "../common/EssentialContract.sol";
 import { Proxied } from "../common/Proxied.sol";
 
@@ -20,9 +23,11 @@ import { Proxied } from "../common/Proxied.sol";
 /// @notice The TaikoToken (TKO), in the protocol is used for prover collateral
 /// in the form of bonds. It is an ERC20 token with 18 decimal places of
 /// precision.
-contract TaikoToken is EssentialContract, ERC20VotesUpgradeable {
-    // ERC20PermitUpgradeable
-
+contract TaikoToken is
+    EssentialContract,
+    ERC20VotesUpgradeable,
+    ERC20PermitUpgradeable
+{
     error TKO_INVALID_ADDR();
     error TKO_INVALID_PREMINT_PARAMS();
 
@@ -41,8 +46,9 @@ contract TaikoToken is EssentialContract, ERC20VotesUpgradeable {
         initializer
     {
         EssentialContract._init(_addressManager);
-        ERC20Upgradeable.__ERC20_init_unchained(_name, _symbol);
-        ERC20VotesUpgradeable.__ERC20Votes_init_unchained();
+        ERC20Upgradeable.__ERC20_init(_name, _symbol);
+        ERC20VotesUpgradeable.__ERC20Votes_init();
+        ERC20PermitUpgradeable.__ERC20Permit_init(_name);
 
         // Mint 1 billion tokens
         _mint(_recipient, 1_000_000_000 ether);
@@ -94,6 +100,27 @@ contract TaikoToken is EssentialContract, ERC20VotesUpgradeable {
     {
         if (to == address(this)) revert TKO_INVALID_ADDR();
         return super.transferFrom(from, to, amount);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        virtual
+        override(NoncesUpgradeable, ERC20PermitUpgradeable)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    )
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._update(from, to, amount);
     }
 }
 
