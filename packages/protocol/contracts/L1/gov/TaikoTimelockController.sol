c452b0e7fe7f823e4bb1cb94788642a278c25569
Daniel Wang <dan@taiko.xyz>
2023-11-29 20:20:25 +0800

    A more flexible DAO role set up

--- packages/protocol/contracts/L1/gov/TaikoTimelockController.sol
+++ packages/protocol/contracts/L1/gov/TaikoTimelockController.sol
@@ -13,11 +13,14 @@ import "../../common/OwnerUUPSUpgradable.sol";
 contract TaikoTimelockController is OwnerUUPSUpgradable, TimelockControllerUpgradeable {
     uint256[50] private __gap;
 
-    error ZERO_ADDRESS();
-
     function init(uint256 minDelay) external initializer {
         _OwnerUUPSUpgradable_init();
         address[] memory nil = new address[](0);
         __TimelockController_init(minDelay, nil, nil, owner());
+    }
+
+    /// @dev Allows the admin to get around of the min delay.
+    function getMinDelay() public view override returns (uint256) {
+        return hasRole(TIMELOCK_ADMIN_ROLE, msg.sender) ? 0 : super.getMinDelay();
     }
 }
--- packages/protocol/script/DeployOnL1.s.sol
+++ packages/protocol/script/DeployOnL1.s.sol
@@ -182,9 +182,15 @@ contract DeployOnL1 is Script {
         // Setup time lock roles
         TaikoTimelockController _timelock = TaikoTimelockController(payable(timelock));
         _timelock.grantRole(_timelock.PROPOSER_ROLE(), governor);
-        _timelock.grantRole(_timelock.EXECUTOR_ROLE(), governor);
-        _timelock.grantRole(_timelock.CANCELLER_ROLE(), address(governor));
+        _timelock.grantRole(_timelock.PROPOSER_ROLE(), securityCouncil);
 
+        _timelock.grantRole(_timelock.EXECUTOR_ROLE(), governor);
+        _timelock.grantRole(_timelock.EXECUTOR_ROLE(), securityCouncil);
+
+        _timelock.grantRole(_timelock.CANCELLER_ROLE(), governor);
+        _timelock.grantRole(_timelock.CANCELLER_ROLE(), securityCouncil);
+
+        _timelock.grantRole(_timelock.TIMELOCK_ADMIN_ROLE(), governor);
         _timelock.grantRole(_timelock.TIMELOCK_ADMIN_ROLE(), securityCouncil);
         _timelock.renounceRole(_timelock.TIMELOCK_ADMIN_ROLE(), msg.sender);
 
