1fad82ada4926db20093f704cd0431430d9eb09f
faker70698 <xxxigm98@gmail.com>
2023-12-03 17:33:29 +0700

    refactor utils in status-page

--- packages/status-page/src/utils/displayStatusValue.ts
+++ packages/status-page/src/utils/displayStatusValue.ts
@@ -2,14 +2,18 @@ import { ethers } from "ethers";
 import type { Status } from "src/domain/status";
 
 export const displayStatusValue = (value: Status) => {
-  if (typeof value === "string") {
-    if (!value) return "0x";
-    if (ethers.utils.isHexString(value)) {
-      return value.substring(0, 14);
-    }
-    return value;
+  switch (typeof value) {
+    case "string":
+      if (!value) return "0x";
+      if (ethers.utils.isHexString(value)) {
+        return value.substring(0, 14);
+      }
+      return value;
+    case "number":
+      return value;
+    case "boolean":
+      return value.toString();
+    default:
+      throw new Error("Invalid status type");
   }
-
-  if (typeof value === "number") return value;
-  if (typeof value === "boolean") return value.toString();
 };
