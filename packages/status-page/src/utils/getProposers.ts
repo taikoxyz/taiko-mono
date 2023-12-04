90b548f5444515c0397dfc62f066e255b65966fa
faker70698 <xxxigm98@gmail.com>
2023-12-04 21:37:24 +0700

    refactor(status-page getProposers)

--- packages/status-page/src/utils/getProposers.ts
+++ packages/status-page/src/utils/getProposers.ts
@@ -6,21 +6,31 @@ export const getProposers = async (
   contractAddress: string
 ): Promise<number> => {
   const contract: Contract = new Contract(contractAddress, TaikoL1, provider);
-  let events = [];
+  const events = await getAllEvents(contract, provider);
+
+  const uniqueProposers = getUniqueProposers(events);
+
+  return uniqueProposers.length;
+};
+
+const getAllEvents = async (contract: Contract, provider: ethers.providers.JsonRpcProvider) => {
+  const events = [];
   const latestBlock = await provider.getBlockNumber();
   const batchSize = 1000;
   for (let i = 0; i < latestBlock; i += batchSize) {
     const end = i + batchSize > latestBlock ? latestBlock : i + batchSize;
-    const e = await contract.queryFilter("BlockProposed", i, end);
-    events = events.concat(e);
+    const filteredEvents = await contract.queryFilter("BlockProposed", i, end);
+    events.push(...filteredEvents);
   }
+  return events;
+};
 
-  const proposers = [];
-  events.map((event) => {
-    if (!proposers.includes(event.args.meta.beneficiary)) {
-      proposers.push(event.args.meta.beneficiary);
+const getUniqueProposers = (events: any[]) => {
+  const uniqueProposers = [];
+  events.forEach((event) => {
+    if (!uniqueProposers.includes(event.args.meta.beneficiary)) {
+      uniqueProposers.push(event.args.meta.beneficiary);
     }
   });
-
-  return proposers.length;
+  return uniqueProposers;
 };
