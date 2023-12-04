eb950d11916666418635937d6feb519a7ef55070
Daniel Wang <dan@taiko.xyz>
2023-12-04 11:59:58 +0800

    fixed solidity version
    According to OZ: Pragma directives should be fixed to clearly identify the Solidity version with which the contracts will be compiled.

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -3,7 +3,7 @@
 // |_   _|_ _(_) |_____  | |   __ _| |__ ___
 //   | |/ _` | | / / _ \ | |__/ _` | '_ (_-<
 //   |_|\__,_|_|_\_\___/ |____\__,_|_.__/__/
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "../common/EssentialContract.sol";
 import "../libs/LibAddress.sol";
--- packages/protocol/contracts/tokenvault/ERC1155Vault.sol
+++ packages/protocol/contracts/tokenvault/ERC1155Vault.sol
@@ -4,7 +4,7 @@
 //   | |/ _` | | / / _ \ | |__/ _` | '_ (_-<
 //   |_|\__,_|_|_\_\___/ |____\__,_|_.__/__/
 
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 import
