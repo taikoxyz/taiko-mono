956e829ecb765e7811d8d4c328d3be4007ae7491
Jeffery Walsh <cyberhorsey@gmail.com>
2023-12-11 19:30:54 -0800

    .

--- packages/guardian-prover-health-check/migrations/1666651001_create_signed_blocks_table.sql
+++ packages/guardian-prover-health-check/migrations/1666651001_create_signed_blocks_table.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS signed_blocks (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     guardian_prover_id int NOT NULL,
     block_id int NOT NULL,
-    signature VARCHAR(5000) NOT NULL,
+    signature varchar(5000) NOT NULL,
     block_hash varchar (42) NOT NULL,
     recovered_address varchar(42) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     UNIQUE key `guardian_prover_id_signature` (`guardian_prover_id`, `signature`),
     UNIQUE key `guardian_prover_id_block_id` (`guardian_prover_id`, `block_id`),
-    UNIQUE key `guardian_prover_id_block_hash` (`guardian_prover_id`, `block_hash`),
+    UNIQUE key `guardian_prover_id_block_hash` (`guardian_prover_id`, `block_hash`)
 );
 
 -- +goose StatementEnd
