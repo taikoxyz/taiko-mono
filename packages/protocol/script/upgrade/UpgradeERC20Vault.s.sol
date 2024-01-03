abc058bd14db784e67aa23d82e1535eb68143052
Jeffery Walsh <cyberhorsey@gmail.com>
2024-01-03 13:28:21 -0800

    upgrade scripts for new proxies

---
+++ packages/protocol/script/upgrade/UpgradeERC20Vault.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+//  _____     _ _         _         _
+// |_   _|_ _(_) |_____  | |   __ _| |__ ___
+//   | |/ _` | | / / _ \ | |__/ _` | '_ (_-<
+//   |_|\__,_|_|_\_\___/ |____\__,_|_.__/__/
+//
+//   Email: security@taiko.xyz
+//   Website: https://taiko.xyz
+//   GitHub: https://github.com/taikoxyz
+//   Discord: https://discord.gg/taikoxyz
+//   Twitter: https://twitter.com/taikoxyz
+//   Blog: https://mirror.xyz/labs.taiko.eth
+//   Youtube: https://www.youtube.com/@taikoxyz
+
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../../contracts/tokenvault/ERC20Vault.sol";
+import "./UpgradeScript.s.sol";
+
+contract UpgradeERC20Vault is UpgradeScript {
+    function run() external setUp {
+        console2.log("upgrading ERC20Vault");
+        ERC20Vault newERC20Vault = new ERC20Vault();
+        upgrade(address(newERC20Vault));
+
+        console2.log("upgraded ERC20Vault to", address(newERC20Vault));
+    }
+}
