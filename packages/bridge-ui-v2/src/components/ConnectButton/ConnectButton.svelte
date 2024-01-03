f913706decc2b0cc7cdb39f508f02cf2215e26eb
Korbinian <korbiniank@outlook.de>
2024-01-04 00:23:12 +0100

    update walletconnect, update conenct button styles

--- packages/bridge-ui-v2/src/components/ConnectButton/ConnectButton.svelte
+++ packages/bridge-ui-v2/src/components/ConnectButton/ConnectButton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { getAccount } from '@wagmi/core';
   import { onDestroy, onMount } from 'svelte';
   import { t } from 'svelte-i18n';
 
@@ -8,15 +9,18 @@
   import { web3modal } from '$libs/connect';
   import { renderEthBalance } from '$libs/util/balance';
   import { noop } from '$libs/util/noop';
+  import { shortenAddress } from '$libs/util/shortenAddress';
   import { ethBalance } from '$stores/balance';
   export let connected = false;
 
   let web3modalOpen = false;
   let unsubscribeWeb3Modal = noop;
 
+  $: address = getAccount().address;
+
   function connectWallet() {
     if (web3modalOpen) return;
-    web3modal.openModal();
+    web3modal.open();
   }
 
   function onWeb3Modal(state: { open: boolean }) {
@@ -24,7 +28,7 @@
   }
 
   onMount(() => {
-    unsubscribeWeb3Modal = web3modal.subscribeModal(onWeb3Modal);
+    unsubscribeWeb3Modal = web3modal.subscribeState(onWeb3Modal);
   });
 
   onDestroy(unsubscribeWeb3Modal);
@@ -32,19 +36,28 @@
 
 {#if connected}
   <Button
-    class="hidden sm:flex  pl-[14px] pr-[20px] h-[38px] mr-[8px] rounded-full"
+    class="hidden sm:flex pl-[14px] pr-[20px] h-[38px] mr-[8px] rounded-full card dark:md:glassy-gradient-card dark:md:glass-background-gradient flex-row"
     type="neutral"
     on:click={connectWallet}>
     <span class="body-regular f-items-center">
       <svelte:component this={EthIcon} size={20} />
       {#if $ethBalance >= 0}
-        <span class="ml-[6px]">{renderEthBalance($ethBalance)}</span>
+        <span class="ml-[6px]">{renderEthBalance($ethBalance, 6)}</span>
       {:else}
-        <Spinner /> <span>Fetching balance...</span>
+        <Spinner /> <span>{$t('common.fetching_balance')}</span>
       {/if}
     </span>
+    <div
+      class="flex justify-center items-center text-center inline-flex dark:border-grey-500 border-grey-100 border-2 min-h-[30px] max-h-[30px] p-0 m-0 rounded-full px-2 mr-[-15px] space-x-[8px]">
+      {#if address}
+        <span class="text-secondary-content">{shortenAddress(address, 4, 6)}</span>
+      {/if}
+      <Icon type="chevron-down" size={16} />
+    </div>
   </Button>
-  <w3m-core-button class="h-[38px]" balance="hide" />
+  <div class="flex sm:hidden">
+    <w3m-button />
+  </div>
 {:else}
   <Button class="px-[20px] py-2 rounded-full w-[215px]" type="neutral" loading={web3modalOpen} on:click={connectWallet}>
     <span class="body-regular f-items-center space-x-2">
--- packages/bridge-ui-v2/src/libs/connect/web3modal.ts
+++ packages/bridge-ui-v2/src/libs/connect/web3modal.ts
@@ -1,57 +1,51 @@
-import { EthereumClient } from '@web3modal/ethereum';
-import { Web3Modal } from '@web3modal/html';
+import { createWeb3Modal } from '@web3modal/wagmi';
 
 import { PUBLIC_WALLETCONNECT_PROJECT_ID } from '$env/static/public';
 import { chains, getChainImages } from '$libs/chain';
 import { wagmiConfig } from '$libs/wagmi';
 
 const projectId = PUBLIC_WALLETCONNECT_PROJECT_ID;
-
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
-
 const chainImages = getChainImages();
 
-export const web3modal = new Web3Modal(
-  {
-    projectId,
-    chainImages,
-    // TODO: can we bring these vars into Tailwind theme?
-    themeVariables: {
-      '--w3m-font-family': '"Public Sans", sans-serif',
-      '--w3m-font-feature-settings': 'normal',
-      '--w3m-button-border-radius': '9999px',
+export const web3modal = createWeb3Modal({
+  wagmiConfig,
+  projectId,
+  chains,
+  chainImages,
+  themeVariables: {
+    '--w3m-font-family': '"Public Sans", sans-serif',
+    '--w3m-border-radius-master': '9999px',
+    // Body small regular
+    // @ts-ignore
+    '--w3m-text-small-regular-line-height': '20px',
 
-      // Body small regular
-      '--w3m-text-small-regular-size': '14px',
-      '--w3m-text-small-regular-weight': '400',
-      '--w3m-text-small-regular-line-height': '20px',
+    // Body regular
+    // @ts-ignore
+    '--w3m-text-medium-regular-size': '16px',
+    '--w3m-text-medium-regular-weight': '400',
+    '--w3m-text-medium-regular-line-height': '24px',
+    '--w3m-text-medium-regular-letter-spacing': 'normal',
 
-      // Body regular
-      '--w3m-text-medium-regular-size': '16px',
-      '--w3m-text-medium-regular-weight': '400',
-      '--w3m-text-medium-regular-line-height': '24px',
-      '--w3m-text-medium-regular-letter-spacing': 'normal',
+    // Title body bold
+    // @ts-ignore
+    '--w3m-text-big-bold-size': '18px',
+    '--w3m-text-big-bold-weight': '700',
+    '--w3m-text-big-bold-line-height': '24px',
 
-      // Title body bold
-      '--w3m-text-big-bold-size': '18px',
-      '--w3m-text-big-bold-weight': '700',
-      '--w3m-text-big-bold-line-height': '24px',
+    // @ts-ignore
+    '--w3m-background-color': 'var(--primary-brand)',
+    '--w3m-overlay-background-color': 'var(--overlay-background)',
+    '--w3m-background-border-radius': '20px',
+    '--w3m-container-border-radius': '0',
 
-      '--w3m-background-color': 'var(--primary-brand)',
-      '--w3m-overlay-background-color': 'var(--overlay-background)',
-      '--w3m-background-border-radius': '20px',
-      '--w3m-container-border-radius': '0',
-
-      // Unofficial variables
-      // @ts-ignore
-      '--w3m-color-fg-1': 'var(--primary-content)',
-      '--w3m-color-bg-1': 'var(--primary-background)',
-      '--w3m-color-bg-2': 'var(--neutral-background)',
-      '--w3m-color-overlay': 'none',
-      '--w3m-accent-color': 'var(--neutral-background)',
-      '--w3m-accent-fill-color': 'var(--dark-background)',
-    },
-    themeMode: (localStorage.getItem('theme') as 'dark' | 'light') ?? 'dark',
+    // // Unofficial variables
+    // // @ts-ignore
+    // '--w3m-color-fg-1': 'var(--primary-content)',
+    // '--w3m-color-bg-1': 'var(--primary-background)',
+    // '--w3m-color-bg-2': 'var(--neutral-background)',
+    // '--w3m-color-overlay': 'none',
+    // '--w3m-accent-color': 'var(--neutral-background)',
+    // '--w3m-accent-fill-color': 'var(--dark-background)',
   },
-  ethereumClient,
-);
+  themeMode: (localStorage.getItem('theme') as 'dark' | 'light') ?? 'dark',
+});
