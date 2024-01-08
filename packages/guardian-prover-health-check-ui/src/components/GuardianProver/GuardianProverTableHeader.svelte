895594b1a2eac33d5698b6bdec80872c6a21110f
Korbinian <korbiniank@outlook.de>
2024-01-08 15:21:24 +0100

    fix padding

--- packages/guardian-prover-health-check-ui/src/components/GuardianProver/GuardianProverTableHeader.svelte
+++ packages/guardian-prover-health-check-ui/src/components/GuardianProver/GuardianProverTableHeader.svelte
@@ -7,9 +7,12 @@
 
 <div class="mt-[40px]">
 	{#if isDesktopOrLarger}
-		<div class="grid grid-cols-12 py-2 pl-10 font-bold">
-			<div class="col-span-1">{$t('overview.table.status')}</div>
-			<div class="col-span-2"></div>
+		<div
+			class="grid grid-cols-12 px-[24px] py-[16px] rounded-[20px] space-x-[18px] max-h-[76px] font-bold"
+		>
+			<div class="col-span-3">
+				{$t('overview.table.status')}
+			</div>
 			<div class="col-span-6">{$t('overview.table.address')}</div>
 			<div class="col-span-2">{$t('overview.table.balance')}</div>
 			<div class="col-span-1">{$t('overview.table.no_blocks_created')}</div>
