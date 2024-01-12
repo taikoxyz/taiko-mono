fd794ef7da63675f72f510ddc9691a718f133c0a
Korbinian <korbiniank@outlook.de>
2024-01-12 09:02:01 +0100

    add cursor to logo button

--- packages/guardian-prover-health-check-ui/src/components/Header/Header.svelte
+++ packages/guardian-prover-health-check-ui/src/components/Header/Header.svelte
@@ -10,7 +10,13 @@
 </script>
 
 <section id="header" class="f-row f-center-space-between justify-end">
-	<div role="button" tabindex="0" on:click={() => goto('/')} on:keydown={() => goto('/')}>
+	<div
+		class="cursor-pointer"
+		role="button"
+		tabindex="0"
+		on:click={() => goto('/')}
+		on:keydown={() => goto('/')}
+	>
 		<img src="/taiko-logo.svg" alt="logo" class="w-[105px] h-[29px]" />
 	</div>
 
