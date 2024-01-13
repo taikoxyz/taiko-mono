5bf916d6b1bb8dadb1470092247aeb79bdad3229
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-01-13 23:57:34 +0800

    fix(protocol): disallow duplicate hooks (#15492)
    Co-authored-by: adaki2004 <keszeydani@gmail.com>
    Co-authored-by: David <david@taiko.xyz>
    Co-authored-by: D <51912515+adaki2004@users.noreply.github.com>

--- packages/protocol/contracts/L1/TaikoErrors.sol
+++ packages/protocol/contracts/L1/TaikoErrors.sol
@@ -34,6 +34,7 @@ abstract contract TaikoErrors {
     error L1_INVALID_BLOCK_ID();
     error L1_INVALID_CONFIG();
     error L1_INVALID_ETH_DEPOSIT();
+    error L1_INVALID_HOOK();
     error L1_INVALID_PARAM();
     error L1_INVALID_PAUSE_STATUS();
     error L1_INVALID_PROOF();
