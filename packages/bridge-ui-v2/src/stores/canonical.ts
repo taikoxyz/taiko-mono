77ce8074c1fcd740cd1d66aacfdbbf616fb8c0ae
Korbinian <korbiniank@outlook.de>
2024-01-21 02:03:36 +0100

    new canonical check util

--- packages/bridge-ui-v2/src/stores/canonical.ts
+++ packages/bridge-ui-v2/src/stores/canonical.ts
@@ -1,6 +1,10 @@
 import type { Address } from '@wagmi/core';
 import { get, writable } from 'svelte/store';
 
+import { getLogger } from '$libs/util/logger';
+
+const log = getLogger('token:canonical');
+
 type TokenInfo = {
   isCanonical: boolean;
   chainId: number;
@@ -9,18 +13,20 @@ type TokenInfo = {
 type CanonicalTokens = Record<Address, TokenInfo>;
 export const canonicalTokens = writable<CanonicalTokens>({});
 
-export const setCanonicalTokenInfo = (tokenAddress: Address, isCanonical: boolean, chainId: number) => {
+export const setCanonicalTokenInfoStore = (tokenAddress: Address, isCanonical: boolean, chainId: number) => {
   canonicalTokens.update((currentTokens) => {
     return { ...currentTokens, [tokenAddress]: { isCanonical, chainId } };
   });
 };
 
-export const getCanonicalStatus = (tokenAddress: Address): boolean => {
+export const getCanonicalStatusFromStore = (tokenAddress: Address): boolean => {
+  log('getting canonical token status from store', tokenAddress);
   const tokens = get(canonicalTokens);
   return tokens[tokenAddress]?.isCanonical ?? false;
 };
 
-export const getCanonicalTokenInfo = (tokenAddress: Address): TokenInfo => {
+export const getCanonicalTokenInfoStore = (tokenAddress: Address): TokenInfo => {
+  log('getting canonical token info from store', tokenAddress);
   const tokens = get(canonicalTokens);
   return tokens[tokenAddress];
 };
