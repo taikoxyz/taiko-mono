dfa46c4835c248b5ab336608780b1a8dee465612
Korbinian <korbiniank@outlook.de>
2024-01-21 02:04:06 +0100

    update approval flows

--- packages/bridge-ui-v2/src/components/Bridge/Actions.svelte
+++ packages/bridge-ui-v2/src/components/Bridge/Actions.svelte
@@ -6,7 +6,8 @@
   import { Icon } from '$components/Icon';
   import { BridgePausedError } from '$libs/error';
   import { TokenType } from '$libs/token';
-  import { getTokenApprovalStatus } from '$libs/token/getTokenApprovalStatus';
+  import { checkTokenApprovalStatus } from '$libs/token/checkTokenApprovalStatus';
+  import { getCanonicalInfoForToken } from '$libs/token/getCanonicalInfoForToken';
   import { account, network } from '$stores';
 
   import {
@@ -52,13 +53,12 @@
 
   onMount(async () => {
     $validatingAmount = true;
-    if ($selectedToken) {
-      if ($selectedTokenIsBridged) {
-        $allApproved = true;
-        $insufficientAllowance = false;
-      } else {
-        getTokenApprovalStatus($selectedToken);
-      }
+    await checkBridgedStatus();
+    if (tokenIsBridged) {
+      $allApproved = true;
+      $insufficientAllowance = false;
+    } else {
+      checkTokenApprovalStatus($selectedToken);
     }
     isValidTokenBalance();
     $validatingAmount = false;
@@ -83,7 +83,33 @@
     }
   };
 
+  const checkBridgedStatus = async () => {
+    if ($selectedToken?.type === TokenType.ETH) {
+      return true;
+    }
+    const srcChainId = $network?.id;
+    const destChainId = $destNetwork?.id;
+
+    if (!srcChainId || !destChainId || !$selectedToken) {
+      tokenIsBridged = false;
+      return;
+    }
+
+    const canonicalInfo = await getCanonicalInfoForToken({ token: $selectedToken, srcChainId, destChainId });
+    if (!canonicalInfo || canonicalInfo.chainId === srcChainId) {
+      tokenIsBridged = false;
+      return;
+    }
+
+    // override checks if the token is bridged
+    $allApproved = true;
+    $insufficientAllowance = false;
+    return true;
+  };
+
   $: isValidBalance = false;
+
+  // $: validating = $enteredAmount > 0;
 
   // Basic conditions so we can even start the bridging process
   $: hasAddress = $recipientAddress || $account?.address;
@@ -95,13 +121,21 @@
   // Conditions for approve/bridge steps
   $: if ($enteredAmount) {
     $validatingAmount = true;
+    checkBridgedStatus().then(() => {
+      if (!tokenIsBridged) checkTokenApprovalStatus($selectedToken);
+    });
+
     isValidTokenBalance();
     getTokenApprovalStatus($selectedToken);
   }
 
+  $: if ($selectedToken) checkBridgedStatus();
+
+  $: tokenIsBridged = false;
+
   // Conditions to disable/enable buttons
   $: disableApprove =
-    !$selectedTokenIsBridged &&
+    !tokenIsBridged &&
     (isERC20
       ? canDoNothing || $insufficientBalance || $validatingAmount || approving || $allApproved || !$enteredAmount
       : isERC721
@@ -152,7 +186,7 @@
   <!-- TODO: temporary enable two styles, remove for UI v2.1 -->
 
   <div class="f-between-center w-full gap-4">
-    {#if !$selectedTokenIsBridged && $selectedToken && !isETH}
+    {#if $selectedToken && !isETH && !tokenIsBridged}
       <ActionButton
         priority="primary"
         disabled={disableApprove}
@@ -186,7 +220,7 @@
   <!-- NFT actions  -->
   <!-- TODO: adopt for bridge design v2.1  -->
   <div class="f-col w-full gap-4">
-    {#if $selectedToken && !isETH && !$selectedTokenIsBridged}
+    {#if $selectedToken && !isETH && !tokenIsBridged}
       <ActionButton
         priority="primary"
         disabled={disableApprove}
