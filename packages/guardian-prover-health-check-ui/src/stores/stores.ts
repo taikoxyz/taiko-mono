aa5b302d6a00f51d6bfde3700ba553a7a389b3c2
Korbinian <korbiniank@outlook.de>
2024-01-24 23:23:33 +0100

    update data fetching, fix status

--- packages/guardian-prover-health-check-ui/src/stores/stores.ts
+++ packages/guardian-prover-health-check-ui/src/stores/stores.ts
@@ -5,7 +5,7 @@ import {
 	type GuardianProverIdsMap,
 	PageTabs
 } from '$lib/types';
-import { type Writable, writable } from 'svelte/store';
+import { type Writable, writable, derived } from 'svelte/store';
 
 // Healtchecks
 export const apiResponse: Writable<HealthCheck[]> = writable([]);
@@ -24,3 +24,31 @@ export const signerPerBlock: Writable<GuardianProverIdsMap> = writable({});
 export const loading: Writable<boolean> = writable(false);
 export const selectedTab = writable<PageTabs>(PageTabs.GUARDIAN_PROVER);
 export const selectedGuardianProver = writable<Guardian>();
+
+interface StatusCounts {
+	dead: number;
+	alive: number;
+	unhealthy: number;
+}
+
+export const guardianStatusCounts = derived(guardianProvers, ($guardianProvers): StatusCounts => {
+	if (!$guardianProvers) return { dead: 0, alive: 0, unhealthy: 0 };
+
+	return $guardianProvers.reduce(
+		(acc, guardian) => {
+			switch (guardian.alive) {
+				case 0: // DEAD
+					acc.dead += 1;
+					break;
+				case 1: // ALIVE
+					acc.alive += 1;
+					break;
+				case 2: // UNHEALTHY
+					acc.unhealthy += 1;
+					break;
+			}
+			return acc;
+		},
+		{ dead: 0, alive: 0, unhealthy: 0 }
+	);
+});
