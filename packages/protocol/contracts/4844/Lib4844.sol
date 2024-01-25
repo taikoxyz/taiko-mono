1e0b23e80a43db4376360eea2652d006fd0f0ff0
Daniel Wang <dan@taiko.xyz>
2024-01-25 21:32:52 +0800

    check POINT_EVALUATION_PRECOMPILE_ADDRESS.staticcall return value

--- packages/protocol/contracts/4844/Lib4844.sol
+++ packages/protocol/contracts/4844/Lib4844.sol
@@ -19,11 +19,12 @@ pragma solidity 0.8.20;
 /// `solc contracts/libs/Lib4844.sol --ir > contracts/libs/Lib4844.yul`
 library Lib4844 {
     address public constant POINT_EVALUATION_PRECOMPILE_ADDRESS = address(0x0A);
-    uint32 public constant FIELD_ELEMENTS_PERBLOB = 4096;
+    uint32 public constant FIELD_ELEMENTS_PER_BLOB = 4096;
     uint256 public constant BLS_MODULUS =
         52_435_875_175_126_190_479_447_740_508_185_965_837_690_552_500_527_637_822_603_658_699_938_581_184_513;
 
-    error EVAL_FAILED();
+    error EVAL_FAILED_1();
+    error EVAL_FAILED_2();
     error POINT_X_TOO_LARGE();
     error POINT_Y_TOO_LARGE();
 
@@ -46,9 +47,20 @@ library Lib4844 {
         if (x >= BLS_MODULUS) revert POINT_X_TOO_LARGE();
         if (y >= BLS_MODULUS) revert POINT_Y_TOO_LARGE();
 
-        (bool ok,) = POINT_EVALUATION_PRECOMPILE_ADDRESS.staticcall(
+        (bool ok, bytes memory ret) = POINT_EVALUATION_PRECOMPILE_ADDRESS.staticcall(
             abi.encodePacked(blobHash, x, y, commitment, pointProof)
         );
-        if (!ok) revert EVAL_FAILED();
+
+        if (!ok) revert EVAL_FAILED_1();
+
+        bytes32 first;
+        bytes32 second;
+        assembly {
+            first := mload(add(ret, 32))
+            second := mload(add(ret, 64))
+        }
+        if (uint256(first) != FIELD_ELEMENTS_PER_BLOB || uint256(second) != BLS_MODULUS) {
+            revert EVAL_FAILED_2();
+        }
     }
 }
