2a73ef66efc2844fbab68c7eff5bc9c1ee69ba40
Daniel Wang <dan@taiko.xyz>
2024-01-28 11:53:48 +0800

    Update LibBytesUtils.sol

--- packages/protocol/contracts/thirdparty/LibBytesUtils.sol
+++ packages/protocol/contracts/thirdparty/LibBytesUtils.sol
@@ -112,19 +112,7 @@ library LibBytesUtils {
     }
 
     function toBytes32(bytes memory _bytes) internal pure returns (bytes32) {
-        if (_bytes.length == 0) {
-            return 0;
-        } else if (_bytes.length < 32) {
-            bytes32 ret;
-            assembly {
-                ret := mload(add(_bytes, 32))
-            }
-
-            uint256 bitsToClear = (32 - _bytes.length) << 3;
-            return (ret >> bitsToClear) << bitsToClear;
-        }
-
-        return abi.decode(_bytes, (bytes32)); // will truncate if input length >32 bytes
+        return _bytes.length < 32 ? bytes32(_bytes) : abi.decode(_bytes, (bytes32));
     }
 
     function toNibbles(bytes memory _bytes) internal pure returns (bytes memory) {
