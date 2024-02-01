8aa18e4fda1f943cdb2a3629cfb10907c400605b
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-02-01 09:15:11 +0800

    Update OwnerUUPSUpgradable.sol

--- packages/protocol/contracts/common/OwnerUUPSUpgradable.sol
+++ packages/protocol/contracts/common/OwnerUUPSUpgradable.sol
@@ -73,6 +73,9 @@ abstract contract OwnerUUPSUpgradable is UUPSUpgradeable, OwnableUpgradeable {
         return _paused == _TRUE;
     }
 
+    function _authorizeUpgrade(address) internal virtual override onlyOwner { }
+    function _authorizePause(address) internal virtual onlyOwner { }
+
     /// @notice Initializes the contract with an address manager.
     // solhint-disable-next-line func-name-mixedcase
     function __OwnerUUPSUpgradable_init() internal virtual {
@@ -84,7 +87,4 @@ abstract contract OwnerUUPSUpgradable is UUPSUpgradeable, OwnableUpgradeable {
     function _inNonReentrant() internal view returns (bool) {
         return _reentry == _TRUE;
     }
-
-    function _authorizeUpgrade(address) internal view virtual override onlyOwner { }
-    function _authorizePause(address) internal view virtual onlyOwner { }
 }
