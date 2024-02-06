48809f83d0e1f8d74462f4da39ee6372c8e56667
Korbinian <korbiniank@outlook.de>
2024-02-06 10:50:21 +0100

    dynamic ipfs gateways

--- packages/bridge-ui-v2/src/libs/util/resolveIPFSUri.ts
+++ packages/bridge-ui-v2/src/libs/util/resolveIPFSUri.ts
@@ -1,17 +1,20 @@
 import axios, { type AxiosRequestConfig } from 'axios';
 
-const REQUEST_TIMEOUT_IN_MS = 200;
-const MAX_RETRY_DURATION_IN_MS = 1000;
-const GATEWAYS = ['https://ipfs.io/ipfs/', 'https://gateway.pinata.cloud/ipfs/'];
+import { ipfsConfig } from '$config';
+import { PUBLIC_IPFS_GATEWAYS } from '$env/static/public';
+import { ConfigError, IpfsError } from '$libs/error';
+
+const gateways = PUBLIC_IPFS_GATEWAYS.split(',') || [];
 
 const axiosConfig: AxiosRequestConfig = {
-  timeout: REQUEST_TIMEOUT_IN_MS,
+  timeout: ipfsConfig.gatewayTimeout,
 };
 
 export async function resolveIPFSUri(uri: string): Promise<string> {
   const cid = uri.replace('ipfs://', '');
   let elapsedTime = 0;
-  for (const gateway of GATEWAYS) {
+  if (gateways.length === 0) throw new ConfigError('No IPFS gateways configured');
+  for (const gateway of gateways) {
     const start = Date.now();
     try {
       const url = `${gateway}${cid}`;
@@ -19,10 +22,10 @@ export async function resolveIPFSUri(uri: string): Promise<string> {
       return url; // Return the first successful gateway URL
     } catch (error) {
       elapsedTime += Date.now() - start;
-      if (elapsedTime > MAX_RETRY_DURATION_IN_MS) {
+      if (elapsedTime > ipfsConfig.overallTimeout) {
         break;
       }
     }
   }
-  throw new Error('Failed to retrieve metadata from IPFS gateways');
+  throw new IpfsError('Failed to retrieve metadata from IPFS gateways');
 }
