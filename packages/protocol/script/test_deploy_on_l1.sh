4eada50dc6f47c15fc73a9feb33bf6b14d73942a
Daniel Wang <dan@taiko.xyz>
2024-02-15 15:37:54 +0800

    Update test_deploy_on_l1.sh

--- packages/protocol/script/test_deploy_on_l1.sh
+++ packages/protocol/script/test_deploy_on_l1.sh
@@ -22,5 +22,6 @@ forge script script/DeployOnL1.s.sol:DeployOnL1 \
     --broadcast \
     --ffi \
     -vvvv \
+    --evm-version cancun \
     --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
     --block-gas-limit 100000000
