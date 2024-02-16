fdd5671513970382f261745a2474679592ccaba6
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-16 15:01:49 -0500

    chore(repo): delete unused packages

--- packages/starter-dapp/src/store/modal.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-
-export const isSwitchEthereumChainModalOpen = writable<boolean>();
-export const isConnectWalletModalOpen = writable<boolean>();
--- packages/status-page/src/utils/layerToDisplayName.ts
+++
@@ -1,4 +0,0 @@
-import { Layer } from "../domain/layer";
-
-export const layerToDisplayName = (layer: Layer) =>
-  layer === Layer.Two ? "Taiko L2" : "Taiko L3";
