eef382750f52daeaa99caf0138493ec0207c2728
Daniel Wang <dan@taiko.xyz>
2024-02-21 17:05:12 +0800

    getInvocationDelays

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -475,6 +475,7 @@ contract Bridge is EssentialContract, IBridge {
                 || block.chainid == 42 // Kovan
                 || block.chainid == 17_000 // Holesky
                 || block.chainid == 11_155_111 // Sepolia
+                || block.chainid == 32_382 // Chain ID for all Taiko devnet L1
         ) {
             return (30 minutes, 384 seconds);
         } else {
