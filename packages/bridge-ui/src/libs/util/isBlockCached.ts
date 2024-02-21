e5db598fcfba34aa79f5d5ef8a2fa4d8e04f474c
Korbinian <korbiniank@outlook.de>
2024-02-21 12:56:47 +0100

    isblockcached util

--- packages/bridge-ui/src/libs/util/isBlockCached.ts
+++ packages/bridge-ui/src/libs/util/isBlockCached.ts
@@ -1,6 +1,7 @@
-import { getStorageAt } from '@wagmi/core';
-import { type Address, encodePacked, type Hex,keccak256, toBytes, toHex } from 'viem';
+import { readContract } from '@wagmi/core';
+import { keccak256, toBytes } from 'viem';
 
+import { signalServiceAbi } from '$abi';
 import { routingContractsMap } from '$bridgeConfig';
 import { config } from '$libs/wagmi';
 
@@ -15,28 +16,42 @@ export const isBlockCached = async ({
   destChainId: number;
   blockNumber: number;
 }) => {
-  //TODO: query relayer, e.g. hasRelayerJustSentCachingTxnForThisBlock()
+  const signalServiceAddress = routingContractsMap[srcChainId][destChainId].signalServiceAddress;
 
-  const signalServiceAddress = routingContractsMap[destChainId][srcChainId].signalServiceAddress;
-  const signal = getSignalForChainData(destChainId, blockNumber);
-  const slot = getSignalSlot(destChainId, signalServiceAddress, signal);
+  try {
+    const latestCachedBlock = await readContract(config, {
+      address: signalServiceAddress,
+      abi: signalServiceAbi,
+      functionName: 'getSyncedChainData',
+      args: [BigInt(destChainId), SIGNAL_ROOT, 0n],
+    });
 
-  const signalRootValue = (await getStorageAt(config, {
-    address: signalServiceAddress,
-    chainId: destChainId,
-    slot,
-  })) as Hex;
-
-  if (signalRootValue && signalRootValue !== toHex(0, { size: 32 })) {
-    return true;
+    return latestCachedBlock[0] >= blockNumber;
+  } catch (error) {
+    console.error('Error checking if block is cached', error);
   }
   return false;
 };
 
-const getSignalForChainData = (chainId: number, blockId: number) => {
-  return keccak256(encodePacked(['uint64', 'bytes32', 'uint64'], [BigInt(chainId), SIGNAL_ROOT, BigInt(blockId)]));
-};
+//   const signal = getSignalForChainData(destChainId, blockNumber);
+//   const slot = getSignalSlot(destChainId, signalServiceAddress, signal);
 
-const getSignalSlot = (chainId: number, app: Address, signal: Hex) => {
-  return keccak256(encodePacked(['string', 'uint64', 'address', 'bytes32'], ['SIGNAL', BigInt(chainId), app, signal]));
-};
+//   const signalRootValue = (await getStorageAt(config, {
+//     address: signalServiceAddress,
+//     chainId: destChainId,
+//     slot,
+//   })) as Hex;
+
+//   if (signalRootValue && signalRootValue !== toHex(0, { size: 32 })) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// const getSignalForChainData = (chainId: number, blockId: number) => {
+//   return keccak256(encodePacked(['uint64', 'bytes32', 'uint64'], [BigInt(chainId), SIGNAL_ROOT, BigInt(blockId)]));
+// };
+
+// const getSignalSlot = (chainId: number, app: Address, signal: Hex) => {
+//   return keccak256(encodePacked(['string', 'uint64', 'address', 'bytes32'], ['SIGNAL', BigInt(chainId), app, signal]));
+// };
