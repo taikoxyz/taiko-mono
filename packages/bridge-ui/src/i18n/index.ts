1be13e0b3ad27e2a64a73591b0b454501506e6a3
DonaldBear <chunkiujohnson@gmail.com>
2024-02-22 14:54:31 +0800

    Add Traditional Chinese in bridge-ui

--- packages/bridge-ui/src/i18n/index.ts
+++ packages/bridge-ui/src/i18n/index.ts
@@ -1,9 +1,11 @@
 import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n';
 
 import en from './en.json';
+import zhHK from './zh-hk.json';
 // TODO: import other languages here...
 
 addMessages('en', en);
+addMessages('zh-HK', zhHK);
 // TODO: add other languages here...
 
 init({
