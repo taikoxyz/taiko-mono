d4a19378c9a061c7c8cdb880e9ebfc3bb91dfecf
Jeffery Walsh <cyberhorsey@gmail.com>
2024-02-22 11:30:47 -0800

    remove bridge-ui-v2

--- packages/bridge-ui-v2/.svelte-kit/generated/client/nodes/0.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/+layout.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/+layout.svelte";
--- packages/bridge-ui-v2/.svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../../../node_modules/.pnpm/@sveltejs+kit@1.22.3_svelte@4.1.0_vite@4.4.9/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
