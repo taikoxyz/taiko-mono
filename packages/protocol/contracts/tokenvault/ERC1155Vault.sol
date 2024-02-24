005cb9b2340513d40d40e3bec2108f666223a46c
Daniel Wang <dan@taiko.xyz>
2024-02-24 10:53:15 +0800

    chore(protocol): use safeBatchTransferFrom in ERC1155Vault

--- packages/protocol/contracts/tokenvault/ERC1155Vault.sol
+++ packages/protocol/contracts/tokenvault/ERC1155Vault.sol
@@ -218,15 +218,7 @@ contract ERC1155Vault is BaseNFTVault, ERC1155ReceiverUpgradeable {
         if (ctoken.chainId == block.chainid) {
             // Token lives on this chain
             token = ctoken.addr;
-            for (uint256 i; i < tokenIds.length; ++i) {
-                ERC1155(token).safeTransferFrom({
-                    from: address(this),
-                    to: to,
-                    id: tokenIds[i],
-                    amount: amounts[i],
-                    data: ""
-                });
-            }
+            ERC1155(token).safeBatchTransferFrom(address(this), to, tokenIds, amounts, "");
         } else {
             // Token does not live on this chain
             token = _getOrDeployBridgedToken(ctoken);
