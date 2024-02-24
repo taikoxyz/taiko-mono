81094642c24b6c7002b6981229feb0f19c61de44
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-02-24 11:34:36 +0800

    Update ERC20Vault.t.sol

--- packages/protocol/test/tokenvault/ERC20Vault.t.sol
+++ packages/protocol/test/tokenvault/ERC20Vault.t.sol
@@ -153,8 +153,6 @@ contract TestERC20Vault is TaikoTest {
 
         addressManager.setAddress(destChainId, "signal_service", address(mockProofSignalService));
 
-        // addressManager.setAddress(uint64(block.chainid), "bridge", address(bridge));
-
         addressManager.setAddress(uint64(block.chainid), "erc20_vault", address(erc20Vault));
 
         addressManager.setAddress(destChainId, "erc20_vault", address(destChainIdERC20Vault));
