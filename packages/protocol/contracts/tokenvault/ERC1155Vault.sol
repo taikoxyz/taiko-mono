485e3439d2fd5e0fb7f09618e2c546e224022377
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-28 16:15:23 -0500

    cleanup header and lock solc version

--- packages/protocol/contracts/tokenvault/ERC1155Vault.sol
+++ packages/protocol/contracts/tokenvault/ERC1155Vault.sol
@@ -1,17 +1,4 @@
 // SPDX-License-Identifier: MIT
-//  _____     _ _         _         _
-// |_   _|_ _(_) |_____  | |   __ _| |__ ___
-//   | |/ _` | | / / _ \ | |__/ _` | '_ (_-<
-//   |_|\__,_|_|_\_\___/ |____\__,_|_.__/__/
-//
-//   Email: security@taiko.xyz
-//   Website: https://taiko.xyz
-//   GitHub: https://github.com/taikoxyz
-//   Discord: https://discord.gg/taikoxyz
-//   Twitter: https://twitter.com/taikoxyz
-//   Blog: https://mirror.xyz/labs.taiko.eth
-//   Youtube: https://www.youtube.com/@taikoxyz
-
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
