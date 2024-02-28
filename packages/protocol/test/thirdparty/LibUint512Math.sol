22b80259ad169b56348292de661164813d876229
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-28 18:16:28 -0500

    update

--- packages/protocol/test/thirdparty/LibUint512Math.sol
+++ packages/protocol/test/thirdparty/LibUint512Math.sol
@@ -2,7 +2,7 @@
 // The MIT License (MIT)
 //
 // Copyright (c) 2021 Remco Bloemen
-// Copyright (c) 2022-2023 Taiko Labs
+// Copyright (c) 2024 Taiko Labs
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
