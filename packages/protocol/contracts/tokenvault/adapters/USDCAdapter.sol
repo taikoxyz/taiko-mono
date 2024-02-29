fedc4a03702b5e2b4259a27af7b3ac69f7735f4a
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-02-29 02:35:22 -0500

    more

--- packages/protocol/contracts/tokenvault/adapters/USDCAdapter.sol
+++ packages/protocol/contracts/tokenvault/adapters/USDCAdapter.sol
@@ -6,17 +6,35 @@ import "../BridgedERC20Base.sol";
 /// @title IUSDC
 /// @custom:security-contact security@taiko.xyz
 interface IUSDC {
+    /// @notice Burns a specific amount of tokens.
+    /// @param amount The amount of token to be burned.
     function burn(uint256 amount) external;
+
+    /// @notice Mints a specific amount of new tokens to an address.
+    /// @param to The address that will receive the minted tokens.
+    /// @param amount The amount of tokens to mint.
     function mint(address to, uint256 amount) external;
+
+    /// @notice Transfers tokens from one address to another.
+    /// @param from The address which you want to send tokens from.
+    /// @param to The address which you want to transfer to.
+    /// @param value The amount of tokens to be transferred.
+    /// @return True if the transfer was successful, otherwise false.
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
 /// @title USDCAdapter
 /// @custom:security-contact security@taiko.xyz
 contract USDCAdapter is BridgedERC20Base {
-    IUSDC public usdc; // slot 1
+    /// @notice The USDC instance.
+    /// @dev Slot 1.
+    IUSDC public usdc;
     uint256[49] private __gap;
 
+    /// @notice Initializes the contract.
+    /// @param _owner The owner of this contract.
+    /// @param _adressManager The address of the {AddressManager} contract.
+    /// @param _usdc The USDC instance.
     function init(address _owner, address _adressManager, IUSDC _usdc) external initializer {
         __Essential_init(_owner, _adressManager);
         usdc = _usdc;
