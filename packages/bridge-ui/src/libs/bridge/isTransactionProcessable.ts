52c4f813b08d9103fdac839b4020c96382336fc4
Korbinian <korbiniank@outlook.de>
2024-03-01 22:36:49 +0100

    update processable check

--- packages/bridge-ui/src/libs/bridge/isTransactionProcessable.ts
+++ packages/bridge-ui/src/libs/bridge/isTransactionProcessable.ts
@@ -1,15 +1,10 @@
-import { readContract } from '@wagmi/core';
-import { keccak256, toBytes } from 'viem';
-
-import { signalServiceAbi } from '$abi';
-import { routingContractsMap } from '$bridgeConfig';
 import { chains } from '$libs/chain';
-import { config } from '$libs/wagmi';
+import { getFirstAvailableBlockInfo } from '$libs/relayer/getFirstAvailableBlockInfo';
 
 import { type BridgeTransaction, MessageStatus } from './types';
 
 export async function isTransactionProcessable(bridgeTx: BridgeTransaction) {
-  const { receipt, message, srcChainId, destChainId, status } = bridgeTx;
+  const { receipt, message, srcChainId, status } = bridgeTx;
 
   // Without these guys there is no way we can process this
   // bridge transaction. The receipt is needed in order to compare
@@ -18,23 +13,12 @@ export async function isTransactionProcessable(bridgeTx: BridgeTransaction) {
 
   if (status !== MessageStatus.NEW) return true;
 
-  const destCrossChainSyncAddress = routingContractsMap[Number(destChainId)][Number(srcChainId)].crossChainSyncAddress;
   const srcChain = chains.find((chain) => chain.id === Number(srcChainId));
 
   if (!srcChain) return false;
 
-  try {
-    const syncedChainData = await readContract(config, {
-      address: destCrossChainSyncAddress,
-      abi: signalServiceAbi,
-      functionName: 'getSyncedChainData',
-      args: [destChainId, keccak256(toBytes('STATE_ROOT')), 0n],
-      chainId: Number(srcChainId),
-    });
-
-    const blockHeight = syncedChainData[0];
-    return blockHeight !== null && receipt.blockNumber <= blockHeight;
-  } catch (error) {
-    return false;
-  }
+  const syncedInfo = await getFirstAvailableBlockInfo(Number(srcChainId));
+  if (!syncedInfo) return false;
+  const { latestBlock } = syncedInfo;
+  return latestBlock !== null && receipt.blockNumber <= latestBlock;
 }
