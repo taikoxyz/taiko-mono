eb81b65e21d0a6f58b3d8922598a63690fc0f380
David <david@taiko.xyz>
2024-03-01 10:20:43 +0800

    chore: ci

--- packages/protocol/contracts/L1/TaikoData.sol
+++ packages/protocol/contracts/L1/TaikoData.sol
@@ -17,7 +17,7 @@ library TaikoData {
         // Group 2: Block level configs
         // ---------------------------------------------------------------------
         // The maximum number of proposals allowed in a single block.
-        uint64 blockMaxProposals;
+        // uint64 blockMaxProposals;
         // Size of the block ring buffer, allowing extra space for proposals.
         uint64 blockRingBufferSize;
         // The maximum number of verifications allowed when a block is proposed.
