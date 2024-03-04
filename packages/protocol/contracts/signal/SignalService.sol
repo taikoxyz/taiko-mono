6134470ff06cc5a4907a3f4a5c1ae20b9eb5d1a0
Daniel Wang <dan@taiko.xyz>
2024-03-04 11:12:13 +0800

    Update SignalService.sol

--- packages/protocol/contracts/signal/SignalService.sol
+++ packages/protocol/contracts/signal/SignalService.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "../common/EssentialContract.sol";
 import "../libs/LibTrieProof.sol";
 import "./ISignalService.sol";
