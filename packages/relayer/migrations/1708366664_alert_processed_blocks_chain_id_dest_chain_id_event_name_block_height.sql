00f5c4ea361b019d6c3ace9cceef19717e4c70e3
xiaodino <ruby@taiko.xyz>
2024-03-05 22:24:14 -0800

    Update

---
+++ packages/relayer/migrations/1708366664_alert_processed_blocks_chain_id_dest_chain_id_event_name_block_height.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE `events` ADD INDEX `idx_processed_blocks_on_chain_dest_chain_event_block` (`chain_id`, `dest_chain_id`, `event_name`, `block_height`);
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX idx_processed_blocks_on_chain_dest_chain_event_block on events;
+-- +goose StatementEnd
