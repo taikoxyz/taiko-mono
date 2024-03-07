54cdf3304d9cb58ca044f546f1600aed07e846bf
xiaodino <ruby@taiko.xyz>
2024-03-06 21:44:36 -0800

    Update

--- packages/relayer/migrations/1708366663_alert_events_name_chain_id_synced_chain_id_block_id_index.sql
+++ packages/relayer/migrations/1708366663_alert_events_name_chain_id_synced_chain_id_block_id_index.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE `events` ADD INDEX `events_name_chain_id_synced_chain_id_block_id_index` (`name`, `chain_id`, `synced_chain_id`, `block_id`);
+ALTER TABLE `events` ADD INDEX `idx_ev_name_cid_scid_bid` (`name`, `chain_id`, `synced_chain_id`, `block_id`);
 
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX events_name_chain_id_synced_chain_id_block_id_index on events;
+DROP INDEX idx_ev_name_cid_scid_bid on events;
 -- +goose StatementEnd
