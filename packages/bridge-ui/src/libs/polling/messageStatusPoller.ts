370458ab41158b8dd334244edde455f019b64c4d
Korbinian <korbiniank@outlook.de>
2024-03-18 00:55:00 +0100

    add proof receipt and delay to message poller

--- packages/bridge-ui/src/libs/polling/messageStatusPoller.ts
+++ packages/bridge-ui/src/libs/polling/messageStatusPoller.ts
@@ -4,19 +4,22 @@ import { createPublicClient, getContract, type Hash, http } from 'viem';
 import { bridgeAbi } from '$abi';
 import { routingContractsMap } from '$bridgeConfig';
 import { bridgeTransactionPoller } from '$config';
+import { getInvocationDelayForTx } from '$libs/bridge/getInvocationDelayForTx';
+import { getProofReceiptForMsgHash } from '$libs/bridge/getProofReceiptForMsgHash';
 import { chains } from '$libs/chain';
-import { BridgeTxPollingError } from '$libs/error';
+import { BridgeTxPollingError, NoDelaysForBridgeError } from '$libs/error';
 import { getLogger } from '$libs/util/logger';
 import { nextTick } from '$libs/util/nextTick';
 
-import { isTransactionProcessable } from './isTransactionProcessable';
-import { type BridgeTransaction, MessageStatus } from './types';
+import { isTransactionProcessable } from '../bridge/isTransactionProcessable';
+import { type BridgeTransaction, type GetProofReceiptResponse, MessageStatus } from '../bridge/types';
 
 const log = getLogger('bridge:messageStatusPoller');
 
 export enum PollingEvent {
   STOP = 'stop',
   STATUS = 'status', // emits MessageStatus
+  DELAY = 'remainingDelayInSeconds', // emits remaining claim delay in seconds
 
   // Whether or not the tx can be clamied/retried/released
   PROCESSABLE = 'processable',
@@ -31,6 +34,12 @@ const hashEmitterMap: Record<Hash, EventEmitter> = {};
 // bridgeTx hash => interval. There might be a polling ongoing
 // associated to this hash, so we don't want to start another one
 const hashIntervalMap: Record<Hash, Interval> = {};
+
+// Keep track of the last delay we fetched for a transaction, so we can stop polling if it's 0
+const hashDelayMap: Record<Hash, bigint> = {};
+
+// bridgeTx hash => proof receipt. We want to keep track of the proof receipt
+const hashProofReceiptMap: Record<Hash, GetProofReceiptResponse> = {};
 
 /**
  * @example
@@ -78,15 +87,16 @@ export function startPolling(bridgeTx: BridgeTransaction, runImmediately = false
   });
 
   const stopPolling = () => {
+    const interval = hashIntervalMap[hash];
     if (interval) {
       log('Stop polling for transaction', bridgeTx);
 
       // Clean up
-      clearInterval(interval);
+      clearInterval(interval as ReturnType<typeof setInterval>); // clearInterval only needs the ID
       delete hashEmitterMap[hash];
       delete hashIntervalMap[hash];
+      hashIntervalMap[hash] = null;
 
-      interval = null;
       emitter.emit(PollingEvent.STOP);
     }
   };
@@ -97,26 +107,62 @@ export function startPolling(bridgeTx: BridgeTransaction, runImmediately = false
   };
 
   const pollingFn = async () => {
+    log('Polling for transaction', bridgeTx.hash);
     const isProcessable = await isTransactionProcessable(bridgeTx);
-
     emitter.emit(PollingEvent.PROCESSABLE, isProcessable);
 
     try {
-      // We want to poll for status changes
-      const messageStatus: MessageStatus = await destBridgeContract.read.messageStatus([msgHash]);
+      const messageStatus: MessageStatus = await destBridgeContract.read.messageStatus([bridgeTx.msgHash]);
+      emitter.emit(PollingEvent.STATUS, messageStatus);
 
-      emitter.emit('status', messageStatus);
+      // 1. check if we already have a proof receipt for this message hash
+      // 2. if not, fetch it and store it
+      // 3. if the stored proof receipt is not 0, get the delays and store them
+      // 4. emit the remaining delay
+      // 5. if the delay is 0, stop polling
+
+      let proofReceipts = null;
+      if (hashProofReceiptMap[hash] === undefined || hashProofReceiptMap[hash] === null) {
+        proofReceipts = await getProofReceiptForMsgHash({
+          msgHash,
+          destChainId,
+          srcChainId,
+        });
+      }
+      if (proofReceipts && proofReceipts.length > 0 && proofReceipts[0] !== 0n) {
+        hashProofReceiptMap[hash] = proofReceipts;
+        log('Proof receipt found for message hash, checking delays', msgHash);
+        if (hashDelayMap[hash] > 0n || hashDelayMap[hash] === null || hashDelayMap[hash] === undefined) {
+          try {
+            const txDelays = await getInvocationDelayForTx(bridgeTx);
+            const remainingDelayInSeconds = txDelays.preferredDelay;
+            log('Remaining delay in seconds', remainingDelayInSeconds);
+            hashDelayMap[hash] = remainingDelayInSeconds;
+            emitter.emit(PollingEvent.DELAY, remainingDelayInSeconds);
+
+            if (remainingDelayInSeconds === 0n) {
+              log(`Delay for hash ${hash} is 0, will not fetch again.`);
+            }
+          } catch (err) {
+            if (err instanceof NoDelaysForBridgeError) {
+              log('Destination chain does not have delays, not fetching again.');
+              hashDelayMap[hash] = 0n;
+            } else {
+              throw err;
+            }
+          }
+        }
+      } else if (hashDelayMap[hash] <= 0n) {
+        log(`Delay for hash ${hash} is already 0 or negative. Skipping.`);
+      }
 
       if (messageStatus === MessageStatus.DONE) {
-        log('Poller has picked up the change of status to DONE');
+        log(`Poller has picked up the change of status to DONE for hash ${hash}.`);
         stopPolling();
       }
     } catch (err) {
       console.error(err);
-
       stopPolling();
-
-      // 😱 UI should handle this error
       throw new BridgeTxPollingError('something bad happened while polling for status', { cause: err });
     }
   };
