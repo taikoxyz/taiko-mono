badfda9e8fb08b42f6d1bda96bd84999ca5aa615
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-04-01 19:16:48 -0400

    fixup

--- packages/bridge-ui/src/libs/util/extractIPFSCidFromUrl.test.ts
+++ packages/bridge-ui/src/libs/util/extractIPFSCidFromUrl.test.ts
@@ -4,7 +4,7 @@ describe('extractIPFSCidFromUrl', () => {
   test('should return the correct CID when a valid IPFS URL is provided', () => {
     const url = 'https://ipfs.io/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme';
     const result = extractIPFSCidFromUrl(url);
-    expect(result).toEqual({ cid: 'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHadWEz79ojWnPbdG' });
+    expect(result).toEqual({ cid: 'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG' });
   });
 
   test('should return null when an invalid IPFS URL is provided', () => {
