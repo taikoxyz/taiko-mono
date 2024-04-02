5009237a670e7f8f629029096790445fae5dc64b
Daniel Wang <dan@taiko.xyz>
2024-04-02 23:35:43 +0800

    Update LibAddress.sol

--- packages/protocol/contracts/libs/LibAddress.sol
+++ packages/protocol/contracts/libs/LibAddress.sol
@@ -35,7 +35,13 @@ library LibAddress {
         // Check for zero-address transactions
         if (_to == address(0)) revert ETH_TRANSFER_FAILED();
 
-        if (((gasleft() * 63) >> 6) < _gasLimit) revert NOT_ENOUGH_GASLEFT();
+        uint256 gasLimit;
+        if (_gasLimit == 0) {
+            gasLimit = gasleft();
+        } else {
+            if (((gasleft() * 63) >> 6) < _gasLimit) revert NOT_ENOUGH_GASLEFT();
+            gasLimit = _gasLimit;
+        }
         // dispatch message to recipient
         // by assembly calling "handle" function
         // we call via assembly to avoid memcopying a very large returndata
@@ -43,7 +49,7 @@ library LibAddress {
         assembly {
             success_ :=
                 call(
-                    _gasLimit, // gas
+                    gasLimit, // gas
                     _to, // recipient
                     _amount, // ether value
                     add(_calldata, 0x20), // inloc
@@ -57,7 +63,8 @@ library LibAddress {
     /// @dev Sends Ether to the specified address. This method will revert if sending ether fails.
     /// @param _to The recipient address.
     /// @param _amount The amount of Ether to send in wei.
-    /// @param _gasLimit The max amount gas to pay for this transaction.
+    /// @param _gasLimit The max amount gas to pay for this transaction. Use 0 to indicate all gas
+    /// left shall be used.
     function sendEtherAndVerify(address _to, uint256 _amount, uint256 _gasLimit) internal {
         if (_amount == 0) return;
         if (!sendEther(_to, _amount, _gasLimit, "")) {
@@ -69,7 +76,7 @@ library LibAddress {
     /// @param _to The recipient address.
     /// @param _amount The amount of Ether to send in wei.
     function sendEtherAndVerify(address _to, uint256 _amount) internal {
-        sendEtherAndVerify(_to, _amount, gasleft());
+        sendEtherAndVerify(_to, _amount, 0);
     }
 
     function supportsInterface(
