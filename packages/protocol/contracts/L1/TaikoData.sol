e61efa6e88cdb5c8ac32d98743f23d8df0a2b662
Daniel Wang <dan@taiko.xyz>
2024-04-04 14:13:45 +0800

    Update TaikoData.sol

--- packages/protocol/contracts/L1/TaikoData.sol
+++ packages/protocol/contracts/L1/TaikoData.sol
@@ -127,7 +127,7 @@ library TaikoData {
         uint96 livenessBond;
         uint64 blockId; // slot 3
         uint64 proposedAt; // timestamp
-        uint64 proposedIn; // L1 block number
+        uint64 proposedIn; // L1 block number, required/used by node/client.
         uint32 nextTransitionId;
         uint32 verifiedTransitionId;
     }
