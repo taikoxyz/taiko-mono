8f8af774799e36eeaefef5d64f5c8818c5597f59
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-04-05 11:52:50 +0200

    typo

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -353,7 +353,7 @@ contract Bridge is EssentialContract, IBridge {
         }
 
         // We check _message.gasLimit > gasleft() to make sure we not only need to bridge invocation
-        // call to succeeed, we also need it to succeed with a gas limit no smaller than the
+        // call to succeed, we also need it to succeed with a gas limit no smaller than the
         // message's gasLimit.
         if (
             (
