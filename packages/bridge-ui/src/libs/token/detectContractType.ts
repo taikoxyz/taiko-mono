cc2c73c43ca416f00b5f7fcdc8bb67aaa76c0178
ricecodekhmer <151774852+ricecodekhmer@users.noreply.github.com>
2024-04-06 20:27:33 +0100

    fix contract type detection

--- packages/bridge-ui/src/libs/token/detectContractType.ts
+++ packages/bridge-ui/src/libs/token/detectContractType.ts
@@ -13,34 +13,43 @@ const log = getLogger('detectContractType');
 
 async function isERC721(address: Address, chainId: number): Promise<boolean> {
   try {
-    await readContract(config, {
+    const isERC721 = await readContract(config, {
       address,
       abi: erc721Abi,
-      functionName: 'ownerOf',
-      args: [0n],
+      functionName: 'supportsInterface',
+      args: ['0x80ac58cd'], // Identifier for ERC-721
       chainId,
     });
-    return true;
+
+    if (isERC721) {
+      return true;
+    }
   } catch (err) {
     // we expect this error to be thrown if the token is a ERC721 and the tokenId is invalid
     return (err as Error)?.message?.includes('ERC721: invalid token ID') ?? false;
   }
+
+  return false;
 }
 // return err instanceof ContractFunctionExecutionError &&
 //   err.cause.message.includes('ERC721: invalid token ID');
 async function isERC1155(address: Address, chainId: number): Promise<boolean> {
   try {
-    await readContract(config, {
+    const isERC1155 = await readContract(config, {
       address,
       abi: erc1155Abi,
-      functionName: 'isApprovedForAll',
-      args: ['0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000'],
+      functionName: 'supportsInterface',
+      args: ['0xd9b67a26'], // Identifier for ERC-1155
       chainId,
     });
-    return true;
+
+    if (isERC1155) {
+      return true;
+    }
   } catch {
     return false;
   }
+  return false;
 }
 
 async function isERC20(address: Address, chainId: number): Promise<boolean> {
@@ -52,6 +61,7 @@ async function isERC20(address: Address, chainId: number): Promise<boolean> {
       args: ['0x0000000000000000000000000000000000000000'],
       chainId,
     });
+
     return true;
   } catch {
     return false;
