b615cf47de2641780cce5fee7ca542ce0ad95b19
Daniel Wang <dan@taiko.xyz>
2024-04-07 16:24:58 +0800

    Create TaikoL1TestGroup4.t.sol

---
+++ packages/protocol/test/L1/TaikoL1TestGroup4.t.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "./TaikoL1TestSetBase.sol";
+
+contract TaikoL1TestGroup4 is TaikoL1TestSetBase {
+    // About this test:
+    // - Alice proposes a block with Bob as the assigned prover
+    // - Bob proves the block within the proving window with the right parent hash
+    // - Taylor proves Bob is wrong correct with a higher tier proof
+    // - Taylor's proof is used to verify the block.
+    function test_taikoL1_group_4_case_1() external {
+        vm.warp(1_000_000);
+
+        giveEthAndTko(Alice, 10_000 ether, 10_000 ether);
+        giveEthAndTko(Bob, 10_000 ether, 10_000 ether);
+        giveEthAndTko(Taylor, 10_000 ether, 10_000 ether);
+        ITierProvider.Tier memory tierOp = TierProviderV1(cp).getTier(LibTiers.TIER_OPTIMISTIC);
+        ITierProvider.Tier memory tierSgx = TierProviderV1(cp).getTier(LibTiers.TIER_SGX);
+
+        console2.log("====== Alice propose a block with bob as the assigned prover");
+        TaikoData.BlockMetadata memory meta = proposeBlock(Alice, Bob);
+
+        uint96 livenessBond = L1.getConfig().livenessBond;
+
+        console2.log("====== Bob proves the block as the assigned prover");
+        bytes32 parentHash = GENESIS_BLOCK_HASH;
+        bytes32 blockHash = bytes32(uint256(10));
+        bytes32 stateRoot = bytes32(uint256(11));
+
+        mineAndWrap(10 seconds);
+        proveBlock(Bob, meta, parentHash, blockHash, stateRoot, meta.minTier, "");
+
+        console2.log("====== Taylor contests Bob with a higher tier proof");
+        bytes32 blockHash2 = bytes32(uint256(20));
+        bytes32 stateRoot2 = bytes32(uint256(21));
+        mineAndWrap(10 seconds);
+        proveBlock(Taylor, meta, parentHash, blockHash2, stateRoot2, LibTiers.TIER_SGX, "");
+
+        {
+            printBlockAndTrans(meta.id);
+
+            TaikoData.Block memory blk = L1.getBlock(meta.id);
+            assertEq(blk.nextTransitionId, 2);
+            assertEq(blk.verifiedTransitionId, 0);
+            assertEq(blk.assignedProver, Bob);
+            assertEq(blk.livenessBond, 0);
+
+            TaikoData.TransitionState memory ts = L1.getTransition(meta.id, 1);
+            assertEq(ts.blockHash, blockHash2);
+            assertEq(ts.stateRoot, stateRoot2);
+            assertEq(ts.tier, LibTiers.TIER_SGX);
+            assertEq(ts.contester, address(0));
+            assertEq(ts.contestBond, 1);
+            assertEq(ts.validityBond, tierSgx.validityBond);
+            assertEq(ts.prover, Taylor);
+            assertEq(ts.timestamp, block.timestamp);
+
+            assertEq(tko.balanceOf(Bob), 10_000 ether - tierOp.validityBond - livenessBond);
+            assertEq(
+                tko.balanceOf(Taylor),
+                10_000 ether - tierSgx.validityBond + (tierOp.validityBond + livenessBond) * 7 / 8
+            );
+        }
+
+        console2.log("====== Verify the block");
+        mineAndWrap(7 days);
+        verifyBlock(1);
+        {
+            printBlockAndTrans(meta.id);
+
+            TaikoData.Block memory blk = L1.getBlock(meta.id);
+
+            assertEq(blk.nextTransitionId, 2);
+            assertEq(blk.verifiedTransitionId, 1);
+            assertEq(blk.assignedProver, Bob);
+            // assertEq(blk.livenessBond, livenessBond);
+
+            TaikoData.TransitionState memory ts = L1.getTransition(meta.id, 1);
+            assertEq(ts.blockHash, blockHash2);
+            assertEq(ts.stateRoot, stateRoot2);
+            assertEq(ts.tier, LibTiers.TIER_SGX);
+            assertEq(ts.contestBond, 1);
+            assertEq(ts.prover, Taylor);
+
+            assertEq(
+                tko.balanceOf(Taylor), 10_000 ether + (tierOp.validityBond + livenessBond) * 7 / 8
+            );
+        }
+    }
+
+    // About this test:
+    // - Alice proposes a block with Bob as the assigned prover
+    // - David proves the block out of the proving window with the right parent hash
+    // - Taylor proves David is wrong correct with a higher tier proof
+    // - Taylor's proof is used to verify the block.
+    function test_taikoL1_group_4_case_2() external {
+        vm.warp(1_000_000);
+
+        giveEthAndTko(Alice, 10_000 ether, 10_000 ether);
+        giveEthAndTko(Bob, 10_000 ether, 10_000 ether);
+        giveEthAndTko(David, 10_000 ether, 10_000 ether);
+        giveEthAndTko(Taylor, 10_000 ether, 10_000 ether);
+        ITierProvider.Tier memory tierOp = TierProviderV1(cp).getTier(LibTiers.TIER_OPTIMISTIC);
+        ITierProvider.Tier memory tierSgx = TierProviderV1(cp).getTier(LibTiers.TIER_SGX);
+
+        console2.log("====== Alice propose a block with bob as the assigned prover");
+        TaikoData.BlockMetadata memory meta = proposeBlock(Alice, Bob);
+
+        uint96 livenessBond = L1.getConfig().livenessBond;
+
+        console2.log("====== Bob proves the block as the assigned prover");
+        bytes32 parentHash = GENESIS_BLOCK_HASH;
+        bytes32 blockHash = bytes32(uint256(10));
+        bytes32 stateRoot = bytes32(uint256(11));
+
+        mineAndWrap(7 days);
+        proveBlock(David, meta, parentHash, blockHash, stateRoot, meta.minTier, "");
+
+        console2.log("====== Taylor contests David with a higher tier proof");
+        bytes32 blockHash2 = bytes32(uint256(20));
+        bytes32 stateRoot2 = bytes32(uint256(21));
+        mineAndWrap(10 seconds);
+        proveBlock(Taylor, meta, parentHash, blockHash2, stateRoot2, LibTiers.TIER_SGX, "");
+
+        {
+            printBlockAndTrans(meta.id);
+
+            TaikoData.Block memory blk = L1.getBlock(meta.id);
+            assertEq(blk.nextTransitionId, 2);
+            assertEq(blk.verifiedTransitionId, 0);
+            assertEq(blk.assignedProver, Bob);
+            assertEq(blk.livenessBond, 0);
+
+            TaikoData.TransitionState memory ts = L1.getTransition(meta.id, 1);
+            assertEq(ts.blockHash, blockHash2);
+            assertEq(ts.stateRoot, stateRoot2);
+            assertEq(ts.tier, LibTiers.TIER_SGX);
+            assertEq(ts.contester, address(0));
+            assertEq(ts.contestBond, 1);
+            assertEq(ts.validityBond, tierSgx.validityBond);
+            assertEq(ts.prover, Taylor);
+            assertEq(ts.timestamp, block.timestamp);
+
+            assertEq(tko.balanceOf(Bob), 10_000 ether - livenessBond);
+            assertEq(tko.balanceOf(David), 10_000 ether - tierOp.validityBond);
+            assertEq(
+                tko.balanceOf(Taylor),
+                10_000 ether - tierSgx.validityBond + tierOp.validityBond * 7 / 8
+            );
+        }
+
+        console2.log("====== Verify the block");
+        mineAndWrap(7 days);
+        verifyBlock(1);
+        {
+            printBlockAndTrans(meta.id);
+
+            TaikoData.Block memory blk = L1.getBlock(meta.id);
+
+            assertEq(blk.nextTransitionId, 2);
+            assertEq(blk.verifiedTransitionId, 1);
+            assertEq(blk.assignedProver, Bob);
+            assertEq(blk.livenessBond, 0);
+
+            TaikoData.TransitionState memory ts = L1.getTransition(meta.id, 1);
+            assertEq(ts.blockHash, blockHash2);
+            assertEq(ts.stateRoot, stateRoot2);
+            assertEq(ts.tier, LibTiers.TIER_SGX);
+            assertEq(ts.contestBond, 1);
+            assertEq(ts.prover, Taylor);
+
+            assertEq(tko.balanceOf(Taylor), 10_000 ether + tierOp.validityBond * 7 / 8);
+        }
+    }
+}
