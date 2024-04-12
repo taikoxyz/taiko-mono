5372314f16858666a8df1745ebbf3d282c0d9954
Daniel Wang <dan@taiko.xyz>
2024-04-12 14:12:06 +0800

    Revert "more"
    This reverts commit 0512cb3eba842bd09ecbbda66d5babab69666ecd.

--- packages/protocol/contracts/tokenvault/BridgedERC20.sol
+++ packages/protocol/contracts/tokenvault/BridgedERC20.sol
@@ -33,8 +33,11 @@ contract BridgedERC20 is
     error BTOKEN_CANNOT_RECEIVE();
     error BTOKEN_UNAUTHORIZED();
 
-    modifier onlyTaikoOrSnapshooter() {
-        if (msg.sender != snapshooter && msg.sender != resolve(LibStrings.B_TAIKO, true)) {
+    modifier onlyOwnerOrTaikoOrSnapshooter() {
+        if (
+            msg.sender != owner() && msg.sender != snapshooter
+                && msg.sender != resolve(LibStrings.B_TAIKO, true)
+        ) {
             revert BTOKEN_UNAUTHORIZED();
         }
         _;
@@ -81,7 +84,7 @@ contract BridgedERC20 is
     }
 
     /// @notice Creates a new token snapshot.
-    function snapshot() external onlyTaikoOrSnapshooter returns (uint256) {
+    function snapshot() external onlyOwnerOrTaikoOrSnapshooter returns (uint256) {
         return _snapshot();
     }
 
