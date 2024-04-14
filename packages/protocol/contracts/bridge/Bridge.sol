476ca24f787a4b9305daf3ff97bc1a8b6e169c94
Daniel Wang <dan@taiko.xyz>
2024-04-14 17:37:25 +0800

    Update Bridge.sol

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -20,16 +20,16 @@ contract Bridge is EssentialContract, IBridge {
     using LibAddress for address payable;
 
     /// @dev Gas overhead if a message is received but not processed in the same transaction.
-    // The measured value  is 104047
+    // The measured value is 104047
     uint256 public constant GAS_RECEIVING = 110_000 + 21_000;
 
     /// @dev Gas overhead if a message is processed.
-    // The measured value  is 97358
+    // The measured value is 97358
     uint256 public constant GAS_PROCESSING = 100_000 + 21_000;
 
     /// @dev Gas overhead if a message is received and processed in the same transaction.
-    // The measured value  is 122083
-    uint256 public constant GAS_RECEIVING_AND_PROCESSING = 124_000 + 21_000;
+    // The measured value is 122083
+    uint256 public constant GAS_RECEIVING_AND_PROCESSING = 13_000 + 21_000;
 
     /// @dev The slot in transient storage of the call context. This is the keccak256 hash
     /// of "bridge.ctx_slot"
