7a615ec1aee0e7451aebcbb218688cb7f4e3524a
Daniel Wang <dan@taiko.xyz>
2024-04-17 22:53:16 +0800

    Update TokenGrant.sol

--- packages/protocol/contracts/team/TokenGrant.sol
+++ packages/protocol/contracts/team/TokenGrant.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
 import "../common/EssentialContract.sol";
 import "./LibTokenGrant.sol";
 
@@ -24,7 +22,7 @@ import "./LibTokenGrant.sol";
 /// - team members, advisors, etc.
 /// - grant program grantees
 /// @custom:security-contact security@taiko.xyz
-contract TokenGrant is EssentialContract, EIP712Upgradeable {
+contract TokenGrant is EssentialContract {
     address public constant TAIKO_TOKEN = address(1);
     address public constant USTC_TOKEN = address(2);
     // TODO
