4015d6e1561ced490948cb6f8302a68942cda639
Korbinian <korbiniank@outlook.de>
2024-04-17 14:53:36 +0200

    mobile styles

--- packages/guardian-prover-health-check-ui/src/components/GuardianProver/GuardianProverTableRow.svelte
+++ packages/guardian-prover-health-check-ui/src/components/GuardianProver/GuardianProverTableRow.svelte
@@ -4,14 +4,15 @@
 	import { signedBlocksPerGuardian } from '$lib/blocks/signedBlocksPerGuardian';
 	import { lastGuardianFetchTimestamp, loading, signedBlocks } from '$stores';
 	import { GuardianProverStatus, type Guardian } from '$lib/types';
+
 	import { truncateDecimal } from '$lib/util/truncateDecimal';
-	import { truncateString } from '$lib/util/truncateString';
 	import { onMount } from 'svelte';
 	import { t } from 'svelte-i18n';
 	import DataPoint from './DataPoint.svelte';
 	import IconFlipper from '$components/Icon/IconFlipper.svelte';
 	import { formatISODateTime } from '$lib/util/formatISODateTime';
 	import { Spinner } from '$components/Spinner';
+	import { shortenAddress } from '$lib/util/shortenAddress';
 
 	export let guardianProver: Guardian;
 	export let single = false;
@@ -72,93 +73,76 @@
 	bg-base-200"
 >
 	<input type="checkbox" id={`guardian-${guardianProver.id}`} class="peer" bind:checked={flipped} />
-	{#if isDesktopOrLarger}
-		<div class="collapse-title text-xl font-medium f-row">
-			<div class="f-row min-w-[150px] items-center gap-4">
-				{#if $loading}
-					<Icon
-						type="question-circle"
-						fillClass="fill-neutral-content"
-						class="min-w-[20px] min-h-[20px]"
-						size={20}
-					/>
-					<Spinner class="w-4 h-4" />
-				{:else}
-					<Icon type={iconType} {fillClass} class="min-w-[20px] min-h-[20px]" size={20} />
-					<div class="f-col">
-						<span class="font-bold">{statusText}</span>
-						<span class="text-sm">{secondsAgo}s ago</span>
-					</div>
-				{/if}
-			</div>
-
-			<div class="f-col grow">
-				<div class="font-bold">
-					{guardianProver.name}
-				</div>
-				<div class="text-secondary-content">{guardianProver.address}</div>
-			</div>
-			{#if !single && !$loading}
-				<IconFlipper
-					bind:flipped
-					iconType1="chevron-left"
-					iconType2="chevron-down"
-					selectedDefault="chevron-left"
-					size={15}
-					noEvent
+	<div class="collapse-title text-xl font-medium f-row">
+		<div class="f-row min-w-[150px] items-center gap-4">
+			{#if $loading}
+				<Icon
+					type="question-circle"
+					fillClass="fill-neutral-content"
+					class="min-w-[20px] min-h-[20px]"
+					size={20}
 				/>
+				<Spinner class="w-4 h-4" />
+			{:else}
+				<Icon type={iconType} {fillClass} class="min-w-[20px] min-h-[20px]" size={20} />
+				<div class="f-col">
+					<span class="font-bold">{statusText}</span>
+					<span class="text-sm">{secondsAgo}s ago</span>
+				</div>
 			{/if}
 		</div>
-		<div class="collapse-content bg-grey-10">
-			<div class="f-row items-center">
-				<div class="min-w-[150px]" />
 
-				<DataPoint
-					headline={$t('overview.table.balance')}
-					dataPoint={truncateDecimal(Number(guardianProver.balance), 3).toString() + ' ETH'}
-				/>
+		<div class="f-col grow">
+			<div class="font-bold">
+				{guardianProver.name}
+			</div>
+			<div class="text-secondary-content">{shortenAddress(guardianProver.address)}</div>
+		</div>
+		{#if !single && !$loading}
+			<IconFlipper
+				bind:flipped
+				iconType1="chevron-left"
+				iconType2="chevron-down"
+				selectedDefault="chevron-left"
+				size={15}
+				noEvent
+			/>
+		{/if}
+	</div>
+	<div class="collapse-content bg-grey-10">
+		<div class="{isDesktopOrLarger ? 'f-row' : 'f-col space-y-[5px] py-[16px]'} items-center">
+			<div class="min-w-[150px]" />
 
-				<DataPoint
-					headline={$t('overview.detail.table.uptime')}
-					dataPoint={truncateDecimal(guardianProver.uptime, 2).toString() + '%'}
-				/>
+			<DataPoint
+				headline={$t('overview.table.balance')}
+				dataPoint={truncateDecimal(Number(guardianProver.balance), 3).toString() + ' ETH'}
+			/>
 
-				<DataPoint
-					headline={$t('overview.table.no_blocks_created')}
-					dataPoint="{signedBlocksPerGuardian(guardianProver.id)}/{$signedBlocks.length}"
-				/>
+			<DataPoint
+				headline={$t('overview.detail.table.uptime')}
+				dataPoint={truncateDecimal(guardianProver.uptime, 2).toString() + '%'}
+			/>
 
-				<DataPoint headline="L1 Node Version" dataPoint={guardianProver?.nodeInfo?.l1NodeVersion} />
-				<DataPoint headline="L2 Node Version" dataPoint={guardianProver?.nodeInfo?.l2NodeVersion} />
-				<DataPoint headline="Revision" dataPoint={guardianProver?.nodeInfo?.revision} />
-				<DataPoint
-					headline="Last restart"
-					dataPoint={formatISODateTime(guardianProver?.lastRestart)}
-				/>
+			<DataPoint
+				headline={$t('overview.table.no_blocks_created')}
+				dataPoint="{signedBlocksPerGuardian(guardianProver.id)}/{$signedBlocks.length}"
+			/>
 
-				{#if !single}
+			<DataPoint headline="L1 Node Version" dataPoint={guardianProver?.nodeInfo?.l1NodeVersion} />
+			<DataPoint headline="L2 Node Version" dataPoint={guardianProver?.nodeInfo?.l2NodeVersion} />
+			<DataPoint headline="Revision" dataPoint={guardianProver?.nodeInfo?.revision} />
+			<DataPoint
+				headline="Last restart"
+				dataPoint={formatISODateTime(guardianProver?.lastRestart)}
+			/>
+
+			{#if !single}
+				<div class="f-row md:w-auto w-full justify-end pt-[20px] md:pt-[0]">
 					<button class="link" on:click>{$t('overview.table.view_details')}</button>
-				{/if}
-			</div>
+				</div>
+			{/if}
 		</div>
-	{:else}
-		<div class="col-span-4 f-row min-w-[150px] items-center">
-			<Icon type={iconType} {fillClass} />
-			<div class="f-col ml-[15px]">
-				<span class="font-bold">{statusText}</span>
-				<span class="text-sm">{secondsAgo}s ago</span>
-			</div>
-		</div>
-		<div class="col-span-8 font-bold">
-			<div class="f-col">
-				<span class="font-bold"
-					>{$t('common.prover')}
-					{guardianProver.id}</span
-				>
-				<span class="font-normal">{truncateString(guardianProver.address, 10)}</span>
-			</div>
-		</div>
-	{/if}
+	</div>
 </div>
 
 <DesktopOrLarger bind:is={isDesktopOrLarger} />
