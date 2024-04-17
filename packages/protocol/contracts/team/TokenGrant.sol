6cd166f87b5c0252b270d2df33c1d4ec58b6aece
Daniel Wang <dan@taiko.xyz>
2024-04-17 23:48:22 +0800

    Update TokenGrant.sol

--- packages/protocol/contracts/team/TokenGrant.sol
+++ packages/protocol/contracts/team/TokenGrant.sol
@@ -11,18 +11,6 @@ import "./LibTokenGrant.sol";
 /// @title TokenGrant
 /// @notice Contract for managing Taiko tokens allocated to different roles and
 /// individuals.
-///
-/// Manages Taiko tokens through a three-state lifecycle: "allocated" to
-/// "granted, owned, and locked," and finally to "granted, owned, and unlocked."
-/// Allocation doesn't transfer ownership unless specified by grant settings.
-/// Conditional allocated tokens can be canceled by invoking `void()`, making
-/// them available for other uses. Once granted and owned, tokens are
-/// irreversible and their unlock schedules are immutable.
-///
-/// We should deploy multiple instances of this contract for different roles:
-/// - investors
-/// - team members, advisors, etc.
-/// - grant program grantees
 /// @custom:security-contact security@taiko.xyz
 contract TokenGrant is EssentialContract {
     using SafeERC20 for IERC20;
