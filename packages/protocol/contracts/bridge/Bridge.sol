f37c1951fcac117a7b787baa0c5bafc4fffd66c0
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-04-19 09:39:40 +0800

    Update packages/protocol/contracts/bridge/Bridge.sol
    Co-authored-by: Brecht Devos <Brechtp.Devos@gmail.com>

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -207,7 +207,7 @@ contract Bridge is EssentialContract, IBridge {
 
     /// @inheritdoc IBridge
     /// @dev This transaction's gas limit must not be smaller than:
-    /// `(message.gasLimit - GAS_RESERVE) * 24 / 23 + GAS_RESERVE`,
+    /// `(message.gasLimit - GAS_RESERVE) * 64 / 63 + GAS_RESERVE`,
     /// Or we can use a simplified rule: `tx.gaslimit = message.gaslimit * 105%`.
     function processMessage(
         Message calldata _message,
