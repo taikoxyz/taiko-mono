75411adc34c09a70d1a959398e4e27d44265c2c0
Daniel Wang <dan@taiko.xyz>
2024-04-19 12:27:57 +0800

    remove_MessageProcessed

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -254,8 +254,6 @@ contract Bridge is EssentialContract, IBridge {
             _updateMessageStatus(msgHash, status);
         }
 
-        emit MessageProcessed(_message);
-
         if (_message.fee != 0) {
             refundAmount += _message.fee;
 
