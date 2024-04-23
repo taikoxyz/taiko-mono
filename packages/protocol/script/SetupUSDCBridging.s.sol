efe83c1978f137ade77fe14958a3b248bf0f4d1d
Daniel Wang <dan@taiko.xyz>
2024-04-23 20:30:08 +0800

    Update SetupUSDCBridging.s.sol

--- packages/protocol/script/SetupUSDCBridging.s.sol
+++ packages/protocol/script/SetupUSDCBridging.s.sol
@@ -9,23 +9,30 @@ import "../contracts/tokenvault/ERC20Vault.sol";
 /// To deploy a native USDC contract on Taiko, run:
 /// https://github.com/taikoxyz/USDC/blob/main/script/DeployUSDC.s.sol
 contract SetupUSDCBridging is DeployCapability {
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant USDC_ON_ETHEREUM = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     uint256 public privateKey = vm.envUint("PRIVATE_KEY");
     address public erc20VaultOnL2 = vm.envAddress("ERC20_VAULT_ON_L2");
+
+    // These are FiatTokenProxy addresses of the USDC coin.
+    address public usdcOnL1 = vm.envAddress("USDC_ADDRESS_ON_L1");
     address public usdcOnL2 = vm.envAddress("USDC_ADDRESS_ON_L2");
 
     function run() external {
         require(erc20VaultOnL2 != address(0) && usdcOnL2 != address(0), "invalid params");
 
+        if (usdcOnL1 == address(0)) {
+            usdcOnL1 = USDC_ON_ETHEREUM;
+        }
+
         ERC20Vault vault = ERC20Vault(erc20VaultOnL2);
 
-        address currBridgedtoken = vault.canonicalToBridged(1, USDC);
+        address currBridgedtoken = vault.canonicalToBridged(1, usdcOnL1);
         console2.log("current btoken for usdc:", currBridgedtoken);
 
         ERC20Vault.CanonicalERC20 memory ctoken = ERC20Vault.CanonicalERC20({
             chainId: 1,
-            addr: USDC,
+            addr: usdcOnL1,
             decimals: 6,
             symbol: "USDC",
             name: "USD Coin"
@@ -38,7 +45,7 @@ contract SetupUSDCBridging is DeployCapability {
         }
         vm.stopBroadcast();
 
-        address newBridgedToken = vault.canonicalToBridged(1, USDC);
+        address newBridgedToken = vault.canonicalToBridged(1, usdcOnL1);
         console2.log("new btoken for usdc:", newBridgedToken);
 
         require(usdcOnL2 == newBridgedToken, "unexpected result");
