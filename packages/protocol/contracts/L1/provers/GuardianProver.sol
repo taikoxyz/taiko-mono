fc733ef6b2e8c623193174ce8547ff7204ae147b
Daniel Wang <dan@taiko.xyz>
2024-04-24 23:38:10 +0800

    Update GuardianProver.sol

--- packages/protocol/contracts/L1/provers/GuardianProver.sol
+++ packages/protocol/contracts/L1/provers/GuardianProver.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "../../common/LibStrings.sol";
 import "../../verifiers/IVerifier.sol";
 import "../tiers/ITierProvider.sol";
@@ -12,6 +14,8 @@ import "./Guardians.sol";
 /// This prover uses itself as the verifier.
 /// @custom:security-contact security@taiko.xyz
 contract GuardianProver is IVerifier, Guardians {
+    using SafeERC20 for IERC20;
+
     error GV_PERMISSION_DENIED();
     error GV_ZERO_ADDRESS();
 
@@ -43,7 +47,7 @@ contract GuardianProver is IVerifier, Guardians {
     function enableTaikoTokenAllowance(bool _enable) external onlyOwner {
         address tko = resolve(LibStrings.B_TAIKO_TOKEN, false);
         address taiko = resolve(LibStrings.B_TAIKO, false);
-        IERC20(tko).approve(taiko, _enable ? type(uint256).max : 0);
+        IERC20(tko).safeApprove(taiko, _enable ? type(uint256).max : 0);
     }
 
     /// @dev Withdraws Taiko Token to a given address.
@@ -54,7 +58,7 @@ contract GuardianProver is IVerifier, Guardians {
 
         IERC20 tko = IERC20(resolve(LibStrings.B_TAIKO_TOKEN, false));
         uint256 amount = _amount == 0 ? tko.balanceOf(address(this)) : _amount;
-        tko.transfer(_to, amount);
+        tko.safeTransfer(_to, amount);
     }
 
     /// @dev Called by guardians to approve a guardian proof
