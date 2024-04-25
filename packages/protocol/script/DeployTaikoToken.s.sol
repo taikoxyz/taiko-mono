3834f65c1b86b8564e907dc5216b31fc058c6441
David <david@taiko.xyz>
2024-04-25 16:48:44 +0800

    feat(protocol): update `DeployTaikoToken` script

--- packages/protocol/script/DeployTaikoToken.s.sol
+++ packages/protocol/script/DeployTaikoToken.s.sol
@@ -6,11 +6,12 @@ import "../contracts/L1/TaikoToken.sol";
 
 contract DeployTaikoToken is DeployCapability {
     uint256 public privateKey = vm.envUint("PRIVATE_KEY");
-    // MAINNET_SECURITY_COUNCIL: council.taiko.eth (0x7C50d60743D3FCe5a39FdbF687AFbAe5acFF49Fd)
     address public securityCouncil = vm.envAddress("SECURITY_COUNCIL");
     string public tokenName = vm.envString("TAIKO_TOKEN_NAME");
     string public tokenSymbol = vm.envString("TAIKO_TOKEN_SYMBOL");
     address public premintRecipient = vm.envAddress("TAIKO_TOKEN_PREMINT_RECIPIENT");
+    address public labMultisig = vm.envAddress("LAB_MULTI_SIG");
+    address public treasuryMultisig = vm.envAddress("TREASURY_MULTI_SIG");
 
     modifier broadcast() {
         require(privateKey != 0, "invalid private key");
@@ -20,13 +21,15 @@ contract DeployTaikoToken is DeployCapability {
     }
 
     function run() external broadcast {
-        // Deploy the TaikoToken contract, use securityCouncil address as the owner.
-        deployProxy({
+        address taikoToken = deployProxy({
             name: "taiko_token",
             impl: address(new TaikoToken()),
             data: abi.encodeCall(
                 TaikoToken.init, (securityCouncil, tokenName, tokenSymbol, premintRecipient)
                 )
         });
+
+        TaikoToken(taikoToken).transfer(labMultisig, 1_000_000_000 ether / 2);
+        TaikoToken(taikoToken).transfer(treasuryMultisig, 1_000_000_000 ether / 2);
     }
 }
