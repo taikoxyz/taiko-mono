4e90dec003e3d8168872ab25677b3b5ba9a28a82
bearni95 <bernatcanal@gmail.com>
2024-04-29 12:00:44 +0200

    cleanup of unused errors

--- packages/taikoon-ui/src/lib/error/errors.ts
+++ packages/taikoon-ui/src/lib/error/errors.ts
@@ -10,22 +10,6 @@ export class InsufficientBalanceError extends Error {
   name = 'InsufficientBalanceError';
 }
 
-export class InsufficientAllowanceError extends Error {
-  name = 'InsufficientAllowanceError';
-}
-
-export class NotApprovedError extends Error {
-  name = 'NotApprovedError';
-}
-
-export class NoAllowanceRequiredError extends Error {
-  name = 'NoAllowanceRequiredError';
-}
-
-export class NoApprovalRequiredError extends Error {
-  name = 'NoApprovalRequiredError';
-}
-
 export class NoTokenAddressError extends Error {
   name = 'NoTokenAddressError';
 }
@@ -36,26 +20,6 @@ export class NoTokenError extends Error {
 
 export class FailedTransactionError extends Error {
   name = 'FailedTransactionError';
-}
-
-export class SendMessageError extends Error {
-  name = 'SendMessageError';
-}
-
-export class SendERC20Error extends Error {
-  name = 'SendERC20Error';
-}
-
-export class SendERC721Error extends Error {
-  name = 'SendERC721Error';
-}
-
-export class SendERC1155Error extends Error {
-  name = 'SendERC1155Error';
-}
-
-export class ApproveError extends Error {
-  name = 'ApproveError';
 }
 
 export class RevertedWithoutMessageError extends Error {
@@ -70,21 +34,6 @@ export class MintError extends Error {
   name = 'MintError';
 }
 
-export class PendingBlockError extends Error {
-  name = 'PendingBlockError';
-}
-
-export class InvalidProofError extends Error {
-  name = 'InvalidProofError';
-}
-
-export class WrongBridgeConfigError extends Error {
-  name = 'WrongBridgeConfigError';
-}
-
-export class MessageStatusError extends Error {
-  name = 'MessageStatusError';
-}
 
 export class WrongOwnerError extends Error {
   name = 'WrongOwnerError';
@@ -94,40 +43,8 @@ export class WrongChainError extends Error {
   name = 'WrongChainError';
 }
 
-export class BridgeTxPollingError extends Error {
-  name = 'BridgeTxPollingError';
-}
-
-export class ProcessMessageError extends Error {
-  name = 'ProcessMessageError';
-}
-
-export class RetryError extends Error {
-  name = 'RetryError';
-}
-
-export class ReleaseError extends Error {
-  name = 'ReleaseError';
-}
-
-export class UnknownTokenTypeError extends Error {
-  name = 'UnknownTokenTypeError';
-}
-
-export class BridgePausedError extends Error {
-  name = 'BridgePausedError';
-}
-
 export class FilterLogsError extends Error {
   name = 'FilterLogsError';
-}
-
-export class NoCanonicalInfoFoundError extends Error {
-  name = 'NoCanonicalInfoFoundError';
-}
-
-export class NoBridgedInfoFoundError extends Error {
-  name = 'NoBridgedInfoFoundError';
 }
 
 export class NoTokenInfoFoundError extends Error {
@@ -136,10 +53,6 @@ export class NoTokenInfoFoundError extends Error {
 
 export class NoMetadataFoundError extends Error {
   name = 'NoMetadataFoundError';
-}
-
-export class InvalidParametersProvidedError extends Error {
-  name = 'InvalidParametersProvidedError';
 }
 
 export class InternalError extends Error {
