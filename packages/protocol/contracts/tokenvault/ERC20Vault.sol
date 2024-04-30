0f6b6b5e27b74ef6e724fa352d2fd5d6f5ea0458
D <51912515+adaki2004@users.noreply.github.com>
2024-04-30 14:53:00 +0200

    feat(protocol): safeguard possible failing calls (#16931)
    Co-authored-by: Keszey Dániel <keszeyd@MacBook-Pro.local>
    Co-authored-by: Daniel Wang <dan@taiko.xyz>
    Co-authored-by: Brecht Devos <Brechtp.Devos@gmail.com>

--- packages/protocol/contracts/tokenvault/ERC20Vault.sol
+++ packages/protocol/contracts/tokenvault/ERC20Vault.sol
@@ -355,14 +355,26 @@ contract ERC20Vault is BaseVault {
             balanceChange_ = _op.amount;
         } else {
             // If it's a canonical token
-            IERC20Metadata meta = IERC20Metadata(_op.token);
             ctoken_ = CanonicalERC20({
                 chainId: uint64(block.chainid),
                 addr: _op.token,
-                decimals: meta.decimals(),
-                symbol: meta.symbol(),
-                name: meta.name()
+                decimals: 0,
+                symbol: "",
+                name: ""
             });
+
+            // Try fill in the boilerplate values, but use try-catch because functions below are
+            // ERC20-optional only.
+            IERC20Metadata meta = IERC20Metadata(_op.token);
+            try meta.decimals() returns (uint8 _decimals) {
+                ctoken_.decimals = _decimals;
+            } catch { }
+            try meta.name() returns (string memory _name) {
+                ctoken_.name = _name;
+            } catch { }
+            try meta.symbol() returns (string memory _symbol) {
+                ctoken_.symbol = _symbol;
+            } catch { }
 
             // Query the balance then query it again to get the actual amount of
             // token transferred into this address, this is more accurate than
