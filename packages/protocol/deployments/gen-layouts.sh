c1df7519d70a54be302a34c8bba9b47fb309383f
Daniel Wang <dan@taiko.xyz>
2024-05-02 20:56:21 +0800

    generate a contract layout file for comparison

--- packages/protocol/deployments/gen-layouts.sh
+++ packages/protocol/deployments/gen-layouts.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Check if a commit hash is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 commit_hash"
-    exit 1
-fi
-
-# The commit hash is taken from the first command line argument
-commit="$1"
-
 # Define the list of contracts to inspect
 contracts=(
     "TaikoL1"
@@ -36,11 +27,8 @@ contracts=(
     "RiscZeroVerifier"
 )
 
-# Switch to the commit in detached mode
-git checkout $commit
-
 # Empty the output file initially
-output_file="layout-${commit:0:7}.txt"
+output_file="contract_layout.md"
 > $output_file
 
 # Loop over each contract
@@ -51,7 +39,7 @@ for contract in "${contracts[@]}"; do
 
     echo "## ${contract}" >> $output_file
     forge inspect ${contract} storagelayout --pretty >> $output_file
+    echo "" >> $output_file
 done
 
-git switch -
 
