4b4a50245184fa5cad24445ab040ddd4ebb4f83b
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-05-03 15:47:08 +0800

    feat(protocol): improve `TransitionProved` event to include previous prover and contester (#16967)

--- packages/protocol/contracts/L1/TaikoEvents.sol
+++ packages/protocol/contracts/L1/TaikoEvents.sol
@@ -48,13 +48,17 @@ abstract contract TaikoEvents {
     /// @dev Emitted when a block transition is proved or re-proved.
     /// @param blockId The ID of the proven block.
     /// @param tran The verified transition.
+    /// @param prevProver The previous prover address.
+    /// @param prevContester The previous contester address.
     /// @param prover The prover address.
     /// @param validityBond The validity bond amount.
     /// @param tier The tier ID of the proof.
     event TransitionProved(
         uint256 indexed blockId,
         TaikoData.Transition tran,
-        address prover,
+        address indexed prevProver,
+        address prevContester,
+        address indexed prover,
         uint96 validityBond,
         uint16 tier
     );
