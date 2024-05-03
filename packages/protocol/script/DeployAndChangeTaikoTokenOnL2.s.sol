cb60c18a20957f094d072724f66eab1fc5e7ccf5
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-05-03 18:55:32 +0200

    deployment script changes

--- packages/protocol/script/DeployAndChangeTaikoTokenOnL2.s.sol
+++ packages/protocol/script/DeployAndChangeTaikoTokenOnL2.s.sol
@@ -5,45 +5,42 @@ import "../test/DeployCapability.sol";
 import "../contracts/tko/BridgedTaikoToken.sol";
 import "../contracts/tokenvault/ERC20Vault.sol";
 
-contract DeployTaikoToken is DeployCapability {
+contract DeployAndChangeTaikoTokenOnL2 is DeployCapability {
     // If the private key to deploy proxy shall be different to the private key to changing the
     // vault's bridge address(changeBridgedToken) then we need 2 private keys.
-    uint256 public privateKey = vm.envUint("PRIVATE_KEY");
 
-    // L2 related
-    address public bridgedTkoOwner = vm.envAddress("BRIDGED_TKO_OWNER");
-    address public l2AddressManager = vm.envAddress("L2_ADDRESS_MANAGER");
+    address public vaultOwner = vm.envAddress("VAULT_OWNER");
+    uint256 public vaultOwnerPrivateKey = vm.envUint("VAULT_OWNER_PRIVATE_KEY");
     address public l2Erc20Vault = vm.envAddress("ERC20_VAULT_ON_L2");
 
-    // L1 related
-    address public l1TaikoToken = vm.envAddress("TKO_ADDRESS_ON_L1");
-
     modifier broadcast() {
-        require(privateKey != 0, "invalid private key");
+        require(vaultOwnerPrivateKey != 0, "invalid private key");
         vm.startBroadcast();
         _;
         vm.stopBroadcast();
     }
 
     function run() external broadcast {
+        // Change BridgedToken
+        ERC20Vault vault = ERC20Vault(l2Erc20Vault);
+
         // Deploy the TaikoToken contract on L2
         address deployedTkoProxy = deployProxy({
             name: "taiko_token",
             impl: address(new BridgedTaikoToken()),
-            data: abi.encodeCall(BridgedTaikoToken.init, (bridgedTkoOwner, l2AddressManager))
+            data: abi.encodeCall(BridgedTaikoToken.init, (vaultOwner, vault.addressManager()))
         });
 
-        // Change BridgedToken
-        ERC20Vault vault = ERC20Vault(l2Erc20Vault);
-
-        address currBridgedtoken = vault.canonicalToBridged(1, l1TaikoToken);
-        console2.log("current btoken for tko:", currBridgedtoken);
-        (address canonicalAddress, uint256 chainId) =
+        (address canonicalTkoAddress, uint256 chainId) =
             BridgedTaikoToken(deployedTkoProxy).canonical();
+
+        address currBridgedtoken = vault.canonicalToBridged(chainId, canonicalTkoAddress);
+        assert(currBridgedtoken == address(0));
+
         vault.changeBridgedToken(
             ERC20Vault.CanonicalERC20({
                 chainId: uint64(chainId),
-                addr: canonicalAddress,
+                addr: canonicalTkoAddress,
                 decimals: 18,
                 symbol: "TKO",
                 name: "Taiko Token"
@@ -54,7 +51,7 @@ contract DeployTaikoToken is DeployCapability {
             vault.unpause();
         }
 
-        address newBridgedToken = vault.canonicalToBridged(1, l1TaikoToken);
+        address newBridgedToken = vault.canonicalToBridged(chainId, canonicalTkoAddress);
         console2.log("new btoken for tko:", newBridgedToken);
 
         require(deployedTkoProxy == newBridgedToken, "unexpected result");
