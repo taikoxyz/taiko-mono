fb033202803c1f2bc3a7b91f6ae184be4fe816e8
Daniel Wang <dan@taiko.xyz>
2024-05-04 22:39:07 +0800

    Update Bridge2_failMessage.t.sol

--- packages/protocol/test/bridge/Bridge2_failMessage.t.sol
+++ packages/protocol/test/bridge/Bridge2_failMessage.t.sol
@@ -3,4 +3,50 @@ pragma solidity 0.8.24;
 
 import "./Bridge2.t.sol";
 
-contract BridgeTest2_failMessage is BridgeTest2 { }
+contract TestToContract {
+    function anotherFunc() external { }
+}
+
+contract BridgeTest2_failMessage is BridgeTest2 {
+    function test_bridge2_failMessage_not_by_destOwner() public transactedBy(Carol) {
+        IBridge.Message memory message;
+        message.destChainId = uint64(block.chainid);
+        message.srcChainId = remoteChainId;
+        message.gasLimit = 1_000_000;
+        message.fee = 1000;
+        message.value = 2 ether;
+        message.destOwner = Alice;
+        message.to = Bob;
+
+        vm.expectRevert(Bridge.B_PERMISSION_DENIED.selector);
+        bridge.failMessage(message);
+    }
+
+    function test_bridge2_failMessage_by_destOwner() public transactedBy(Alice) {
+        TestToContract target = new TestToContract();
+
+        IBridge.Message memory message;
+
+        message.destChainId = uint64(block.chainid);
+        message.srcChainId = remoteChainId;
+
+        message.gasLimit = 1_000_000;
+        message.fee = 5_000_000;
+        message.value = 2 ether;
+        message.destOwner = Alice;
+        message.to = address(target);
+        message.data = abi.encodeCall(TestToContract.anotherFunc, ());
+
+        vm.expectRevert(Bridge.B_INVALID_STATUS.selector);
+        bridge.failMessage(message);
+
+        bridge.processMessage(message, fakeProof);
+        bytes32 hash = bridge.hashMessage(message);
+        assertTrue(bridge.messageStatus(hash) == IBridge.Status.RETRIABLE);
+
+        bridge.failMessage(message);
+
+        hash = bridge.hashMessage(message);
+        assertTrue(bridge.messageStatus(hash) == IBridge.Status.FAILED);
+    }
+}
