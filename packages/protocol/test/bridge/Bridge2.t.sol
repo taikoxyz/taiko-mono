07a3975352879d82552aa9446428ffe71f9bd915
Daniel Wang <dan@taiko.xyz>
2024-05-04 11:42:34 +0800

    add empty files

---
+++ packages/protocol/test/bridge/Bridge2.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "../TaikoTest.sol";
+
+contract BridgeTest2 is TaikoTest { }
---
+++ packages/protocol/test/bridge/Bridge2_recallMessage.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "./Bridge2.t.sol";
+
+contract BridgeTest2_recallMessage is BridgeTest2 { }
