d6a7ee00641874e99c9aa31caf2857198feb8fda
bearni95 <bernatcanal@gmail.com>
2024-05-09 16:01:30 +0200

    check corrections

--- packages/taikoon-ui/src/tests/mocks/index.ts
+++ packages/taikoon-ui/src/tests/mocks/index.ts
@@ -1,7 +1,4 @@
 export * from './addresses';
 export * from './blocks';
 export * from './chains';
-export * from './messages';
 export * from './proofs';
-export * from './tokens';
-export * from './transactions';
