f34b29bcb089150e61a2c4d207079a7155012428
Daniel Wang <dan@taiko.xyz>
2024-05-13 14:34:07 +0800

    Update Vest.s.sol

--- packages/supplementary-contracts/script/tokenVesting/Vest.s.sol
+++ packages/supplementary-contracts/script/tokenVesting/Vest.s.sol
@@ -28,22 +28,25 @@ contract VestTokenUnlocking is Script {
         );
 
         for (uint256 i; i < items.length; i++) {
-            address proxy = items[i].proxy;
-            console2.log("Grantee unlocking contract address:", proxy);
-            console2.log("Vest amount (TKO):", items[i].vestAmount);
+            if (items[i].vestAmount != 0) {
+                address proxy = items[i].proxy;
+                console2.log("Grantee unlocking contract address:", proxy);
+                console2.log("Vest amount (TKO):", items[i].vestAmount);
 
-            require(TokenUnlocking(proxy).owner() == msg.sender, "msg.sender not owner");
-            require(
-                TokenUnlocking(proxy).recipient() == items[i].recipient, "inconsistent recipient"
-            );
+                require(TokenUnlocking(proxy).owner() == msg.sender, "msg.sender not owner");
+                require(
+                    TokenUnlocking(proxy).recipient() == items[i].recipient,
+                    "inconsistent recipient"
+                );
 
-            uint128 vestAmount = uint128(items[i].vestAmount * 1e18);
-            require(tko.balanceOf(msg.sender) >= vestAmount, "insufficient TKO balance");
+                uint128 vestAmount = uint128(items[i].vestAmount * 1e18);
+                require(tko.balanceOf(msg.sender) >= vestAmount, "insufficient TKO balance");
 
-            tko.approve(proxy, vestAmount);
-            TokenUnlocking(proxy).vest(vestAmount);
+                tko.approve(proxy, vestAmount);
+                TokenUnlocking(proxy).vest(vestAmount);
 
-            console2.log("Vested!\n");
+                console2.log("Vested!\n");
+            }
         }
 
         vm.stopBroadcast();
