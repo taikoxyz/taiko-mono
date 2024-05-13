d35ad166050f5de5be7e5cf1591a6c2c3330700c
Daniel Wang <dan@taiko.xyz>
2024-05-13 14:32:07 +0800

    Update Deploy.s.sol

--- packages/supplementary-contracts/script/tokenunlocking/Deploy.s.sol
+++ packages/supplementary-contracts/script/tokenunlocking/Deploy.s.sol
@@ -11,9 +11,9 @@ import "../../contracts/tokenUnlocking/TokenUnlocking.sol";
 contract DeployTokenUnlocking is Script {
     using stdJson for string;
 
-    address public OWNER = 0x9CBeE534B5D8a6280e01a14844Ee8aF350399C7F;  // admin.taiko.eth
+    address public OWNER = 0x9CBeE534B5D8a6280e01a14844Ee8aF350399C7F; // admin.taiko.eth
     address public TAIKO_TOKEN = 0x10dea67478c5F8C5E2D90e5E9B26dBe60c54d800; // token.taiko.eth
-    uint64 public TGE = 1716767999; // Date and time (GMT): Sunday, May 26, 2024 11:59:59 PM
+    uint64 public TGE = 1_716_767_999; // Date and time (GMT): Sunday, May 26, 2024 11:59:59 PM
     address public IMPL = vm.envAddress("TOKEN_VESTING_IMPL");
 
     function setUp() public { }
@@ -32,9 +32,7 @@ contract DeployTokenUnlocking is Script {
             vm.startBroadcast();
             deployProxy({
                 impl: impl,
-                data: abi.encodeCall(
-                    TokenUnlocking.init, (OWNER, TAIKO_TOKEN, recipients[i], TGE)
-                    )
+                data: abi.encodeCall(TokenUnlocking.init, (OWNER, TAIKO_TOKEN, recipients[i], TGE))
             });
             vm.stopBroadcast();
             console2.log("Deployed!\n");
