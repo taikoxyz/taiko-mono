ce18da59f325820bdccfc997e4a7264fd0d56df5
Korbinian <korbiniank@outlook.de>
2024-05-14 09:24:13 +0200

    cleanup

--- packages/guardian-prover-health-check-ui/src/lib/dataFetcher.ts
+++ packages/guardian-prover-health-check-ui/src/lib/dataFetcher.ts
@@ -1,9 +1,9 @@
 import {
-	GuardianProverStatus,
-	type BlockInfo,
-	type Guardian,
-	type SignedBlocks,
-	type VersionInfo
+  GuardianProverStatus,
+  type BlockInfo,
+  type Guardian,
+  type SignedBlocks,
+  type VersionInfo
 } from './types';
 import { fetchSignedBlocksFromApi } from './api/signedBlocksApiCalls';
 import { getGuardianProverIdsPerBlockNumber } from './blocks/getGuardianProverIdsPerBlockNumber';
@@ -11,20 +11,20 @@ import { sortSignedBlocksDescending } from './blocks/sortSignedBlocks';
 import { publicClient } from './wagmi/publicClient';
 import { formatEther, type Address } from 'viem';
 import {
-	fetchLatestGuardianProverHealthCheckFromApi,
-	fetchNodeInfoFromApi,
-	fetchStartupDataFromApi,
-	fetchUptimeFromApi
+  fetchLatestGuardianProverHealthCheckFromApi,
+  fetchNodeInfoFromApi,
+  fetchStartupDataFromApi,
+  fetchUptimeFromApi
 } from './api';
 import { fetchGuardianProverRequirementsFromContract } from './guardianProver/fetchGuardianProverRequirementsFromContract';
 import {
-	minGuardianRequirement,
-	lastGuardianFetchTimestamp,
-	guardianProvers,
-	signedBlocks,
-	signerPerBlock,
-	loading,
-	totalGuardianProvers
+  minGuardianRequirement,
+  lastGuardianFetchTimestamp,
+  guardianProvers,
+  signedBlocks,
+  signerPerBlock,
+  loading,
+  totalGuardianProvers
 } from '$stores';
 import { get } from 'svelte/store';
 import { getPseudonym } from './guardianProver/addressToPseudonym';
@@ -38,190 +38,185 @@ let guardiansIntervalId;
 let blocksAndLivelinessIntervalId;
 
 export async function startFetching() {
-	await refreshData();
+  await refreshData();
 
-	loading.set(false);
+  loading.set(false);
 
-	guardiansIntervalId = setInterval(fetchGuardians, 10000);
-	blocksAndLivelinessIntervalId = setInterval(async () => {
-		await Promise.all([fetchSignedBlockStats(), determineLiveliness()]);
-	}, 12000);
+  guardiansIntervalId = setInterval(fetchGuardians, 10000);
+  blocksAndLivelinessIntervalId = setInterval(async () => {
+    await Promise.all([fetchSignedBlockStats(), determineLiveliness()]);
+  }, 12000);
 }
 
 export function stopFetching() {
-	if (guardiansIntervalId) {
-		clearInterval(guardiansIntervalId);
-		guardiansIntervalId = null;
-	}
-	if (blocksAndLivelinessIntervalId) {
-		clearInterval(blocksAndLivelinessIntervalId);
-		blocksAndLivelinessIntervalId = null;
-	}
+  if (guardiansIntervalId) {
+    clearInterval(guardiansIntervalId);
+    guardiansIntervalId = null;
+  }
+  if (blocksAndLivelinessIntervalId) {
+    clearInterval(blocksAndLivelinessIntervalId);
+    blocksAndLivelinessIntervalId = null;
+  }
 }
 
 export async function refreshData() {
-	if (get(loading) === true) return;
-	loading.set(true);
+  if (get(loading) === true) return;
+  loading.set(true);
 
-	if (!get(guardianProvers)) {
-		// Initial data fetch
-		await initializeGuardians();
-		await fetchGuardians();
-		await fetchSignedBlockStats();
+  if (!get(guardianProvers)) {
 
-		await Promise.all([determineLiveliness(), fetchStats()]);
-	} else {
-		// Subsequent data refresh
-		await fetchGuardians();
-		const block = fetchSignedBlockStats();
-		const liveness = determineLiveliness();
-		const stats = fetchStats();
-		await Promise.all([block, stats, liveness]);
-	}
+    // Initial data fetch
+    await initializeGuardians();
+    await fetchGuardians();
+    await fetchSignedBlockStats();
 
-	loading.set(false);
+    await Promise.all([determineLiveliness(), fetchStats()]);
+  } else {
+
+    // Subsequent data refresh
+    await fetchGuardians();
+    const block = fetchSignedBlockStats();
+    const liveness = determineLiveliness();
+    const stats = fetchStats();
+    await Promise.all([block, stats, liveness]);
+  }
+
+  loading.set(false);
 }
 
 async function initializeGuardians() {
-	const guardiansMap = await loadGuardians();
-	const rawGuardians: Guardian[] = Object.entries(guardiansMap).map(([address, name], index) => ({
-		name: name,
-		address: address as Address,
-		id: index + 1, // add +1 as guardian contract numbers starts at 1
-		latestHealthCheck: null,
-		alive: GuardianProverStatus.UNKNOWN,
-		balance: null,
-		lastRestart: null,
-		uptime: null,
-		nodeInfo: null
-	}));
+  const guardiansMap = await loadGuardians();
+  const rawGuardians: Guardian[] = Object.entries(guardiansMap).map(([address, name], index) => ({
+    name: name,
+    address: address as Address,
+    id: index + 1, // add +1 as guardian contract numbers starts at 1
+    latestHealthCheck: null,
+    alive: GuardianProverStatus.UNKNOWN,
+    balance: null,
+    lastRestart: null,
+    uptime: null,
+    nodeInfo: null
+  }));
 
-	guardianProvers.set(rawGuardians);
+  guardianProvers.set(rawGuardians);
 }
 
 async function fetchGuardians() {
-	const existingGuardians = get(guardianProvers);
+  const existingGuardians = get(guardianProvers);
 
-	const [required] = await Promise.all([fetchGuardianProverRequirementsFromContract()]);
+  const [required] = await Promise.all([fetchGuardianProverRequirementsFromContract()]);
 
-	minGuardianRequirement.set(required);
-	totalGuardianProvers.set(existingGuardians?.length);
+  minGuardianRequirement.set(required);
+  totalGuardianProvers.set(existingGuardians?.length);
 
-	const guardianFetchPromises = existingGuardians.map(async (newGuardian) => {
-		const guardian = existingGuardians.find((g) => g.address === newGuardian.address) || {
-			...newGuardian,
-			alive: GuardianProverStatus.UNKNOWN
-		};
-		console.log('fetching guardian info for', guardian.address, guardian.id);
+  const guardianFetchPromises = existingGuardians.map(async (newGuardian) => {
+    const guardian = existingGuardians.find((g) => g.address === newGuardian.address) || {
+      ...newGuardian,
+      alive: GuardianProverStatus.UNKNOWN
+    };
+    guardian.name = await getPseudonym(guardian.address);
 
-		guardian.name = await getPseudonym(guardian.address);
+    const [status, uptime, balance] = await Promise.all([
+      fetchLatestGuardianProverHealthCheckFromApi(
+        import.meta.env.VITE_GUARDIAN_PROVER_API_URL,
+        guardian.address
+      ),
+      fetchUptimeFromApi(import.meta.env.VITE_GUARDIAN_PROVER_API_URL, guardian.address),
+      publicClient.getBalance({ address: guardian.address as Address })
+    ]);
 
-		console.log('getting balance for', guardian.name, guardian.id, guardian.address);
+    guardian.balance = formatEther(balance);
 
-		const [status, uptime, balance] = await Promise.all([
-			fetchLatestGuardianProverHealthCheckFromApi(
-				import.meta.env.VITE_GUARDIAN_PROVER_API_URL,
-				guardian.address
-			),
-			fetchUptimeFromApi(import.meta.env.VITE_GUARDIAN_PROVER_API_URL, guardian.address),
-			publicClient.getBalance({ address: guardian.address as Address })
-		]);
+    guardian.latestHealthCheck = status;
+    guardian.uptime = Math.min(uptime, 100);
 
-		guardian.balance = formatEther(balance);
-		console.log('balance', guardian.name, guardian.balance);
+    return guardian;
+  });
 
-		guardian.latestHealthCheck = status;
-		guardian.uptime = Math.min(uptime, 100);
-
-		return guardian;
-	});
-
-	const updatedGuardians = await Promise.all(guardianFetchPromises);
-	guardianProvers.set(updatedGuardians);
-	lastGuardianFetchTimestamp.set(Date.now());
-	console.log('updatedGuardians', updatedGuardians);
+  const updatedGuardians = await Promise.all(guardianFetchPromises);
+  guardianProvers.set(updatedGuardians);
+  lastGuardianFetchTimestamp.set(Date.now());
 }
 
 async function fetchSignedBlockStats() {
-	const blocks: SignedBlocks = await fetchSignedBlocksFromApi(
-		import.meta.env.VITE_GUARDIAN_PROVER_API_URL
-	);
+  const blocks: SignedBlocks = await fetchSignedBlocksFromApi(
+    import.meta.env.VITE_GUARDIAN_PROVER_API_URL
+  );
 
-	signedBlocks.set(sortSignedBlocksDescending(blocks));
+  signedBlocks.set(sortSignedBlocksDescending(blocks));
 
-	const signer = await getGuardianProverIdsPerBlockNumber(blocks);
-	signerPerBlock.set(signer);
+  const signer = await getGuardianProverIdsPerBlockNumber(blocks);
+  signerPerBlock.set(signer);
 }
 
 async function determineLiveliness(): Promise<void> {
-	const now = new Date();
-	guardianProvers.update((guardians) =>
-		guardians.map((guardian) => {
-			const latestCheck = guardian.latestHealthCheck;
-			const createdAt = new Date(latestCheck.createdAt);
-			const secondsSinceLastCheck = (now.getTime() - createdAt.getTime()) / 1000;
-			let aliveStatus = guardian.alive;
+  const now = new Date();
+  guardianProvers.update((guardians) =>
+    guardians.map((guardian) => {
+      const latestCheck = guardian.latestHealthCheck;
+      const createdAt = new Date(latestCheck.createdAt);
+      const secondsSinceLastCheck = (now.getTime() - createdAt.getTime()) / 1000;
+      let aliveStatus = guardian.alive;
 
-			if (secondsSinceLastCheck > HEALTHCHECK_TIMEOUT_IN_SECONDS) {
-				aliveStatus = GuardianProverStatus.DEAD;
-			} else {
-				let countSignedBlocks = 0;
-				const recentSignedBlocks = get(signedBlocks).slice(0, BLOCKS_TO_CHECK);
-				for (const block of recentSignedBlocks) {
-					if (block.blocks.some((b) => b.guardianProverID === Number(guardian.id))) {
-						countSignedBlocks++;
-					}
-				}
-				aliveStatus =
-					countSignedBlocks >= THRESHOLD
-						? GuardianProverStatus.ALIVE
-						: GuardianProverStatus.UNHEALTHY;
-			}
+      if (secondsSinceLastCheck > HEALTHCHECK_TIMEOUT_IN_SECONDS) {
+        aliveStatus = GuardianProverStatus.DEAD;
+      } else {
+        let countSignedBlocks = 0;
+        const recentSignedBlocks = get(signedBlocks).slice(0, BLOCKS_TO_CHECK);
+        for (const block of recentSignedBlocks) {
+          if (block.blocks.some((b) => b.guardianProverID === Number(guardian.id))) {
+            countSignedBlocks++;
+          }
+        }
+        aliveStatus =
+          countSignedBlocks >= THRESHOLD
+            ? GuardianProverStatus.ALIVE
+            : GuardianProverStatus.UNHEALTHY;
+      }
 
-			return { ...guardian, alive: aliveStatus };
-		})
-	);
+      return { ...guardian, alive: aliveStatus };
+    })
+  );
 }
 async function fetchStats(): Promise<void> {
-	const guardians = get(guardianProvers);
+  const guardians = get(guardianProvers);
 
-	const updatedGuardiansPromises = guardians.map(async (guardian) => {
-		console.log('fetching stats for', guardian.address);
-		const startupDataFetch = fetchStartupDataFromApi(
-			import.meta.env.VITE_GUARDIAN_PROVER_API_URL,
-			guardian.address
-		);
+  const updatedGuardiansPromises = guardians.map(async (guardian) => {
+    const startupDataFetch = fetchStartupDataFromApi(
+      import.meta.env.VITE_GUARDIAN_PROVER_API_URL,
+      guardian.address
+    );
 
-		const nodeInfoFetch = fetchNodeInfoFromApi(
-			import.meta.env.VITE_GUARDIAN_PROVER_API_URL,
-			guardian.address
-		);
+    const nodeInfoFetch = fetchNodeInfoFromApi(
+      import.meta.env.VITE_GUARDIAN_PROVER_API_URL,
+      guardian.address
+    );
 
-		const [startupData, nodeInfo] = await Promise.all([startupDataFetch, nodeInfoFetch]);
+    const [startupData, nodeInfo] = await Promise.all([startupDataFetch, nodeInfoFetch]);
 
-		const versions: VersionInfo = {
-			guardianProverAddress: startupData.guardianProverAddress,
-			guardianProverID: startupData.guardianProverID,
-			guardianVersion: nodeInfo.guardianVersion,
-			l1NodeVersion: nodeInfo.l1NodeVersion,
-			l2NodeVersion: nodeInfo.l2NodeVersion,
-			revision: startupData.revision
-		};
+    const versions: VersionInfo = {
+      guardianProverAddress: startupData.guardianProverAddress,
+      guardianProverID: startupData.guardianProverID,
+      guardianVersion: nodeInfo.guardianVersion,
+      l1NodeVersion: nodeInfo.l1NodeVersion,
+      l2NodeVersion: nodeInfo.l2NodeVersion,
+      revision: startupData.revision
+    };
 
-		const blockInfo: BlockInfo = {
-			latestL1BlockNumber: nodeInfo.latestL1BlockNumber,
-			latestL2BlockNumber: nodeInfo.latestL2BlockNumber
-		};
+    const blockInfo: BlockInfo = {
+      latestL1BlockNumber: nodeInfo.latestL1BlockNumber,
+      latestL2BlockNumber: nodeInfo.latestL2BlockNumber
+    };
 
-		return {
-			...guardian,
-			id: versions.guardianProverID,
-			versionInfo: versions,
-			lastRestart: startupData.createdAt,
-			blockInfo: blockInfo
-		};
-	});
+    return {
+      ...guardian,
+      id: versions.guardianProverID,
+      versionInfo: versions,
+      lastRestart: startupData.createdAt,
+      blockInfo: blockInfo
+    };
+  });
 
-	const updatedGuardians = await Promise.all(updatedGuardiansPromises);
-	guardianProvers.set(updatedGuardians);
+  const updatedGuardians = await Promise.all(updatedGuardiansPromises);
+  guardianProvers.set(updatedGuardians);
 }
