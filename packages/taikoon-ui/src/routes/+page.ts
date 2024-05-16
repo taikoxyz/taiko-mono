6022f3d3c5d21f8b4ee827191a523228e26be4cd
bearni95 <bernatcanal@gmail.com>
2024-05-16 15:30:49 +0200

    testing with vercel's package

--- packages/taikoon-ui/src/routes/+page.ts
+++ packages/taikoon-ui/src/routes/+page.ts
@@ -1,4 +1,5 @@
 import { redirect } from '@sveltejs/kit';
+import { geolocation } from '@vercel/edge';
 
 const blacklistedCountries = [
   'AF', // Afghanistan (AF)
@@ -27,6 +28,8 @@ const blacklistedCountries = [
 export function load(event: any) {
   try {
     console.warn('PAGE.ts', 'onLoad', event);
+    const res = geolocation(event)
+    console.error('geolocation res?', {res})
     const country = event.request.headers.get('x-vercel-ip-country') ?? 'dev';
     console.warn('PAGE.ts', 'page load event', {
       country,
