c501c9ecb921cfdea23f8960ab3d8a6833387e57
bearni95 <bernatcanal@gmail.com>
2024-05-16 15:18:32 +0200

    removed ERC1976Proxy unused reference

--- packages/supplementary-contracts/script/blacklist/Deploy.sol
+++ packages/supplementary-contracts/script/blacklist/Deploy.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.24;
 import "forge-std/src/Script.sol";
 import "forge-std/src/console2.sol";
 
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
 import "../../contracts/blacklist/Blacklist.sol";
 
 contract DeployBlacklist is Script {
