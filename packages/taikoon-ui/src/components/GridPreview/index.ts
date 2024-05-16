02c1a26614e767d900b053a9919d0a1611176630
bearni95 <bernatcanal@gmail.com>
2024-05-16 21:29:37 +0200

    emergency commit

---
+++ packages/taikoon-ui/src/components/GridPreview/index.ts
@@ -0,0 +1,1 @@
+export { default as GridPreview } from './GridPreview.svelte';
---
+++ packages/taikoon-ui/src/components/sections/Preview.section.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import { t } from 'svelte-i18n';
+    import { AnimatedEyes } from '$components/AnimatedTaikoon';
+    import { classNames } from '$lib/util/classNames';
+    import { Section } from '$ui/Section';
+    import { onMount } from 'svelte';
+
+    const titleClasses = classNames(
+      'w-full',
+      'text-left',
+      'text-primary',
+      'mb-4',
+      'uppercase',
+      'tracking-normal',
+      'text-[16px]/[24px]',
+      'font-bold',
+      'font-sans',
+      'leading-relaxed',
+    );
+    const contentClasses = classNames(
+      'w-3/4',
+      'md:font-normal',
+      'md:text-[57px]/[64px]',
+      'text-content-primary',
+      'font-medium',
+      'font-clash-grotesk',
+      'text-4xl',
+    );
+
+    $: eyes = Array.from({ length: 12 }, (_, i) => i)
+
+  </script>
+
+  <Section>
+
+    <div class={classNames('py-32')}>
+    <p class={titleClasses}>
+      {$t('content.sections.information.title')}
+    </p>
+
+    <div class={contentClasses}>
+      {$t('content.sections.information.text')}
+    </div>
+
+    <div class={classNames(
+      'w-full',
+      'h-min',
+      'flex',
+      'absolute',
+      'left-0',
+      'bottom-0',
+    )}>
+
+    {#each eyes as eye}
+    <div class={classNames(
+      'w-1/12',
+      'h-max',
+      'flex',
+      'justify-center',
+      'items-center',
+    )}>
+
+   <AnimatedEyes/>
+  </div>
+    {/each}</div>
+
+  </div>
+  </Section>
