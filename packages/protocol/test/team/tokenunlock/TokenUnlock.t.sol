9b4a78083068f4bd6eee75e23ae45aed677adc7b
Daniel Wang <dan@taiko.xyz>
2024-05-20 10:48:56 +0800

    test

--- packages/protocol/test/team/tokenunlock/TokenUnlock.t.sol
+++ packages/protocol/test/team/tokenunlock/TokenUnlock.t.sol
@@ -32,6 +32,8 @@ contract MyERC20 is ERC20, ERC20Votes {
 
 contract TestTokenUnlock is TaikoTest {
     AddressManager private addressManager;
+    address public assignmentHook = vm.addr(0x1000);
+    address public taikoL1 = vm.addr(0x2000);
     uint64 private TGE = 1_000_000;
 
     TokenUnlock private target;
@@ -47,6 +49,9 @@ contract TestTokenUnlock is TaikoTest {
         );
 
         addressManager.setAddress(uint64(block.chainid), "taiko_token", address(tko));
+        addressManager.setAddress(uint64(block.chainid), "assignment_hook", assignmentHook);
+        addressManager.setAddress(uint64(block.chainid), "taiko", taikoL1);
+        addressManager.setAddress(uint64(block.chainid), "prover_set", address(new ProverSet()));
 
         vm.warp(TGE);
 
@@ -254,6 +259,55 @@ contract TestTokenUnlock is TaikoTest {
     function test_tokenunlock_proverset() public {
         vm.startPrank(Alice);
         target.vest(100 ether);
+        tko.transfer(address(target), 20 ether);
+        vm.warp(TGE + target.ONE_YEAR() * 2);
+
+        vm.expectRevert(TokenUnlock.PERMISSION_DENIED.selector);
+        target.createProverSet();
         vm.stopPrank();
+
+        vm.startPrank(Bob);
+        vm.expectRevert(TokenUnlock.NOT_PROVER_SET.selector);
+        target.depositToProverSet(vm.addr(0x1234), 1 ether);
+
+        ProverSet set1 = ProverSet(target.createProverSet());
+        assertEq(set1.owner(), target.owner());
+        assertEq(set1.admin(), address(target));
+
+        assertTrue(target.isProverSet(address(set1)));
+
+        vm.expectRevert(); //  ERC20: transfer amount exceeds balance
+        target.depositToProverSet(address(set1), 121 ether);
+
+        target.depositToProverSet(address(set1), 120 ether);
+        assertEq(tko.balanceOf(address(set1)), 120 ether);
+        assertEq(tko.balanceOf(address(target)), 0 ether);
+        assertEq(target.amountVested(), 100 ether);
+        assertEq(target.amountWithdrawable(), 0 ether);
+
+        vm.expectRevert(); //  ERC20: transfer amount exceeds balance
+        set1.withdrawToAdmin(121 ether);
+
+        set1.withdrawToAdmin(120 ether);
+        assertEq(tko.balanceOf(address(set1)), 0 ether);
+        assertEq(tko.balanceOf(address(target)), 120 ether);
+        assertEq(target.amountVested(), 100 ether);
+        assertEq(target.amountWithdrawable(), 70 ether);
+
+        set1.enableProver(Carol, true);
+        assertTrue(set1.isProver(Carol));
+
+        // create another one
+        target.createProverSet();
+
+        vm.stopPrank();
+
+        vm.prank(target.owner());
+        vm.expectRevert(TokenUnlock.PERMISSION_DENIED.selector);
+        set1.enableProver(David, true);
+
+        vm.prank(David);
+        vm.expectRevert(TokenUnlock.PERMISSION_DENIED.selector);
+        set1.enableProver(Carol, true);
     }
 }
