c1e0f4eb673dcf3c550840d6ab523b9c910a647d
Daniel Wang <dan@taiko.xyz>
2024-05-20 13:48:18 +0800

    Update Deploy.s.sol

--- packages/protocol/script/tokenunlock/Deploy.s.sol
+++ packages/protocol/script/tokenunlock/Deploy.s.sol
@@ -13,7 +13,7 @@ contract DeployTokenUnlock is Script {
     address public OWNER = 0x9CBeE534B5D8a6280e01a14844Ee8aF350399C7F; // admin.taiko.eth
     address public ROLLUP_ADDRESS_MANAGER = 0x579f40D0BE111b823962043702cabe6Aaa290780;
     uint64 public TGE = 1_716_767_999; // Date and time (GMT): Sunday, May 26, 2024 11:59:59 PM
-    address public IMPL = 0x244108e321FE03b0E33FE63Ef62285F05d191a62;
+    address public TOKEN_UNLOCK_IMPL = 0x88F4039C2fD4489cdca4E2ce6917a81a38509F2E;
 
     function setUp() public { }
 
@@ -24,17 +24,17 @@ contract DeployTokenUnlock is Script {
         );
 
         for (uint256 i; i < recipients.length; i++) {
-            console2.log("Grantee:", recipients[i]);
+            console2.log("grantee:", recipients[i]);
 
             vm.startBroadcast();
-            deployProxy({
-                impl: IMPL,
+            address proxy = deployProxy({
+                impl: TOKEN_UNLOCK_IMPL,
                 data: abi.encodeCall(
                     TokenUnlock.init, (OWNER, ROLLUP_ADDRESS_MANAGER, recipients[i], TGE)
                 )
             });
             vm.stopBroadcast();
-            console2.log("Deployed!\n");
+            console2.log("tokenUnlock:", proxy);
         }
     }
 
