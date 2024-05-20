80fcea76b0ce9b2c38f672c6dbad0b4d522f88cb
Daniel Wang <dan@taiko.xyz>
2024-05-20 14:02:52 +0800

    Update TokenUnlock.sol

--- packages/protocol/contracts/team/tokenunlock/TokenUnlock.sol
+++ packages/protocol/contracts/team/tokenunlock/TokenUnlock.sol
@@ -78,11 +78,11 @@ contract TokenUnlock is EssentialContract {
         uint64 _tgeTimestamp
     )
         external
+        nonZeroAddr(_recipient)
+        nonZeroValue(bytes32(_tgeTimestamp))
         initializer
     {
-        if (_owner == _recipient || _recipient == address(0) || _tgeTimestamp == 0) {
-            revert INVALID_PARAM();
-        }
+        if (_owner == _recipient) revert INVALID_PARAM();
 
         __Essential_init(_owner, _addressManager);
 
