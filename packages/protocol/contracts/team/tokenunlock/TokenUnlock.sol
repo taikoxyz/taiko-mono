4b8ea315fed472475d0ada69e3b335004adf0a78
Daniel Wang <dan@taiko.xyz>
2024-05-20 14:00:50 +0800

    Update TokenUnlock.sol

--- packages/protocol/contracts/team/tokenunlock/TokenUnlock.sol
+++ packages/protocol/contracts/team/tokenunlock/TokenUnlock.sol
@@ -25,11 +25,11 @@ contract TokenUnlock is EssentialContract {
     uint256 public constant ONE_YEAR = 365 days;
     uint256 public constant FOUR_YEARS = 4 * ONE_YEAR;
 
-    uint256 public amountVested;
-    address public recipient;
+    uint256 public amountVested; // slot 1
+    address public recipient; // slot 2
     uint64 public tgeTimestamp;
 
-    mapping(address proverSet => bool valid) public isProverSet;
+    mapping(address proverSet => bool valid) public isProverSet; // slot 3
 
     uint256[47] private __gap;
 
