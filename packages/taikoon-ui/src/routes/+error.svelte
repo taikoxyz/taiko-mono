1c80f9c2e4fb18cd752314a43d3e994f0b3cecd8
bearni95 <bernatcanal@gmail.com>
2024-05-20 11:05:29 +0200

    included banned countries on error page; cleanup

--- packages/taikoon-ui/src/routes/+error.svelte
+++ packages/taikoon-ui/src/routes/+error.svelte
@@ -2,6 +2,10 @@
   import { page } from '$app/stores';
 </script>
 
+<svelte:head>
+  <title>Taikoons | Error</title>
+</svelte:head>
+
 {#if $page && $page.error && $page.error.message}
   <h1>{$page.error.message}</h1>
 {/if}
--- packages/taikoon-ui/src/routes/+page.server.ts
+++ packages/taikoon-ui/src/routes/+page.server.ts
@@ -1,35 +1,38 @@
 import { error } from '@sveltejs/kit';
 
-const blacklistedCountries = [
-  'AF', // Afghanistan (AF)
-  'BY', // Belarus (BY)
-  'MM', // Burma/Myanmar (MM)
-  'CF', // Central African Republic (CF)
-  'CN', // China (CN)
-  'CU', // Cuba (CU)
-  'CD', // Democratic Republic of the Congo (CD)
-  'ET', // Ethiopia (ET)
-  'IR', // Iran (IR)
-  'IQ', // Iraq (IQ)
-  'LB', // Lebanon (LB)
-  'LY', // Libya (LY)
-  'ML', // Mali (ML)
-  'NI', // Nicaragua (NI)
-  'KP', // North Korea (KP)
-  'RU', // Russia (RU)
-  'SO', // Somalia (SO)
-  'SS', // South Sudan (SS)
-  'SD', // Sudan (SD)
-  'SY', // Syria (SY)
-  'VE', // Venezuela (VE)
-  'US', // United States (US)
-];
+const bannedCountries: Record<string, string> = {
+  AF: 'Afghanistan',
+  BY: 'Belarus',
+  MM: 'Burma/Myanmar',
+  CF: 'Central African Republic',
+  CN: 'China',
+  CU: 'Cuba',
+  CD: 'Democratic Republic of the Congo',
+  ET: 'Ethiopia',
+  IR: 'Iran',
+  IQ: 'Iraq',
+  LB: 'Lebanon',
+  LY: 'Libya',
+  ML: 'Mali',
+  NI: 'Nicaragua',
+  KP: 'North Korea',
+  RU: 'Russia',
+  SO: 'Somalia',
+  SS: 'South Sudan',
+  SD: 'Sudan',
+  SY: 'Syria',
+  VE: 'Venezuela',
+  US: 'United States',
+};
 
+const bannedCountryCodes = Object.keys(bannedCountries);
 export function load(event: any) {
   const country = event.request.headers.get('x-vercel-ip-country') ?? false;
-  const isDev = event.url.hostname === 'localhost';
-  if (!isDev && (!country || blacklistedCountries.includes(country))) {
-    return error(400, { message: 'This site is not available in your country.' });
+  const isDev = false; // event.url.hostname === 'localhost';
+  if (!isDev && (!country || bannedCountryCodes.includes(country))) {
+    return error(400, {
+      message: `The site is not available on the following countries: ${Object.values(bannedCountries).join(', ')}`,
+    });
   }
   return {
     location: { country },
