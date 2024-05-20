f1490c4d1b3f572d03e2d2c9788bedc13265ce87
bearni95 <bernatcanal@gmail.com>
2024-05-20 13:29:07 +0200

    migrated snaefell code to nfts package; compiles and passing tests

--- packages/nfts/script/snaefell/sol/Utils.s.sol
+++ packages/nfts/script/snaefell/sol/Utils.s.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.24;
 
 import { Script, console } from "forge-std/src/Script.sol";
 import "forge-std/src/StdJson.sol";
-import { IMinimalBlacklist } from "@taiko/blacklist/IMinimalBlacklist.sol";
-import { MockBlacklist } from "../../../test/util/Blacklist.sol";
 
 contract UtilsScript is Script {
     using stdJson for string;
@@ -23,15 +21,9 @@ contract UtilsScript is Script {
         if (chainId == 31_337) {
             lowercaseNetworkKey = "localhost";
             uppercaseNetworkKey = "LOCALHOST";
-        } else if (chainId == 17_000) {
-            lowercaseNetworkKey = "holesky";
-            uppercaseNetworkKey = "HOLESKY";
         } else if (chainId == 167_001) {
             lowercaseNetworkKey = "devnet";
             uppercaseNetworkKey = "DEVNET";
-        } else if (chainId == 11_155_111) {
-            lowercaseNetworkKey = "sepolia";
-            uppercaseNetworkKey = "SEPOLIA";
         } else if (chainId == 167_008) {
             lowercaseNetworkKey = "katla";
             uppercaseNetworkKey = "KATLA";
@@ -52,23 +44,11 @@ contract UtilsScript is Script {
 
     function getContractJsonLocation() public view returns (string memory) {
         string memory root = vm.projectRoot();
-        return string.concat(root, "/deployments/taikoon/", lowercaseNetworkKey, ".json");
+        return string.concat(root, "/deployments/", lowercaseNetworkKey, ".json");
     }
 
     function getIpfsBaseURI() public view returns (string memory) {
         return vm.envString("IPFS_BASE_URI");
-    }
-
-    function getBlacklist() public returns (IMinimalBlacklist blacklistAddress) {
-        if (block.chainid == 1) {
-            // mainnet blacklist address
-            blacklistAddress = IMinimalBlacklist(0x97044531D0fD5B84438499A49629488105Dc58e6);
-        } else {
-            // deploy a mock blacklist otherwise
-            blacklistAddress = IMinimalBlacklist(new MockBlacklist());
-        }
-
-        return blacklistAddress;
     }
 
     function run() public { }
