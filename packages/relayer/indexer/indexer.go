1d67bd8efc88baa99873622d53865d34ff527c51
Jeffery Walsh <cyberhorsey@gmail.com>
2024-05-21 09:14:42 -0700

    lint

--- packages/relayer/indexer/indexer.go
+++ packages/relayer/indexer/indexer.go
@@ -178,6 +178,7 @@ func InitFromConfig(ctx context.Context, i *Indexer, cfg *Config) (err error) {
 
 	// taikoL1 will only be set when initializing a L1 - L2 indexer
 	var taikoL1 *taikol1.TaikoL1
+
 	if cfg.SrcTaikoAddress != ZeroAddress {
 		slog.Info("setting srcTaikoAddress", "addr", cfg.SrcTaikoAddress.Hex())
 
@@ -188,6 +189,7 @@ func InitFromConfig(ctx context.Context, i *Indexer, cfg *Config) (err error) {
 	}
 
 	var signalService relayer.SignalService
+
 	if cfg.SrcSignalServiceAddress != ZeroAddress {
 		slog.Info("setting srcSignalServiceAddress", "addr", cfg.SrcSignalServiceAddress.Hex())
 
