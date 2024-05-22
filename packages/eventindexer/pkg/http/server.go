b158da8e96b1cb42a0e4300b7ffae3b7a995eb40
ddl <ddl196526@163.com>
2024-05-22 17:51:01 +0800

    fix(eventindexer):remove redundant opts.CorsOrigins checks

--- packages/eventindexer/pkg/http/server.go
+++ packages/eventindexer/pkg/http/server.go
@@ -53,10 +53,6 @@ func (opts NewServerOpts) Validate() error {
 		return eventindexer.ErrNoEventRepository
 	}
 
-	if opts.CorsOrigins == nil {
-		return eventindexer.ErrNoCORSOrigins
-	}
-
 	if opts.NFTBalanceRepo == nil {
 		return eventindexer.ErrNoNFTBalanceRepository
 	}
