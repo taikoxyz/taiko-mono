e734513b226dc1e7dbcff96e57d20ebadac5334b
Daniel Wang <dan@taiko.xyz>
2024-05-27 13:07:17 +0700

    more

--- packages/protocol/script/L2PostGenesisConfig.s.sol
+++ packages/protocol/script/L2PostGenesisConfig.s.sol
@@ -22,7 +22,6 @@ contract L2PostGenesisConfig is Script {
     address public erc20Vault = 0x1670000000000000000000000000000000000002;
     address public bridge = 0x1670000000000000000000000000000000000001;
     address public sam = 0x1670000000000000000000000000000000000006;
-    address public ram = 0x1670000000000000000000000000000000010002;
 
     function run() external view {
         bytes memory call;
@@ -48,13 +47,6 @@ contract L2PostGenesisConfig is Script {
             console2.log("--- sam set tko token");
             console2.log(sam);
             console.logBytes(call);
-
-            call = abi.encodeCall(
-                AddressManager.setAddress, (167_000, LibStrings.B_TAIKO_TOKEN, bridgedTKO)
-            );
-            console2.log("--- ram set tko token");
-            console2.log(ram);
-            console.logBytes(call);
         }
 
         if (bridgedUSDC != address(0)) {
@@ -75,46 +67,46 @@ contract L2PostGenesisConfig is Script {
             console2.log(erc20Vault);
             console.logBytes(call);
         }
+        {
+            call = abi.encodeCall(
+                AddressManager.setAddress,
+                (1, LibStrings.B_SIGNAL_SERVICE, 0x9e0a24964e5397B566c1ed39258e21aB5E35C77C)
+            );
+            console2.log("--- sam set signal service chain_id=1");
+            console.logBytes(call);
 
-        call = abi.encodeCall(
-            AddressManager.setAddress,
-            (1, LibStrings.B_SIGNAL_SERVICE, 0x9e0a24964e5397B566c1ed39258e21aB5E35C77C)
-        );
-        console2.log("--- sam set signal service chain_id=1");
-        console2.log(sam);
-        console.logBytes(call);
+            call = abi.encodeCall(
+                AddressManager.setAddress,
+                (1, LibStrings.B_BRIDGE, 0xd60247c6848B7Ca29eDdF63AA924E53dB6Ddd8EC)
+            );
+            console2.log("--- sam set bridge chain_id=1");
+            console2.log(sam);
+            console.logBytes(call);
 
-        call = abi.encodeCall(
-            AddressManager.setAddress,
-            (1, LibStrings.B_BRIDGE, 0xd60247c6848B7Ca29eDdF63AA924E53dB6Ddd8EC)
-        );
-        console2.log("--- sam set bridge chain_id=1");
-        console2.log(sam);
-        console.logBytes(call);
+            call = abi.encodeCall(
+                AddressManager.setAddress,
+                (1, LibStrings.B_ERC20_VAULT, 0x996282cA11E5DEb6B5D122CC3B9A1FcAAD4415Ab)
+            );
+            console2.log("--- sam set erc20 vault chain_id=1");
+            console2.log(sam);
+            console.logBytes(call);
 
-        call = abi.encodeCall(
-            AddressManager.setAddress,
-            (1, LibStrings.B_ERC20_VAULT, 0x996282cA11E5DEb6B5D122CC3B9A1FcAAD4415Ab)
-        );
-        console2.log("--- sam set erc20 vault chain_id=1");
-        console2.log(sam);
-        console.logBytes(call);
+            call = abi.encodeCall(
+                AddressManager.setAddress,
+                (1, LibStrings.B_ERC721_VAULT, 0x0b470dd3A0e1C41228856Fb319649E7c08f419Aa)
+            );
+            console2.log("--- sam set erc721 vault chain_id=1");
+            console2.log(sam);
+            console.logBytes(call);
 
-        call = abi.encodeCall(
-            AddressManager.setAddress,
-            (1, LibStrings.B_ERC721_VAULT, 0x0b470dd3A0e1C41228856Fb319649E7c08f419Aa)
-        );
-        console2.log("--- sam set erc721 vault chain_id=1");
-        console2.log(sam);
-        console.logBytes(call);
-
-        call = abi.encodeCall(
-            AddressManager.setAddress,
-            (1, LibStrings.B_ERC1155_VAULT, 0xaf145913EA4a56BE22E120ED9C24589659881702)
-        );
-        console2.log("--- sam set erc1155 vault chain_id=1");
-        console2.log(sam);
-        console.logBytes(call);
+            call = abi.encodeCall(
+                AddressManager.setAddress,
+                (1, LibStrings.B_ERC1155_VAULT, 0xaf145913EA4a56BE22E120ED9C24589659881702)
+            );
+            console2.log("--- sam set erc1155 vault chain_id=1");
+            console2.log(sam);
+            console.logBytes(call);
+        }
 
         // Bridge(bridge).unpause();
         call = abi.encodeCall(EssentialContract.unpause, ());
