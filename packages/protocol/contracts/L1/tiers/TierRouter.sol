2ff393100c3af4128027bc9dacc54066421392b7
Daniel Wang <dan@taiko.xyz>
2024-05-28 20:42:13 +0700

    Update TierRouter.sol

--- packages/protocol/contracts/L1/tiers/TierRouter.sol
+++ packages/protocol/contracts/L1/tiers/TierRouter.sol
@@ -7,8 +7,14 @@ import "./ITierRouter.sol";
 /// @title TierProviderBase
 /// @dev Labeled in AddressResolver as "tier_router"
 /// @custom:security-contact security@taiko.xyz
-abstract contract TierRouter is EssentialContract, ITierRouter {
+contract TierRouter is EssentialContract, ITierRouter {
     uint256[50] private __gap;
+
+    /// @notice Initializes the contract.
+    /// @param _owner The owner of this contract. msg.sender will be used if this value is zero.
+    function init(address _owner) external initializer {
+        __Essential_init(_owner);
+    }
 
     /// @inheritdoc ITierRouter
     function getProvider(uint256 /*_blockId*/ ) external pure returns (address) {
