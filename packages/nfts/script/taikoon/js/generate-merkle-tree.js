918b2ae9bb96544be4b7813004eb3dade8ca7279
Kenk <kenghin_lim@hotmail.com>
2024-05-29 09:57:18 +0700

    update generate script

--- packages/nfts/script/taikoon/js/generate-merkle-tree.js
+++ packages/nfts/script/taikoon/js/generate-merkle-tree.js
@@ -3,32 +3,50 @@ const path = require("path");
 const fs = require("fs");
 const ConvertCsvToJson = require("convert-csv-to-json");
 
-async function main(network) {
-  const inputFile = path.join(
-    __dirname,
-    `../../../data/taikoon/whitelist/${network}.csv`,
-  );
-  const outputFile = path.join(
-    __dirname,
-    `../../../data/taikoon/whitelist/${network}.json`,
-  );
-  const rawJson =
-    ConvertCsvToJson.fieldDelimiter(",").getJsonFromCsv(inputFile);
+async function main() {
+  // Get Files in taiko/whitelist folder
+  const whitelistDir = path.join(__dirname, "../../../data/taikoon/whitelist");
+  const whitelistFiles = fs.readdirSync(whitelistDir);
 
-  const values = rawJson.map((entry) => {
-    return [entry.address, entry.freeMints];
+  whitelistFiles.forEach((file) => {
+    // Skip files that are not csv
+    if (file.endsWith(".csv") === false) {
+      return;
+    }
+
+    // extract filename without extension
+    const network = file.split(".")[0];
+
+    const inputFile = path.join(
+      __dirname,
+      `../../../data/taikoon/whitelist/${network}.csv`,
+    );
+    const outputFile = path.join(
+      __dirname,
+      `../../../data/taikoon/whitelist/${network}.json`,
+    );
+    const rawJson =
+      ConvertCsvToJson.fieldDelimiter(",").getJsonFromCsv(inputFile);
+
+    const values = rawJson.map((entry) => {
+      return [entry.address, entry.freeMints];
+    });
+
+    const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
+
+    fs.writeFileSync(
+      outputFile,
+      JSON.stringify({ ...tree.dump(), root: tree.root }, null, 2),
+    );
+
+    console.log(`Merkle Root for network ${network}`, tree.root);
   });
-
-  const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
-
-  fs.writeFileSync(
-    outputFile,
-    JSON.stringify({ ...tree.dump(), root: tree.root }, null, 2),
-  );
-
-  console.log(`Merkle Root for network ${network}`, tree.root);
 }
 
-main("hardhat");
-main("devnet");
-main('mainnet')
+main()
+  .then(() => {
+    console.log("Done");
+  })
+  .catch((error) => {
+    console.error("Error", error);
+  });
