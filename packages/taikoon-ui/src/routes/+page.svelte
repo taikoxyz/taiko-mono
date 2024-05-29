16640c72533142cde0a6cd326aebfb7d28b87d1a
bearni95 <bernatcanal@gmail.com>
2024-05-29 19:11:55 +0200

    format, lint

--- packages/taikoon-ui/src/routes/+page.svelte
+++ packages/taikoon-ui/src/routes/+page.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
   import { t } from 'svelte-i18n';
 
-  import {
-    CountdownSection,
-    FaqSection,
-    FooterSection,
-    HeadingSection,
-    InformationSection,
-    PreviewSection,
-  } from '$components/sections';
-  import isCountdownActive from '$lib/util/isCountdownActive';
+  import { FaqSection, FooterSection, HeadingSection, InformationSection, PreviewSection } from '$components/sections';
   import { Button } from '$ui/Button';
   import { SectionContainer } from '$ui/Section';
 
@@ -26,13 +18,12 @@
 </svelte:head>
 
 <SectionContainer>
-
-    <HeadingSection>
-      <div class="bottom-16 left-0 w-full flex justify-center absolute">
-        <Button type="ghost" size="lg" iconRight="ArrowDown" on:click={scrollToFaq} class="uppercase"
-          >{$t('buttons.learnMore')}</Button>
-      </div>
-    </HeadingSection>
+  <HeadingSection>
+    <div class="bottom-16 left-0 w-full flex justify-center absolute">
+      <Button type="ghost" size="lg" iconRight="ArrowDown" on:click={scrollToFaq} class="uppercase"
+        >{$t('buttons.learnMore')}</Button>
+    </div>
+  </HeadingSection>
   <InformationSection />
   <PreviewSection />
   <div bind:this={scrollTarget}>
