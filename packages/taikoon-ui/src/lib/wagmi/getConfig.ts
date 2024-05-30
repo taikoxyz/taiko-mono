688127bc0b61991aff4d66c5a5d49c5a26b9c445
bearni95 <bernatcanal@gmail.com>
2024-05-30 13:26:48 +0200

    proper holesky default

--- packages/taikoon-ui/src/lib/wagmi/getConfig.ts
+++ packages/taikoon-ui/src/lib/wagmi/getConfig.ts
@@ -1,4 +1,6 @@
-import { config, publicConfig, taiko } from '$wagmi-config';
+import { holesky } from 'viem/chains';
+
+import { config, publicConfig } from '$wagmi-config';
 
 import type { IChainId } from '../../types';
 import { web3modal } from '../connect';
@@ -7,7 +9,8 @@ export default function getConfig() {
   const { selectedNetworkId } = web3modal.getState();
   const wagmiConfig = selectedNetworkId ? config : publicConfig;
 
-  const chainId = selectedNetworkId ? selectedNetworkId : taiko.id;
+  // const chainId = selectedNetworkId ? selectedNetworkId : taiko.id;
+  const chainId = selectedNetworkId ? selectedNetworkId : holesky.id;
 
   return {
     config: wagmiConfig,
--- packages/taikoon-ui/src/lib/wagmi/publicClient.ts
+++ packages/taikoon-ui/src/lib/wagmi/publicClient.ts
@@ -1,14 +1,18 @@
 import { createPublicClient, http } from 'viem';
+import { holesky } from 'viem/chains';
 
-import { chainIdToChain } from '$lib/chain/chains';
-
-const devnet = chainIdToChain(167001);
+//const devnet = chainIdToChain(167001);
 
 export default async function publicClient() {
+  /*
   const client = createPublicClient({
     chain: devnet,
     transport: http('https://rpc.internal.taiko.xyz'),
-  });
+  });*/
 
+  const client = createPublicClient({
+    chain: holesky,
+    transport: http('https://1rpc.io/holesky'),
+  });
   return client;
 }
