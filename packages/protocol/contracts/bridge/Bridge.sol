9c1c5b73b553d965fd56d4be405eed95fd84b8bd
Daniel Wang <dan@taiko.xyz>
2024-05-30 16:45:17 +0700

    Update Bridge.sol

--- packages/protocol/contracts/bridge/Bridge.sol
+++ packages/protocol/contracts/bridge/Bridge.sol
@@ -25,6 +25,7 @@ contract Bridge is EssentialContract, IBridge {
         uint32 gasUsedInFeeCalc;
         uint32 proofSize;
         uint32 numCacheOps;
+        bool processedByRelayer;
     }
 
     /// @dev A debug event for fine-tuning gas related constants in the future.
@@ -227,9 +228,12 @@ contract Bridge is EssentialContract, IBridge {
             revert B_INVALID_CHAINID();
         }
 
+        ProcessingStats memory stats;
+        stats.processedByRelayer = msg.sender != _message.destOwner;
+
         // If the gas limit is set to zero, only the owner can process the message.
-        if (_message.gasLimit == 0 && msg.sender != _message.destOwner) {
-            revert B_PERMISSION_DENIED();
+        if (_message.gasLimit == 0) {
+            if (stats.processedByRelayer) revert B_PERMISSION_DENIED();
         }
 
         bytes32 msgHash = hashMessage(_message);
@@ -237,13 +241,12 @@ contract Bridge is EssentialContract, IBridge {
 
         address signalService = resolve(LibStrings.B_SIGNAL_SERVICE, false);
 
-        ProcessingStats memory stats;
         stats.proofSize = uint32(_proof.length);
         stats.numCacheOps =
             _proveSignalReceived(signalService, msgHash, _message.srcChainId, _proof);
 
         if (!_consumeEtherQuota(_message.value + _message.fee)) {
-            if (msg.sender != _message.destOwner) revert B_OUT_OF_ETH_QUOTA();
+            if (stats.processedByRelayer) revert B_OUT_OF_ETH_QUOTA();
             status_ = Status.RETRIABLE;
             reason_ = StatusReason.OUT_OF_ETH_QUOTA;
         } else {
@@ -255,15 +258,10 @@ contract Bridge is EssentialContract, IBridge {
                 status_ = Status.DONE;
                 reason_ = StatusReason.INVOCATION_PROHIBITED;
             } else {
-                uint256 gasLimit = msg.sender == _message.destOwner
-                    ? gasleft() // ignore _message.gasLimit
-                    : _invocationGasLimit(_message);
+                uint256 gasLimit =
+                    stats.processedByRelayer ? _invocationGasLimit(_message) : gasleft();
 
-                if (
-                    _invokeMessageCall(
-                        _message, msgHash, gasLimit, msg.sender != _message.destOwner
-                    )
-                ) {
+                if (_invokeMessageCall(_message, msgHash, gasLimit, stats.processedByRelayer)) {
                     status_ = Status.DONE;
                     reason_ = StatusReason.INVOCATION_OK;
                 } else {
@@ -275,7 +273,7 @@ contract Bridge is EssentialContract, IBridge {
             if (_message.fee != 0) {
                 refundAmount += _message.fee;
 
-                if (msg.sender != _message.destOwner && _message.gasLimit != 0) {
+                if (stats.processedByRelayer && _message.gasLimit != 0) {
                     unchecked {
                         uint256 refund = stats.numCacheOps * _GAS_REFUND_PER_CACHE_OPERATION;
                         stats.gasUsedInFeeCalc = uint32(GAS_OVERHEAD + gasStart - gasleft());
