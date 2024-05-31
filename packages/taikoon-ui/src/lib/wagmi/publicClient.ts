27b40bc39b0255b8ca3f316975198d45dbe375b3
bearni95 <bernatcanal@gmail.com>
2024-05-31 15:16:58 +0200

    fixed metamask on mobile

--- packages/taikoon-ui/src/lib/wagmi/publicClient.ts
+++ packages/taikoon-ui/src/lib/wagmi/publicClient.ts
@@ -1,15 +1,7 @@
 import { createPublicClient, http } from 'viem';
 import { holesky } from 'viem/chains';
 
-//const devnet = chainIdToChain(167001);
-
 export default async function publicClient() {
-  /*
-  const client = createPublicClient({
-    chain: devnet,
-    transport: http('https://rpc.internal.taiko.xyz'),
-  });*/
-
   const client = createPublicClient({
     chain: holesky,
     transport: http('https://1rpc.io/holesky'),
