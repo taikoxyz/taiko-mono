3f35a5743c7302f625ca3828f429f41997eb7a18
bearni95 <bernatcanal@gmail.com>
2024-05-31 08:59:55 +0200

    fixed overlapping footer

--- packages/taikoon-ui/src/components/sections/Footer.section.svelte
+++ packages/taikoon-ui/src/components/sections/Footer.section.svelte
@@ -10,7 +10,7 @@
   const sectionClasses = classNames('justify-end align-end', 'mb-5');
 </script>
 
-<Section height={windowSize === 'sm' ? 'fit' : 'min'} background="footer" class={sectionClasses} width="xl">
+<Section height={'fit'} background="footer" class={sectionClasses} width="xl">
   <Footer />
 </Section>
 
