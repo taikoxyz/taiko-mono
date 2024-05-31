957ae7996fe182e1c898482fcbd3136ee76deb3b
bearni95 <bernatcanal@gmail.com>
2024-05-31 13:39:17 +0200

    scaffolding

---
+++ packages/nfts/contracts/trailblazers-badges/TrailblazersBadges.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { ERC1155Upgradeable } from
+    "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import { MerkleWhitelist } from "../common/MerkleWhitelist.sol";
+import { IMinimalBlacklist } from "@taiko/blacklist/IMinimalBlacklist.sol";
+
+
+contract TrailblazersBadges is ERC1155Upgradeable, MerkleWhitelist {
+    // badgeId => ipfsBadgeURI
+
+
+    uint256[48] private __gap;
+
+
+function initialize(
+        address _owner,
+        string memory _rootURI,
+        bytes32 _merkleRoot,
+        IMinimalBlacklist _blacklistAddress
+    )
+        external
+        initializer
+    {
+        __ERC1155_init(_rootURI);
+        __MerkleWhitelist_init(_owner, _merkleRoot, _blacklistAddress);
+    }
+}
