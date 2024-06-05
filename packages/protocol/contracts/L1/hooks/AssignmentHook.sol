84a071d0e8c5826e14b7d2c2b73d3da2300f7fe1
Daniel Wang <dan@taiko.xyz>
2024-06-05 11:29:40 +0800

    Update AssignmentHook.sol

--- packages/protocol/contracts/L1/hooks/AssignmentHook.sol
+++ packages/protocol/contracts/L1/hooks/AssignmentHook.sol
@@ -34,8 +34,6 @@ contract AssignmentHook is EssentialContract, IHook {
         uint256 tip; // A tip to L1 block builder
     }
 
-    event EtherPaymentFailed(address to, uint256 maxGas);
-
     /// @notice Max gas paying the prover.
     /// @dev This should be large enough to prevent the worst cases for the prover.
     /// To assure a trustless relationship between the proposer and the prover it's
@@ -110,16 +108,16 @@ contract AssignmentHook is EssentialContract, IHook {
 
         // The proposer irrevocably pays a fee to the assigned prover, either in
         // Ether or ERC20 tokens.
-        if (assignment.feeToken == address(0)) {
-            // Paying Ether even when proverFee is 0 to trigger a potential receive() function call.
-            // Note that this payment may fail if it cost more gas
-            bool success = _blk.assignedProver.sendEther(proverFee, MAX_GAS_PAYING_PROVER, "");
-            if (!success) emit EtherPaymentFailed(_blk.assignedProver, MAX_GAS_PAYING_PROVER);
-        } else if (proverFee != 0 && _meta.sender != _blk.assignedProver) {
-            // Paying ERC20 tokens
-            IERC20(assignment.feeToken).safeTransferFrom(
-                _meta.sender, _blk.assignedProver, proverFee
-            );
+        if (proverFee != 0) {
+            if (assignment.feeToken == address(0)) {
+                // Paying Ether
+                _blk.assignedProver.sendEtherAndVerify(proverFee);
+            } else if (_meta.sender != _blk.assignedProver) {
+                // Paying ERC20 tokens
+                IERC20(assignment.feeToken).safeTransferFrom(
+                    _meta.sender, _blk.assignedProver, proverFee
+                );
+            }
         }
 
         // block.coinbase can be address(0) in tests
