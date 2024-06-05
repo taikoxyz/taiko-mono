600a12ad19aa822b815a1d4f22f4fd77963d12a4
Korbinian <korbiniank@outlook.de>
2024-06-05 14:00:13 +0200

    lint

--- packages/bridge-ui/src/components/Bridge/FungibleBridgeComponents/ReviewStep/ReviewStep.svelte
+++ packages/bridge-ui/src/components/Bridge/FungibleBridgeComponents/ReviewStep/ReviewStep.svelte
@@ -9,7 +9,7 @@
   import { destNetwork as destChain, enteredAmount, processingFee, selectedToken } from '$components/Bridge/state';
   import { PUBLIC_SLOW_L1_BRIDGING_WARNING } from '$env/static/public';
   import { LayerType } from '$libs/chain';
-  import { isStablecoin, isSupported, isWrapped, type Token,TokenType } from '$libs/token';
+  import { isStablecoin, isSupported, isWrapped, type Token, TokenType } from '$libs/token';
   import { isToken } from '$libs/token/isToken';
   import { ethBalance } from '$stores/balance';
   import { connectedSourceChain } from '$stores/network';
