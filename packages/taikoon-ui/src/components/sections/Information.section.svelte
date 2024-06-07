fbfcc7f3810d0122f46673944c39e5f4d759d4e0
Bernat Canal Garceran <bernatcanal@gmail.com>
2024-06-07 11:18:30 +0200

    fix(nfts): taikoon-ui mobile visual fixes (#17490)

--- packages/taikoon-ui/src/components/sections/Information.section.svelte
+++ packages/taikoon-ui/src/components/sections/Information.section.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { ResponsiveController } from '@taiko/ui-lib';
   import { t } from 'svelte-i18n';
 
   import { AnimatedEyes } from '$components/AnimatedTaikoon';
@@ -18,20 +19,21 @@
     'leading-relaxed',
   );
   const contentClasses = classNames(
-    'w-3/4',
+    'w-[80%]',
     'md:font-normal',
     'md:text-[57px]/[64px]',
     'text-content-primary',
     'font-medium',
     'font-clash-grotesk',
-    'text-4xl',
+    'text-3xl',
   );
+  let windowSize: 'sm' | 'md' | 'lg' = 'md';
 
-  $: eyes = Array.from({ length: 12 }, (_, i) => i);
+  $: eyes = Array.from({ length: windowSize === 'sm' ? 6 : 12 }, (_, i) => i);
 </script>
 
 <Section height="fit">
-  <div class={classNames('py-32', 'mb-32')}>
+  <div class={classNames('md:py-32', 'mb-32')}>
     <p class={titleClasses}>
       {$t('content.sections.information.title')}
     </p>
@@ -43,10 +45,18 @@
     <div class={classNames('w-full', 'h-min', 'flex', 'absolute', 'left-0', 'mt-12', 'bottom-0')}>
       <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
       {#each eyes as _}
-        <div class={classNames('w-1/12', 'h-max', 'flex', 'justify-center', 'items-center')}>
+        <div
+          class={classNames(
+            windowSize === 'sm' ? 'w-1/6' : 'w-1/12',
+            'h-max',
+            'flex',
+            'justify-center',
+            'items-center',
+          )}>
           <AnimatedEyes />
         </div>
       {/each}
     </div>
   </div>
 </Section>
+<ResponsiveController bind:windowSize />
