4279ccd59024578d51b39a6b26a0138409a42f44
Korbinian <KorbinianK@users.noreply.github.com>
2024-06-09 07:35:18 +0700

    fix(bridge-ui): bigint conversion (#17534)

--- packages/bridge-ui/src/libs/relayer/RelayerAPIService.ts
+++ packages/bridge-ui/src/libs/relayer/RelayerAPIService.ts
@@ -168,6 +168,7 @@ export class RelayerAPIService {
     }
 
     const items = RelayerAPIService._filterDuplicateAndWrongBridge(apiTxs.items);
+
     const txs: BridgeTransaction[] = items.map((tx: APIResponseTransaction) => {
       let data: string | Hex = tx.data.Message.Data;
       if (data === '') {
@@ -178,11 +179,10 @@ export class RelayerAPIService {
       }
 
       const tokenType: TokenType = _eventToTokenType(tx.eventType);
-      const value = tokenType === TokenType.ETH ? BigInt(tx.amount) : BigInt(0);
 
       const transformedTx = {
         status: tx.status,
-        amount: BigInt(tx.amount),
+        amount: BigInt(tx.amount.toString()),
         symbol: tx.canonicalTokenSymbol || 'ETH',
         decimals: tx.canonicalTokenDecimals,
         hash: tx.data.Raw.transactionHash,
@@ -200,11 +200,11 @@ export class RelayerAPIService {
           data: data as Hex,
           srcOwner: tx.data.Message.SrcOwner,
           from: tx.data.Message.From,
-          gasLimit: Number(tx.data.Message.GasLimit),
-          value: value,
+          gasLimit: tx.data.Message.GasLimit,
+          value: BigInt(tx.data.Message.Value.toString()),
           srcChainId: BigInt(tx.data.Message.SrcChainId),
           destChainId: BigInt(tx.data.Message.DestChainId),
-          fee: BigInt(tx.data.Message.Fee),
+          fee: BigInt(tx.data.Message.Fee.toString()),
         },
       } satisfies BridgeTransaction;
 
@@ -213,6 +213,7 @@ export class RelayerAPIService {
 
     const txsPromises = txs.map(async (bridgeTx) => {
       if (!bridgeTx) return;
+
       if (bridgeTx.from.toLowerCase() !== address.toLowerCase()) return;
       const { destChainId, srcChainId, hash, msgHash } = bridgeTx;
 
@@ -237,6 +238,7 @@ export class RelayerAPIService {
 
       // Update the status
       bridgeTx.msgStatus = msgStatus;
+
       return bridgeTx;
     });
 
