e6aab1344f5ccd4a7dd85a4e0fcfd8bab9318415
Kenk <kenghin_lim@hotmail.com>
2024-06-10 12:31:43 +0700

    make erc721

--- packages/nfts/contracts/trailblazers-badges/TrailblazersBadges.sol
+++ packages/nfts/contracts/trailblazers-badges/TrailblazersBadges.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { ERC1155Upgradeable } from
-    "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import { ERC721Upgradeable } from
+    "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
 import { ECDSAWhitelist } from "./ECDSAWhitelist.sol";
 import { IMinimalBlacklist } from "@taiko/blacklist/IMinimalBlacklist.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { BasedOrBoosted } from "./BasedOrBoosted.sol";
 
-contract TrailblazersBadges is ERC1155Upgradeable, ECDSAWhitelist {
+contract TrailblazersBadges is ERC721Upgradeable, ECDSAWhitelist {
     /// @notice Base URI required to interact with IPFS
     string private _baseURIExtended;
+
+    /// @notice Reference to BasedOrBoosted contract
+    BasedOrBoosted public basedOrBoosted;
+
+    // TODO: Add mapping between tokenId and BadgeId
+
+    // Add token counter
+    uint256 private _tokenCounter;
 
     /// @notice Badge IDs
     uint256 public constant BADGE_RAVERS = 0;
@@ -34,25 +44,34 @@ contract TrailblazersBadges is ERC1155Upgradeable, ECDSAWhitelist {
     /// @param _rootURI Base URI for the token metadata
     /// @param _mintSigner The address that can authorize minting badges
     /// @param _blacklistAddress The address of the blacklist contract
+    /// @param _basedOrBoostedAddress The address of the BasedOrBoosted contract
     function initialize(
         address _owner,
         string memory _rootURI,
         address _mintSigner,
-        IMinimalBlacklist _blacklistAddress
+        IMinimalBlacklist _blacklistAddress,
+        address _basedOrBoostedAddress
     )
-        external
+        public
         initializer
     {
-        __ERC1155_init(_rootURI);
-        _baseURIExtended = _rootURI;
+        __ERC721_init("TrailblazersBadges", "TBB");
+        __Ownable_init(_owner);
         __ECDSAWhitelist_init(_owner, _mintSigner, _blacklistAddress);
+
+        _baseURIExtended = _rootURI;
+        basedOrBoosted = BasedOrBoosted(_basedOrBoostedAddress);
+        transferOwnership(_owner);
     }
 
     /// @notice Get the URI for a badge
-    /// @param _badgeId The badge ID
+    /// @param _tokenId The badge ID
     /// @return The URI for the badge
-    function uri(uint256 _badgeId) public view override returns (string memory) {
-        return string(abi.encodePacked(_baseURIExtended, Strings.toString(_badgeId)));
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        BasedOrBoosted.Movement movement = getMovement(ownerOf(_tokenId));
+
+        // TODO: Should return BASE_URL/{MOVEMENT}/{BADGE_ID}.json
+        return "";
     }
 
     /// @notice Mint a badge from the calling wallet
@@ -70,7 +89,6 @@ contract TrailblazersBadges is ERC1155Upgradeable, ECDSAWhitelist {
     /// @dev Admin only method
     function mint(bytes memory _signature, address _minter, uint256 _badgeId) public onlyOwner {
         if (!canMint(_signature, _minter, _badgeId)) revert MINTER_NOT_WHITELISTED();
-
         _mintBadgeTo(_signature, _minter, _badgeId);
     }
 
@@ -82,11 +100,15 @@ contract TrailblazersBadges is ERC1155Upgradeable, ECDSAWhitelist {
         if (_badgeId > BADGE_SHINTO) revert INVALID_BADGE_ID();
         if (!canMint(_signature, _minter, _badgeId)) revert MINTER_NOT_WHITELISTED();
 
-        _mint(
-            _minter,
-            _badgeId,
-            1, // amount
-            "" // empty data
-        );
+        _safeMint(_minter, _badgeId);
     }
+
+    /// @notice Get the movement type of the user
+    /// @param _user The address of the user
+    /// @return movement The movement type as a string
+    function getMovement(address _user) internal view returns (BasedOrBoosted.Movement movement) {
+        movement = basedOrBoosted.isBasedOrBoosted(_user);
+    }
+
+    // TODO: Add total supply based on _tokenCounter
 }
