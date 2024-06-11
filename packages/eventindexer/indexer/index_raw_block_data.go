920a574705357814a72caeac096bff161c873be7
Jeffery Walsh <cyberhorsey@gmail.com>
2024-06-11 07:36:24 -0700

    .

--- packages/eventindexer/indexer/index_raw_block_data.go
+++ packages/eventindexer/indexer/index_raw_block_data.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log/slog"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/pkg/errors"
@@ -22,53 +23,53 @@ func (i *Indexer) indexRawBlockData(
 	slog.Info("indexRawBlockData", "start", start, "end", end)
 
 	// only index block/transaction data on L2
-	// if i.layer == Layer2 {
-	// 	for j := start; j < end; j++ {
-	// 		id := j
+	if i.layer == Layer2 {
+		for j := start; j < end; j++ {
+			id := j
 
-	// 		wg.Go(func() error {
-	// 			slog.Info("processing block data", "blockNum", id)
+			wg.Go(func() error {
+				slog.Info("processing block data", "blockNum", id)
 
-	// 			block, err := i.ethClient.BlockByNumber(ctx, big.NewInt(int64(id)))
+				block, err := i.ethClient.BlockByNumber(ctx, big.NewInt(int64(id)))
 
-	// 			if err != nil {
-	// 				return errors.Wrap(err, "i.ethClient.BlockByNumber")
-	// 			}
+				if err != nil {
+					return errors.Wrap(err, "i.ethClient.BlockByNumber")
+				}
 
-	// 			txs := block.Transactions()
+				txs := block.Transactions()
 
-	// 			for _, tx := range txs {
-	// 				slog.Info("transaction found", "hash", tx.Hash())
-	// 				receipt, err := i.ethClient.TransactionReceipt(ctx, tx.Hash())
+				for _, tx := range txs {
+					slog.Info("transaction found", "hash", tx.Hash())
+					receipt, err := i.ethClient.TransactionReceipt(ctx, tx.Hash())
 
-	// 				if err != nil {
-	// 					return err
-	// 				}
+					if err != nil {
+						return err
+					}
 
-	// 				sender, err := i.ethClient.TransactionSender(ctx, tx, block.Hash(), receipt.TransactionIndex)
-	// 				if err != nil {
-	// 					return err
-	// 				}
+					sender, err := i.ethClient.TransactionSender(ctx, tx, block.Hash(), receipt.TransactionIndex)
+					if err != nil {
+						return err
+					}
 
-	// 				if err := i.accountRepo.Save(ctx, sender, time.Unix(int64(block.Time()), 0)); err != nil {
-	// 					return err
-	// 				}
+					if err := i.accountRepo.Save(ctx, sender, time.Unix(int64(block.Time()), 0)); err != nil {
+						return err
+					}
 
-	// 				if err := i.txRepo.Save(ctx,
-	// 					tx,
-	// 					sender,
-	// 					block.Number(),
-	// 					time.Unix(int64(block.Time()), 0),
-	// 					receipt.ContractAddress,
-	// 				); err != nil {
-	// 					return err
-	// 				}
-	// 			}
+					if err := i.txRepo.Save(ctx,
+						tx,
+						sender,
+						block.Number(),
+						time.Unix(int64(block.Time()), 0),
+						receipt.ContractAddress,
+					); err != nil {
+						return err
+					}
+				}
 
-	// 			return nil
-	// 		})
-	// 	}
-	// }
+				return nil
+			})
+		}
+	}
 
 	// LOGS parsing
 	query := ethereum.FilterQuery{
