5809edb8c7fac51db0dd5b6b752f1a1a82e36240
Jeffery Walsh <cyberhorsey@gmail.com>
2024-06-12 09:55:28 -0700

    .

--- packages/relayer/migrations/1708366668_index_data.sql
+++ packages/relayer/migrations/1708366668_index_data.sql
@@ -1,17 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE `events` 
-ADD INDEX `message_owner_event_type_chain_id_event_data_index` (`message_owner`, `event_type`, `chain_id`, `event`, `dest_owner_json`),
-ADD INDEX `message_owner_event_type_chain_id_data_index` (`message_owner`, `event_type`, `chain_id`, `dest_owner_json`), 
-ADD INDEX `message_owner_event_type_data_index` (`message_owner`, `event_type`, `dest_owner_json`),
+ADD INDEX `message_owner_event_type_chain_id_event_data_index` (`message_owner`, `dest_owner_json`, `event_type`, `chain_id`, `event`),
+ADD INDEX `message_owner_event_type_chain_id_data_index` (`message_owner`, `dest_owner_json`, `event_type`, `chain_id`), 
+ADD INDEX `message_owner_event_type_data_index` (`message_owner`, `dest_owner_json`, `event_type`),
 ADD INDEX `message_owner_data_index` (`message_owner`, `dest_owner_json`),
-ADD INDEX `message_owner_chain_id_data_index` (`message_owner`, `chain_id`, `dest_owner_json`),
-ADD INDEX `message_owner_event_data_index` (`message_owner`, `event`, `dest_owner_json`),
-ADD INDEX `msg_hash_event_data_index` (`msg_hash`, `event`, `dest_owner_json`),
-ADD INDEX `data_event_type_chain_id_index` (`event_type`, `chain_id`, `dest_owner_json`), 
-ADD INDEX `data_event_type_index` (`event_type`, `dest_owner_json`),
+ADD INDEX `message_owner_chain_id_data_index` (`message_owner`, `dest_owner_json`, `chain_id`),
+ADD INDEX `message_owner_event_data_index` (`message_owner`, `dest_owner_json`, `event`),
+ADD INDEX `msg_hash_event_data_index` (`msg_hash`, `dest_owner_json`, `event`),
+ADD INDEX `data_event_type_chain_id_index` (`event_type`,`dest_owner_json`, `chain_id`), 
+ADD INDEX `data_event_type_index` (`dest_owner_json`, `event_type`),
 ADD INDEX `data_index` (`dest_owner_json`),
-ADD INDEX `data_chain_id_index` (`chain_id`, `dest_owner_json`),
+ADD INDEX `data_chain_id_index` (`dest_owner_json`, `chain_id`),
 ADD INDEX `data_event_index` (`dest_owner_json`, `event`);
 
 -- +goose StatementEnd
