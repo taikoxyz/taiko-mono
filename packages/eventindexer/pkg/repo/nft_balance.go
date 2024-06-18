8065ff1bed7263f39079172de585636b629bfb73
Jeffery Walsh <cyberhorsey@gmail.com>
2024-06-18 13:09:43 -0700

    handle deadlocks

--- packages/eventindexer/pkg/repo/nft_balance.go
+++ packages/eventindexer/pkg/repo/nft_balance.go
@@ -3,6 +3,8 @@ package repo
 import (
 	"context"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/morkid/paginate"
 	"github.com/pkg/errors"
@@ -114,20 +116,36 @@ func (r *NFTBalanceRepository) IncreaseAndDecreaseBalancesInTx(
 	increaseOpts eventindexer.UpdateNFTBalanceOpts,
 	decreaseOpts eventindexer.UpdateNFTBalanceOpts,
 ) (increasedBalance *eventindexer.NFTBalance, decreasedBalance *eventindexer.NFTBalance, err error) {
-	err = r.db.GormDB().Transaction(func(tx *gorm.DB) (err error) {
-		increasedBalance, err = r.increaseBalanceInDB(ctx, tx, increaseOpts)
-		if err != nil {
+	retries := 10
+	for retries > 0 {
+		err = r.db.GormDB().Transaction(func(tx *gorm.DB) (err error) {
+			increasedBalance, err = r.increaseBalanceInDB(ctx, tx, increaseOpts)
+			if err != nil {
+				return err
+			}
+
+			if decreaseOpts.Amount != 0 {
+				decreasedBalance, err = r.decreaseBalanceInDB(ctx, tx, decreaseOpts)
+			}
+
 			return err
+		})
+
+		if err == nil {
+			break
 		}
 
-		if decreaseOpts.Amount != 0 {
-			decreasedBalance, err = r.decreaseBalanceInDB(ctx, tx, decreaseOpts)
+		if strings.Contains(err.Error(), "Deadlock") {
+			retries--
+			time.Sleep(100 * time.Millisecond) // backoff before retrying
+			continue
 		}
 
-		return err
-	})
-	if err != nil {
 		return nil, nil, errors.Wrap(err, "r.db.Transaction")
+	}
+
+	if err != nil {
+		return nil, nil, err
 	}
 
 	return increasedBalance, decreasedBalance, nil
