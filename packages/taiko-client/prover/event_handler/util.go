63df5a7f2388cf3d1370e9c7e9b1b672ab012fea
David <david@taiko.xyz>
2024-06-21 22:52:43 +0800

    Revert "feat(taiko-client): skip `stateRoot` check when `transition.stateRoot` is `0`"
    This reverts commit babbc47c1bd9e5db931fa11e4363aa34884ab82e.

--- packages/taiko-client/prover/event_handler/util.go
+++ packages/taiko-client/prover/event_handler/util.go
@@ -49,13 +49,9 @@ func isValidProof(
 		return false, err
 	}
 
-	// If the stateRoot is not empty, we need to check
-	// if the stateRoot matches the one in the header.
-	if stateRoot != (common.Hash{}) && l2Header.Root != stateRoot {
-		return false, nil
-	}
-
-	return parent.Hash() == parentHash && l2Header.Hash() == blockHash, nil
+	return parent.Hash() == parentHash &&
+		l2Header.Hash() == blockHash &&
+		l2Header.Root == stateRoot, nil
 }
 
 // getProvingWindow returns the provingWindow of the given tier.
