830ba24762e503ea35d66f2b3754e33c99f9e732
Jeffery Walsh <cyberhorsey@gmail.com>
2024-06-26 13:36:39 -0700

    fix

--- packages/eventindexer/migrations/1666650713_alter_nft_metadata_add_indexes.sql
+++ packages/eventindexer/migrations/1666650713_alter_nft_metadata_add_indexes.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE `nft_metadata` ADD INDEX `nft_metadata_contract_address_token_id_index` (`contract_address`, `token_id`, `chain_id`);
+ALTER TABLE `nft_metadata` ADD INDEX `nft_metadata_contract_address_token_id_index` (`contract_address`, `token_id`);
 
 -- +goose StatementEnd
 -- +goose Down
