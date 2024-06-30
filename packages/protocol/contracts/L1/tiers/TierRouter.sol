38b1794f6c59ae465c03c896b8b0b5f9462a488f
Daniel Wang <dan@taikocha.in>
2024-06-30 20:53:02 +0800

    TierProviderV3

--- packages/protocol/contracts/L1/tiers/TierRouter.sol
+++ packages/protocol/contracts/L1/tiers/TierRouter.sol
@@ -7,8 +7,14 @@ import "./ITierRouter.sol";
 /// @dev Labeled in AddressResolver as "tier_router"
 /// @custom:security-contact security@taiko.xyz
 contract TierRouter is ITierRouter {
+    uint256 public constant FORK_HEIGHT = 324_512 * 2;
     /// @inheritdoc ITierRouter
-    function getProvider(uint256 /*_blockId*/ ) external pure returns (address) {
-        return 0x4cffe56C947E26D07C14020499776DB3e9AE3a23;
+
+    function getProvider(uint256 _blockId) external pure returns (address) {
+        if (_blockId <= FORK_HEIGHT) {
+            return 0x4cffe56C947E26D07C14020499776DB3e9AE3a23; // TierProviderV2
+        } else {
+            return address(0); // TODO(daniel): return a TierProviderV3 address
+        }
     }
 }
