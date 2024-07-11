7c48daaaf2f295ce238671844871bcc2db7fb8bc
David <david@taiko.xyz>
2024-07-11 15:13:13 +0800

    docs(protocol): update Hekla deployment

---
+++ packages/protocol/script/upgrade/UpgradeProverSet.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "forge-std/src/Script.sol";
+import "forge-std/src/console2.sol";
+import "../../contracts/team/proving/ProverSet.sol";
+import "./UpgradeScript.s.sol";
+
+contract UpgradeProverSet is UpgradeScript {
+    function run() external setUp {
+        upgrade("ProverSet", address(new ProverSet()));
+    }
+}
