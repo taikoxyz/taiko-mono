ac422a529bd13ef702fc6c9f2624c8019c378f9c
Daniel Wang <dan@taiko.xyz>
2024-07-14 09:10:31 +0800

    more

--- packages/protocol/contracts/mainnet/MainnetSharedAddressManager.sol
+++ packages/protocol/contracts/mainnet/MainnetSharedAddressManager.sol
@@ -13,8 +13,8 @@ contract MainnetSharedAddressManager is AddressManager {
             if (_name == LibStrings.B_TAIKO_TOKEN) {
                 return 0x10dea67478c5F8C5E2D90e5E9B26dBe60c54d800;
             }
-            if (_name == LibStrings.B_SIGNAL_SERVICE) {
-                return 0x9e0a24964e5397B566c1ed39258e21aB5E35C77C;
+            if (_name == LibStrings.B_QUOTA_MANAGER) {
+                return 0x91f67118DD47d502B1f0C354D0611997B022f29E;
             }
             if (_name == LibStrings.B_BRIDGE) {
                 return 0xd60247c6848B7Ca29eDdF63AA924E53dB6Ddd8EC;
@@ -28,8 +28,8 @@ contract MainnetSharedAddressManager is AddressManager {
             if (_name == LibStrings.B_ERC1155_VAULT) {
                 return 0xaf145913EA4a56BE22E120ED9C24589659881702;
             }
-            if (_name == LibStrings.B_QUOTA_MANAGER) {
-                return 0x91f67118DD47d502B1f0C354D0611997B022f29E;
+            if (_name == LibStrings.B_SIGNAL_SERVICE) {
+                return 0x9e0a24964e5397B566c1ed39258e21aB5E35C77C;
             }
         } else if (_chainId == 167_000) {
             if (_name == LibStrings.B_BRIDGE) {
--- packages/protocol/contracts/mainnet/MainnetTaikoL1.sol
+++ packages/protocol/contracts/mainnet/MainnetTaikoL1.sol
@@ -2,23 +2,14 @@
 pragma solidity 0.8.24;
 
 import "../L1/TaikoL1.sol";
+import "./LibAddressCache.sol";
 
 /// @title MainnetTaikoL1
 /// @notice See the documentation in {TaikoL1}.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetTaikoL1 is TaikoL1 {
     function _getAddress(uint64 _chainId, bytes32 _name) internal view override returns (address) {
-        if (_chainId == 1) {
-            if (_name == LibStrings.B_TAIKO_TOKEN) {
-                return 0x10dea67478c5F8C5E2D90e5E9B26dBe60c54d800;
-            }
-            if (_name == LibStrings.B_SIGNAL_SERVICE) {
-                return 0x9e0a24964e5397B566c1ed39258e21aB5E35C77C;
-            }
-            if (_name == LibStrings.B_TIER_ROUTER) {
-                return 0x6E997f1F22C40ba37F633B08f3b07E10Ed43155a;
-            }
-        }
-        return super._getAddress(_chainId, _name);
+        address addr = LibAddressCache.getAddress(_chainId, _name);
+        return addr != address(0) ? addr : super._getAddress(_chainId, _name);
     }
 }
