8434456f5d1cbc0e6a28824e2aeac1bdc0fb64c3
Daniel Wang <dan@taikocha.in>
2024-07-16 23:09:32 +0800

    add MainnetSgxVerifier

--- packages/protocol/contracts/mainnet/MainnetTierRouter.sol
+++ packages/protocol/contracts/mainnet/MainnetTierRouter.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "./ITierRouter.sol";
+import "../L1/tiers/ITierRouter.sol";
 
-/// @title TierRouter
+/// @title MainnetTierRouter
 /// @dev Labeled in AddressResolver as "tier_router"
 /// @custom:security-contact security@taiko.xyz
-contract TierRouter is ITierRouter {
+contract MainnetTierRouter is ITierRouter {
     /// @inheritdoc ITierRouter
     function getProvider(uint256 /*_blockId*/ ) external pure returns (address) {
-        return 0x4cffe56C947E26D07C14020499776DB3e9AE3a23;
+        return 0x4cffe56C947E26D07C14020499776DB3e9AE3a23; // TierProviderV2
     }
 }
