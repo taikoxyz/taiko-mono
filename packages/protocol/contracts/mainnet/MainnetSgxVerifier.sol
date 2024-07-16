928626b0a282db4e73aa04a1c03201148cf54a9b
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-07-16 23:15:58 +0800

    Update packages/protocol/contracts/mainnet/MainnetSgxVerifier.sol

--- packages/protocol/contracts/mainnet/MainnetSgxVerifier.sol
+++ packages/protocol/contracts/mainnet/MainnetSgxVerifier.sol
@@ -7,7 +7,7 @@ import "./LibRollupAddressCache.sol";
 /// @title MainnetSgxVerifier
 /// @dev This contract shall be deployed to replace its parent contract on Ethereum for Taiko
 /// mainnet to reduce gas cost.
-/// @notice See the documentation in {TaikoL1}.
+/// @notice See the documentation in {SgxVerifier}.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetSgxVerifier is SgxVerifier {
     function _getAddress(uint64 _chainId, bytes32 _name) internal view override returns (address) {
