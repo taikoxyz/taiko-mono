6fb0731b2df36f8fe9657adfc78a6b9b21be357f
David <david@taiko.xyz>
2024-07-17 15:20:38 +0800

    feat: add config pkg

---
+++ packages/taiko-client/pkg/config/chain_config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/params"
+)
+
+// ChainConfig is the core config which determines the blockchain settings.
+type ChainConfig struct {
+	ChainID     *big.Int `json:"chainId"`     // ChainId identifies the current chain
+	OnTakeBlock *big.Int `json:"onTakeBlock"` // Ontake switch block (nil = no fork, 0 = already on ontake)
+}
+
+// NetworkNames are user friendly names to use in the chain spec banner.
+var NetworkNames = map[string]string{
+	params.TaikoMainnetNetworkID.String():     "Taiko Mainnet",
+	params.HeklaNetworkID.String():            "Taiko Hekla Testnet",
+	params.TaikoInternalL2ANetworkID.String(): "Taiko Internal Devnet",
+}
+
+// Description returns a human-readable description of ChainConfig.
+func (c *ChainConfig) Description() string {
+	var banner string
+
+	// Create some basic network config output
+	network := NetworkNames[c.ChainID.String()]
+	if network == "" {
+		network = "unknown"
+	}
+	banner += fmt.Sprintf("Chain ID:  %v (%s)\n", c.ChainID, network)
+
+	// Create a list of forks with a short description of them.
+	banner += "Pre-Merge hard forks (block based):\n"
+	banner += fmt.Sprintf(" - Ontake:                   #%-8v\n", c.OnTakeBlock)
+	banner += "\n"
+
+	return banner
+}
+
+// IsOntake returns whether num is either equal to the ontake block or greater.
+func (c *ChainConfig) IsOntake(num *big.Int) bool {
+	return isBlockForked(c.OnTakeBlock, num)
+}
+
+// isBlockForked returns whether a fork scheduled at block s is active at the
+// given head block.
+func isBlockForked(s, head *big.Int) bool {
+	if s == nil || head == nil {
+		return false
+	}
+	return s.Cmp(head) <= 0
+}
