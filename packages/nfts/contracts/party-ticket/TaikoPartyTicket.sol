70c03b391625c4402f40acab6e53f95af460c6ee
bearni95 <bernatcanal@gmail.com>
2024-07-17 14:30:37 +0200

    contract and basic tests

---
+++ packages/nfts/contracts/party-ticket/TaikoPartyTicket.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import { ERC721EnumerableUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+
+contract TaikoPartyTicket is ERC721EnumerableUpgradeable, PausableUpgradeable, AccessControlUpgradeable {
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+
+    uint256 public mintFee;
+    bool public mintActive;
+    uint256 private _nextTokenId;
+    mapping(uint256 => bool) public winners;
+    string public baseURI;
+    string public winnerBaseURI;
+
+    address public payoutAddress;
+
+    function initialize(
+        address _payoutAddress,
+        uint256 _mintFee,
+        string memory _baseURI,
+        string memory _winnerBaseURI
+    ) external initializer {
+        __ERC721_init("TaikoPartyTicket", "TPT");
+
+        mintFee = _mintFee;
+        baseURI = _baseURI;
+        winnerBaseURI = _winnerBaseURI;
+        payoutAddress = _payoutAddress;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(OWNER_ROLE, _payoutAddress);
+
+    }
+
+    function mint() external payable whenNotPaused {
+        require(msg.value >= mintFee, "Insufficient mint fee");
+
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function mint(address to) public whenNotPaused onlyRole(DEFAULT_ADMIN_ROLE) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    function setWinners(uint256[] calldata _winners) external whenNotPaused onlyRole(DEFAULT_ADMIN_ROLE) {
+        for (uint256 i = 0; i < _winners.length; i++) {
+            winners[_winners[i]] = true;
+        }
+        pause();
+    }
+
+    function revokeAndReplaceWinner(uint256 revokeId, uint256 newWinnerId) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(winners[revokeId], "Revoke ID not a winner");
+        winners[revokeId] = false;
+        winners[newWinnerId] = true;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (winners[tokenId]) {
+            return string(abi.encodePacked(winnerBaseURI, Strings.toString(tokenId)));
+        }
+        return string(abi.encodePacked(baseURI, Strings.toString(tokenId)));
+    }
+
+    function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
+
+    function withdraw() external whenPaused onlyRole(DEFAULT_ADMIN_ROLE){
+        payable(payoutAddress).transfer(address(this).balance);
+    }
+
+    function isWinner(uint256 tokenId) public view returns (bool) {
+        return winners[tokenId];
+    }
+
+    function isWinner(address minter) public view returns (bool) {
+        for (uint256 i = 0; i < balanceOf(minter); i++) {
+            if (winners[tokenOfOwnerByIndex(minter, i)]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+      function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721EnumerableUpgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ packages/nfts/test/party-ticket/TaikoPartyTicket.t.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { Test } from "forge-std/src/Test.sol";
+
+import { TaikoPartyTicket } from "../../contracts/party-ticket/TaikoPartyTicket.sol";
+import { Merkle } from "murky/Merkle.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { MockBlacklist } from "../util/Blacklist.sol";
+
+contract TaikoPartyTicketTest is Test {
+
+    TaikoPartyTicket public token;
+
+    address public payoutWallet = vm.addr(0x5);
+    address public admin = vm.addr(0x6);
+
+    address[3] public minters = [vm.addr(0x1), vm.addr(0x2), vm.addr(0x3)];
+
+    uint256 constant MINT_FEE = 1.1 ether;
+    uint256 constant INITIAL_BALANCE = 2 ether;
+
+    MockBlacklist public blacklist;
+
+    function setUp() public {
+        blacklist = new MockBlacklist();
+        // create whitelist merkle tree
+        vm.startBroadcast(admin);
+
+        // deploy token with empty root
+        address impl = address(new TaikoPartyTicket());
+        address proxy = address(
+            new ERC1967Proxy(
+                impl,
+                abi.encodeCall(
+                    TaikoPartyTicket.initialize,
+                    (
+                        payoutWallet,
+                        MINT_FEE,
+                        "ipfs://participant",
+                        "ipfs://winner"
+                    ))
+            )
+        );
+
+        token = TaikoPartyTicket(proxy);
+
+        // assign initial balance to all minters
+        for (uint256 i = 0; i < minters.length; i++) {
+            vm.deal(minters[i], INITIAL_BALANCE);
+        }
+
+        vm.stopBroadcast();
+
+    }
+
+    function test_metadata() public view {
+        assertEq(token.name(), "TaikoPartyTicket");
+        assertEq(token.symbol(), "TPT");
+        assertEq(token.totalSupply(), 0);
+    }
+
+    function test_mint() public {
+        vm.prank(minters[0]);
+        token.mint{value: MINT_FEE}();
+        assertEq(token.totalSupply(), 1);
+        assertEq(token.ownerOf(0), minters[0]);
+        assertEq(minters[0].balance, INITIAL_BALANCE - MINT_FEE);
+    }
+
+    function test_mint_admin() public {
+        vm.prank(admin);
+        token.mint(minters[1]);
+        assertEq(token.totalSupply(), 1);
+        assertEq(token.ownerOf(0), minters[1]);
+    }
+
+    function test_winnerFlow() public {
+        // have all minters mint
+        vm.prank(minters[0]);
+        token.mint{value: MINT_FEE}();
+        vm.prank(minters[1]);
+        token.mint{value: MINT_FEE}();
+        vm.prank(minters[2]);
+        token.mint{value: MINT_FEE}();
+
+        // arbitrarly, minters[0] wins
+        uint256 winnerTokenId = 0;
+
+        // set minters[0] as winner
+        vm.prank(admin);
+        uint256[] memory winners = new uint256[](1);
+        winners[0] = winnerTokenId;
+        token.setWinners(winners);
+        // check winner with both tokenId and address
+        assertTrue(token.isWinner(winnerTokenId));
+        assertTrue(token.isWinner(minters[0]));
+        // and the contract is paused
+        assertTrue(token.paused());
+        // ensure the contract's balance
+        assertEq(address(token).balance, MINT_FEE * minters.length);
+    }
+
+    function test_withdraw() public {
+        test_winnerFlow();
+        uint256 collectedEth = address(token).balance;
+        vm.prank(admin);
+        token.withdraw();
+        assertEq(payoutWallet.balance, collectedEth);
+        assertEq(address(token).balance, 0);
+    }
+}
