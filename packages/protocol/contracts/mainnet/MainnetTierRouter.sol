e00507d4811c82bfb2bb732cd96d2c444ddee84e
Daniel Wang <dan@taiko.xyz>
2024-07-17 10:43:03 +0800

    Update MainnetTierRouter.sol

--- packages/protocol/contracts/mainnet/MainnetTierRouter.sol
+++ packages/protocol/contracts/mainnet/MainnetTierRouter.sol
@@ -7,7 +7,9 @@ import "../L1/tiers/ITierRouter.sol";
 /// @dev Labeled in AddressResolver as "tier_router"
 /// @custom:security-contact security@taiko.xyz
 contract MainnetTierRouter is ITierRouter {
+    uint256 public constant ONTAKE_FORK_HEIGHT = 367_200; // = 7200 * 52
     /// @inheritdoc ITierRouter
+
     function getProvider(uint256 _blockId) external pure returns (address) {
         if (_blockId <= ONTAKE_FORK_HEIGHT) {
             return 0x4cffe56C947E26D07C14020499776DB3e9AE3a23; // TierProviderV2
