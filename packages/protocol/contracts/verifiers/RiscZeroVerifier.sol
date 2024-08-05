ec503b03e0f84b44d4b4b9681aab8e29abd10125
Daniel Wang <dan@taiko.xyz>
2024-08-05 21:59:42 +0800

    Update RiscZeroVerifier.sol

--- packages/protocol/contracts/verifiers/RiscZeroVerifier.sol
+++ packages/protocol/contracts/verifiers/RiscZeroVerifier.sol
@@ -7,7 +7,6 @@ import "../common/LibStrings.sol";
 import "../L1/ITaikoL1.sol";
 import "./IVerifier.sol";
 import "./libs/LibPublicInput.sol";
-import { IRiscZeroVerifier } from "risc0-ethereum/contracts/src/IRiscZeroVerifier.sol";
 
 /// @title RiscZeroVerifier
 /// @custom:security-contact security@taiko.xyz
