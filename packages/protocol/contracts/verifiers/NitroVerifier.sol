ac901ddf9b49b709f3aa00a09f4d14c41c4b7e0b
Ivan Temchenko <35359595i@proton.me>
2024-08-08 14:01:53 -0700

    feat: AttestationVerifier import

--- packages/protocol/contracts/verifiers/NitroVerifier.sol
+++ packages/protocol/contracts/verifiers/NitroVerifier.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "oyster-contracts/AttestationVerifier.sol";
 import "../L1/ITaikoL1.sol";
-import "../common/EssentialContract.sol";
 
-contract NitroVerifier is EssentialContract {
+contract NitroVerifier is AttestationVerifier {
     using ECDSA for bytes32;
 
     // The address of the L1 contract that this verifier is used with.
