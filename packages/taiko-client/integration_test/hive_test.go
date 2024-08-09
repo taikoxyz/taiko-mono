f5a93e7474fe685846f47ebc4e1c709f88904efe
maskpp <maskpp266@gmail.com>
2024-08-09 16:15:30 +0800

    add hive test

---
+++ packages/taiko-client/integration_test/hive_test.go
@@ -0,0 +1,32 @@
+package integration_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/ethereum/hive/hivesim"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHiveHandler(t *testing.T) {
+	baseDir := os.Getenv("HIVE_BASE_DIR")
+	if baseDir == "" {
+		t.SkipNow()
+	}
+	handler, err := hivesim.NewHiveFramework(&hivesim.HiveConfig{
+		BaseDir:        baseDir,
+		SimPattern:     "taiko2",
+		SimTestPattern: "eth2-deneb-testnet/test-deneb-genesis",
+		Clients: []string{
+			"taiko2/geth",
+			"taiko2/prysm-bn",
+			"taiko2/prysm-vc",
+		},
+	})
+	assert.NoError(t, err)
+
+	failedCount, err := handler.Run(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, 0, failedCount)
+}
