d864cea2eb8346127992acfbd9012e675a3400cc
David <david@taiko.xyz>
2024-08-12 10:45:08 +0800

    feat(protocol): add `DevnetTaikoL1` (#17900)
    Co-authored-by: davidtaikocha <davidtaikocha@users.noreply.github.com>
    Co-authored-by: Daniel Wang <dan@taiko.xyz>
    Co-authored-by: dantaik <dantaik@users.noreply.github.com>

---
+++ packages/protocol/contracts/devnet/DevnetTaikoL1.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "../L1/TaikoL1.sol";
+
+/// @title DevnetTaikoL1
+/// @dev Labeled in AddressResolver as "taiko"
+/// @custom:security-contact security@taiko.xyz
+contract DevnetTaikoL1 is TaikoL1 {
+    /// @inheritdoc ITaikoL1
+    function getConfig() public pure override returns (TaikoData.Config memory) {
+        return TaikoData.Config({
+            chainId: 167_001,
+            blockMaxProposals: 324_000,
+            blockRingBufferSize: 360_000,
+            maxBlocksToVerify: 16,
+            blockMaxGasLimit: 240_000_000,
+            livenessBond: 125e18, // 125 Taiko token
+            stateRootSyncInternal: 16,
+            maxAnchorHeightOffset: 64,
+            basefeeAdjustmentQuotient: 8,
+            basefeeSharingPctg: 75,
+            gasIssuancePerSecond: 5_000_000,
+            ontakeForkHeight: 2
+        });
+    }
+}
