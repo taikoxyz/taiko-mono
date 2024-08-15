05d49b07f9131bc034d00ad6cb7b7868a9af2bfc
maskpp <maskpp266@gmail.com>
2024-08-15 15:10:33 +0800

    chore(taiko-client): update `hive_tests.sh` (#17923)

--- packages/taiko-client/integration_test/hive_tests.sh
+++ packages/taiko-client/integration_test/hive_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$HIVE_BASE_DIR" == "" ]; then
-  HIVE_BASE_DIR=/tmp/hive
-  rm -rf $HIVE_BASE_DIR
-  git clone git@github.com:taikoxyz/hive.git $HIVE_BASE_DIR
+HIVE_BASE_DIR=${HIVE_BASE_DIR:-/tmp/hive}
+if [ "$HIVE_BASE_DIR" == "/tmp/hive" ]; then
+  rm -rf "$HIVE_BASE_DIR"
+  git clone git@github.com:taikoxyz/hive.git "$HIVE_BASE_DIR"
 fi
 
-go test -v -p=1 ./integration_test -timeout=700s
+export HIVE_BASE_DIR=$HIVE_BASE_DIR && go test -v -p=1 ./integration_test -timeout=700s
