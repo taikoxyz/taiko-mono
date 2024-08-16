e46cf294862c293b73b817574669115b85e973a7
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-08-16 16:10:43 +0800

    refactor(protocol): introduce BlockV2 for client-side compability (#17935)

--- packages/protocol/contracts/L1/libs/LibData.sol
+++ packages/protocol/contracts/L1/libs/LibData.sol
@@ -84,4 +84,23 @@ library LibData {
             baseFeeConfig: TaikoData.BaseFeeConfig(0, 0, 0, 0, 0)
         });
     }
+
+    function blockV2toV1(
+        TaikoData.BlockV2 memory _v2
+    )
+        internal
+        pure
+        returns (TaikoData.Block memory)
+    {
+        return TaikoData.Block({
+            metaHash: _v2.metaHash,
+            assignedProver: _v2.assignedProver,
+            livenessBond: _v2.livenessBond,
+            blockId: _v2.blockId,
+            proposedAt: _v2.proposedAt,
+            proposedIn: _v2.proposedIn,
+            nextTransitionId: _v2.nextTransitionId,
+            verifiedTransitionId: _v2.verifiedTransitionId
+        });
+    }
 }
