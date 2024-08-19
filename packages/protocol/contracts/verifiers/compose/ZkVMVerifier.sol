1eaed4dae20158fdab4eb605b6e270800a619be0
Daniel Wang <dan@taiko.xyz>
2024-08-19 12:36:15 +0800

    Update ZkVMVerifier.sol

--- packages/protocol/contracts/verifiers/compose/ZkVMVerifier.sol
+++ packages/protocol/contracts/verifiers/compose/ZkVMVerifier.sol
@@ -8,24 +8,24 @@ import "./ComposeVerifier.sol";
 /// Verifiers.
 /// @custom:security-contact security@taiko.xyz
 contract ZkVMVerifier is ComposeVerifier {
-    address internal immutable RISK_ZERO_VERIFIER;
-    address internal immutable SP1_VERIFIER;
+    address internal immutable _RISK_ZERO_VERIFIER;
+    address internal immutable _SP1_VERIFIER;
 
     constructor(address _risc0Verifier, address _sp1Verifier) {
-        RISK_ZERO_VERIFIER = _risc0Verifier;
-        SP1_VERIFIER = _sp1Verifier;
+        _RISK_ZERO_VERIFIER = _risc0Verifier;
+        _SP1_VERIFIER = _sp1Verifier;
     }
 
     /// @notice Returns the address of the Risc0 verifier.
     /// @return The address of the Risc0 verifier.
     function getRisc0Verifier() public view virtual returns (address) {
-        return RISK_ZERO_VERIFIER;
+        return _RISK_ZERO_VERIFIER;
     }
 
     /// @notice Returns the address of the SP1 verifier.
     /// @return The address of the SP1 verifier.
     function getSp1Verifier() public view virtual returns (address) {
-        return SP1_VERIFIER;
+        return _SP1_VERIFIER;
     }
 
     /// @inheritdoc ComposeVerifier
