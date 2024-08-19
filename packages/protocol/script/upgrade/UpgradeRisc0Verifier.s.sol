be0e024b6a3027fc2ce34ab8ef89214fa92c2a18
YoGhurt111 <YoGhurt111@users.noreply.github.com>
2024-08-19 01:24:47 +0000

    forge fmt & update contract layout table

--- packages/protocol/script/upgrade/UpgradeRisc0Verifier.s.sol
+++ packages/protocol/script/upgrade/UpgradeRisc0Verifier.s.sol
@@ -5,7 +5,7 @@ import "forge-std/src/Script.sol";
 import "forge-std/src/console2.sol";
 
 import "./UpgradeScript.s.sol";
-import {RiscZeroVerifier} from "../../contracts/verifiers/RiscZeroVerifier.sol";
+import { RiscZeroVerifier } from "../../contracts/verifiers/RiscZeroVerifier.sol";
 
 contract UpgradeRisc0Verifier is UpgradeScript {
     function run() external setUp {
