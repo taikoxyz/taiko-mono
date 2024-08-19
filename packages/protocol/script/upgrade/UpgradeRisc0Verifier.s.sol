07c56195f8578e4cd548b3773ff4c5b80abc3b41
gavin <gavin@taiko.xyz>
2024-08-19 09:13:07 +0800

    feat(protocol): script of `UpgradeRisc0Verifier`

---
+++ packages/protocol/script/upgrade/UpgradeRisc0Verifier.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "forge-std/src/Script.sol";
+import "forge-std/src/console2.sol";
+
+import "./UpgradeScript.s.sol";
+import {RiscZeroVerifier} from "../../contracts/verifiers/RiscZeroVerifier.sol";
+
+contract UpgradeRisc0Verifier is UpgradeScript {
+    function run() external setUp {
+        upgrade("Risc0Verifier", address(new RiscZeroVerifier()));
+    }
+}
