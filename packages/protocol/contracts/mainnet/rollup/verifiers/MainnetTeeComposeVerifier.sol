e8c54ec26b866116bf8d74698281eb1850262c3d
Daniel Wang <dan@taiko.xyz>
2024-08-21 11:26:26 +0800

    fix

--- packages/protocol/contracts/mainnet/rollup/verifiers/MainnetTeeComposeVerifier.sol
+++ packages/protocol/contracts/mainnet/rollup/verifiers/MainnetTeeComposeVerifier.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "../../verifiers/compose/TeeComposeVerifier.sol";
-import "../LibRollupAddressCache.sol";
+import "../../../verifiers/compose/TeeComposeVerifier.sol";
+import "../../addrcache/RollupAddressCache.sol";
 
 /// @title MainnetTeeComposeVerifier
 /// @custom:security-contact security@taiko.xyz
-contract MainnetTeeComposeVerifier is TeeComposeVerifier {
+contract MainnetTeeComposeVerifier is TeeComposeVerifier, RollupAddressCache {
     function _getAddress(uint64 _chainId, bytes32 _name) internal view override returns (address) {
-        (bool found, address addr) = LibRollupAddressCache.getAddress(_chainId, _name);
-        return found ? addr : super._getAddress(_chainId, _name);
+        return getAddressFromCache(_chainId, _name, super._getAddress);
     }
 }
