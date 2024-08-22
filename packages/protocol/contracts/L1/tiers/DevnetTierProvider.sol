e35248991ba2a00ac0ab0254b2646c393b4ac173
Daniel Wang <dan@taikocha.in>
2024-08-22 11:04:54 +0800

    Update DevnetTierProvider.sol

--- packages/protocol/contracts/L1/tiers/DevnetTierProvider.sol
+++ packages/protocol/contracts/L1/tiers/DevnetTierProvider.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "./TierProviderV2.sol";
+import "./TierProviderBase.sol";
 import "./ITierRouter.sol";
 
 /// @title DevnetTierProvider
 /// @custom:security-contact security@taiko.xyz
-contract DevnetTierProvider is TierProviderV2, ITierRouter {
+contract DevnetTierProvider is TierProviderBase, ITierRouter {
     /// @inheritdoc ITierRouter
     function getProvider(uint256) external view returns (address) {
         return address(this);
