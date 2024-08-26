6448edd98968a6d88f603c2d78af19c0f43f6558
gavin <gavin@taiko.xyz>
2024-08-26 16:04:30 +0800

    fix(taiko-client): fix some issue about `calculateBaseFee`

--- packages/taiko-client/proposer/proposer.go
+++ packages/taiko-client/proposer/proposer.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"math/big"
 	"math/rand"
 	"sync"
 	"time"
@@ -47,6 +46,8 @@ type Proposer struct {
 
 	// Protocol configurations
 	protocolConfigs *bindings.TaikoDataConfig
+
+	chainConfig *config.ChainConfig
 
 	lastProposedAt time.Time
 	totalEpochs    uint64
@@ -122,7 +123,8 @@ func (p *Proposer) InitFromConfig(
 		}
 	}
 
-	chainConfig := config.NewChainConfig(p.rpc.L2.ChainID, new(big.Int).SetUint64(p.protocolConfigs.OntakeForkHeight))
+	chainConfig := config.NewChainConfig(p.protocolConfigs)
+	p.chainConfig = chainConfig
 
 	if cfg.BlobAllowed {
 		p.txBuilder = builder.NewBlobTransactionBuilder(
@@ -208,6 +210,7 @@ func (p *Proposer) fetchPoolContent(filterPoolContent bool) ([]types.Transaction
 		p.LocalAddresses,
 		p.MaxProposedTxListsPerEpoch,
 		minTip,
+		p.chainConfig,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch transaction pool content: %w", err)
