fcaf689b354ba499b8fdbcc5e44af97f5cfb7d82
Daniel Wang <dan@taiko.xyz>
2024-08-26 21:04:06 +0800

    Revert "delete"
    This reverts commit f206780c2a25195c813528b42ebffd2514a7642e.

---
+++ packages/protocol/contracts/verifiers/compose/ComposeVerifier.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "../../common/EssentialContract.sol";
+import "../IVerifier.sol";
+
+/// @title ComposeVerifier
+/// @notice This contract is an abstract verifier that composes multiple sub-verifiers to validate
+/// proofs.
+/// It ensures that a set of sub-proofs are verified by their respective verifiers before
+/// considering the overall proof as valid.
+/// @custom:security-contact security@taiko.xyz
+abstract contract ComposeVerifier is EssentialContract, IVerifier {
+    struct SubProof {
+        address verifier;
+        bytes proof;
+    }
+
+    event InvalidSubProof(address indexed verifier, bytes returnData);
+
+    error INVALID_VERIFIER();
+    error INSUFFICIENT_PROOF();
+
+    /// @notice Initializes the contract.
+    /// @param _owner The owner of this contract. msg.sender will be used if this value is zero.
+    /// @param _rollupAddressManager The address of the {AddressManager} contract.
+    function init(address _owner, address _rollupAddressManager) external initializer {
+        __Essential_init(_owner, _rollupAddressManager);
+    }
+
+    /// @notice Verifies one or more sub-proofs.
+    /// @param _ctx The context of the proof verification.
+    /// @param _tran The transition to verify.
+    /// @param _proof The proof to verify.
+    function verifyProof(
+        Context calldata _ctx,
+        TaikoData.Transition calldata _tran,
+        TaikoData.TierProof calldata _proof
+    )
+        external
+        onlyFromNamed(LibStrings.B_TAIKO)
+    {
+        (address[] memory verifiers, uint256 threshold) = getSubVerifiersAndThreshold();
+        SubProof[] memory subproofs = abi.decode(_proof.data, (SubProof[]));
+        uint256 numVerified;
+
+        for (uint256 i; i < subproofs.length; ++i) {
+            if (subproofs[i].verifier == address(0)) revert INVALID_VERIFIER();
+
+            // find the verifier
+            bool verifierFound;
+            for (uint256 j; j < verifiers.length; ++j) {
+                if (verifiers[j] == subproofs[i].verifier) {
+                    verifierFound = true;
+                    verifiers[j] = address(0);
+                }
+            }
+
+            if (!verifierFound) revert INVALID_VERIFIER();
+
+            (bool success, bytes memory returnData) = subproofs[i].verifier.call(
+                abi.encodeCall(
+                    IVerifier.verifyProof,
+                    (_ctx, _tran, TaikoData.TierProof(_proof.tier, subproofs[i].proof))
+                )
+            );
+            if (success) {
+                unchecked {
+                    numVerified += 1;
+                }
+            } else {
+                emit InvalidSubProof(subproofs[i].verifier, returnData);
+            }
+        }
+
+        if (numVerified < threshold) {
+            revert INSUFFICIENT_PROOF();
+        }
+    }
+
+    /// @notice Returns the list of sub-verifiers and calculates the threshold.
+    /// @return verifiers_ An array of addresses of sub-verifiers.
+    /// @return threshold_ The threshold number of successful verifications required.
+    function getSubVerifiersAndThreshold()
+        public
+        view
+        virtual
+        returns (address[] memory verifiers_, uint256 threshold_);
+}
---
+++ packages/protocol/contracts/verifiers/compose/ZkAnyVerifier.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "../../common/EssentialContract.sol";
+import "../../common/LibStrings.sol";
+import "./ComposeVerifier.sol";
+
+/// @title ZkAnyVerifier
+/// @notice This contract is a verifier for the Mainnet ZkVM that composes RiscZero and SP1
+/// Verifiers.
+/// @custom:security-contact security@taiko.xyz
+contract ZkAnyVerifier is EssentialContract, ComposeVerifier {
+    /// @inheritdoc ComposeVerifier
+    function getSubVerifiersAndThreshold()
+        public
+        view
+        override
+        returns (address[] memory verifiers_, uint256 threshold_)
+    {
+        verifiers_ = new address[](2);
+        verifiers_[0] = resolve(LibStrings.B_TIER_ZKVM_RISC0, false);
+        verifiers_[1] = resolve(LibStrings.B_TIER_ZKVM_SP1, false);
+        threshold_ = 1;
+    }
+}
