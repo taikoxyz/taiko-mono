9744d28d04c9aea59c558d188666c5eb4f4c2ed2
Daniel Wang <dan@taiko.xyz>
2024-08-28 15:25:20 +0800

    comments

--- packages/protocol/contracts/mainnet/rollup/verifiers/MainnetTeeAnyVerifier.sol
+++ packages/protocol/contracts/mainnet/rollup/verifiers/MainnetTeeAnyVerifier.sol
@@ -5,6 +5,8 @@ import "../../../verifiers/compose/TeeAnyVerifier.sol";
 import "../../addrcache/RollupAddressCache.sol";
 
 /// @title MainnetTeeAnyVerifier
+/// @dev This contract shall be deployed to replace its parent contract on Ethereum for Taiko
+/// mainnet to reduce gas cost.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetTeeAnyVerifier is TeeAnyVerifier, RollupAddressCache {
     function _getAddress(uint64 _chainId, bytes32 _name) internal view override returns (address) {
--- packages/protocol/contracts/mainnet/rollup/verifiers/MainnetZkAndTeeVerifier.sol
+++ packages/protocol/contracts/mainnet/rollup/verifiers/MainnetZkAndTeeVerifier.sol
@@ -5,6 +5,8 @@ import "../../../verifiers/compose/ZkAndTeeVerifier.sol";
 import "../../addrcache/RollupAddressCache.sol";
 
 /// @title MainnetZkAndTeeVerifier
+/// @dev This contract shall be deployed to replace its parent contract on Ethereum for Taiko
+/// mainnet to reduce gas cost.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetZkAndTeeVerifier is ZkAndTeeVerifier, RollupAddressCache {
     function _getAddress(uint64 _chainId, bytes32 _name) internal view override returns (address) {
