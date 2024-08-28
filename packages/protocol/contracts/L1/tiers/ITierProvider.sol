18ff421296aacf4069058104a97997f04c957924
Daniel Wang <dan@taiko.xyz>
2024-08-28 11:43:50 +0800

    more

--- packages/protocol/contracts/L1/tiers/ITierProvider.sol
+++ packages/protocol/contracts/L1/tiers/ITierProvider.sol
@@ -33,37 +33,3 @@ interface ITierProvider {
     /// @return The tier id.
     function getMinTier(address proposer, uint256 rand) external view returns (uint16);
 }
-
-/// @title LibTiers
-/// @dev Unique identifiers for all supported tiers. Each tier must have a distinct ID to avoid
-/// conflicts.
-/// @dev Tier ID cannot be zero!
-/// @custom:security-contact security@taiko.xyz
-library LibTiers {
-    /// @notice Optimistic tier ID.
-    uint16 public constant TIER_OPTIMISTIC = 100;
-
-    /// @notice SGX proof
-    uint16 public constant TIER_SGX = 200;
-
-    /// @notice TDX proof
-    uint16 public constant TIER_TDX = 200;
-
-    /// @notice Any TEE proof
-    uint16 public constant TIER_TEE_ANY = 200;
-
-    /// @notice Risc0's ZKVM proof
-    uint16 public constant TIER_ZKVM_RISC0 = 290;
-
-    /// @notice SP1's ZKVM proof
-    uint16 public constant TIER_ZKVM_SP1 = 290;
-
-    /// @notice Any ZKVM proof
-    uint16 public constant TIER_ZKVM_ANY = 290;
-
-    /// @notice Guardian tier ID with minority approval.
-    uint16 public constant TIER_GUARDIAN_MINORITY = 900;
-
-    /// @notice Guardian tier ID with majority approval.
-    uint16 public constant TIER_GUARDIAN = 1000;
-}
