4b091bd8c4754238de3be2e38c9ae8591dbd8cf5
k-kaddal <khaledkaddal@gmail.com>
2024-08-29 17:47:04 +0100

    feat(protocol): OpenZeppelin defenders

---
+++ packages/protocol/monitors/GuardianProver-GuardiansUpdated.js
@@ -0,0 +1,126 @@
+const {ethers} = require("ethers");
+const {Defender} = require("@openzeppelin/defender-sdk");
+
+const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint32",
+        name: "version",
+        type: "uint32",
+      },
+      {
+        indexed: false,
+        internalType: "address[]",
+        name: "guardians",
+        type: "address[]",
+      },
+    ],
+    name: "GuardiansUpdated",
+    type: "event",
+  },
+];
+
+function alertOrg(notificationClient, message) {
+  notificationClient.send({
+    channelAlias: "discord_configs",
+    subject: "GuardianProver: GuardiansUpdated Alert",
+    message: message,
+  });
+}
+
+async function getLatestBlockNumber(provider) {
+  const currentBlock = await provider.getBlock("latest");
+  return currentBlock.number;
+}
+
+async function fetchLogsFromL1(
+  eventName,
+  fromBlock,
+  toBlock,
+  address,
+  abi,
+  provider
+) {
+  const iface = new ethers.utils.Interface(abi);
+  const eventTopic = iface.getEventTopic(eventName);
+
+  try {
+    const logs = await provider.getLogs({
+      address,
+      fromBlock,
+      toBlock,
+      topics: [eventTopic],
+    });
+
+    return logs.map((log) =>
+      iface.decodeEventLog(eventName, log.data, log.topics)
+    );
+  } catch (error) {
+    console.error(`Error fetching logs for ${eventName}:`, error);
+    return [];
+  }
+}
+
+function createProvider(apiKey, apiSecret, relayerApiKey, relayerApiSecret) {
+  const client = new Defender({
+    apiKey,
+    apiSecret,
+    relayerApiKey,
+    relayerApiSecret,
+  });
+
+  return client.relaySigner.getProvider();
+}
+
+async function calculateBlockTime(provider) {
+  const latestBlock = await provider.getBlock("latest");
+  const previousBlock = await provider.getBlock(latestBlock.number - 100);
+
+  const timeDiff = latestBlock.timestamp - previousBlock.timestamp;
+  const blockDiff = latestBlock.number - previousBlock.number;
+
+  const blockTime = timeDiff / blockDiff;
+  return blockTime;
+}
+
+exports.handler = async function (event, context) {
+  const {notificationClient} = context;
+  const {apiKey, apiSecret, taikoL1ApiKey, taikoL1ApiSecret} = event.secrets;
+
+  const taikoL1Provider = createProvider(
+    apiKey,
+    apiSecret,
+    taikoL1ApiKey,
+    taikoL1ApiSecret
+  );
+
+  const currentBlockNumber = await getLatestBlockNumber(taikoL1Provider);
+  const blockTimeInSeconds = await calculateBlockTime(taikoL1Provider);
+  const blocksInFiveMinutes = Math.floor((5 * 60) / blockTimeInSeconds);
+
+  const fromBlock = currentBlockNumber - blocksInFiveMinutes;
+  const toBlock = currentBlockNumber;
+
+  const logs = await fetchLogsFromL1(
+    "GuardiansUpdated",
+    fromBlock,
+    toBlock,
+    "0xE3D777143Ea25A6E031d1e921F396750885f43aC",
+    ABI,
+    taikoL1Provider
+  );
+
+  console.log(`Logs found: ${logs.length}`);
+
+  if (logs.length > 0) {
+    alertOrg(
+      notificationClient,
+      `GuardiansUpdated event detected! Details: ${JSON.stringify(logs)}`
+    );
+  }
+
+  return true;
+};
---
+++ packages/protocol/monitors/TaikoL1-CalldataTxListCount.js
@@ -0,0 +1,138 @@
+const {ethers} = require("ethers");
+const {Defender} = require("@openzeppelin/defender-sdk");
+
+const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "blockId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "bytes",
+        name: "txList",
+        type: "bytes",
+      },
+    ],
+    name: "CalldataTxList",
+    type: "event",
+  },
+];
+
+function alertOrg(notificationClient, message) {
+  notificationClient.send({
+    channelAlias: "discord_blocks",
+    subject: "TaikoL1: CalldataTxList Count",
+    message: message,
+  });
+}
+
+async function getLatestBlockNumber(provider) {
+  const currentBlock = await provider.getBlock("latest");
+  return currentBlock.number;
+}
+
+async function calculateBlockTime(provider) {
+  const latestBlock = await provider.getBlock("latest");
+  const previousBlock = await provider.getBlock(latestBlock.number - 100);
+
+  const timeDiff = latestBlock.timestamp - previousBlock.timestamp;
+  const blockDiff = latestBlock.number - previousBlock.number;
+
+  const blockTime = timeDiff / blockDiff;
+  return blockTime;
+}
+
+async function calculateBlockRange(provider) {
+  const currentBlockNumber = await getLatestBlockNumber(provider);
+  const blockTimeInSeconds = await calculateBlockTime(provider);
+  const blocksIn24Hours = Math.floor((24 * 60 * 60) / blockTimeInSeconds); // 24 hours in seconds
+
+  const fromBlock = currentBlockNumber - blocksIn24Hours;
+  const toBlock = currentBlockNumber;
+
+  console.log(`Calculated block range: from ${fromBlock} to ${toBlock}`);
+
+  return {fromBlock, toBlock};
+}
+
+async function fetchLogsFromL1(
+  eventNames,
+  fromBlock,
+  toBlock,
+  address,
+  abi,
+  provider
+) {
+  const iface = new ethers.utils.Interface(abi);
+  const eventTopics = eventNames.map((eventName) =>
+    iface.getEventTopic(eventName)
+  );
+
+  console.log(`eventTopics: ${eventTopics}`);
+
+  try {
+    const logs = await provider.getLogs({
+      address,
+      fromBlock,
+      toBlock,
+      topics: [eventTopics],
+    });
+    console.log(`Fetched logs: ${logs.length}`);
+    return logs.map((log) => {
+      const parsedLog = iface.parseLog(log);
+      console.log(`Parsed log: ${JSON.stringify(parsedLog)}`);
+      return parsedLog;
+    });
+  } catch (error) {
+    console.error("Error fetching L1 logs:", error);
+    return [];
+  }
+}
+
+function createProvider(apiKey, apiSecret, relayerApiKey, relayerApiSecret) {
+  const client = new Defender({
+    apiKey,
+    apiSecret,
+    relayerApiKey,
+    relayerApiSecret,
+  });
+
+  return client.relaySigner.getProvider();
+}
+
+exports.handler = async function (event, context) {
+  const {notificationClient} = context;
+  const {apiKey, apiSecret, taikoL1ApiKey, taikoL1ApiSecret} = event.secrets;
+
+  const taikoL1Provider = createProvider(
+    apiKey,
+    apiSecret,
+    taikoL1ApiKey,
+    taikoL1ApiSecret
+  );
+
+  const {fromBlock, toBlock} = await calculateBlockRange(taikoL1Provider);
+
+  const logs = await fetchLogsFromL1(
+    ["CalldataTxList"],
+    fromBlock,
+    toBlock,
+    "0x06a9Ab27c7e2255df1815E6CC0168d7755Feb19a",
+    ABI,
+    taikoL1Provider
+  );
+
+  if (logs.length > 0) {
+    alertOrg(
+      notificationClient,
+      `Detected ${logs.length} CalldataTxList events in the last 24 hours on TaikoL1!`
+    );
+  }
+
+  return true;
+};
