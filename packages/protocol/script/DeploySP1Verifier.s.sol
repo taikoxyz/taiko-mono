16ac02e2a18bfcd5728008ab76997bc4c526d7c4
YoGhurt111 <YoGhurt111@users.noreply.github.com>
2024-09-02 02:31:04 +0000

    forge fmt & update contract layout table

--- packages/protocol/script/DeploySP1Verifier.s.sol
+++ packages/protocol/script/DeploySP1Verifier.s.sol
@@ -9,7 +9,7 @@ contract DeploySP1Verifier is DeployCapability {
     uint256 public deployerPrivKey = vm.envUint("PRIVATE_KEY");
     address public rollupAddressManager = vm.envAddress("ROLLUP_ADDRESS_MANAGER");
 
-    function run() external{
+    function run() external {
         require(deployerPrivKey != 0, "invalid deployer priv key");
         require(rollupAddressManager != address(0), "invalid rollup address manager address");
 
@@ -19,7 +19,6 @@ contract DeploySP1Verifier is DeployCapability {
         SP1Verifier120rc sp1Verifier120rc = new SP1Verifier120rc();
         register(rollupAddressManager, "sp1_remote_verifier", address(sp1Verifier120rc));
 
-
         deployProxy({
             name: "tier_zkvm_sp1",
             impl: address(new SP1Verifier()),
@@ -28,6 +27,5 @@ contract DeploySP1Verifier is DeployCapability {
         });
 
         vm.stopBroadcast();
-
     }
 }
