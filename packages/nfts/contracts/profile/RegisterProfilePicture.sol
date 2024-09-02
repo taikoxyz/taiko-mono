0582c9293c22aaab92c93436a9f1fb12b2ccfef0
ben <bennett@taiko.xyz>
2024-09-02 12:40:58 +0900

    feat: tests

--- packages/nfts/contracts/profile/RegisterProfilePicture.sol
+++ packages/nfts/contracts/profile/RegisterProfilePicture.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
-contract RegisterProfilePicture {
+contract RegisterProfilePicture is Initializable, UUPSUpgradeable, Ownable2StepUpgradeable {
 
-    error InvalidERC721Contract(address nftContract);
+    error InvalidNFTContract(address nftContract);
     error NotTokenOwner(address nftContract, uint256 tokenId, address caller);
 
     // Mapping from user address to the selected NFT (Collection address and token ID)
@@ -14,20 +18,27 @@ contract RegisterProfilePicture {
         uint256 tokenId;
     }
 
-    mapping(address user => ProfilePicture profilePicture) public profilePictures;
+    mapping(address => ProfilePicture) public profilePictures;
 
     event ProfilePictureSet(address indexed user, address indexed nftContract, uint256 indexed tokenId);
 
+    function initialize() public initializer {}
+
     // Function to set the PFP by providing the NFT contract address and token ID
     function setPFP(address nftContract, uint256 tokenId) external {
-        // Check if the provided contract address is a valid ERC721 contract
-        if (!IERC721(nftContract).supportsInterface(type(IERC721).interfaceId)) {
-            revert InvalidERC721Contract(nftContract);
-        }
-
-        // Check if the caller owns the token they are trying to set as their PFP
-        if (IERC721(nftContract).ownerOf(tokenId) != msg.sender) {
-            revert NotTokenOwner(nftContract, tokenId, msg.sender);
+        if (ERC165Upgradeable(nftContract).supportsInterface(type(ERC721Upgradeable).interfaceId)) {
+            // Check if the provided contract address is a valid ERC721 contract
+            if (ERC721Upgradeable(nftContract).ownerOf(tokenId) != msg.sender) {
+                revert NotTokenOwner(nftContract, tokenId, msg.sender);
+            }
+        } else if (ERC165Upgradeable(nftContract).supportsInterface(type(ERC1155Upgradeable).interfaceId)) {
+            // Check if the provided contract address is a valid ERC1155 contract
+            if (ERC1155Upgradeable(nftContract).balanceOf(msg.sender, tokenId) == 0) {
+                revert NotTokenOwner(nftContract, tokenId, msg.sender);
+            }
+        } else {
+            // If the contract does not support ERC721 or ERC1155 interfaces
+            revert InvalidNFTContract(nftContract);
         }
 
         // Set the PFP
@@ -35,5 +46,29 @@ contract RegisterProfilePicture {
 
         emit ProfilePictureSet(msg.sender, nftContract, tokenId);
     }
-}
 
+    // Function to get the image URI of the NFT set as the user's PFP
+    function getProfilePicture(address user) external view returns (string memory) {
+        ProfilePicture memory profilePicture = profilePictures[user];
+
+        if (ERC165Upgradeable(profilePicture.nftContract).supportsInterface(type(ERC721Upgradeable).interfaceId)) {
+            // ERC721 case: Check ownership before returning the URI
+            if (ERC721Upgradeable(profilePicture.nftContract).ownerOf(profilePicture.tokenId) != user) {
+                revert NotTokenOwner(profilePicture.nftContract, profilePicture.tokenId, user);
+            }
+            return ERC721Upgradeable(profilePicture.nftContract).tokenURI(profilePicture.tokenId);
+        } else if (ERC165Upgradeable(profilePicture.nftContract).supportsInterface(type(ERC1155Upgradeable).interfaceId)) {
+            // ERC1155 case: Check ownership before returning the URI
+            if (ERC1155Upgradeable(profilePicture.nftContract).balanceOf(user, profilePicture.tokenId) == 0) {
+                revert NotTokenOwner(profilePicture.nftContract, profilePicture.tokenId, user);
+            }
+            return ERC1155Upgradeable(profilePicture.nftContract).uri(profilePicture.tokenId);
+        } else {
+            // If the contract does not support ERC721 or ERC1155 interfaces
+            revert InvalidNFTContract(profilePicture.nftContract);
+        }
+    }
+
+    /// @notice Internal method to authorize an upgrade
+    function _authorizeUpgrade(address) internal virtual override onlyOwner { }
+}
--- packages/nfts/test/profile/RegisterProfilePicture.t.sol
+++ packages/nfts/test/profile/RegisterProfilePicture.t.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { Test, console } from "forge-std/src/Test.sol";
+import { Test } from "forge-std/src/Test.sol";
 import { RegisterProfilePicture } from "../../contracts/profile/RegisterProfilePicture.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import { ERC721Mock, ERC1155Mock, MockInvalidNFT } from "../util/MockTokens.sol";
+import { ERC721Mock, ERC1155Mock } from "../util/MockToken.sol";
 
 contract RegisterProfilePictureTest is Test {
     RegisterProfilePicture public registerProfilePicture;
     ERC721Mock public erc721Mock;
     ERC1155Mock public erc1155Mock;
-    MockInvalidNFT public invalidNFT;
 
     address public owner;
     address public user;
@@ -19,9 +18,7 @@ contract RegisterProfilePictureTest is Test {
     uint256 public constant ERC721_TOKEN_ID = 1;
     uint256 public constant ERC1155_TOKEN_ID = 2;
 
-    event ProfilePictureSet(
-        address indexed user, address indexed nftContract, uint256 indexed tokenId
-    );
+    event ProfilePictureSet(address indexed user, address indexed nftContract, uint256 tokenId);
 
     function setUp() public {
         owner = vm.addr(0x1);
@@ -31,236 +28,86 @@ contract RegisterProfilePictureTest is Test {
         vm.startBroadcast(owner);
 
         RegisterProfilePicture impl = new RegisterProfilePicture();
-        ERC1967Proxy proxy =
-            new ERC1967Proxy(address(impl), abi.encodeCall(RegisterProfilePicture.initialize, ()));
+        ERC1967Proxy proxy = new ERC1967Proxy(
+            address(impl),
+            abi.encodeCall(RegisterProfilePicture.initialize, ())
+        );
         registerProfilePicture = RegisterProfilePicture(address(proxy));
 
-        erc721Mock = new ERC721Mock();
-        erc721Mock.initialize("MockERC721", "M721");
+        erc721Mock = new ERC721Mock("MockERC721", "M721");
         erc721Mock.mint(user, ERC721_TOKEN_ID);
 
-        erc1155Mock = new ERC1155Mock();
-        erc1155Mock.initialize("https://token-cdn-domain/{id}.json");
+        erc1155Mock = new ERC1155Mock("https://token-cdn-domain/{id}.json");
         erc1155Mock.mint(user, ERC1155_TOKEN_ID, 1, "");
-
-        invalidNFT = new MockInvalidNFT();
 
         vm.stopBroadcast();
     }
 
-    function test_SetPFPWithERC721() public {
+    function testSetPFPWithERC721() public {
         vm.startPrank(user);
 
+        vm.expectEmit(true, true, true, true);
+        emit ProfilePictureSet(user, address(erc721Mock), ERC721_TOKEN_ID);
         registerProfilePicture.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
 
-        (address nftContract, uint256 tokenId) = registerProfilePicture.profilePicture(user);
+        (address nftContract, uint256 tokenId) = registerProfilePicture.profilePictures(user);
         assertEq(nftContract, address(erc721Mock));
         assertEq(tokenId, ERC721_TOKEN_ID);
 
         vm.stopPrank();
     }
 
-    function test_SetPFPWithERC1155() public {
+    function testSetPFPWithERC1155() public {
         vm.startPrank(user);
 
+        vm.expectEmit(true, true, true, true);
+        emit ProfilePictureSet(user, address(erc1155Mock), ERC1155_TOKEN_ID);
         registerProfilePicture.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
 
-        (address nftContract, uint256 tokenId) = registerProfilePicture.profilePicture(user);
+        (address nftContract, uint256 tokenId) = registerProfilePicture.profilePictures(user);
         assertEq(nftContract, address(erc1155Mock));
         assertEq(tokenId, ERC1155_TOKEN_ID);
 
         vm.stopPrank();
     }
 
-    function test_GetProfilePictureERC721() public {
+    function testGetProfilePicture() public {
         vm.startPrank(user);
-        registerProfilePicture.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
-        string memory uri = registerProfilePicture.getProfilePicture(user);
-        assertEq(uri, erc721Mock.tokenURI(ERC721_TOKEN_ID));
-        vm.stopPrank();
-    }
 
-    function test_GetProfilePictureERC1155() public {
-        vm.startPrank(user);
+        registerProfilePicture.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
+        assertEq(registerProfilePicture.getProfilePicture(user), erc721Mock.tokenURI(ERC721_TOKEN_ID));
+
         registerProfilePicture.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
-        string memory uri = registerProfilePicture.getProfilePicture(user);
-        assertEq(uri, erc1155Mock.uri(ERC1155_TOKEN_ID));
-        vm.stopPrank();
-    }
-
-    function test_CannotSetPFPWithInvalidNFTContract() public {
-        vm.startPrank(user);
-
-        // Expect any kind of revert when trying to set PFP with invalid NFT contract
-        vm.expectRevert();
-        registerProfilePicture.setPFP(address(invalidNFT), 1);
+        assertEq(registerProfilePicture.getProfilePicture(user), erc1155Mock.uri(ERC1155_TOKEN_ID));
 
         vm.stopPrank();
     }
 
-    function test_CannotSetPFPWithNonOwnedERC721() public {
+    function testCannotSetPFPWithNonOwnedToken() public {
         vm.startPrank(otherUser);
 
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "NotTokenOwner(address,uint256,address)",
-                address(erc721Mock),
-                ERC721_TOKEN_ID,
-                otherUser
-            )
-        );
+        vm.expectRevert(abi.encodeWithSignature("NotTokenOwner(address,uint256,address)", address(erc721Mock), ERC721_TOKEN_ID, otherUser));
         registerProfilePicture.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
 
-        vm.stopPrank();
-    }
-
-    function test_CannotSetPFPWithNonOwnedERC1155() public {
-        vm.startPrank(otherUser);
-
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "NotTokenOwner(address,uint256,address)",
-                address(erc1155Mock),
-                ERC1155_TOKEN_ID,
-                otherUser
-            )
-        );
+        vm.expectRevert(abi.encodeWithSignature("NotTokenOwner(address,uint256,address)", address(erc1155Mock), ERC1155_TOKEN_ID, otherUser));
         registerProfilePicture.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
 
         vm.stopPrank();
     }
 
-    function test_CannotGetProfilePictureAfterTransferERC721() public {
+    function testCannotGetProfilePictureAfterTransfer() public {
         vm.startPrank(user);
 
         registerProfilePicture.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
-        erc721Mock.transferFrom(user, otherUser, ERC721_TOKEN_ID);
-
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "NotTokenOwner(address,uint256,address)", address(erc721Mock), ERC721_TOKEN_ID, user
-            )
-        );
+        erc721Mock.safeTransferFrom(user, otherUser, ERC721_TOKEN_ID);
+        vm.expectRevert(abi.encodeWithSignature("NotTokenOwner(address,uint256,address)", address(erc721Mock), ERC721_TOKEN_ID, user));
         registerProfilePicture.getProfilePicture(user);
-
-        vm.stopPrank();
-    }
-
-    function test_CannotGetProfilePictureAfterTransferERC1155() public {
-        vm.startPrank(user);
 
         registerProfilePicture.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
         erc1155Mock.safeTransferFrom(user, otherUser, ERC1155_TOKEN_ID, 1, "");
-
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "NotTokenOwner(address,uint256,address)",
-                address(erc1155Mock),
-                ERC1155_TOKEN_ID,
-                user
-            )
-        );
+        vm.expectRevert(abi.encodeWithSignature("NotTokenOwner(address,uint256,address)", address(erc1155Mock), ERC1155_TOKEN_ID, user));
         registerProfilePicture.getProfilePicture(user);
 
-        vm.stopPrank();
-    }
-
-    function test_ChangeProfilePicture() public {
-        vm.startPrank(user);
-
-        registerProfilePicture.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
-
-        registerProfilePicture.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
-
-        (address nftContract, uint256 tokenId) = registerProfilePicture.profilePicture(user);
-        assertEq(nftContract, address(erc1155Mock));
-        assertEq(tokenId, ERC1155_TOKEN_ID);
-
-        vm.stopPrank();
-    }
-
-    function test_upgrade() public {
-        // Step 1: Deploy the initial implementation (v1) using the proxy
-        vm.startPrank(owner);
-
-        // Deploy v1 implementation and proxy pointing to v1
-        address implV1 = address(new RegisterProfilePicture());
-        ERC1967Proxy proxy = new ERC1967Proxy(implV1, abi.encodeWithSignature("initialize()"));
-        RegisterProfilePicture tokenV1 = RegisterProfilePicture(address(proxy));
-
-        vm.stopPrank();
-
-        // Step 2: Interact with v1
-        vm.startPrank(user);
-        tokenV1.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
-
-        // Verify that the PFP was set correctly
-        string memory uri = tokenV1.getProfilePicture(user);
-        assertEq(uri, erc721Mock.tokenURI(ERC721_TOKEN_ID));
-        vm.stopPrank();
-
-        // Step 3: Upgrade to v2
-        vm.startPrank(owner);
-        address implV2 = address(new RegisterProfilePicture());
-        tokenV1.upgradeToAndCall(implV2, "");
-
-        // Verify that the previous state is still relevant after the upgrade
-        RegisterProfilePicture tokenV2 = RegisterProfilePicture(address(proxy));
-        uri = tokenV2.getProfilePicture(user);
-        assertEq(uri, erc721Mock.tokenURI(ERC721_TOKEN_ID));
-        vm.stopPrank();
-
-        // Step 4: Test setting and retrieving PFP with ERC1155 after the upgrade
-        vm.startPrank(user);
-        tokenV2.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
-
-        uri = tokenV2.getProfilePicture(user);
-        assertEq(uri, erc1155Mock.uri(ERC1155_TOKEN_ID));
-        vm.stopPrank();
-    }
-
-    function test_upgrade_throw() public {
-        // Step 1: Deploy the initial implementation (v1) using the proxy
-        vm.startPrank(owner);
-
-        // Deploy v1 implementation and proxy pointing to v1
-        address implV1 = address(new RegisterProfilePicture());
-        ERC1967Proxy proxy = new ERC1967Proxy(implV1, abi.encodeWithSignature("initialize()"));
-        RegisterProfilePicture tokenV1 = RegisterProfilePicture(address(proxy));
-
-        vm.stopPrank();
-
-        // Step 2: Interact with v1
-        vm.startPrank(user);
-        tokenV1.setPFP(address(erc721Mock), ERC721_TOKEN_ID);
-
-        // Verify that the PFP was set correctly
-        string memory uri = tokenV1.getProfilePicture(user);
-        assertEq(uri, erc721Mock.tokenURI(ERC721_TOKEN_ID));
-
-        // Attempt to upgrade as a non-owner and expect a revert
-        address implV2 = address(new RegisterProfilePicture());
-        vm.expectRevert();
-        tokenV1.upgradeToAndCall(implV2, "");
-        vm.stopPrank();
-
-        // Step 3: Upgrade to v2 by the owner
-        vm.startPrank(owner);
-        implV2 = address(new RegisterProfilePicture());
-        tokenV1.upgradeToAndCall(implV2, "");
-
-        // Verify that the previous state is still relevant after the upgrade
-        RegisterProfilePicture tokenV2 = RegisterProfilePicture(address(proxy));
-        uri = tokenV2.getProfilePicture(user);
-        assertEq(uri, erc721Mock.tokenURI(ERC721_TOKEN_ID));
-        vm.stopPrank();
-
-        // Step 4: Test setting and retrieving PFP with ERC1155 after the upgrade
-        vm.startPrank(user);
-        tokenV2.setPFP(address(erc1155Mock), ERC1155_TOKEN_ID);
-
-        uri = tokenV2.getProfilePicture(user);
-        assertEq(uri, erc1155Mock.uri(ERC1155_TOKEN_ID));
         vm.stopPrank();
     }
 }
