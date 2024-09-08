4c9a88ed7c30f84b7c0ee1d7ec81fca8af4d6407
Daniel Wang <dan@taikocha.in>
2024-09-08 22:38:39 +0800

    fmt

--- packages/protocol/contracts/L2/TaikoL2.sol
+++ packages/protocol/contracts/L2/TaikoL2.sol
@@ -93,8 +93,7 @@ contract TaikoL2 is EssentialContract, IBlockHashReader {
 
         if (block.number == 0) {
             // This is the case in real L2 genesis
-        }
-        else if (block.number == 1) {
+        } else if (block.number == 1) {
             // This is the case in tests
             uint256 parentHeight = block.number - 1;
             _l2Hashes[parentHeight] = blockhash(parentHeight);
@@ -355,9 +354,7 @@ contract TaikoL2 is EssentialContract, IBlockHashReader {
         );
     }
 
-    function _calcPublicInputHash(
-        uint256 _blockId
-    )
+    function _calcPublicInputHash(uint256 _blockId)
         private
         view
         returns (bytes32 publicInputHashOld, bytes32 publicInputHashNew)
