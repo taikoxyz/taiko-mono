5adf10e3d13b917a48a40ea24706543dbda3cbef
Daniel Wang <dan@taiko.xyz>
2024-09-11 02:06:17 +0800

    Update gen-layouts.sh

--- packages/protocol/script/gen-layouts.sh
+++ packages/protocol/script/gen-layouts.sh
@@ -95,3 +95,5 @@ for contract in "${contracts[@]}"; do
     FOUNDRY_PROFILE=${profile} forge inspect -C ./contracts/${profile} -o ./out/${profile} ${contract} storagelayout --pretty >> $output_file
     echo "" >> $output_file
 done
+
+sed -i '' 's/contracts\/.*\/\([^\/]*\)\.sol:\1/\1/g' contract_layout_layer1.md $output_file
