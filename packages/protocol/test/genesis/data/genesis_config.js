39b059c7508dd84bd50de9e37e3affaf80adb74a
Jeffery Walsh <cyberhorsey@gmail.com>
2024-09-13 13:07:05 -0700

    protocl config

--- packages/protocol/test/genesis/data/genesis_config.js
+++ packages/protocol/test/genesis/data/genesis_config.js
@@ -3,19 +3,18 @@ const ADDRESS_LENGTH = 40;
 
 module.exports = {
   // Owner address of the pre-deployed L2 contracts.
-  contractOwner: "0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39",
-  l1ChainId: 1,
-  // Chain ID of the Taiko L2 network.
-  chainId: 167,
+  contractOwner: "0x91c34396e640e3bB4eE80f2e523c23dDa2b3f78F",
+  l1ChainId: 7014190335,
+  chainId: 167010,
   // Account address and pre-mint ETH amount as key-value pairs.
   seedAccounts: [
-    { "0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39": 1000 },
+    { "0x91c34396e640e3bB4eE80f2e523c23dDa2b3f78F": 1000 },
     { "0x79fcdef22feed20eddacbb2587640e45491b757f": 1000 },
   ],
   // Owner Chain ID, Security Council, and Timelock Controller
   l1ChainId: 31337,
-  ownerSecurityCouncil: "0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39",
-  ownerTimelockController: "0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39",
+  ownerSecurityCouncil: "0x91c34396e640e3bB4eE80f2e523c23dDa2b3f78F",
+  ownerTimelockController: "0x91c34396e640e3bB4eE80f2e523c23dDa2b3f78F",
   get contractAddresses() {
     return {
       // ============ Implementations ============
@@ -48,7 +47,7 @@ module.exports = {
   },
   // L2 EIP-1559 baseFee calculation related fields.
   param1559: {
-    gasExcess: 1,
+    gasExcess: 0,
   },
   // Option to pre-deploy an ERC-20 token.
   predeployERC20: true,
