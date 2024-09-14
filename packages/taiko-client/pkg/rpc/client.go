3199175c53c114f19612549d0259b926a806e436
maskpp <maskpp266@gmail.com>
2024-09-15 06:18:02 +0800

    revert change

--- packages/taiko-client/pkg/rpc/client.go
+++ packages/taiko-client/pkg/rpc/client.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
@@ -80,7 +81,7 @@ func NewClient(ctx context.Context, cfg *ClientConfig) (*Client, error) {
 		}
 
 		// NOTE: when running tests, we do not have a L1 beacon endpoint.
-		if cfg.L1BeaconEndpoint != "" {
+		if cfg.L1BeaconEndpoint != "" && os.Getenv("RUN_TESTS") == "" {
 			if l1BeaconClient, err = NewBeaconClient(cfg.L1BeaconEndpoint, defaultTimeout); err != nil {
 				log.Error("Failed to connect to L1 beacon endpoint, retrying", "endpoint", cfg.L1BeaconEndpoint, "err", err)
 				return err
