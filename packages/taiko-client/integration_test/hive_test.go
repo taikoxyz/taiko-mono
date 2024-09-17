7d6db54d413ef08728733fae4be886bf32ff3ef5
Gavin Yu <gavin@taiko.xyz>
2024-09-17 21:41:24 +0800

    fix(taiko-client): revert path changes about SocialScan endpoint (#18119)

--- packages/taiko-client/integration_test/hive_test.go
+++ packages/taiko-client/integration_test/hive_test.go
@@ -18,7 +18,7 @@ func TestHiveHandler(t *testing.T) {
 
 	clientGroups := [][]string{
 		{
-			"taiko/anvil",
+			"anvil",
 			"taiko/taiko-geth",
 			"taiko/driver",
 			"taiko/proposer",
@@ -46,6 +46,50 @@ func TestHiveHandler(t *testing.T) {
 	t.Run("taiko-reorg/taiko-reorg/clusters(1)", func(t *testing.T) {
 		testDenebReorg(t, "taiko-reorg/taiko-reorg", [][]string{clientGroups[0]})
 	})
+
+	t.Run("taiko-blob/blob-l1-beacon", func(t *testing.T) {
+		testBlobScan(t, "taiko-blob/blob-server", []string{
+			"geth",
+			"prysm/prysm-bn",
+			"prysm/prysm-vc",
+			"taiko/taiko-geth",
+			"taiko/driver",
+			"taiko/proposer",
+			"taiko/prover",
+			"storage/redis",
+			"storage/postgres",
+			"blobscan/blobscan-api",
+			"blobscan/blobscan-indexer",
+		})
+	})
+
+	t.Run("taiko-blob/blob-server", func(t *testing.T) {
+		testBlobScan(t, "taiko-blob/blob-l1-beacon", []string{
+			"geth",
+			"prysm/prysm-bn",
+			"prysm/prysm-vc",
+			"taiko/taiko-geth",
+			"taiko/driver",
+			"taiko/proposer",
+			"taiko/prover",
+		})
+	})
+}
+
+func testBlobScan(t *testing.T, pattern string, clients []string) {
+	handler, err := hivesim.NewHiveFramework(&hivesim.HiveConfig{
+		BuildOutput:     false,
+		ContainerOutput: true,
+		BaseDir:         "/Users/huan/projects/taiko/hive",
+		SimPattern:      "taiko",
+		SimTestPattern:  pattern,
+		ClientGroups:    [][]string{clients},
+	})
+	assert.NoError(t, err)
+
+	failedCount, err := handler.Run(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, 0, failedCount)
 }
 
 func testDenebGenesis(t *testing.T, simPattern string, clientGroups [][]string) {
