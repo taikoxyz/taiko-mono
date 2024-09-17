2a4248814ef354f46c486ac2f6cdcb6e3dd42255
Jeffery Walsh <cyberhorsey@gmail.com>
2024-09-17 12:16:36 -0700

    remove nft metadata indexing, far too slowy and bug riddeen right now.

--- packages/eventindexer/migrations/20270906208847_alter_nft_metadata_table_update_symbol_length.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE nft_metadata MODIFY symbol VARCHAR(42) DEFAULT NULL;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE nft_metadata MODIFY symbol VARCHAR(10) DEFAULT NULL;
--- +goose StatementEnd
