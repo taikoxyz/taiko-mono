36e66019848754f5eee551a78036f500d8670df0
David <david@taiko.xyz>
2024-09-24 14:22:44 +0800

    test(taiko-client): introduce `TestProposeTxListOntake`

--- packages/taiko-client/proposer/proposer_test.go
+++ packages/taiko-client/proposer/proposer_test.go
@@ -2,6 +2,7 @@ package proposer
 
 import (
 	"context"
+	"math/big"
 	"os"
 	"testing"
 	"time"
@@ -281,6 +282,41 @@ func (s *ProposerTestSuite) TestProposeEmptyBlockOp() {
 	s.p.MinProposingInternal = 1 * time.Second
 	s.p.lastProposedAt = time.Now().Add(-10 * time.Second)
 	s.Nil(s.p.ProposeOp(context.Background()))
+}
+
+func (s *ProposerTestSuite) TestProposeTxListOntake() {
+	for i := 0; i < int(s.p.protocolConfigs.OntakeForkHeight); i++ {
+		s.ProposeAndInsertValidBlock(s.p, s.s)
+	}
+
+	l2Head, err := s.p.rpc.L2.HeaderByNumber(context.Background(), nil)
+	s.Nil(err)
+	s.GreaterOrEqual(l2Head.Number.Uint64(), s.p.protocolConfigs.OntakeForkHeight)
+
+	sink := make(chan *bindings.TaikoL1ClientBlockProposedV2)
+	sub, err := s.p.rpc.TaikoL1.WatchBlockProposedV2(nil, sink, nil)
+	s.Nil(err)
+	defer func() {
+		sub.Unsubscribe()
+		close(sink)
+	}()
+	s.Nil(s.p.ProposeTxListOntake(context.Background(), []types.Transactions{{}, {}}))
+	s.Nil(s.s.ProcessL1Blocks(context.Background()))
+
+	var l1Height *big.Int
+	for i := 0; i < 2; i++ {
+		event := <-sink
+		if l1Height == nil {
+			l1Height = new(big.Int).SetUint64(event.Raw.BlockNumber)
+			continue
+		}
+		s.Equal(l1Height.Uint64(), event.Raw.BlockNumber)
+	}
+
+	newL2head, err := s.p.rpc.L2.HeaderByNumber(context.Background(), nil)
+	s.Nil(err)
+
+	s.Equal(l2Head.Number.Uint64()+2, newL2head.Number.Uint64())
 }
 
 func (s *ProposerTestSuite) TestUpdateProposingTicker() {
