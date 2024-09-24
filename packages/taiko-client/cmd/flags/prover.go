1f46ea91f6a8040de07a53a3c5ed60c958f0f119
Gavin Yu <gavin@taiko.xyz>
2024-09-24 16:14:04 +0800

    Update packages/taiko-client/cmd/flags/prover.go
    Co-authored-by: David <david@taiko.xyz>

--- packages/taiko-client/cmd/flags/prover.go
+++ packages/taiko-client/cmd/flags/prover.go
@@ -206,7 +206,7 @@ var (
 	}
 	ProveInterval = &cli.DurationFlag{
 		Name: "prover.interval",
-		Usage: "Time interval to prove blocks when the number of pending proof don not exceed prover.batchSize, " +
+		Usage: "Time interval to prove blocks when the number of pending proof do not exceed prover.batchSize, " +
 			"this flag only works post Ontake fork",
 		Category: proposerCategory,
 		Value:    12 * time.Second,
