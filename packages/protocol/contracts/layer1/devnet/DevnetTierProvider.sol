f4a9ac85683174cc65dc57974498f879a7337a81
gavin <gavin@taiko.xyz>
2024-09-26 13:57:39 +0800

    temp

--- packages/protocol/contracts/layer1/devnet/DevnetTierProvider.sol
+++ packages/protocol/contracts/layer1/devnet/DevnetTierProvider.sol
@@ -14,14 +14,21 @@ contract DevnetTierProvider is TierProviderBase, ITierRouter {
 
     /// @inheritdoc ITierProvider
     function getTierIds() public pure override returns (uint16[] memory tiers_) {
-        tiers_ = new uint16[](3);
+        tiers_ = new uint16[](6);
         tiers_[0] = LibTiers.TIER_OPTIMISTIC;
-        tiers_[1] = LibTiers.TIER_GUARDIAN_MINORITY;
-        tiers_[2] = LibTiers.TIER_GUARDIAN;
+        tiers_[1] = LibTiers.TIER_SGX;
+        tiers_[2] = LibTiers.TIER_ZKVM_RISC0;
+        tiers_[3] = LibTiers.TIER_GUARDIAN_MINORITY;
+        tiers_[4] = LibTiers.TIER_GUARDIAN;
+        tiers_[5] = LibTiers.TIER_ZKVM_SP1;
     }
 
     /// @inheritdoc ITierProvider
-    function getMinTier(address, uint256) public pure override returns (uint16) {
-        return LibTiers.TIER_OPTIMISTIC;
+    function getMinTier(address, uint256 _rand) public pure override returns (uint16) {
+        if (_rand % 4 == 0) {
+            return LibTiers.TIER_SGX;
+        } else {
+            return LibTiers.TIER_OPTIMISTIC;
+        }
     }
 }
