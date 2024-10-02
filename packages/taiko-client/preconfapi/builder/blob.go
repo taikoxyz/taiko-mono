b0a9cc7188fe4709d282ec461d68eeb5df7d73b4
fbrv <fabio@gattaca.com>
2024-10-02 17:45:02 +0100

    fix out of range for emptyTxLists

--- packages/taiko-client/preconfapi/builder/blob.go
+++ packages/taiko-client/preconfapi/builder/blob.go
@@ -177,7 +177,7 @@ func (b *BlobTransactionBuilder) BuildBlocksUnsigned(
 		encodedParams = append(encodedParams, encoded)
 	}
 
-	emptyTxLists := make([][]byte, 0)
+	emptyTxLists := make([][]byte, len(encodedParams))
 
 	for i := range encodedParams {
 		emptyTxLists[i] = []byte{0xff}
