64224cadeaec9242cae210b226037946463530aa
David <david@taiko.xyz>
2024-10-03 14:24:14 +0800

    bump op && prysm

--- packages/taiko-client/internal/metrics/metrics.go
+++ packages/taiko-client/internal/metrics/metrics.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	opMetrics "github.com/ethereum-optimism/optimism/op-service/metrics"
-	"github.com/ethereum-optimism/optimism/op-service/opio"
 	txmgrMetrics "github.com/ethereum-optimism/optimism/op-service/txmgr/metrics"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/urfave/cli/v2"
 
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/cmd/flags"
+	"github.com/taikoxyz/taiko-mono/packages/taiko-client/pkg/rpc"
 )
 
 // Metrics
@@ -92,7 +92,7 @@ func Serve(ctx context.Context, c *cli.Context) error {
 		}
 	}()
 
-	opio.BlockOnInterruptsContext(ctx)
+	rpc.BlockOnInterruptsContext(ctx)
 
 	return nil
 }
