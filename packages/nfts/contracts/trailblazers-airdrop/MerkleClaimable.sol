921d7439aff1d5798cfb4f3be399ffd3ae7c8592
bearni95 <bernatcanal@gmail.com>
2024-10-03 11:14:27 +0200

    added updated contracts

--- packages/nfts/contracts/trailblazers-airdrop/MerkleClaimable.sol
+++ packages/nfts/contracts/trailblazers-airdrop/MerkleClaimable.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "../../../shared/common/EssentialContract.sol";
+
+import { UUPSUpgradeable } from
+    "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { Ownable2StepUpgradeable } from
+    "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import { ContextUpgradeable } from
+    "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 
 /// @title MerkleClaimable
 /// @notice Contract for managing Taiko token airdrop for eligible users
 /// @custom:security-contact security@taiko.xyz
-abstract contract MerkleClaimable is EssentialContract {
+abstract contract MerkleClaimable is
+    ContextUpgradeable,
+    UUPSUpgradeable,
+    Ownable2StepUpgradeable
+{
     /// @notice Mapping of hashes and their claim status
     mapping(bytes32 hash => bool claimed) public isClaimed;
 
@@ -26,11 +37,19 @@ abstract contract MerkleClaimable is EssentialContract {
     /// @param hash Hash of the claim
     event Claimed(bytes32 hash);
 
+    /// @notice Event emitted when config is changed
+    /// @param claimStart Unix timestamp for claim start
+    /// @param claimEnd Unix timestamp for claim end
+    /// @param merkleRoot Merkle root of the tree
+    event ConfigChanged(uint64 claimStart, uint64 claimEnd, bytes32 merkleRoot);
+
+    /// @notice Errors
     error CLAIM_NOT_ONGOING();
     error CLAIMED_ALREADY();
     error INVALID_PARAMS();
     error INVALID_PROOF();
 
+    /// @notice Modifier to check if the claim is ongoing
     modifier ongoingClaim() {
         if (
             merkleRoot == 0x0 || claimStart == 0 || claimEnd == 0 || claimStart > block.timestamp
@@ -54,6 +73,10 @@ abstract contract MerkleClaimable is EssentialContract {
         _setConfig(_claimStart, _claimEnd, _merkleRoot);
     }
 
+    /// @notice Initialize the contract
+    /// @param _claimStart Unix timestamp for claim start
+    /// @param _claimEnd Unix timestamp for claim end
+    /// @param _merkleRoot Merkle root of the tree
     function __MerkleClaimable_init(
         uint64 _claimStart,
         uint64 _claimEnd,
@@ -62,9 +85,13 @@ abstract contract MerkleClaimable is EssentialContract {
         internal
         onlyInitializing
     {
+        __Context_init();
         _setConfig(_claimStart, _claimEnd, _merkleRoot);
     }
 
+    /// @notice Verify an airdrop claim
+    /// @param data Data to be hashed
+    /// @param proof Merkle proof
     function _verifyClaim(bytes memory data, bytes32[] calldata proof) internal ongoingClaim {
         bytes32 hash = keccak256(abi.encode("CLAIM_TAIKO_AIRDROP", data));
 
@@ -75,6 +102,11 @@ abstract contract MerkleClaimable is EssentialContract {
         emit Claimed(hash);
     }
 
+    /// @notice Verify a Merkle proof
+    /// @param _proof Merkle proof
+    /// @param _merkleRoot Merkle root
+    /// @param _value Value to verify
+    /// @return Whether the proof is valid
     function _verifyMerkleProof(
         bytes32[] calldata _proof,
         bytes32 _merkleRoot,
@@ -88,11 +120,19 @@ abstract contract MerkleClaimable is EssentialContract {
         return MerkleProof.verify(_proof, _merkleRoot, _value);
     }
 
+    /// @notice Set config parameters
+    /// @param _claimStart Unix timestamp for claim start
+    /// @param _claimEnd Unix timestamp for claim end
+    /// @param _merkleRoot Merkle root of the tree
     function _setConfig(uint64 _claimStart, uint64 _claimEnd, bytes32 _merkleRoot) private {
         if (_claimStart > _claimEnd) revert INVALID_PARAMS();
 
         claimStart = _claimStart;
         claimEnd = _claimEnd;
         merkleRoot = _merkleRoot;
+        emit ConfigChanged(_claimStart, _claimEnd, _merkleRoot);
     }
+
+    /// @notice Internal method to authorize an upgrade
+    function _authorizeUpgrade(address) internal virtual override onlyOwner { }
 }
