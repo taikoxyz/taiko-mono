9acde2a2dab7896d3864da4dfedf14036909bc57
David <david@taiko.xyz>
2024-10-05 09:08:46 +0800

    Revert "Revert "fix(taiko-client): correct finalized and safe block hash (#18211)""
    This reverts commit f0cc9d80f9dfb509102c419c1b0c5f04451d659f.

--- packages/taiko-client/driver/chain_syncer/blob/syncer.go
+++ packages/taiko-client/driver/chain_syncer/blob/syncer.go
@@ -409,7 +409,16 @@ func (s *Syncer) insertNewHead(
 		return nil, fmt.Errorf("failed to create execution payloads: %w", err)
 	}
 
-	fc := &engine.ForkchoiceStateV1{HeadBlockHash: payload.BlockHash}
+	lastVerifiedBlockHash, err := s.rpc.GetLastVerifiedBlockHash(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch the last verified block hash: %w", err)
+	}
+
+	fc := &engine.ForkchoiceStateV1{
+		HeadBlockHash:      payload.BlockHash,
+		SafeBlockHash:      lastVerifiedBlockHash,
+		FinalizedBlockHash: lastVerifiedBlockHash,
+	}
 
 	// Update the fork choice
 	fcRes, err := s.rpc.L2Engine.ForkchoiceUpdate(ctx, fc, nil)
