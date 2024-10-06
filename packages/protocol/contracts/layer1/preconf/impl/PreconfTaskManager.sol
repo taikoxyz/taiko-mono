9204901fc25c89bfb5c77e7b8ae58acaf31486d1
Daniel Wang <dan@taikocha.in>
2024-10-06 21:17:56 +0800

    better impl of proveIncorrectPreconfirmation

--- packages/protocol/contracts/layer1/preconf/impl/PreconfTaskManager.sol
+++ packages/protocol/contracts/layer1/preconf/impl/PreconfTaskManager.sol
@@ -7,6 +7,7 @@ import "../../based/ITaikoL1.sol";
 import "../../based/TaikoData.sol";
 import "../libs/LibNames.sol";
 import "../libs/LibMerkle.sol";
+import "../libs/LibBlockHeader.sol";
 import "../iface/ILookahead.sol";
 import "../iface/IPreconfServiceManager.sol";
 import "../iface/IPreconfTaskManager.sol";
@@ -21,31 +22,26 @@ contract PreconfTaskManager is IPreconfTaskManager, EssentialContract {
         uint64 blockId;
         uint64 chainId;
         uint32 position;
+        bool isExecutionPreconf;
         bytes32 txHash;
-    }
-
-    struct BlockHeader {
-        // TODO
-        bytes32 parentHash;
-        bytes32 stateRoot;
-        bytes32 transactionsHash;
-        uint64 timestamp;
-        uint64 difficulty;
-        uint64 nonce;
+        bytes signature;
     }
 
     error ChainIdMismatch();
     error BlockHashMismatch();
     error BlockMetadataMismatch();
     error BlockNotVerified();
+    error ExecutionPreconfNotSupported();
     error InvalidSignature();
     error PositionOutOfBounds();
     error SenderNotCurrentPreconfer();
     error TxHashMismatch();
+    error TxIncluded();
     error TxRootHashMismatch();
 
     modifier onlyFromCurrentPreconfer() {
-        require(_lookahead().isCurrentPreconfer(msg.sender), SenderNotCurrentPreconfer());
+        ILookahead lookahead = ILookahead(resolve(LibNames.B_LOOKAHEAD, false));
+        require(lookahead.isCurrentPreconfer(msg.sender), SenderNotCurrentPreconfer());
         _;
     }
 
@@ -62,7 +58,8 @@ contract PreconfTaskManager is IPreconfTaskManager, EssentialContract {
         nonReentrant
         returns (TaikoData.BlockMetadataV2 memory)
     {
-        return _taikoL1().proposeBlockV2(_params, _txList);
+        ITaikoL1 taiko = ITaikoL1(resolve(LibNames.B_TAIKO, false));
+        return taiko.proposeBlockV2(_params, _txList);
     }
 
     /// @notice Proposes multiple Taiko L2 blocks (version 2)
@@ -78,13 +75,13 @@ contract PreconfTaskManager is IPreconfTaskManager, EssentialContract {
         nonReentrant
         returns (TaikoData.BlockMetadataV2[] memory)
     {
-        return _taikoL1().proposeBlocksV2(_paramsArr, _txListArr);
+        ITaikoL1 taiko = ITaikoL1(resolve(LibNames.B_TAIKO, false));
+        return taiko.proposeBlocksV2(_paramsArr, _txListArr);
     }
 
     function proveIncorrectPreconfirmation(
         TaikoData.BlockMetadataV2 calldata _meta,
         Receipt calldata _receipt,
-        bytes calldata _receiptSignature,
         BlockHeader calldata _blockHeader,
         bytes32[] calldata _transactionHashes
     )
@@ -92,48 +89,72 @@ contract PreconfTaskManager is IPreconfTaskManager, EssentialContract {
         onlyFromCurrentPreconfer
         nonReentrant
     {
-        require(_transactionHashes.length > _receipt.position, PositionOutOfBounds());
-        require(_transactionHashes[_receipt.position] == _receipt.txHash, TxHashMismatch());
+        // For now only tx-inclusion preconfirmation are supported
+        require(!_receipt.isExecutionPreconf, ExecutionPreconfNotSupported());
+
+        // This function verifies that the provided list of transaction hashes matches the
+        // transactions root in the block header.
+        // Note: The data cost for _transactionHashes can be very high with this implementation.
+        // Consider optimizing this function to reduce data costs.
         require(
-            _blockHeader.transactionsHash == _transactionHashes.keccakMerkleize(),
-            TxRootHashMismatch()
+            _blockHeader.transactionsRoot == _transactionHashes.merklize(), TxRootHashMismatch()
         );
 
-        ITaikoL1 taiko = _taikoL1();
+        ITaikoL1 taiko = ITaikoL1(resolve(LibNames.B_TAIKO, false));
+
+        // Verify chainId
         require(taiko.getConfig().chainId == _receipt.chainId, ChainIdMismatch());
 
+        // Get the block data for the given block ID.
+        // Note that this function may revert as only a few days of transactions are available in
+        // Taiko BCR protocol's ring buffer.
         TaikoData.BlockV2 memory blk = taiko.getBlockV2(_meta.id);
+
+        // Verify the block has been verified.
         require(blk.verifiedTransitionId != 0, BlockNotVerified());
         require(blk.metaHash == keccak256(abi.encode(_meta)), BlockMetadataMismatch());
-        require(
-            hashReceipt(_receipt).recover(_receiptSignature) == _meta.proposer, InvalidSignature()
-        );
 
+        // Verify the preconfirmation is signed by the block's proposer.
+        require(recoverPreconfer(_receipt) == _meta.proposer, InvalidSignature());
+
+        // Retrieve the block's block hash to verify the provided block header is correct.
         TaikoData.TransitionState memory tran =
             taiko.getTransition(_meta.id, blk.verifiedTransitionId);
-        require(hashBlockHeader(_blockHeader) == tran.blockHash, BlockHashMismatch());
+        require(LibBlockHeader.hashBlockHeader(_blockHeader) == tran.blockHash, BlockHashMismatch());
 
-        // Slash The preconfirer
-        _preconfServiceManager().slashOperator(_meta.proposer);
+        // Verify the transaction is not already included in the block.
+        require(
+            _receipt.position >= _transactionHashes.length
+                || _transactionHashes[_receipt.position] != _receipt.txHash,
+            TxIncluded()
+        );
+
+        // Slash the preconfer
+        IPreconfServiceManager psm =
+            IPreconfServiceManager(resolve(LibNames.B_PRECONF_SERVICE_MANAGER, false));
+        psm.slashOperator(_meta.proposer);
     }
 
+    /// @notice Hashes a receipt into a bytes32 hash to be signed by the preconfer.
+    /// @param _receipt The receipt to hash.
+    /// @return hash_ The hash of the receipt.
     function hashReceipt(Receipt calldata _receipt) public pure returns (bytes32) {
-        return keccak256(abi.encode("TAIKO_PRECONFIRMATION_RECEIPT", _receipt));
+        return keccak256(
+            abi.encode(
+                "TAIKO_PRECONFIRMATION_RECEIPT",
+                _receipt.blockId,
+                _receipt.chainId,
+                _receipt.position,
+                _receipt.isExecutionPreconf,
+                _receipt.txHash
+            )
+        ).toEthSignedMessageHash();
     }
 
-    function hashBlockHeader(BlockHeader calldata _blockHeader) public pure returns (bytes32) {
-        // TODO
-    }
-
-    function _taikoL1() private view returns (ITaikoL1) {
-        return ITaikoL1(resolve(LibNames.B_TAIKO, false));
-    }
-
-    function _lookahead() private view returns (ILookahead) {
-        return ILookahead(resolve(LibNames.B_LOOKAHEAD, false));
-    }
-
-    function _preconfServiceManager() private view returns (IPreconfServiceManager) {
-        return IPreconfServiceManager(resolve(LibNames.B_PRECONF_SERVICE_MANAGER, false));
+    /// @notice Recovers the preconfer's address from a receipt's signature.
+    /// @param _receipt The receipt to recover the preconfer's address from.
+    /// @return preconfer_ The preconfer's address.
+    function recoverPreconfer(Receipt calldata _receipt) public pure returns (address) {
+        return hashReceipt(_receipt).recover(_receipt.signature);
     }
 }
--- packages/protocol/contracts/layer1/preconf/libs/LibMerkle.sol
+++ packages/protocol/contracts/layer1/preconf/libs/LibMerkle.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.24;
 /// @title LibMerkle
 /// @custom:security-contact security@taiko.xyz
 library LibMerkle {
-    function keccakMerkleize(bytes32[] calldata inputs) internal pure returns (bytes32) {
+    /// @dev Returns the trie root of the inputs
+    function merklize(bytes32[] calldata inputs) internal pure returns (bytes32) {
         // TODO
     }
 }
