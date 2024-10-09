8cdf2d39d09cf22bcf75c538f52fd26c02765416
Daniel Wang <dan@taiko.xyz>
2024-10-09 14:32:05 +0800

    Update IPreconfViolationProver.sol

--- packages/protocol/contracts/layer1/preconf/receipt/IPreconfViolationProver.sol
+++ packages/protocol/contracts/layer1/preconf/receipt/IPreconfViolationProver.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.24;
 /// @title IPreconfViolationProver
 /// @custom:security-contact security@taiko.xyz
 interface IPreconfViolationProver {
+    /// @notice Validates the integrity and authenticity of a receipt.
+    /// @param _receipt The serialized receipt data to validate.
+    /// @return Returns true if the receipt is valid, otherwise false.
+    function isReceiptValid(bytes calldata _receipt) external view returns (bool);
+
     /// @notice Validates if a preconfer breached a preconfirmation commitment.
     /// This function must revert if the validation fails, and it should not return a null address.
     /// @param _receipt Serialized receipt suspected to be in breach.
