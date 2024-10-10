1dcc18a78d0b34d711796a05be8c4ffde47da55a
Daniel Wang <dan@taiko.xyz>
2024-10-10 13:53:58 +0800

    fix comments

--- packages/protocol/contracts/layer1/based/TaikoL1.sol
+++ packages/protocol/contracts/layer1/based/TaikoL1.sol
@@ -290,7 +290,7 @@ contract TaikoL1 is EssentialContract, ITaikoL1, TaikoEvents {
          });
     }
 
-    /// @dev chain_pauser is supposed to be a cold wallet.
+    /// @dev chain watchdog is supposed to be a cold wallet.
     function _authorizePause(
         address,
         bool
