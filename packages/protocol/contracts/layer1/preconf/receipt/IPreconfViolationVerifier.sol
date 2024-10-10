911a38c399827e8694995d42a039a3a765b34b27
Daniel Wang <dan@taiko.xyz>
2024-10-10 09:47:09 +0800

    rename

--- packages/protocol/contracts/layer1/preconf/receipt/IPreconfViolationVerifier.sol
+++ packages/protocol/contracts/layer1/preconf/receipt/IPreconfViolationVerifier.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-/// @title IPreconfViolationProver
+/// @title IPreconfViolationVerifier
 /// @custom:security-contact security@taiko.xyz
-interface IPreconfViolationProver {
+interface IPreconfViolationVerifier{
     /// @notice Validates the integrity and authenticity of a receipt.
     /// @param _receipt The serialized receipt data to validate.
     /// @return Returns true if the receipt is valid, otherwise false.
     function isReceiptValid(bytes calldata _receipt) external view returns (bool);
 
-    /// @notice Validates if a preconfer breached a preconfirmation commitment.
+    /// @notice Verifies if a preconfer has violated a preconfirmation commitment.
     /// This function must revert if the validation fails, and it should not return a null address.
+    /// @dev Be aware that this function could modify storage when utilizing proofs such as SGX.
     /// @param _receipt Serialized receipt suspected to be in breach.
     /// @param _proof Proof supporting the violiation.
     /// @return The address of the preconfer responsible for the violiation.
-    function provePreconfViolation(
+    function verifyPreconfViolation(
         bytes calldata _receipt,
         bytes calldata _proof
     )
         external
-        view
         returns (address);
 }
