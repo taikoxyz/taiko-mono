db6a97cce5186eee6d6bdeed5e5d88a3e8d39d74
Daniel Wang <dan@taiko.xyz>
2024-10-11 10:51:16 +0800

    more

--- packages/protocol/contracts/layer1/preconf/iface/IPreconfTaskManager.sol
+++ packages/protocol/contracts/layer1/preconf/iface/IPreconfTaskManager.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "src/layer1/based/TaikoData.sol";
 import "./ILookahead.sol";
 import "./IReceiptProver.sol";
-import "../../based/TaikoData.sol";
 
 /// @title IPreconfTaskManager
 /// @custom:security-contact security@taiko.xyz
