4912932a0444693107db563784f059f5480d099b
Jeffery Walsh <cyberhorsey@gmail.com>
2024-10-15 10:43:20 -0700

    one blobHash

--- packages/blobstorage/pkg/http/get_blob.go
+++ packages/blobstorage/pkg/http/get_blob.go
@@ -3,11 +3,9 @@ package http
 import (
 	"errors"
 	"net/http"
-	"strings"
 
 	"github.com/cyberhorsey/webutils"
 	echo "github.com/labstack/echo/v4"
-	"gorm.io/gorm"
 )
 
 type getBlobResponse struct {
@@ -32,61 +30,34 @@ type blobData struct {
 //	@Success	200	{object}	getBlobResponse
 //	@Router		/getBlob [get]
 func (srv *Server) GetBlob(c echo.Context) error {
-	blobHashes := c.Param("blobHash")
-	if blobHashes == "" {
+	blobHash := c.Param("blobHash")
+	if blobHash == "" {
 		return webutils.LogAndRenderErrors(c, http.StatusBadRequest, errors.New("empty blobHash queryparam"))
 	}
 
-	data, err := srv.getBlobData(strings.Split(blobHashes, ","))
+	data, err := srv.getBlobData(blobHash)
 
 	if err != nil {
 		return webutils.LogAndRenderErrors(c, http.StatusBadRequest, err)
 	}
 
-	response := getBlobResponse{
-		Data: make([]blobData, 0),
-	}
-
-	// Convert data to the correct type
-	for _, d := range data {
-		response.Data = append(response.Data, blobData{
-			BlobHash:   d.BlobHash,
-			Commitment: d.Commitment,
-			Data:       d.Data,
-		},
-		)
-	}
-
-	return c.JSON(http.StatusOK, response)
+	return c.JSON(http.StatusOK, blobData{
+		BlobHash:   data.BlobHash,
+		Commitment: data.Commitment,
+		Data:       data.Data,
+	})
 }
 
 // getBlobData retrieves blob data from MySQL based on blobHashes.
-func (srv *Server) getBlobData(blobHashes []string) ([]blobData, error) {
-	var results []blobData
-
-	for _, blobHash := range blobHashes {
-		var result blobData
-
-		bh, err := srv.blobHashRepo.FirstByBlobHash(blobHash)
-
-		if err != nil {
-			if err == gorm.ErrRecordNotFound {
-				// Handle case where blob hash is not found
-				result.BlobHash = "NOT_FOUND"
-				result.Commitment = "NOT_FOUND"
-				result.Data = "NOT_FOUND"
-			} else {
-				// Return error for other types of errors
-				return nil, err
-			}
-		} else {
-			result.BlobHash = bh.BlobHash
-			result.Commitment = bh.KzgCommitment
-			result.Data = bh.BlobData
-
-			results = append(results, result)
-		}
+func (srv *Server) getBlobData(blobHash string) (*blobData, error) {
+	bh, err := srv.blobHashRepo.FirstByBlobHash(blobHash)
+	if err != nil {
+		return nil, err
 	}
 
-	return results, nil
+	return &blobData{
+		BlobHash:   bh.BlobHash,
+		Commitment: bh.KzgCommitment,
+		Data:       bh.BlobData,
+	}, nil
 }
