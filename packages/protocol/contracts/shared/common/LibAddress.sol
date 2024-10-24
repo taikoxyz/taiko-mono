38077c72c0b28309a76b67e035eda0641256e1d8
Daniel Wang <dan@taiko.xyz>
2024-10-24 11:59:53 +0800

    Update LibAddress.sol

--- packages/protocol/contracts/shared/common/LibAddress.sol
+++ packages/protocol/contracts/shared/common/LibAddress.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 
 /// @title LibAddress
 /// @dev Provides utilities for address-related operations.
