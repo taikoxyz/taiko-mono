1a706b294e28ce59eb279c3960977961a487ecdb
Daniel Wang <dan@taikocha.in>
2024-10-25 18:41:29 +0800

    more

--- packages/protocol/contracts/layer1/based/LibProposing.sol
+++ packages/protocol/contracts/layer1/based/LibProposing.sol
@@ -149,7 +149,9 @@ library LibProposing {
         }
 
         if (local.params.anchorBlockId == 0) {
-            local.params.anchorBlockId = uint64(block.number - 1);
+            unchecked {
+                local.params.anchorBlockId = uint64(block.number - 1);
+            }
         }
 
         if (local.params.timestamp == 0) {
