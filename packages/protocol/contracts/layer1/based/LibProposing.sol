63ac08b83032e6dcac49d6bb77868c9c4c3c3fc4
Daniel Wang <dan@taiko.xyz>
2024-10-27 11:20:21 +0800

    Update LibProposing.sol

--- packages/protocol/contracts/layer1/based/LibProposing.sol
+++ packages/protocol/contracts/layer1/based/LibProposing.sol
@@ -150,7 +150,7 @@ library LibProposing {
 
         if (local.params.anchorBlockId == 0) {
             unchecked {
-              local.params.anchorBlockId = uint64(block.number - 1);
+                local.params.anchorBlockId = uint64(block.number - 1);
             }
         }
 
@@ -186,7 +186,6 @@ library LibProposing {
                 || local.params.timestamp < parentBlk.proposedAt
         ) {
             revert L1_INVALID_TIMESTAMP();
-
         }
 
         // Check if parent block has the right meta hash. This is to allow the proposer to make
