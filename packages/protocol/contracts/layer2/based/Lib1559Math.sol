44286615a0e0b0a17892fe83aad96546a6b1aca1
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-10-29 14:13:25 +0800

    fix(protocol): small fix to 1559 error check (#18339)

--- packages/protocol/contracts/layer2/based/Lib1559Math.sol
+++ packages/protocol/contracts/layer2/based/Lib1559Math.sol
@@ -57,7 +57,7 @@ library Lib1559Math {
 
         uint256 f = FixedPointMathLib.WAD;
         uint256 ratio = f * _newGasTarget / _gasTarget;
-        if (ratio > uint256(type(int256).max)) revert EIP1559_INVALID_PARAMS();
+        if (ratio == 0 || ratio > uint256(type(int256).max)) revert EIP1559_INVALID_PARAMS();
 
         int256 lnRatio = FixedPointMathLib.lnWad(int256(ratio)); // may be negative
 
