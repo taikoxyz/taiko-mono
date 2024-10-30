63455f91d202d88583d70bce69e799032523eb18
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2024-10-30 14:56:43 +0800

    feat(protocol): user smaller cooldown windows (#18345)
    Co-authored-by: dantaik <dantaik@users.noreply.github.com>

--- packages/protocol/contracts/layer1/hekla/HeklaTierRouter.sol
+++ packages/protocol/contracts/layer1/hekla/HeklaTierRouter.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "src/layer1/tiers/ITierRouter.sol";
-import "src/layer1/tiers/TierProviderBase.sol";
+import "../tiers/TierProviderBase.sol";
+import "../tiers/ITierRouter.sol";
 
-/// @title MainnetTierRouter
-/// @dev Labeled in AddressResolver as "tier_router"
+/// @title HeklaTierRouter
 /// @custom:security-contact security@taiko.xyz
-contract MainnetTierRouter is ITierRouter, TierProviderBase {
+contract HeklaTierRouter is TierProviderBase, ITierRouter {
     address public immutable DAO_FALLBACK_PROPOSER;
 
     constructor(address _daoFallbackProposer) {
@@ -22,18 +21,23 @@ contract MainnetTierRouter is ITierRouter, TierProviderBase {
 
     /// @inheritdoc ITierProvider
     function getTierIds() external pure returns (uint16[] memory tiers_) {
-        tiers_ = new uint16[](4);
-        tiers_[0] = LibTiers.TIER_SGX;
-        tiers_[1] = LibTiers.TIER_ZKVM_ANY;
-        tiers_[2] = LibTiers.TIER_GUARDIAN_MINORITY;
-        tiers_[3] = LibTiers.TIER_GUARDIAN;
+        tiers_ = new uint16[](6);
+        tiers_[0] = LibTiers.TIER_OPTIMISTIC;
+        tiers_[1] = LibTiers.TIER_SGX;
+        tiers_[2] = LibTiers.TIER_ZKVM_RISC0;
+        tiers_[3] = LibTiers.TIER_ZKVM_SP1;
+        tiers_[4] = LibTiers.TIER_GUARDIAN_MINORITY;
+        tiers_[5] = LibTiers.TIER_GUARDIAN;
     }
 
     /// @inheritdoc ITierProvider
     function getMinTier(address _proposer, uint256 _rand) public view override returns (uint16) {
         if (_proposer == DAO_FALLBACK_PROPOSER) {
-            return _rand % 500 == 0 ? LibTiers.TIER_ZKVM_ANY : LibTiers.TIER_SGX;
+            if (_rand % 1000 == 0) return LibTiers.TIER_ZKVM_RISC0;
+            else if (_rand % 1000 == 1) return LibTiers.TIER_ZKVM_SP1;
+            else return LibTiers.TIER_SGX;
         }
-        return LibTiers.TIER_SGX;
+
+        return _rand % 2 == 0 ? LibTiers.TIER_SGX : LibTiers.TIER_OPTIMISTIC;
     }
 }
