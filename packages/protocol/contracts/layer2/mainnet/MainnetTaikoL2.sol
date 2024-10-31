f4bb20be42152f7b1d1ef44d8c6682f4110d1aa1
Daniel Wang <dan@taiko.xyz>
2024-10-31 11:19:31 +0800

    fix

--- packages/protocol/contracts/layer2/mainnet/MainnetTaikoL2.sol
+++ packages/protocol/contracts/layer2/mainnet/MainnetTaikoL2.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "../based/TaikoL2.sol";
+import "../based/TaikoL2V2.sol";
 
 /// @title MainnetTaikoL2
 /// @custom:security-contact security@taiko.xyz
-contract MainnetTaikoL2 is TaikoL2 {
+contract MainnetTaikoL2 is TaikoL2V2 {
     function ontakeForkHeight() public pure override returns (uint64) {
         return 374_400; // = 7200 * 52
     }
