51faacd65d85180d7d3c0f5506d916edd76b0e9e
Daniel Wang <dan@taiko.xyz>
2024-11-02 09:46:15 +0800

    Update MainnetTierRouter.sol

--- packages/protocol/contracts/layer1/mainnet/rollup/MainnetTierRouter.sol
+++ packages/protocol/contracts/layer1/mainnet/rollup/MainnetTierRouter.sol
@@ -29,6 +29,7 @@ contract MainnetTierRouter is ITierRouter, TierProviderBase {
     }
 
     /// @inheritdoc ITierProvider
+    // solhint-disable-next-line no-unused-vars
     function getMinTier(address _proposer, uint256 _rand) public view override returns (uint16) {
         return LibTiers.TIER_SGX;
     }
