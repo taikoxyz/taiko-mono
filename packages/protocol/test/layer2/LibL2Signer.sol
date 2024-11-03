4a0581e727954ff648f613fb68f732457dce9c4f
Daniel Wang <dan@taiko.xyz>
2024-11-03 14:25:18 +0800

    Update LibL2Signer.sol

--- packages/protocol/test/layer2/LibL2Signer.sol
+++ packages/protocol/test/layer2/LibL2Signer.sol
@@ -53,7 +53,7 @@ library LibL2Signer {
         view
         returns (uint8 v, uint256 r, uint256 s)
     {
-        require(k == 1 || k == 2, L2_INVALID_GOLDEN_TOUCH_K());
+        if (k != 1 && k != 2) revert L2_INVALID_GOLDEN_TOUCH_K();
 
         r = k == 1 ? GX : GX2;
 
