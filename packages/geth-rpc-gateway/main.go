1c955e5bb8e1bcd378d1153f09b17fdde04fdfad
xiaodino <ruby@taiko.xyz>
2024-11-04 11:07:01 -0800

    Update

--- packages/geth-rpc-gateway/main.go
+++ packages/geth-rpc-gateway/main.go
@@ -75,13 +75,13 @@ func enableCORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", r.Method)
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == "OPTIONS" {
-			log.Printf("Preflight request for %s %s", r.Method, r.URL.Path)
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+		// if r.Method == "OPTIONS" {
+		log.Printf("Preflight request for %s %s", r.Method, r.URL.Path)
+		w.WriteHeader(http.StatusOK)
 
-		next.ServeHTTP(w, r)
+		// }
+
+		// next.ServeHTTP(w, r)
 	})
 }
 
