563c64e4f8611f81ffc1838d586e56d1f8feb666
Daniel Wang <dan@taiko.xyz>
2024-11-05 13:40:36 +0800

    Update TaikoL2Deprecated.sol

--- packages/protocol/contracts/layer2/based/TaikoL2Deprecated.sol
+++ packages/protocol/contracts/layer2/based/TaikoL2Deprecated.sol
@@ -1,24 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-import "src/shared/data/LibSharedData.sol";
-import "src/shared/common/EssentialContract.sol";
-import "src/shared/common/LibStrings.sol";
-import "src/shared/common/LibAddress.sol";
-import "src/shared/common/LibMath.sol";
-import "src/shared/signal/ISignalService.sol";
-import "./LibEIP1559.sol";
-import "./LibL2Config.sol";
-import "./IBlockHash.sol";
-
 /// @title TaikoL2Deprecated
 /// @notice This contract includes deprecated functions whose ABI are still used by client for old
 /// blocks.
 /// @custom:security-contact security@taiko.xyz
 abstract contract TaikoL2Deprecated {
+    error L2_DEPRECATED_METHOD();
+
     function anchor(
         bytes32, /*_l1BlockHash*/
         bytes32, /*_l1StateRoot*/
@@ -26,8 +15,9 @@ abstract contract TaikoL2Deprecated {
         uint32 /*_parentGasUsed */
     )
         external
-        notImplemented
-    { }
+    {
+        revert L2_DEPRECATED_METHOD();
+    }
 
     function getBasefee(
         uint64 _anchorBlockId,
@@ -35,7 +25,6 @@ abstract contract TaikoL2Deprecated {
     )
         public
         view
-        notImplemented
         returns (uint256, /*basefee_*/ uint64 /*parentGasExcess_*/ )
     { }
 }
