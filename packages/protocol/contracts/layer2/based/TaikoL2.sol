82b95cf596b58f533f2ae983276164788948a5c3
Daniel Wang <dan@taiko.xyz>
2024-11-07 13:45:20 +0800

    more

--- packages/protocol/contracts/layer2/based/TaikoL2.sol
+++ packages/protocol/contracts/layer2/based/TaikoL2.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "src/shared/data/LibSharedData.sol";
+import "src/shared/based/LibSharedData.sol";
 import "src/shared/common/EssentialContract.sol";
 import "src/shared/libs/LibStrings.sol";
 import "src/shared/libs/LibAddress.sol";
