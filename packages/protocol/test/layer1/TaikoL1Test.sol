7259a76c0640a23ea5e84ec2a2ee453c2d90ab21
Daniel Wang <dan@taiko.xyz>
2024-11-09 12:59:40 +0800

    more

--- packages/protocol/test/layer1/TaikoL1Test.sol
+++ packages/protocol/test/layer1/TaikoL1Test.sol
@@ -7,9 +7,40 @@ import "src/layer1/verifiers/SgxVerifier.sol";
 import "src/layer1/verifiers/SP1Verifier.sol";
 import "src/layer1/verifiers/Risc0Verifier.sol";
 import "src/layer1/provers/GuardianProver.sol";
+import "src/layer1/tiers/LibTiers.sol";
 import "src/layer1/team/airdrop/ERC20Airdrop.sol";
 import "src/shared/bridge/QuotaManager.sol";
-// import "../layer1/based/TestTierRouter.sol";
-import "../shared/TaikoTest.sol";
+import "test/shared/TaikoTest.sol";
 
-abstract contract TaikoL1Test is TaikoTest { }
+abstract contract TaikoL1Test is TaikoTest {
+
+
+    function deployTaikoL1(address impl) internal returns (TaikoL1) {
+     return   TaikoL1(
+            deploy({
+                name: "taiko",
+                impl: impl,
+                data: ""
+            })
+        );
+    }
+
+    function deployGuardianProver() internal returns (GuardianProver) {
+     return   GuardianProver(
+            deploy({
+                name: "guardian_prover",
+                impl: address(new GuardianProver()),
+                data: abi.encodeCall(GuardianProver.init, (address(0), address(resolver)))
+            })
+        );
+    }
+
+    function deploySgxVerifier() internal returns (SgxVerifier) {
+        return SgxVerifier(
+            deploy({
+                name: "tier_sgx",
+                impl: address(new SgxVerifier()),
+                data: abi.encodeCall(SgxVerifier.init, (address(0), address(resolver)))
+            })
+        );
+    } }
--- packages/protocol/test/layer1/based/GuardianProver1.t.sol
+++ packages/protocol/test/layer1/based/GuardianProver1.t.sol
@@ -10,13 +10,13 @@ contract DummyGuardianProver is GuardianProver {
         __Essential_init(address(0));
     }
 
-    function approve(bytes32 hash) public returns (bool) {
+    function approve(bytes32 hash) external returns (bool) {
         return _saveApproval(operationId++, hash);
     }
 }
 
-contract TestGuardianProver1 is TaikoL1Test {
-    DummyGuardianProver target;
+contract TestGuardianProver is TaikoL1Test {
+    DummyGuardianProver guardianProver;
 
     function getSigners(uint256 numGuardians) internal returns (address[] memory signers) {
         signers = new address[](numGuardians);
@@ -27,7 +27,7 @@ contract TestGuardianProver1 is TaikoL1Test {
     }
 
     function setUpOnEthereum() internal override {
-        target = DummyGuardianProver(
+        guardianProver = DummyGuardianProver(
             deploy({
                 name: "guardians",
                 impl: address(new DummyGuardianProver()),
@@ -38,49 +38,108 @@ contract TestGuardianProver1 is TaikoL1Test {
 
     function test_guardian_prover_set_guardians() public transactBy(deployer) {
         vm.expectRevert(GuardianProver.GP_INVALID_GUARDIAN_SET.selector);
-        target.setGuardians(getSigners(0), 0, true);
+        guardianProver.setGuardians(getSigners(0), 0, true);
 
         vm.expectRevert(GuardianProver.GP_INVALID_MIN_GUARDIANS.selector);
-        target.setGuardians(getSigners(5), 0, true);
+        guardianProver.setGuardians(getSigners(5), 0, true);
 
         vm.expectRevert(GuardianProver.GP_INVALID_MIN_GUARDIANS.selector);
-        target.setGuardians(getSigners(5), 6, true);
+        guardianProver.setGuardians(getSigners(5), 6, true);
     }
 
     function test_guardian_prover_set_guardians2() public transactBy(deployer) {
         address[] memory signers = getSigners(5);
         signers[0] = address(0);
         vm.expectRevert(GuardianProver.GP_INVALID_GUARDIAN.selector);
-        target.setGuardians(signers, 4, true);
+        guardianProver.setGuardians(signers, 4, true);
 
         signers[0] = signers[1];
         vm.expectRevert(GuardianProver.GP_INVALID_GUARDIAN_SET.selector);
-        target.setGuardians(signers, 4, true);
+        guardianProver.setGuardians(signers, 4, true);
     }
 
     function test_guardian_prover_approve() public {
         address[] memory signers = getSigners(6);
         vm.prank(deployer);
-        target.setGuardians(signers, 4, true);
+        guardianProver.setGuardians(signers, 4, true);
 
         bytes32 hash = keccak256("paris");
         for (uint256 i; i < 6; ++i) {
             vm.prank(signers[0]);
-            assertEq(target.approve(hash), false);
+            assertEq(guardianProver.approve(hash), false);
         }
 
         hash = keccak256("singapore");
         for (uint256 i; i < 6; ++i) {
             vm.startPrank(signers[i]);
-            target.approve(hash);
+            guardianProver.approve(hash);
 
-            assertEq(target.approve(hash), i >= 3);
+            assertEq(guardianProver.approve(hash), i >= 3);
             vm.stopPrank();
         }
 
         // changing the settings will invalid all approval history
         vm.prank(deployer);
-        target.setGuardians(signers, 3, true);
-        assertEq(target.version(), 2);
+        guardianProver.setGuardians(signers, 3, true);
+        assertEq(guardianProver.version(), 2);
+    }
+
+        // Tests `verifyProof()` with the correct prover
+    function test_guardian_prover_verifyProof() public view {
+        // Context
+        IVerifier.Context memory ctx = IVerifier.Context({
+            metaHash: bytes32(0),
+            blobHash: bytes32(0),
+            prover: address(guardianProver),
+            msgSender: address(guardianProver),
+            blockId: 10,
+            isContesting: false,
+            blobUsed: false
+        });
+
+        // Transition
+        TaikoData.Transition memory transition = TaikoData.Transition({
+            parentHash: bytes32(0),
+            blockHash: bytes32(0),
+            stateRoot: bytes32(0),
+            graffiti: bytes32(0)
+        });
+
+        // TierProof
+        TaikoData.TierProof memory proof =
+            TaikoData.TierProof({ tier: LibTiers.TIER_GUARDIAN, data: "" });
+
+        // `verifyProof()`
+        guardianProver.verifyProof(ctx, transition, proof);
+    }
+
+       // Tests `verifyProof()` with the wrong prover
+    function test_guardian_prover_verifyProof_invalidProver() public {
+        // Context
+        IVerifier.Context memory ctx = IVerifier.Context({
+            metaHash: bytes32(0),
+            blobHash: bytes32(0),
+            prover: Alice, // invalid
+            msgSender: Alice,
+            blockId: 10,
+            isContesting: false,
+            blobUsed: false
+        });
+
+        // Transition
+        TaikoData.Transition memory transition = TaikoData.Transition({
+            parentHash: bytes32(0),
+            blockHash: bytes32(0),
+            stateRoot: bytes32(0),
+            graffiti: bytes32(0)
+        });
+
+        // TierProof
+        TaikoData.TierProof memory proof =
+            TaikoData.TierProof({ tier: LibTiers.TIER_GUARDIAN, data: "" });
+
+        // `verifyProof()` with invalid ctx.prover
+        vm.expectRevert(GuardianProver.GV_PERMISSION_DENIED.selector);
+        guardianProver.verifyProof(ctx, transition, proof);
     }
 }
