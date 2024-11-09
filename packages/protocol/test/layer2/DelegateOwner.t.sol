9192949e1b46f73688b45e7c4540f1b398083ad4
Daniel Wang <dan@taiko.xyz>
2024-11-09 10:47:51 +0800

    Update DelegateOwner.t.sol

--- packages/protocol/test/layer2/DelegateOwner.t.sol
+++ packages/protocol/test/layer2/DelegateOwner.t.sol
@@ -11,151 +11,152 @@ contract Target is EssentialContract {
 }
 
 contract TestDelegateOwner is TaikoL2Test {
-    address public srcBridge = randAddress();
+    // Contracts on Ethereum
+    address public eBridge = randAddress();
 
-    Multicall3 public multicall;
-    SignalService public signalService;
-    Bridge public bridge;
-    DelegateOwner public delegateOwner;
+    // Contracts on Taiko
+    Multicall3 public tMulticall;
+    SignalService public tSignalService;
+    Bridge public tBridge;
+    DelegateOwner public tDelegateOwner;
 
     function setUpOnEthereum() internal override {
-        // srcBridge = randAddress();
-        register("bridge", srcBridge);
+        register("bridge", eBridge);
     }
 
     function setUpOnTaiko() internal override {
-        multicall = new Multicall3();
-        delegateOwner = deployDelegateOwner(srcBridge, ethereumChainId);
-        signalService = deploySignalService(address(new SignalServiceNoProofCheck()));
-        bridge = deployBridge(address(new Bridge()));
+        tMulticall = new Multicall3();
+        tDelegateOwner = deployDelegateOwner(eBridge, ethereumChainId);
+        tSignalService = deploySignalService(address(new SignalServiceNoProofCheck()));
+        tBridge = deployBridge(address(new Bridge()));
     }
 
     function test_delegate_owner_single_non_delegatecall() public onTaiko {
         vm.startPrank(deployer);
-        Target target1 = _deployTarget("target1", address(new Target()));
+        Target tkoTarget1 = _deployTarget("tkoTarget1", address(new Target()));
         vm.stopPrank();
 
         bytes memory data = abi.encode(
             DelegateOwner.Call(
                 uint64(0),
-                address(target1),
+                address(tkoTarget1),
                 false, // CALL
                 abi.encodeCall(EssentialContract.pause, ())
             )
         );
 
         vm.expectRevert(DelegateOwner.DO_DRYRUN_SUCCEEDED.selector);
-        delegateOwner.dryrunInvocation(data);
+        tDelegateOwner.dryrunInvocation(data);
 
         IBridge.Message memory message;
-        message.from = srcBridge;
+        message.from = eBridge;
         message.destChainId = taikoChainId;
         message.srcChainId = ethereumChainId;
         message.destOwner = Bob;
         message.data = abi.encodeCall(DelegateOwner.onMessageInvocation, (data));
-        message.to = address(delegateOwner);
+        message.to = address(tDelegateOwner);
 
         vm.prank(Bob);
-        bridge.processMessage(message, "");
+        tBridge.processMessage(message, "");
 
-        bytes32 hash = bridge.hashMessage(message);
-        assertTrue(bridge.messageStatus(hash) == IBridge.Status.DONE);
+        bytes32 hash = tBridge.hashMessage(message);
+        assertTrue(tBridge.messageStatus(hash) == IBridge.Status.DONE);
 
-        assertEq(delegateOwner.nextTxId(), 1);
-        assertTrue(target1.paused());
+        assertEq(tDelegateOwner.nextTxId(), 1);
+        assertTrue(tkoTarget1.paused());
     }
 
     function test_delegate_owner_single_non_delegatecall_self() public onTaiko {
-        address delegateOwnerImpl2 = address(new DelegateOwner());
+        address tDelegateOwnerImpl2 = address(new DelegateOwner());
 
         bytes memory data = abi.encode(
             DelegateOwner.Call(
                 uint64(0),
-                address(delegateOwner),
+                address(tDelegateOwner),
                 false, // CALL
-                abi.encodeCall(UUPSUpgradeable.upgradeTo, (delegateOwnerImpl2))
+                abi.encodeCall(UUPSUpgradeable.upgradeTo, (tDelegateOwnerImpl2))
             )
         );
 
         vm.expectRevert(DelegateOwner.DO_DRYRUN_SUCCEEDED.selector);
-        delegateOwner.dryrunInvocation(data);
+        tDelegateOwner.dryrunInvocation(data);
 
         IBridge.Message memory message;
-        message.from = srcBridge;
+        message.from = eBridge;
         message.destChainId = taikoChainId;
         message.srcChainId = ethereumChainId;
         message.destOwner = Bob;
         message.data = abi.encodeCall(DelegateOwner.onMessageInvocation, (data));
-        message.to = address(delegateOwner);
+        message.to = address(tDelegateOwner);
 
         vm.prank(Bob);
-        bridge.processMessage(message, "");
+        tBridge.processMessage(message, "");
 
-        bytes32 hash = bridge.hashMessage(message);
-        assertTrue(bridge.messageStatus(hash) == IBridge.Status.DONE);
+        bytes32 hash = tBridge.hashMessage(message);
+        assertTrue(tBridge.messageStatus(hash) == IBridge.Status.DONE);
 
-        assertEq(delegateOwner.nextTxId(), 1);
-        assertEq(delegateOwner.impl(), delegateOwnerImpl2);
+        assertEq(tDelegateOwner.nextTxId(), 1);
+        assertEq(tDelegateOwner.impl(), tDelegateOwnerImpl2);
     }
 
-    function test_delegate_owner_delegate_multicall() public onTaiko {
-        address delegateOwnerImpl2 = address(new DelegateOwner());
+    function test_delegate_owner_delegate_tMulticall() public onTaiko {
+        address tDelegateOwnerImpl2 = address(new DelegateOwner());
         address impl1 = address(new Target());
         address impl2 = address(new Target());
 
         vm.startPrank(deployer);
-        Target target1 = _deployTarget("target1", impl1);
-        Target target2 = _deployTarget("target2", impl2);
+        Target tkoTarget1 = _deployTarget("tkoTarget1", impl1);
+        Target tTarget2 = _deployTarget("tTarget2", impl2);
         vm.stopPrank();
 
         Multicall3.Call3[] memory calls = new Multicall3.Call3[](4);
-        calls[0].target = address(target1);
+        calls[0].target = address(tkoTarget1);
         calls[0].allowFailure = false;
         calls[0].callData = abi.encodeCall(EssentialContract.pause, ());
 
-        calls[1].target = address(target2);
+        calls[1].target = address(tTarget2);
         calls[1].allowFailure = false;
         calls[1].callData = abi.encodeCall(UUPSUpgradeable.upgradeTo, (impl2));
 
-        calls[2].target = address(delegateOwner);
+        calls[2].target = address(tDelegateOwner);
         calls[2].allowFailure = false;
-        calls[2].callData = abi.encodeCall(UUPSUpgradeable.upgradeTo, (delegateOwnerImpl2));
+        calls[2].callData = abi.encodeCall(UUPSUpgradeable.upgradeTo, (tDelegateOwnerImpl2));
 
-        calls[3].target = address(delegateOwner);
+        calls[3].target = address(tDelegateOwner);
         calls[3].allowFailure = false;
         calls[3].callData = abi.encodeCall(DelegateOwner.setAdmin, (David));
 
         bytes memory data = abi.encode(
             DelegateOwner.Call(
                 uint64(0),
-                address(multicall),
+                address(tMulticall),
                 true, // DELEGATECALL
                 abi.encodeCall(Multicall3.aggregate3, (calls))
             )
         );
 
         vm.expectRevert(DelegateOwner.DO_DRYRUN_SUCCEEDED.selector);
-        delegateOwner.dryrunInvocation(data);
+        tDelegateOwner.dryrunInvocation(data);
 
         IBridge.Message memory message;
-        message.from = srcBridge;
+        message.from = eBridge;
         message.destChainId = taikoChainId;
         message.srcChainId = ethereumChainId;
         message.destOwner = Bob;
         message.data = abi.encodeCall(DelegateOwner.onMessageInvocation, (data));
-        message.to = address(delegateOwner);
+        message.to = address(tDelegateOwner);
 
         vm.prank(Bob);
-        bridge.processMessage(message, "");
+        tBridge.processMessage(message, "");
 
-        bytes32 hash = bridge.hashMessage(message);
-        assertTrue(bridge.messageStatus(hash) == IBridge.Status.DONE);
+        bytes32 hash = tBridge.hashMessage(message);
+        assertTrue(tBridge.messageStatus(hash) == IBridge.Status.DONE);
 
-        assertEq(delegateOwner.nextTxId(), 1);
-        assertTrue(target1.paused());
-        assertEq(target2.impl(), impl2);
-        assertEq(delegateOwner.impl(), delegateOwnerImpl2);
-        assertEq(delegateOwner.admin(), David);
+        assertEq(tDelegateOwner.nextTxId(), 1);
+        assertTrue(tkoTarget1.paused());
+        assertEq(tTarget2.impl(), impl2);
+        assertEq(tDelegateOwner.impl(), tDelegateOwnerImpl2);
+        assertEq(tDelegateOwner.admin(), David);
     }
 
     function _deployTarget(bytes32 name, address impl) private returns (Target) {
@@ -163,7 +164,7 @@ contract TestDelegateOwner is TaikoL2Test {
             deploy({
                 name: name,
                 impl: impl,
-                data: abi.encodeCall(Target.init, (address(delegateOwner)))
+                data: abi.encodeCall(Target.init, (address(tDelegateOwner)))
             })
         );
     }
