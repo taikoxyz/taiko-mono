d16478111397ff496874ea8ba00c5362b9674465
Daniel Wang <dan@taiko.xyz>
2024-11-22 14:03:23 +0800

    more

--- packages/protocol/contracts/layer1/based/TaikoInbox.sol
+++ packages/protocol/contracts/layer1/based/TaikoInbox.sol
@@ -10,16 +10,9 @@ import "./LibVerifying.sol";
 import "./TaikoEvents.sol";
 import "./ITaikoL1.sol";
 
-/// @title TaikoL1
-/// @notice This contract serves as the "base layer contract" of the Taiko protocol, providing
-/// functionalities for proposing, proving, and verifying blocks. The term "base layer contract"
-/// means that although this is usually deployed on L1, it can also be deployed on L2s to create
-/// L3s. The contract also handles the deposit and withdrawal of Taiko tokens and Ether.
-/// Additionally, this contract doesn't hold any Ether. Ether deposited to L2 are held by the Bridge
-/// contract.
-/// @dev Labeled in AddressResolver as "taiko"
+/// @title TaikoInbox
 /// @custom:security-contact security@taiko.xyz
-contract TaikoL1 is EssentialContract {
+contract TaikoInbox is EssentialContract {
 	/// @notice The TaikoL1 state.
     TaikoData.State public state;
 }
