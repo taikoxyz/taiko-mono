516a7f0136729449c5d7509e5725f71718738347
Daniel Wang <dan@taiko.xyz>
2024-11-24 12:48:40 +0800

    Update TaikoData.sol

--- packages/protocol/contracts/layer1/based/TaikoData.sol
+++ packages/protocol/contracts/layer1/based/TaikoData.sol
@@ -38,23 +38,6 @@ library TaikoData {
         bytes data;
     }
 
-    /// @notice DEPRECATED but used by node/client for syncing old blocks
-    /// @notice Hook and its data (currently used only during proposeBlock)
-    struct HookCall {
-        address hook;
-        bytes data;
-    }
-
-    /// @notice DEPRECATED but used by node/client for syncing old blocks
-    /// @notice Represents proposeBlock's _data input parameter
-    struct BlockParams {
-        address assignedProver; // DEPRECATED, value ignored.
-        address coinbase;
-        bytes32 extraData;
-        bytes32 parentMetaHash;
-        HookCall[] hookCalls; // DEPRECATED, value ignored.
-        bytes signature; // DEPRECATED, value ignored.
-    }
 
     struct BlockParamsV2 {
         address proposer;
@@ -67,26 +50,7 @@ library TaikoData {
         uint8 blobIndex; // NEW
     }
 
-    /// @notice DEPRECATED but used by node/client for syncing old blocks
-    /// @notice Struct containing data only required for proving a block
-    /// @notice Note: On L2, `block.difficulty` is the pseudo name of `block.prevrandao`, which
-    /// returns a random number provided by the layer 1 chain.
-    struct BlockMetadata {
-        bytes32 l1Hash;
-        bytes32 difficulty;
-        bytes32 blobHash; //or txListHash (if Blob not yet supported)
-        bytes32 extraData;
-        bytes32 depositsHash;
-        address coinbase; // L2 coinbase,
-        uint64 id;
-        uint32 gasLimit;
-        uint64 timestamp;
-        uint64 l1Height;
-        uint16 minTier;
-        bool blobUsed;
-        bytes32 parentMetaHash;
-        address sender; // a.k.a proposer
-    }
+ 
 
     struct BlockMetadataV2 {
         bytes32 anchorBlockHash; // `_l1BlockHash` in TaikoL2's anchor tx.
@@ -132,21 +96,6 @@ library TaikoData {
         uint8 __reserved1;
     }
 
-    /// @notice DEPRECATED but used by node/client for syncing old blocks
-    /// @notice Struct containing data required for verifying a block.
-    /// @notice 3 slots used.
-    struct Block {
-        bytes32 metaHash; // slot 1
-        address assignedProver;
-        uint96 livenessBond;
-        uint64 blockId; // slot 3
-        uint64 proposedAt;
-        uint64 proposedIn;
-        uint32 nextTransitionId;
-        // The ID of the transaction that is used to verify this block. However, if this block is
-        // not verified as the last block in a batch, verifiedTransitionId will remain zero.
-        uint32 verifiedTransitionId;
-    }
 
     /// @notice Struct containing data required for verifying a block.
     /// @notice 3 slots used.
@@ -162,16 +111,6 @@ library TaikoData {
         // The ID of the transaction that is used to verify this block. However, if this block is
         // not verified as the last block in a batch, verifiedTransitionId will remain zero.
         uint24 verifiedTransitionId;
-    }
-
-    /// @notice DEPRECATED but used by node/client for syncing old blocks
-    /// @notice Struct representing an Ethereum deposit.
-    /// @notice 2 slot used. Currently removed from protocol, but to be backwards compatible, the
-    /// struct and return values stayed for now.
-    struct EthDeposit {
-        address recipient;
-        uint96 amount;
-        uint64 id;
     }
 
     /// @notice Forge is only able to run coverage in case the contracts by default capable of
