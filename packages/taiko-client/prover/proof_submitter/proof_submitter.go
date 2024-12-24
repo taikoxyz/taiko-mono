0b1836c253f28a2899c62ef7d4162fc0865204a4
gavin <gavin@taiko.xyz>
2024-12-24 16:44:16 +0800

    rename

--- packages/taiko-client/prover/proof_submitter/proof_submitter.go
+++ packages/taiko-client/prover/proof_submitter/proof_submitter.go
@@ -479,7 +479,7 @@ func (s *ProofSubmitter) AggregateProofs(ctx context.Context) error {
 			if err != nil {
 				if errors.Is(err, proofProducer.ErrProofInProgress) ||
 					errors.Is(err, proofProducer.ErrRetry) {
-					log.Info("Aggregating", "err", err)
+					log.Info("Aggregating", "status", err)
 				} else {
 					log.Error("Failed to request proof aggregation", "err", err)
 				}
