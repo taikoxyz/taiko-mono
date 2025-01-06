cd09adc201b9039f4fdbe81a52a17f4d3a93a8e3
Daniel Wang <dan@taiko.xyz>
2025-01-06 15:25:02 +0800

    Update LibTrieProof.sol

--- packages/protocol/contracts/shared/libs/LibTrieProof.sol
+++ packages/protocol/contracts/shared/libs/LibTrieProof.sol
@@ -1,9 +1,4 @@
 // SPDX-License-Identifier: MIT
-//  _____     _ _         _         _
-// |_   _|_ _(_) |_____  | |   __ _| |__ ___
-//   | |/ _` | | / / _ \ | |__/ _` | '_ (_-<
-//   |_|\__,_|_|_\_\___/ |____\__,_|_.__/__/
-
 pragma solidity ^0.8.24;
 
 import "@optimism/packages/contracts-bedrock/src/libraries/rlp/RLPReader.sol";
