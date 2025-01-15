82f45da437174f113104349fd14dbe8fb0877c8b
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2025-01-15 15:18:35 +0800

    chore(protocol): change maxBlocksPerBatch to 768 to support 0.5s average block time (#18768)

--- packages/protocol/test/layer1/based/InboxTest_BondMechanics.t.sol
+++ packages/protocol/test/layer1/based/InboxTest_BondMechanics.t.sol
@@ -26,7 +26,7 @@ contract InboxTest_BondMechanics is InboxTestBase {
              }),
             provingWindow: 1 hours,
             maxSignalsToReceive: 16,
-            maxBlocksPerBatch: 256,
+            maxBlocksPerBatch: 768,
             forkHeights: ITaikoInbox.ForkHeights({ ontake: 0, pacaya: 0 })
         });
     }
--- packages/protocol/test/layer1/based/InboxTest_Params.t.sol
+++ packages/protocol/test/layer1/based/InboxTest_Params.t.sol
@@ -23,7 +23,7 @@ contract InboxTest_Params is InboxTestBase {
              }),
             provingWindow: 1 hours,
             maxSignalsToReceive: 16,
-            maxBlocksPerBatch: 256,
+            maxBlocksPerBatch: 768,
             forkHeights: ITaikoInbox.ForkHeights({ ontake: 0, pacaya: 0 })
         });
     }
