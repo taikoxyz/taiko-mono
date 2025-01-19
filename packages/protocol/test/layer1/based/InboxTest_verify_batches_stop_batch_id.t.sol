241608ae8077bc573fa6b8cfb228dd3bde684e77
maskpp <maskpp266@gmail.com>
2025-01-19 15:00:20 +0800

    Add test_inbox_propose_stopBatchId test case to verify stopBatchId

---
+++ packages/protocol/test/layer1/based/InboxTest_verify_batches_stop_batch_id.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "./InboxTestBase.sol";
+
+contract InboxTest_ProposeAndProve is InboxTestBase {
+    function getConfig() internal pure override returns (ITaikoInbox.Config memory) {
+        return ITaikoInbox.Config({
+            chainId: LibNetwork.TAIKO_MAINNET,
+            maxBatchProposals: 10,
+            batchRingBufferSize: 15,
+            maxBatchesToVerify: 1,
+            blockMaxGasLimit: 240_000_000,
+            livenessBondBase: 125e18, // 125 Taiko token per batch
+            livenessBondPerBlock: 5e18, // 5 Taiko token per block
+            stateRootSyncInternal: 5,
+            maxAnchorHeightOffset: 64,
+            baseFeeConfig: LibSharedData.BaseFeeConfig({
+            adjustmentQuotient: 8,
+            sharingPctg: 75,
+            gasIssuancePerSecond: 5_000_000,
+            minGasExcess: 1_340_000_000, // correspond to 0.008847185 gwei basefee
+            maxGasIssuancePerBlock: 600_000_000 // two minutes: 5_000_000 * 120
+        }),
+            provingWindow: 1 hours,
+            maxSignalsToReceive: 16,
+            maxBlocksPerBatch: 768,
+            forkHeights: ITaikoInbox.ForkHeights({ ontake: 0, pacaya: 0 })
+        });
+    }
+
+    function setUpOnEthereum() internal override {
+        super.setUpOnEthereum();
+        bondToken = deployBondToken();
+    }
+
+    function test_inbox_propose_stopBatchId(
+    )
+    external
+    transactBy(Alice)
+    WhenMultipleBatchesAreProposedWithDefaultParameters(9)
+    WhenMultipleBatchesAreProvedWithCorrectTransitions(1, 10)
+    WhenLogAllBatchesAndTransitions
+    {
+        ITaikoInbox.Stats2 memory _stats2 = inbox.getStats2();
+        assertEq(getConfig().maxBatchesToVerify * 9, _stats2.lastVerifiedBatchId);
+    }
+}
