ef3da24f17bbb75559a5fa31bfee6928ab8ebe69
Anshu Jalan <anshujalan206@gmail.com>
2025-01-21 11:49:20 +0530

    feat(protocol): add preconfirmation whitelist and router (#18791)
    Co-authored-by: Daniel Wang <99078276+dantaik@users.noreply.github.com>

---
+++ packages/protocol/contracts/layer1/preconf/impl/PreconfRouter.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../iface/IPreconfRouter.sol";
+import "../iface/IPreconfWhitelist.sol";
+import "src/layer1/based/ITaikoInbox.sol";
+import "src/shared/libs/LibStrings.sol";
+import "src/shared/common/EssentialContract.sol";
+
+/// @title PreconfRouter
+/// @custom:security-contact security@taiko.xyz
+contract PreconfRouter is EssentialContract, IPreconfRouter {
+    uint256[50] private __gap;
+
+    function init(address _owner, address _sharedResolver) external initializer {
+        __Essential_init(_owner, _sharedResolver);
+    }
+
+    /// @inheritdoc IPreconfRouter
+    function proposePreconfedBlocks(
+        bytes calldata,
+        bytes calldata _batchParams,
+        bytes calldata _batchTxList
+    )
+        external
+        returns (ITaikoInbox.BatchMetadata memory meta_)
+    {
+        // Sender must be the selected operator for the epoch
+        address selectedOperator =
+            IPreconfWhitelist(resolve(LibStrings.B_PRECONF_WHITELIST, false)).getOperatorForEpoch();
+        require(msg.sender == selectedOperator, NotTheOperator());
+
+        // Call the proposeBatch function on the TaikoInbox
+        address taikoInbox = resolve(LibStrings.B_TAIKO, false);
+        meta_ = ITaikoInbox(taikoInbox).proposeBatch(_batchParams, _batchTxList);
+
+        // Verify that the sender had set itself as the proposer
+        require(meta_.proposer == msg.sender, ProposerIsNotTheSender());
+    }
+}
---
+++ packages/protocol/contracts/layer1/preconf/libs/LibPreconfConstants.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "src/shared/libs/LibNetwork.sol";
+
+/// @title LibPreconfConstants
+/// @custom:security-contact security@taiko.xyz
+library LibPreconfConstants {
+    uint256 internal constant ETHEREUM_MAINNET_BEACON_GENESIS = 1_606_824_023;
+    uint256 internal constant ETHEREUM_HOLESKY_BEACON_GENESIS = 1_695_902_100;
+
+    uint256 internal constant SECONDS_IN_SLOT = 12;
+    uint256 internal constant SECONDS_IN_EPOCH = SECONDS_IN_SLOT * 32;
+    uint256 internal constant TWO_EPOCHS = 2 * SECONDS_IN_EPOCH;
+    uint256 internal constant DISPUTE_PERIOD = 2 * SECONDS_IN_EPOCH;
+
+    function getGenesisTimestamp(uint256 _chainid) internal pure returns (uint256) {
+        if (_chainid == LibNetwork.ETHEREUM_MAINNET) {
+            return ETHEREUM_MAINNET_BEACON_GENESIS;
+        } else if (_chainid == LibNetwork.ETHEREUM_HOLESKY) {
+            return ETHEREUM_HOLESKY_BEACON_GENESIS;
+        }
+        return uint256(0);
+    }
+
+    function getBeaconBlockRootContract() internal pure returns (address) {
+        return 0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02;
+    }
+}
