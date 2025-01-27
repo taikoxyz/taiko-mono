06225d80c00886e8494e9e8adfe12549fe783741
Daniel Wang <dan@taiko.xyz>
2025-01-27 12:11:38 +0800

    Update gen-layouts.sh

--- packages/protocol/script/gen-layouts.sh
+++ packages/protocol/script/gen-layouts.sh
@@ -37,6 +37,8 @@ contracts_layer1=(
 "contracts/layer1/team/TokenUnlock.sol:TokenUnlock"
 "contracts/layer1/provers/ProverSet.sol:ProverSet"
 "contracts/layer1/based/ForkRouter.sol:ForkRouter"
+"contracts/layer1/forced-inclusion/ForcedInclusionInbox"
+"contracts/layer1/forced-inclusion/ForcedInclusionStore"
 )
 
 # Layer 2 contracts
