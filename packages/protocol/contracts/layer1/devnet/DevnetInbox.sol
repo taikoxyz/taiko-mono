67a871f2aa6d5de0ae459f70f78a8a3ec9e3fd0a
Daniel Wang <dan@taiko.xyz>
2025-01-31 13:53:00 +0800

    more

--- packages/protocol/contracts/layer1/devnet/DevnetInbox.sol
+++ packages/protocol/contracts/layer1/devnet/DevnetInbox.sol
@@ -29,7 +29,7 @@ contract DevnetInbox is TaikoInbox {
                 maxGasIssuancePerBlock: 600_000_000
             }),
             provingWindow: 2 hours,
-            cooldownWindow: 1 hours,
+            cooldownWindow: 0 hours,
             maxSignalsToReceive: 16,
             maxBlocksPerBatch: 768,
             forkHeights: ITaikoInbox.ForkHeights({ ontake: 0, pacaya: 0 })
