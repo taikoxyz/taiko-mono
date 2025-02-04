d1844381ec9d611b2062cc207d56f59331b82cdf
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-02-04 16:40:30 +0800

    build

--- packages/taiko-client/pkg/rpc/client.go
+++ packages/taiko-client/pkg/rpc/client.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
