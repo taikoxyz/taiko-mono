002d6a5fd509c4d47746913551be3c5693dee277
maskpp <maskpp266@gmail.com>
2025-02-06 11:08:15 +0800

    update pacaya devnet hardfork number

--- packages/protocol/contracts/layer1/devnet/DevnetInbox.sol
+++ packages/protocol/contracts/layer1/devnet/DevnetInbox.sol
@@ -33,7 +33,7 @@ contract DevnetInbox is TaikoInbox {
             cooldownWindow: 0 hours,
             maxSignalsToReceive: 16,
             maxBlocksPerBatch: 768,
-            forkHeights: ITaikoInbox.ForkHeights({ontake: 0, pacaya: 1000})
+            forkHeights: ITaikoInbox.ForkHeights({ontake: 0, pacaya: 10})
         });
     }
 }
