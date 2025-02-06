d56437f1291873f6360afe45cc07915558cc3b4f
Daniel Wang <dan@taiko.xyz>
2025-02-06 12:07:43 +0800

    Update SgxVerifier.sol

--- packages/protocol/contracts/layer1/verifiers/SgxVerifier.sol
+++ packages/protocol/contracts/layer1/verifiers/SgxVerifier.sol
@@ -152,31 +152,30 @@ contract SgxVerifier is EssentialContract, IVerifier {
         // 4 bytes + 20 bytes + 20 bytes + 65 bytes (signature) = 109
         require(_proof.length == 109, SGX_INVALID_PROOF());
 
-        uint32 id = uint32(bytes4(_proof[:4]));
         address oldInstance = address(bytes20(_proof[4:24]));
         address newInstance = address(bytes20(_proof[24:44]));
-        bytes memory signature = _proof[44:];
 
         // Collect public inputs
-        bytes32[] memory publicInputs = new bytes32[](_ctxs.length + 2);
+        uint256 size = _ctxs.length;
+        bytes32[] memory publicInputs = new bytes32[](size + 2);
         // First public input is the current instance public key
         publicInputs[0] = bytes32(uint256(uint160(oldInstance)));
         publicInputs[1] = bytes32(uint256(uint160(newInstance)));
+
         // All other inputs are the block program public inputs (a single 32 byte value)
-        {
-            uint256 size = _ctxs.length;
-            for (uint256 i; i < size; ++i) {
-                // TODO(Yue): For now this assumes the new instance public key to remain the same
-                publicInputs[i + 2] = LibPublicInput.hashPublicInputs(
-                    _ctxs[i].transition, address(this), newInstance, _ctxs[i].metaHash, taikoChainId
-                );
-            }
+        for (uint256 i; i < size; ++i) {
+            // TODO(Yue): For now this assumes the new instance public key to remain the same
+            publicInputs[i + 2] = LibPublicInput.hashPublicInputs(
+                _ctxs[i].transition, address(this), newInstance, _ctxs[i].metaHash, taikoChainId
+            );
         }
 
         bytes32 signatureHash = keccak256(abi.encodePacked(publicInputs));
         // Verify the blocks
+        bytes memory signature = _proof[44:];
         require(oldInstance == ECDSA.recover(signatureHash, signature), SGX_INVALID_PROOF());
 
+        uint32 id = uint32(bytes4(_proof[:4]));
         require(_isInstanceValid(id, oldInstance), SGX_INVALID_INSTANCE());
 
         if (newInstance != oldInstance && newInstance != address(0)) {
