05e43ab2322c4d0c45877a6ae634e1327cc70227
Håvard Anda Estensen <haavard.ae@gmail.com>
2025-02-19 10:09:56 +0100

    chore(taiko-client): use `EncodeAndCompressTxList()` instead of `EncodeToBytes()` then `Compress()` (#18972)

--- packages/taiko-client/proposer/transaction_builder/blob.go
+++ packages/taiko-client/proposer/transaction_builder/blob.go
@@ -12,7 +12,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rlp"
 
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/encoding"
 	pacayaBindings "github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/pacaya"
@@ -193,13 +192,9 @@ func (b *BlobTransactionBuilder) BuildPacaya(
 		})
 	}
 
-	rlpEncoded, err := rlp.EncodeToBytes(allTxs)
+	txListsBytes, err := utils.EncodeAndCompressTxList(allTxs)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode transactions: %w", err)
-	}
-	txListsBytes, err := utils.Compress(rlpEncoded)
-	if err != nil {
-		return nil, fmt.Errorf("failed to compress transactions: %w", err)
+		return nil, err
 	}
 
 	if blobs, err = b.splitToBlobs(txListsBytes); err != nil {
--- packages/taiko-client/proposer/transaction_builder/calldata.go
+++ packages/taiko-client/proposer/transaction_builder/calldata.go
@@ -10,7 +10,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rlp"
 
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/encoding"
 	pacayaBindings "github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/pacaya"
@@ -177,13 +176,9 @@ func (b *CalldataTransactionBuilder) BuildPacaya(
 		})
 	}
 
-	rlpEncoded, err := rlp.EncodeToBytes(allTxs)
+	txListsBytes, err := utils.EncodeAndCompressTxList(allTxs)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode transactions: %w", err)
-	}
-	txListsBytes, err := utils.Compress(rlpEncoded)
-	if err != nil {
-		return nil, fmt.Errorf("failed to compress transactions: %w", err)
+		return nil, err
 	}
 
 	if encodedParams, err = encoding.EncodeBatchParamsWithForcedInclusion(
