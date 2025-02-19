9a8916669b6a22587b23782012201930e418ffa5
Gavin Yu <gavin@taiko.xyz>
2025-02-19 15:11:01 +0800

    chore(protocol): keep `provingWindow` 5h for both TEE and ZK (#18963)
    Co-authored-by: Gavin “yoghurt” Yu <guoyu960223@gmail.com>
    Co-authored-by: YoGhurt111 <23009382+YoGhurt111@users.noreply.github.com>

--- packages/protocol/contracts/layer1/tiers/TierProviderBase.sol
+++ packages/protocol/contracts/layer1/tiers/TierProviderBase.sol
@@ -22,34 +22,34 @@ abstract contract TierProviderBase is ITierProvider {
 
         // TEE Tiers
         if (_tierId == LibTiers.TIER_SGX) {
-            // cooldownWindow is 240 minutes and provingWindow is 60 minutes
-            return _buildTier(LibStrings.B_TIER_SGX, 2, 240, 60);
+            // cooldownWindow is 240 minutes and provingWindow is 300 minutes
+            return _buildTier(LibStrings.B_TIER_SGX, 2, 240, 300);
         }
         if (_tierId == LibTiers.TIER_TDX) {
-            // cooldownWindow is 240 minutes and provingWindow is 60 minutes
-            return _buildTier(LibStrings.B_TIER_TDX, 2, 240, 60);
+            // cooldownWindow is 240 minutes and provingWindow is 300 minutes
+            return _buildTier(LibStrings.B_TIER_TDX, 2, 240, 300);
         }
         if (_tierId == LibTiers.TIER_TEE_ANY) {
-            // cooldownWindow is 240 minutes and provingWindow is 60 minutes
-            return _buildTier(LibStrings.B_TIER_TEE_ANY, 2, 240, 60);
+            // cooldownWindow is 240 minutes and provingWindow is 300 minutes
+            return _buildTier(LibStrings.B_TIER_TEE_ANY, 2, 240, 300);
         }
 
-        // ZKVM Tiers: Allowing 120 minutes for proof aggregation.
+        // ZKVM Tiers: Allowing 420 minutes for proof aggregation.
         if (_tierId == LibTiers.TIER_ZKVM_RISC0) {
-            // cooldownWindow is 240 minutes and provingWindow is 120 minutes
-            return _buildTier(LibStrings.B_TIER_ZKVM_RISC0, 3, 240, 120);
+            // cooldownWindow is 240 minutes and provingWindow is 420 minutes
+            return _buildTier(LibStrings.B_TIER_ZKVM_RISC0, 3, 240, 420);
         }
         if (_tierId == LibTiers.TIER_ZKVM_SP1) {
-            // cooldownWindow is 240 minutes and provingWindow is 120 minutes
-            return _buildTier(LibStrings.B_TIER_ZKVM_SP1, 3, 240, 120);
+            // cooldownWindow is 240 minutes and provingWindow is 420 minutes
+            return _buildTier(LibStrings.B_TIER_ZKVM_SP1, 3, 240, 420);
         }
         if (_tierId == LibTiers.TIER_ZKVM_ANY) {
-            // cooldownWindow is 240 minutes and provingWindow is 90 minutes
-            return _buildTier(LibStrings.B_TIER_ZKVM_ANY, 3, 240, 120);
+            // cooldownWindow is 240 minutes and provingWindow is 420 minutes
+            return _buildTier(LibStrings.B_TIER_ZKVM_ANY, 3, 240, 420);
         }
         if (_tierId == LibTiers.TIER_ZKVM_AND_TEE) {
-            // cooldownWindow is 240 minutes and provingWindow is 90 minutes
-            return _buildTier(LibStrings.B_TIER_ZKVM_AND_TEE, 3, 240, 120);
+            // cooldownWindow is 240 minutes and provingWindow is 420 minutes
+            return _buildTier(LibStrings.B_TIER_ZKVM_AND_TEE, 3, 240, 420);
         }
 
         // Guardian Minority Tiers
