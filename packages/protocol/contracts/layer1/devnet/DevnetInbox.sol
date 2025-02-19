650cb1febe0f6e872947ec9eb9bf15904189d4b0
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2025-02-19 11:51:24 +0800

    chore(protocol): update Inbox configs (#18965)

--- packages/protocol/contracts/layer1/devnet/DevnetInbox.sol
+++ packages/protocol/contracts/layer1/devnet/DevnetInbox.sol
@@ -36,7 +36,7 @@ contract DevnetInbox is TaikoInbox {
                 maxGasIssuancePerBlock: 600_000_000
             }),
             provingWindow: 2 hours,
-            cooldownWindow: 0 hours,
+            cooldownWindow: 2 hours,
             maxSignalsToReceive: 16,
             maxBlocksPerBatch: 768,
             forkHeights: ITaikoInbox.ForkHeights({ ontake: 0, pacaya: 10, shasta: 0, unzen: 0 })
