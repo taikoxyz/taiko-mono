b66b6008a3133adbcb140fc4c2a08970dce96415
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-02-19 15:56:35 +0800

    feat(taiko-client): include `ParentMetaHash` for better revert protection

--- packages/taiko-client/proposer/transaction_builder/calldata.go
+++ packages/taiko-client/proposer/transaction_builder/calldata.go
@@ -73,13 +73,27 @@ func (b *CalldataTransactionBuilder) BuildOntake(
 		encodedParamsArray [][]byte
 	)
 
-	for range txListBytesArray {
-		encodedParams, err := encoding.EncodeBlockParamsOntake(&encoding.BlockParamsV2{
+	for i := range txListBytesArray {
+		params := &encoding.BlockParamsV2{
 			Coinbase:       b.l2SuggestedFeeRecipient,
 			ParentMetaHash: [32]byte{},
 			AnchorBlockId:  0,
 			Timestamp:      0,
-		})
+		}
+		if i == 0 && b.revertProtectionEnabled {
+			state, err := b.rpc.GetProtocolStateVariables(nil)
+			if err != nil {
+				return nil, err
+			}
+
+			blockInfo, err := b.rpc.GetL2BlockInfoV2(ctx, new(big.Int).SetUint64(state.B.NumBlocks-1))
+			if err != nil {
+				return nil, err
+			}
+
+			params.ParentMetaHash = blockInfo.MetaHash
+		}
+		encodedParams, err := encoding.EncodeBlockParamsOntake(params)
 		if err != nil {
 			return nil, err
 		}
