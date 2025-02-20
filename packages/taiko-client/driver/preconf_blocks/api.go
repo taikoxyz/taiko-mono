cad436f65ac5c532676e9979bde6e14bec099cee
Jeffery Walsh <cyberhorsey@gmail.com>
2025-02-20 11:58:18 -0800

    log

--- packages/taiko-client/driver/preconf_blocks/api.go
+++ packages/taiko-client/driver/preconf_blocks/api.go
@@ -32,6 +32,8 @@ func (b *BuildPreconfBlockRequestBody) ValidateSignature() (bool, error) {
 		return false, err
 	}
 
+	log.Info("Signature verification", "pubKey", crypto.PubkeyToAddress(*pubKey).Hex(), "feeRecipient", b.ExecutableData.FeeRecipient.Hex())
+
 	return crypto.PubkeyToAddress(*pubKey).Hex() == b.ExecutableData.FeeRecipient.Hex(), nil
 }
 
