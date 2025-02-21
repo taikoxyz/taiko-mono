be4c25077f797d18f10762dd2118aea2b5df58c8
Jeffery Walsh <cyberhorsey@gmail.com>
2025-02-20 16:21:22 -0800

    improve log

--- packages/taiko-client/driver/preconf_blocks/server.go
+++ packages/taiko-client/driver/preconf_blocks/server.go
@@ -206,7 +206,7 @@ func (s *PreconfBlockAPIServer) P2PSequencerAddress() common.Address {
 		return common.Address{}
 	}
 
-	log.Info("operator address", "address", operatorAddress.Hex())
+	log.Info("current operator address for epoch as p2psequencer", "address", operatorAddress.Hex())
 
 	return operatorAddress
 }
