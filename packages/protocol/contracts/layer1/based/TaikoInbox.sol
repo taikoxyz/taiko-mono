193656bf0f2b33718e274bf9cd7dd647b7bc1940
David <david@taiko.xyz>
2025-02-27 17:47:02 +0800

    feat(protocol): add `ITaikoInbox.Config` to `ITaikoInbox.BatchInfo`

--- packages/protocol/contracts/layer1/based/TaikoInbox.sol
+++ packages/protocol/contracts/layer1/based/TaikoInbox.sol
@@ -165,7 +165,7 @@ abstract contract TaikoInbox is EssentialContract, ITaikoInbox, IProposeBatch, I
                 // Data for the L2 anchor transaction, shared by all blocks in the batch
                 anchorBlockId: anchorBlockId,
                 anchorBlockHash: blockhash(anchorBlockId),
-                baseFeeConfig: config.baseFeeConfig
+                config: config
             });
 
             require(info_.anchorBlockHash != 0, ZeroAnchorBlockHash());
