4a348e762e81c4dfa67afc799c0c9847460007a3
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2025-03-01 10:53:12 +0800

    feat(protocol): revert 3 previous PRs (#19012)

--- packages/protocol/contracts/layer1/based/ITaikoInbox.sol
+++ packages/protocol/contracts/layer1/based/ITaikoInbox.sol
@@ -18,6 +18,15 @@ import "src/shared/based/LibSharedData.sol";
 /// @custom:security-contact security@taiko.xyz
 interface ITaikoInbox {
     struct BlockParams {
+        // the max number of transactions in this block. Note that if there are not enough
+        // transactions in calldata or blobs, the block will contains as many transactions as
+        // possible.
+        uint16 numTransactions;
+        // For the first block in a batch,  the block timestamp is the batch params' `timestamp`
+        // plus this time shift value;
+        // For all other blocks in the same batch, the block timestamp is its parent block's
+        // timestamp plus this time shift value.
+        uint8 timeShift;
         // Signals sent on L1 and need to sync to this L2 block.
         bytes32[] signalSlots;
     }
@@ -44,6 +53,7 @@ interface ITaikoInbox {
         address coinbase;
         bytes32 parentMetaHash;
         uint64 anchorBlockId;
+        uint64 lastBlockTimestamp;
         bool revertIfNotFirstProposal;
         // Specifies the number of blocks to be generated from this batch.
         BlobParams blobParams;
@@ -65,12 +75,13 @@ interface ITaikoInbox {
         uint32 blobByteSize;
         uint32 gasLimit;
         uint64 lastBlockId;
+        uint64 lastBlockTimestamp;
         // Data for the L2 anchor transaction, shared by all blocks in the batch
         uint64 anchorBlockId;
         // corresponds to the `_anchorStateRoot` parameter in the anchor transaction.
         // The batch's validity proof shall verify the integrity of these two values.
         bytes32 anchorBlockHash;
-        Config config;
+        LibSharedData.BaseFeeConfig baseFeeConfig;
     }
 
     /// @dev This struct holds batch metadata essential for proving the batch.
@@ -106,6 +117,7 @@ interface ITaikoInbox {
         uint96 reserved3;
         uint96 livenessBond;
         uint64 batchId; // slot 3
+        uint64 lastBlockTimestamp;
         uint64 anchorBlockId;
         uint24 nextTransitionId;
         uint8 reserved4;
@@ -277,6 +289,9 @@ interface ITaikoInbox {
     error ParentMetaHashMismatch();
     error SameTransition();
     error SignalNotSent();
+    error TimestampSmallerThanParent();
+    error TimestampTooLarge();
+    error TimestampTooSmall();
     error TooManyBatches();
     error TooManyBlocks();
     error TooManySignals();
