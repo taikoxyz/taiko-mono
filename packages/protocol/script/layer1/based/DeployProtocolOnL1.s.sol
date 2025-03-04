9898337fca9883d4a8fee846c33d16500e2860c8
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-03-04 20:46:18 +0800

    fix

--- packages/protocol/script/layer1/based/DeployProtocolOnL1.s.sol
+++ packages/protocol/script/layer1/based/DeployProtocolOnL1.s.sol
@@ -367,7 +367,7 @@ contract DeployProtocolOnL1 is DeployCapability {
                     address(rollupResolver), taikoInboxAddr, taikoInbox.bondToken(), taikoInboxAddr
                 )
             ),
-            data: abi.encodeCall(ProverSetBase.init, (owner, vm.envAddress("PROVER_SET_ADMIN"))),
+            data: abi.encodeCall(ProverSetBase.init, (address(0), vm.envAddress("PROVER_SET_ADMIN"))),
             registerTo: rollupResolver
         });
     }
