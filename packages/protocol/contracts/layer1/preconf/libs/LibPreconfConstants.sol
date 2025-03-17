24266503953c039640d883f955d77abbd0763559
dantaik <99078276+dantaik@users.noreply.github.com>
2025-03-17 08:56:42 +0000

    forge fmt & update contract layout tables

--- packages/protocol/contracts/layer1/preconf/libs/LibPreconfConstants.sol
+++ packages/protocol/contracts/layer1/preconf/libs/LibPreconfConstants.sol
@@ -9,7 +9,7 @@ library LibPreconfConstants {
     uint256 internal constant ETHEREUM_MAINNET_BEACON_GENESIS = 1_606_824_023;
     uint256 internal constant ETHEREUM_HOLESKY_BEACON_GENESIS = 1_695_902_100;
     uint256 internal constant ETHEREUM_HELDER_BEACON_GENESIS = 1_718_967_660;
-    uint256 internal constant ETHEREUM_HOODI_BEACON_GENESIS = 1742212800;
+    uint256 internal constant ETHEREUM_HOODI_BEACON_GENESIS = 1_742_212_800;
 
     uint256 internal constant SECONDS_IN_SLOT = 12;
     uint256 internal constant SECONDS_IN_EPOCH = SECONDS_IN_SLOT * 32;
