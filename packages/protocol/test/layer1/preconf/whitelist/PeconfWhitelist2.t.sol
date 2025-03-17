bb518cfd870cacb0fea7b43722e3b53b12fdb0ce
Daniel Wang <dan@taikocha.in>
2025-03-17 21:55:27 +0800

    Update PeconfWhitelist2.t.sol

--- packages/protocol/test/layer1/preconf/whitelist/PeconfWhitelist2.t.sol
+++ packages/protocol/test/layer1/preconf/whitelist/PeconfWhitelist2.t.sol
@@ -80,6 +80,79 @@ contract TestPreconfWhitelist2 is Layer1Test {
         assertEq(whitelist.operatorMapping(0), address(0));
     }
 
+    function test_whitelist2_addThenRemoveTwoOperators() external {
+        _setBeaconBlockRoot(bytes32(uint256(7)));
+
+        assertEq(whitelist.getOperatorForCurrentEpoch(), address(0));
+        assertEq(whitelist.getOperatorForNextEpoch(), address(0));
+
+        vm.prank(whitelistOwner);
+        whitelist.addOperator(Alice);
+        vm.prank(whitelistOwner);
+        whitelist.addOperator(Bob);
+
+        assertEq(whitelist.operatorCount(), 2);
+        assertEq(whitelist.operatorMapping(0), Alice);
+        assertEq(whitelist.operatorMapping(1), Bob);
+
+        (uint64 activeSince, uint64 inactiveSince, uint8 index) = whitelist.operators(Alice);
+        assertEq(activeSince, whitelist.epochTimestamp(2));
+        assertEq(inactiveSince, 0);
+        assertEq(index, 0);
+
+        (activeSince, inactiveSince, index) = whitelist.operators(Bob);
+        assertEq(activeSince, whitelist.epochTimestamp(2));
+        assertEq(inactiveSince, 0);
+        assertEq(index, 1);
+
+        // Verify operator for current epoch
+        assertEq(whitelist.getOperatorForCurrentEpoch(), address(0));
+        assertEq(whitelist.getOperatorForNextEpoch(), address(0));
+
+        _advanceOneEpoch();
+        assertEq(whitelist.getOperatorForCurrentEpoch(), address(0));
+        assertEq(whitelist.getOperatorForNextEpoch(), Bob);
+
+        _advanceOneEpoch();
+        assertEq(whitelist.getOperatorForCurrentEpoch(), Bob);
+        assertEq(whitelist.getOperatorForNextEpoch(), Bob);
+
+        vm.prank(whitelistOwner);
+        whitelist.removeOperator(Alice);
+        vm.prank(whitelistOwner);
+        whitelist.removeOperator(Bob);
+
+        assertEq(whitelist.operatorCount(), 2);
+        assertEq(whitelist.operatorMapping(0), Alice);
+        assertEq(whitelist.operatorMapping(1), Bob);
+
+        (activeSince, inactiveSince, index) = whitelist.operators(Alice);
+        assertTrue(activeSince != 0);
+        assertEq(inactiveSince, whitelist.epochTimestamp(2));
+        assertEq(index, 0);
+
+        (activeSince, inactiveSince, index) = whitelist.operators(Bob);
+        assertTrue(activeSince != 0);
+        assertEq(inactiveSince, whitelist.epochTimestamp(2));
+        assertEq(index, 1);
+
+        assertEq(whitelist.getOperatorForCurrentEpoch(), Bob);
+        assertEq(whitelist.getOperatorForNextEpoch(), Bob);
+
+        _advanceOneEpoch();
+        assertEq(whitelist.getOperatorForCurrentEpoch(), Bob);
+        assertEq(whitelist.getOperatorForNextEpoch(), address(0));
+
+        _advanceOneEpoch();
+        assertEq(whitelist.getOperatorForCurrentEpoch(), address(0));
+        assertEq(whitelist.getOperatorForNextEpoch(), address(0));
+
+        whitelist.consolidate();
+        assertEq(whitelist.operatorCount(), 0);
+        assertEq(whitelist.operatorMapping(0), address(0));
+        assertEq(whitelist.operatorMapping(1), address(0));
+    }
+
     function _setBeaconBlockRoot(bytes32 _root) internal {
         vm.etch(
             LibPreconfConstants.getBeaconBlockRootContract(),
