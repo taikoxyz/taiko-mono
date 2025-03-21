e90880a5fd72d6f6579bc808724097b5d4cdf082
Gavin Yu <gavin@taiko.xyz>
2025-03-21 12:18:33 +0800

    Update packages/taiko-client/prover/init.go
    Co-authored-by: David <david@taiko.xyz>

--- packages/taiko-client/prover/init.go
+++ packages/taiko-client/prover/init.go
@@ -195,7 +195,7 @@ func (p *Prover) initPacayaProofSubmitter(txBuilder *transaction.ProveBlockTxBui
 		Verifier:            pivotVerifierAddress,
 		RaikoHostEndpoint:   p.cfg.RaikoHostEndpoint,
 		JWT:                 p.cfg.RaikoJWT,
-		Dummy:               true, // Note: since the pivot proof is not ready, so we use p proof instead
+		Dummy:               true, // Note: since the pivot proof is not ready, so we use dummy proof instead
 		RaikoRequestTimeout: p.cfg.RaikoRequestTimeout,
 	}
 
