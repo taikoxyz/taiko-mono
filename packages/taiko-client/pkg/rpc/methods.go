a65d5798cc81356ea223374ca7950f41c1a2aa7f
David <david@taiko.xyz>
2025-03-27 11:27:14 +0800

    feat: update genesis check

--- packages/taiko-client/pkg/rpc/methods.go
+++ packages/taiko-client/pkg/rpc/methods.go
@@ -673,6 +673,10 @@ func (c *Client) CheckL1Reorg(ctx context.Context, blockID *big.Int) (*ReorgChec
 	)
 	defer cancel()
 
+	if blockID.Cmp(common.Big0) == 0 {
+		return result, nil
+	}
+
 	for {
 		// If we rollback to the genesis block, then there is no L1Origin information recorded in the L2 execution
 		// engine for that block, so we will query the protocol to use `GenesisHeight` value to reset the L1 cursor.
