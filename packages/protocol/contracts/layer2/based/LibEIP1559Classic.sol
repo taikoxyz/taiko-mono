9ca19fdd1e04f42f4dc4f2efe8074d4c8a73d7d0
Daniel Wang <dan@taiko.xyz>
2025-04-01 13:55:06 +0900

    Update LibEIP1559Classic.sol

--- packages/protocol/contracts/layer2/based/LibEIP1559Classic.sol
+++ packages/protocol/contracts/layer2/based/LibEIP1559Classic.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "src/shared/libs/LibMath.sol";
+
 /// @title LibEIP1559Classic
 /// @notice Implements classic EIP-1559 base fee calculation.
 /// @custom:security-contact security@taiko.xyz
 library LibEIP1559Classic {
+    using LibMath for uint256;
     /// @notice Calculates the classic base fee using the given parameters.
     /// @param _parentBasefee The base fee of the parent block.
     /// @param _parentGasUsed The gas used in the parent block.
@@ -12,20 +15,22 @@ library LibEIP1559Classic {
     /// @param _gasPerSeconds The gas issuance rate per second.
     /// @param _blockTime The time duration of the block.
     /// @return The calculated classic base fee.
+
     function calculateClassicBaseFee(
         uint256 _parentBasefee,
         uint256 _parentGasUsed,
         uint256 _baseFeeChangeDenominator,
         uint256 _gasPerSeconds,
-        uint256 _blockTime
+        uint256 _blockTime,
+        uint256 _blockTimeCap
     )
         internal
         pure
         returns (uint256)
     {
-        uint256 gasIssuance = _gasPerSeconds * _blockTime;
+        uint256 gasIssuance = _gasPerSeconds * _blockTime.min(_blockTimeCap);
         if (gasIssuance == 0) {
-            gasIssuance = 1;
+            return _parentBasefee;
         }
         return calculateClassicBaseFee(
             _parentBasefee, _parentGasUsed, _baseFeeChangeDenominator, gasIssuance
