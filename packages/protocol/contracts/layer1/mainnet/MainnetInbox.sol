d4d77340d95952a5fa47d3d7a1e690ab51679bd1
Daniel Wang <dan@taiko.xyz>
2025-04-04 18:35:08 +0800

    Update MainnetInbox.sol

--- packages/protocol/contracts/layer1/mainnet/MainnetInbox.sol
+++ packages/protocol/contracts/layer1/mainnet/MainnetInbox.sol
@@ -49,7 +49,7 @@ contract MainnetInbox is TaikoInbox {
                 maxGasIssuancePerBlock: 600_000_000 // two minutes: 5_000_000 * 120
              }),
             provingWindow: 2 hours,
-            cooldownWindow: 2 hours,
+            cooldownWindow: 7 days,
             maxSignalsToReceive: 16,
             maxBlocksPerBatch: 768,
             forkHeights: ITaikoInbox.ForkHeights({
