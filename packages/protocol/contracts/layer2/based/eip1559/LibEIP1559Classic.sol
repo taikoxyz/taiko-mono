bc67821f3a1541990b44424cfbf2c0566f604ec8
Daniel Wang <dan@taikocha.in>
2025-04-05 23:34:30 +0800

    dynamic base fee change denominator

--- packages/protocol/contracts/layer2/based/eip1559/LibEIP1559Classic.sol
+++ packages/protocol/contracts/layer2/based/eip1559/LibEIP1559Classic.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "src/shared/libs/LibMath.sol";
+import "src/shared/libs/LibNetwork.sol";
 
 /// @title LibEIP1559Classic
 /// @notice Implements classic EIP-1559 base fee calculation.
@@ -14,44 +15,54 @@ library LibEIP1559Classic {
 
     /// @dev This value is the minimum base fee for Ontake.
     uint256 public constant MIN_BASE_FEE = 0.008847185 gwei;
+    uint256 public constant MAX_BASE_FEE = 100 gwei;
 
-    /// @dev Gas should not be issued for more than 12 seconds.
-    uint256 public constant GAS_ISSUANCE_TIME_CAP = 12 seconds;
+    /// @dev Max value for block time calculation.
+    /// @dev This value is set to 13 to ensure calculation never overflow or underflow.
+    uint256 public constant BLOCK_TIME_CALCULATION_CAP = LibNetwork.ETHEREUM_BLOCK_TIME;
 
     /// @notice Calculates the classic base fee using the given parameters.
     /// @param _parentBasefee The base fee of the parent block.
     /// @param _parentGasUsed The gas used in the parent block.
-    /// @param _adjustmentQuotient The denominator for base fee change calculation.
+    /// @param _adjustmentQuotient The denominator for base fee change calculation per 12-second.
     /// @param _gasPerSeconds The gas issuance rate per second.
     /// @param _blockTime The time duration of the block.
     /// @return The calculated classic base fee.
-    function calculateClassicBaseFee(
+    function calculateBaseFee(
         uint256 _parentBasefee,
         uint256 _parentGasUsed,
-        uint256 _adjustmentQuotient,
-        uint256 _gasPerSeconds,
+        uint8 _adjustmentQuotient,
+        uint32 _gasPerSeconds,
         uint256 _blockTime
     )
         internal
         pure
         returns (uint256)
     {
+        // TODO(daniel): how to make sure the following calculation never overflow or underflow?
         require(_blockTime != 0, ZeroBlockTime());
         require(_gasPerSeconds != 0, ZeroGasPerSecond());
 
-        return _calculateClassicBaseFee(
-            _parentBasefee,
-            _parentGasUsed,
-            _adjustmentQuotient,
-            _gasPerSeconds * _blockTime.min(GAS_ISSUANCE_TIME_CAP)
-        ).max(MIN_BASE_FEE);
+        uint256 changePerSecondDenominator =
+            uint256(_adjustmentQuotient) * LibNetwork.ETHEREUM_BLOCK_TIME;
+        uint256 effectiveBlockTime = _blockTime.min(BLOCK_TIME_CALCULATION_CAP);
+        uint256 changeDenominator = changePerSecondDenominator ** effectiveBlockTime;
+        uint256 changeNumerator =
+            (changePerSecondDenominator + 1) ** effectiveBlockTime - changeDenominator;
+
+        uint256 gasIssuance = effectiveBlockTime * _gasPerSeconds;
+
+        return _calculateBaseFee(
+            _parentBasefee, _parentGasUsed, gasIssuance, changeNumerator, changeDenominator
+        ).max(MIN_BASE_FEE).min(MAX_BASE_FEE);
     }
 
-    function _calculateClassicBaseFee(
+    function _calculateBaseFee(
         uint256 _parentBasefee,
         uint256 _parentGasUsed,
-        uint256 _adjustmentQuotient,
-        uint256 _gasIssuance
+        uint256 _gasIssuance,
+        uint256 _changeNumerator,
+        uint256 _changeDenominator
     )
         private
         pure
@@ -59,10 +70,12 @@ library LibEIP1559Classic {
     {
         if (_parentGasUsed >= _gasIssuance) {
             return _parentBasefee
-                + _parentBasefee * (_parentGasUsed - _gasIssuance) / _adjustmentQuotient / _gasIssuance;
+                + _parentBasefee * (_parentGasUsed - _gasIssuance) / _gasIssuance * _changeNumerator
+                    / _changeDenominator;
         } else {
             return _parentBasefee
-                - _parentBasefee * (_gasIssuance - _parentGasUsed) / _adjustmentQuotient / _gasIssuance;
+                - _parentBasefee * (_gasIssuance - _parentGasUsed) / _gasIssuance * _changeNumerator
+                    / _changeDenominator;
         }
     }
 }
