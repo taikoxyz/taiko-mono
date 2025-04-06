267c9eaccac59808892584373743e2f6edf1a577
Daniel Wang <dan@taikocha.in>
2025-04-06 19:56:24 +0800

    Update LibEIP1559Classic.t.sol

--- packages/protocol/test/layer2/based/eip1559/LibEIP1559Classic.t.sol
+++ packages/protocol/test/layer2/based/eip1559/LibEIP1559Classic.t.sol
@@ -4,9 +4,36 @@ pragma solidity ^0.8.24;
 import "test/layer2/Layer2Test.sol";
 import "src/layer2/based/eip1559/LibEIP1559Classic.sol";
 
+contract LibEIP1559ClassicWrapper {
+    // Expose the calculateBaseFee function of LibEIP1559Classic so revert can be tested with new
+    // Foundry release. See https://github.com/foundry-rs/foundry/issues/5367?utm_source=chatgpt.com
+    function calculateBaseFee(
+        uint256 _parentBasefee,
+        uint64 _parentGasUsed,
+        uint8 _adjustmentQuotient,
+        uint32 _gasPerSeconds,
+        uint256 _blockTime
+    )
+        external
+        pure
+        returns (uint256)
+    {
+        return LibEIP1559Classic.calculateBaseFee(
+            _parentBasefee, _parentGasUsed, _adjustmentQuotient, _gasPerSeconds, _blockTime
+        );
+    }
+}
+
 contract LibEIP1559ClassicTest is Layer2Test {
+    LibEIP1559ClassicWrapper private wrapper;
+
+    function setUp() public override {
+        super.setUp();
+        wrapper = new LibEIP1559ClassicWrapper();
+    }
+
     // Test that base fee stays the same when gas used equals gas issuance
-    function test_1559classic_baseFeeStaysSameWhenGasUsedEqualsIssuance() public pure {
+    function test_1559classic_baseFeeStaysSameWhenGasUsedEqualsIssuance() public view {
         // Test parameters
         uint256 parentBaseFee = 1 gwei;
         uint8 adjustmentQuotient = 8; // A reasonable adjustment quotient
@@ -18,7 +45,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         uint64 gasUsed = uint64(blockTime * gasPerSecond);
 
         // Calculate new base fee
-        uint256 newBaseFee = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee = wrapper.calculateBaseFee(
             parentBaseFee, gasUsed, adjustmentQuotient, gasPerSecond, blockTime
         );
 
@@ -33,7 +60,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
     // Test with different parent base fees to ensure consistency
     function test_1559classic_baseFeeStaysSameWithDifferentBaseFees(uint256 parentBaseFee)
         public
-        pure
+        view
     {
         // Bound the parent base fee to reasonable values
         parentBaseFee =
@@ -46,7 +73,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         // Gas used equals issuance
         uint64 gasUsed = uint64(blockTime * gasPerSecond);
 
-        uint256 newBaseFee = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee = wrapper.calculateBaseFee(
             parentBaseFee, gasUsed, adjustmentQuotient, gasPerSecond, blockTime
         );
 
@@ -60,7 +87,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
     // Test with different block times (up to cap)
     function test_1559classic_baseFeeStaysSameWithDifferentBlockTimes(uint256 blockTime)
         public
-        pure
+        view
     {
         // Bound block time to reasonable values up to cap
         blockTime = bound(blockTime, 1, LibEIP1559Classic.BLOCK_TIME_CALCULATION_CAP);
@@ -75,7 +102,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
             : blockTime;
         uint64 gasUsed = uint64(effectiveBlockTime * gasPerSecond);
 
-        uint256 newBaseFee = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee = wrapper.calculateBaseFee(
             parentBaseFee, gasUsed, adjustmentQuotient, gasPerSecond, blockTime
         );
 
@@ -86,7 +113,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         );
     }
 
-    function test_1559classic_calculateBaseFee_MaxInputs_ShouldNotRevert() public pure {
+    function test_1559classic_calculateBaseFee_MaxInputs_ShouldNotRevert() public view {
         // Test with maximum possible values for each input
         uint256 parentBaseFee = LibEIP1559Classic.MAX_BASE_FEE;
         uint64 parentGasUsed = type(uint64).max;
@@ -95,7 +122,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         uint256 blockTime = LibEIP1559Classic.BLOCK_TIME_CALCULATION_CAP;
 
         // This should not revert
-        uint256 newBaseFee = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee = wrapper.calculateBaseFee(
             parentBaseFee, parentGasUsed, adjustmentQuotient, gasPerSecond, blockTime
         );
 
@@ -103,11 +130,11 @@ contract LibEIP1559ClassicTest is Layer2Test {
         assertLe(newBaseFee, LibEIP1559Classic.MAX_BASE_FEE);
     }
 
-    function test_1559classic_calculateBaseFee_MaxInputsCombinations() public pure {
+    function test_1559classic_calculateBaseFee_MaxInputsCombinations() public view {
         // Test various combinations of max values
 
         // Test 1: Max parent base fee with normal other values
-        uint256 newBaseFee1 = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee1 = wrapper.calculateBaseFee(
             type(uint256).max, // parentBaseFee
             15_000_000 * 12, // normal gas used
             8, // normal adjustment quotient
@@ -117,7 +144,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         assertLe(newBaseFee1, LibEIP1559Classic.MAX_BASE_FEE);
 
         // Test 2: Max gas used with normal other values
-        uint256 newBaseFee2 = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee2 = wrapper.calculateBaseFee(
             1 gwei, // normal parent base fee
             type(uint64).max, // parentGasUsed
             8, // normal adjustment quotient
@@ -127,7 +154,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         assertLe(newBaseFee2, LibEIP1559Classic.MAX_BASE_FEE);
 
         // Test 3: Max adjustment quotient with normal other values
-        uint256 newBaseFee3 = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee3 = wrapper.calculateBaseFee(
             1 gwei, // normal parent base fee
             15_000_000 * 12, // normal gas used
             type(uint8).max, // max adjustment quotient
@@ -137,7 +164,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         assertLe(newBaseFee3, LibEIP1559Classic.MAX_BASE_FEE);
 
         // Test 4: Max gas per second with normal other values
-        uint256 newBaseFee4 = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee4 = wrapper.calculateBaseFee(
             1 gwei, // normal parent base fee
             15_000_000 * 12, // normal gas used
             8, // normal adjustment quotient
@@ -147,7 +174,7 @@ contract LibEIP1559ClassicTest is Layer2Test {
         assertLe(newBaseFee4, LibEIP1559Classic.MAX_BASE_FEE);
 
         // Test 5: Max block time with normal other values
-        uint256 newBaseFee5 = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee5 = wrapper.calculateBaseFee(
             1 gwei, // normal parent base fee
             15_000_000 * 12, // normal gas used
             8, // normal adjustment quotient
@@ -157,9 +184,9 @@ contract LibEIP1559ClassicTest is Layer2Test {
         assertLe(newBaseFee5, LibEIP1559Classic.MAX_BASE_FEE);
     }
 
-    function test_1559classic_calculateBaseFee_MinInputs_ShouldNotRevert() public pure {
+    function test_1559classic_calculateBaseFee_MinInputs_ShouldNotRevert() public view {
         // Test with minimum valid values
-        uint256 newBaseFee = LibEIP1559Classic.calculateBaseFee(
+        uint256 newBaseFee = wrapper.calculateBaseFee(
             LibEIP1559Classic.MIN_BASE_FEE, // minimum valid base fee
             1, // minimum gas used
             1, // minimum valid adjustment quotient
@@ -174,24 +201,24 @@ contract LibEIP1559ClassicTest is Layer2Test {
     function test_1559classic_calculateBaseFee_ZeroBlockTime_ShouldRevert() public {
         // Test that zero block time reverts
         vm.expectRevert(LibEIP1559Classic.ZeroBlockTime.selector);
-        LibEIP1559Classic.calculateBaseFee(
-            1 gwei,         // parent base fee
-            15_000_000,     // gas used
-            8,              // adjustment quotient
-            15_000_000,     // gas per second
-            0               // zero block time - should revert
+        wrapper.calculateBaseFee(
+            1 gwei, // parent base fee
+            15_000_000, // gas used
+            8, // adjustment quotient
+            15_000_000, // gas per second
+            0 // zero block time - should revert
         );
     }
 
     function test_1559classic_calculateBaseFee_ZeroGasPerSecond_ShouldRevert() public {
         // Test that zero gas per second reverts
         vm.expectRevert(LibEIP1559Classic.ZeroGasPerSecond.selector);
-        LibEIP1559Classic.calculateBaseFee(
-            1 gwei,         // parent base fee
-            15_000_000,     // gas used
-            8,              // adjustment quotient
-            0,              // zero gas per second - should revert
-            12              // block time
+        wrapper.calculateBaseFee(
+            1 gwei, // parent base fee
+            15_000_000, // gas used
+            8, // adjustment quotient
+            0, // zero gas per second - should revert
+            12 // block time
         );
     }
 }
