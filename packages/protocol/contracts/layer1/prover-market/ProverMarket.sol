77b25535f85805ca880807fd3f6de0a152b21196
Daniel Wang <dan@taiko.xyz>
2025-04-10 10:29:00 +0800

    Update ProverMarket.sol

--- packages/protocol/contracts/layer1/prover-market/ProverMarket.sol
+++ packages/protocol/contracts/layer1/prover-market/ProverMarket.sol
@@ -11,7 +11,9 @@ interface IProverMarket { }
 abstract contract ProverMarket is EssentialContract, IProverMarket {
     using SafeERC20 for IERC20;
 
-    event ProverAssigned(uint64 indexed batchId, address indexed prover, uint64 provingFee, uint32 numAssignments);
+    event ProverAssigned(
+        uint64 indexed batchId, address indexed prover, uint64 provingFee, uint32 assignmentId
+    );
     event ProverChanged(
         address indexed prevProver,
         uint64 prevProvingFee,
@@ -23,13 +25,14 @@ abstract contract ProverMarket is EssentialContract, IProverMarket {
     error NotCurrentProver();
     error InvalidThresholds();
     error InsufficientDeposit();
+    error TooEarly();
     error NotEnoughAssignments();
 
-    address public immutable feeToken;
     address public immutable inbox;
+    IERC20 public immutable taikoToken;
     uint256 public immutable biddingThreshold;
     uint256 public immutable provingThreshold;
-    uint256 public immutable minAssignmentsBeforeExisting;
+    uint256 public immutable minAssignmentCommitment;
 
     address internal prover;
     uint64 internal provingFee;
@@ -37,23 +40,24 @@ abstract contract ProverMarket is EssentialContract, IProverMarket {
     uint32 internal nextAssignmentId;
 
     constructor(
-        address _feeToken,
         address _inbox,
+        address _taikoToken,
         uint256 _biddingThreshold,
         uint256 _provingThreshold,
-        uint256 _minAssignmentsBeforeExisting
+        uint256 _minAssignmentCommitment
     )
-        nonZeroAddr(_feeToken)
         nonZeroAddr(_inbox)
+        nonZeroAddr(_taikoToken)
         nonZeroValue(_biddingThreshold)
         nonZeroValue(_provingThreshold)
+        EssentialContract(address(0))
     {
         require(_biddingThreshold > _provingThreshold, InvalidThresholds());
-        feeToken = _feeToken;
         inbox = _inbox;
+        taikoToken = IERC20(_taikoToken);
         biddingThreshold = _biddingThreshold;
         provingThreshold = _provingThreshold;
-        minAssignmentsBeforeExisting = _minAssignmentsBeforeExisting;
+        minAssignmentCommitment = _minAssignmentCommitment;
     }
 
     function bid(uint64 _provingFee) external {
@@ -67,7 +71,7 @@ abstract contract ProverMarket is EssentialContract, IProverMarket {
 
     function exit() external {
         require(msg.sender != prover, NotCurrentProver());
-        require(nextAssignmentId > minAssignmentsBeforeExisting, NotEnoughAssignments());
+        require(nextAssignmentId > minAssignmentCommitment, TooEarly());
         emit ProverChanged(prover, provingFee, address(0), 0);
         prover = address(0);
         provingFee = 0;
@@ -97,11 +101,13 @@ abstract contract ProverMarket is EssentialContract, IProverMarket {
 
     function deposit(uint256 _amount) external {
         deposits[msg.sender] += _amount;
+        taikoToken.safeTransferFrom(msg.sender, address(this), _amount);
     }
 
     function withdraw(uint256 _amount) external {
         require(msg.sender != prover, NotCurrentProver());
         deposits[msg.sender] -= _amount;
+        taikoToken.safeTransfer(msg.sender, _amount);
     }
 
     function _checkBiddingFee(uint64 _provingFee) internal virtual;
