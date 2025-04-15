9ddc3b1e820ab04973ebc831b7e4e08882729348
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2025-04-15 17:46:58 +0800

    Update packages/protocol/contracts/layer1/based/IProposeBatch.sol

--- packages/protocol/contracts/layer1/based/IProposeBatch.sol
+++ packages/protocol/contracts/layer1/based/IProposeBatch.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "./ITaikoInbox.sol";
 
 /// @title IProposeBatch
-/// @notice This interface defines the ProposeBatch function that is also part of the ITaikoInbox
+/// @notice This interface defines the v4ProposeBatch function that is also part of the ITaikoInbox
 /// interface.
 /// @custom:security-contact security@taiko.xyz
 interface IProposeBatch {
