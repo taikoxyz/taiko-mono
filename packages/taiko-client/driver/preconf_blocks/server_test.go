0d7616bdb00843237a09d0a869322ddae5bb98b8
Jeffery Walsh <cyberhorsey@gmail.com>
2025-04-16 12:23:13 -0700

    slotsperepoch on ebacon

--- packages/taiko-client/driver/preconf_blocks/server_test.go
+++ packages/taiko-client/driver/preconf_blocks/server_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/internal/testutils"
+	"github.com/taikoxyz/taiko-mono/packages/taiko-client/pkg/rpc"
 )
 
 type PreconfBlockAPIServerTestSuite struct {
@@ -73,6 +74,9 @@ func (s *PreconfBlockAPIServerTestSuite) TestCheckLookaheadHandover() {
 		s.T().Run(tt.name, func(t *testing.T) {
 			s.s.handoverSlots = tt.handoverSlots
 			s.s.lookahead = tt.lookahead
+			s.s.rpc.L1Beacon = &rpc.BeaconClient{
+				SlotsPerEpoch: 32,
+			}
 
 			s.Equal(tt.wantErr, s.s.checkLookaheadHandover(tt.feeRecipient, tt.slotInEpoch))
 		})
