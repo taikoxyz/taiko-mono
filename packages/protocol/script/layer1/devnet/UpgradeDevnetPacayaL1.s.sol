877f8e84de74edefa29582e9880fb266ef2ac853
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-18 05:37:59 +0300

    docs(protocol): update verifier names in `UpgradeDevnetPacayaL1` contract (#19289)

--- packages/protocol/script/layer1/devnet/UpgradeDevnetPacayaL1.s.sol
+++ packages/protocol/script/layer1/devnet/UpgradeDevnetPacayaL1.s.sol
@@ -212,7 +212,7 @@ contract UpgradeDevnetPacayaL1 is DeployCapability {
             new RiscZeroGroth16Verifier(ControlID.CONTROL_ROOT, ControlID.BN254_CONTROL_ID);
         register(rollupResolver, "risc0_groth16_verifier", address(risc0Groth16Verifier));
         risc0Verifier = deployProxy({
-            name: "risc0_verifier",
+            name: "risc0_reth_verifier",
             impl: address(new Risc0Verifier(l2ChainId, address(risc0Groth16Verifier))),
             data: abi.encodeCall(Risc0Verifier.init, (address(0))),
             registerTo: rollupResolver
@@ -222,7 +222,7 @@ contract UpgradeDevnetPacayaL1 is DeployCapability {
         SuccinctVerifier sp1RemoteVerifier = new SuccinctVerifier();
         register(rollupResolver, "sp1_remote_verifier", address(sp1RemoteVerifier));
         sp1Verifier = deployProxy({
-            name: "sp1_verifier",
+            name: "sp1_reth_verifier",
             impl: address(new SP1Verifier(l2ChainId, address(sp1RemoteVerifier))),
             data: abi.encodeCall(SP1Verifier.init, (address(0))),
             registerTo: rollupResolver
@@ -254,7 +254,7 @@ contract UpgradeDevnetPacayaL1 is DeployCapability {
         });
 
         sgxVerifier = deployProxy({
-            name: "sgx_verifier",
+            name: "sgx_reth_verifier",
             impl: address(new SgxVerifier(l2ChainId, taikoInbox, proofVerifier, automataProxy)),
             data: abi.encodeCall(SgxVerifier.init, (address(0))),
             registerTo: rollupResolver
