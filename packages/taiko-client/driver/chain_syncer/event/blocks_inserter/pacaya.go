5bbf5585b9861a8d7e5ddc838e3d28c45489535b
Jeffery Walsh <cyberhorsey@gmail.com>
2025-04-29 16:20:03 -0700

    .

--- packages/taiko-client/driver/chain_syncer/event/blocks_inserter/pacaya.go
+++ packages/taiko-client/driver/chain_syncer/event/blocks_inserter/pacaya.go
@@ -243,7 +243,7 @@ func (i *BlocksInserterPacaya) InsertPreconfBlocksFromExecutionPayloads(
 	for j, executableData := range executionPayloads {
 		header, err := i.insertPreconfBlockFromExecutionPayload(ctx, executableData)
 		if err != nil {
-			return nil, fmt.Errorf("failed to insert preconf block %v: %w", executableData.BlockNumber.String(), err)
+			return nil, fmt.Errorf("failed to insert preconf block %v: %w", executableData.BlockNumber, err)
 		}
 		log.Info(
 			"⏰ New preconfirmation L2 block inserted",
