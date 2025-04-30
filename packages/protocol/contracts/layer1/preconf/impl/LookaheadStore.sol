123d4b37b8543086f66401d1500e03c53215427a
Keszey Dániel <keszeyd@MacBook-Pro.local>
2025-04-30 09:31:51 +0200

    gas optimization and preconf fallback logic fix

--- packages/protocol/contracts/layer1/preconf/impl/LookaheadStore.sol
+++ packages/protocol/contracts/layer1/preconf/impl/LookaheadStore.sol
@@ -74,7 +74,7 @@ contract LookaheadStore is ILookaheadStore, EssentialContract {
         pure
         returns (bytes26)
     {
-        return _calculateLookaheadHash(_epochTimestamp, _lookaheadSlots);
+        return LibPreconfUtils.calculateLookaheadHash(_epochTimestamp, _lookaheadSlots);
     }
 
     /// @inheritdoc ILookaheadStore
@@ -117,7 +117,8 @@ contract LookaheadStore is ILookaheadStore, EssentialContract {
         if (_lookaheadPayloads.length == 0) {
             // The poster claims that the lookahead for the next epoch has no preconfers
             lookaheadSlots = new LookaheadSlot[](0);
-            lookaheadHash = _calculateLookaheadHash(_nextEpochTimestamp, lookaheadSlots);
+            lookaheadHash =
+                LibPreconfUtils.calculateLookaheadHash(_nextEpochTimestamp, lookaheadSlots);
         } else {
             lookaheadSlots = new LookaheadSlot[](_lookaheadPayloads.length);
 
@@ -152,7 +153,8 @@ contract LookaheadStore is ILookaheadStore, EssentialContract {
             );
 
             // Hash the lookahead slots and update the lookahead hash for next epoch
-            lookaheadHash = _calculateLookaheadHash(_nextEpochTimestamp, lookaheadSlots);
+            lookaheadHash =
+                LibPreconfUtils.calculateLookaheadHash(_nextEpochTimestamp, lookaheadSlots);
         }
 
         _setLookaheadHash(_nextEpochTimestamp, lookaheadHash);
@@ -278,16 +280,5 @@ contract LookaheadStore is ILookaheadStore, EssentialContract {
         returns (LookaheadHash storage)
     {
         return lookahead[_epochTimestamp % getConfig().lookaheadBufferSize];
-    }
-
-    function _calculateLookaheadHash(
-        uint256 _epochTimestamp,
-        LookaheadSlot[] memory _lookaheadSlots
-    )
-        internal
-        pure
-        returns (bytes26)
-    {
-        return bytes26(keccak256(abi.encode(_epochTimestamp, _lookaheadSlots)));
     }
 }
--- packages/protocol/contracts/layer1/preconf/impl/PreconfRouter2.sol
+++ packages/protocol/contracts/layer1/preconf/impl/PreconfRouter2.sol
@@ -22,6 +22,15 @@ contract PreconfRouter2 is IPreconfRouter2, EssentialContract {
 
     uint256[50] private __gap;
 
+    modifier onlyFromPreconferOrFallback() {
+        require(
+            msg.sender == fallbackPreconfer
+                || msg.sender == preconfWhitelist.getOperatorForCurrentEpoch(),
+            NotPreconferOrFallback()
+        );
+        _;
+    }
+
     constructor(
         address _resolver,
         address _lookaheadStore,
@@ -96,14 +105,7 @@ contract PreconfRouter2 is IPreconfRouter2, EssentialContract {
 
     // Internal functions ----------------------------------------------------------------------
 
-    function _validateWhitelistPreconfer() internal view {
-        address preconfer = preconfWhitelist.getOperatorForCurrentEpoch();
-        if (preconfer != address(0)) {
-            require(msg.sender == preconfer, NotPreconfer());
-        } else if (fallbackPreconfer != address(0)) {
-            require(msg.sender == fallbackPreconfer, NotFallbackPreconfer());
-        }
-    }
+    function _validateWhitelistPreconfer() internal view onlyFromPreconferOrFallback { }
 
     function _validateLookaheadPreconfer(ILookaheadStore.LookaheadSlot memory _lookaheadSlot)
         internal
@@ -137,7 +139,7 @@ contract PreconfRouter2 is IPreconfRouter2, EssentialContract {
     {
         // Validate the current lookahead data
         require(
-            lookaheadStore.calculateLookaheadHash(_epochTimestamp, _currentLookahead)
+            LibPreconfUtils.calculateLookaheadHash(_epochTimestamp, _currentLookahead)
                 == _currentLookaheadHash,
             InvalidCurrentLookahead()
         );
@@ -182,7 +184,7 @@ contract PreconfRouter2 is IPreconfRouter2, EssentialContract {
                 bytes26 previousLookaheadHash =
                     lookaheadStore.getLookaheadHash(previousEpochTimestamp);
                 require(
-                    lookaheadStore.calculateLookaheadHash(
+                    LibPreconfUtils.calculateLookaheadHash(
                         previousEpochTimestamp, _previousLookahead
                     ) == previousLookaheadHash,
                     InvalidPreviousLookahead()
@@ -213,11 +215,11 @@ contract PreconfRouter2 is IPreconfRouter2, EssentialContract {
         bytes26 _lookaheadHash
     )
         internal
-        view
+        pure
         returns (bool)
     {
         return _lookaheadHash
-            == lookaheadStore.calculateLookaheadHash(
+            == LibPreconfUtils.calculateLookaheadHash(
                 _epochTimestamp, new ILookaheadStore.LookaheadSlot[](0)
             );
     }
