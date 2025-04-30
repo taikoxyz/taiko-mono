9048a285b534738f76ecc50f0eab5b4324c175d8
Daniel Wang <dan@taikocha.in>
2025-04-30 17:14:24 +0800

    Update LookaheadStore.sol

--- packages/protocol/contracts/layer1/preconf/impl/LookaheadStore.sol
+++ packages/protocol/contracts/layer1/preconf/impl/LookaheadStore.sol
@@ -153,8 +153,7 @@ contract LookaheadStore is ILookaheadStore, EssentialContract {
 
             // Validate that the last slot timestamp is within the next epoch
             require(
-                lookaheadSlots[lookaheadSlots.length - 1].slotTimestamp
-                    <= _nextEpochTimestamp + LibPreconfConstants.SECONDS_IN_EPOCH,
+                prevSlotTimestamp <= _nextEpochTimestamp + LibPreconfConstants.SECONDS_IN_EPOCH,
                 InvalidLookaheadEpoch()
             );
         }
