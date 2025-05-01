568cadcfd27948aec1661b386422727a18da6fd1
Jeffery Walsh <cyberhorsey@gmail.com>
2025-05-01 09:11:02 -0700

    update the highest seen unsafe block via api call

--- packages/taiko-client/driver/preconf_blocks/api.go
+++ packages/taiko-client/driver/preconf_blocks/api.go
@@ -137,6 +137,12 @@ func (s *PreconfBlockAPIServer) BuildPreconfBlock(c echo.Context) error {
 
 	header := headers[0]
 
+	// If the block number is greater than the highest unsafe L2 payload block ID,
+	// update the highest unsafe L2 payload block ID.
+	if header.Number.Uint64() > s.highestUnsafeL2PayloadBlockID {
+		s.highestUnsafeL2PayloadBlockID = header.Number.Uint64()
+	}
+
 	// Propagate the preconfirmation block to the P2P network, if the current server
 	// connects to the P2P network.
 	if s.p2pNode != nil && !reflect2.IsNil(s.p2pSigner) {
