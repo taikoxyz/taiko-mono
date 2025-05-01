f99c185a0ede657e3ee27f2c91c8fc952a00a89e
Jeffery Walsh <cyberhorsey@gmail.com>
2025-05-01 08:51:44 -0700

    json param

--- packages/taiko-client/driver/preconf_blocks/api.go
+++ packages/taiko-client/driver/preconf_blocks/api.go
@@ -295,7 +295,7 @@ type Status struct {
 	// @param has received from the P2P network, if its zero, it means the current server has not received
 	// @param any preconfirmation block from the P2P network yet.
 	HighestUnsafeL2PayloadBlockID uint64 `json:"highestUnsafeL2PayloadBlockID"`
-	EndOfSequencingMarkerReceived bool
+	EndOfSequencingMarkerReceived bool   `json:"endOfSequencingMarkerReceived"`
 }
 
 // GetStatus returns the current status of the preconfirmation block server.
