183b62d50bbfd4046c1a28378ff6beda6257727d
Daniel Wang <dan@taiko.xyz>
2025-05-02 08:57:02 +0800

    IBlobRefRegistry

---
+++ packages/protocol/contracts/layer1/blobs/BlobRefRegistry.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+import "./IBlobRefRegistry.sol";
+
+/// @title BlobRefRegistry
+contract BlobRefRegistry is IBlobRefRegistry {
+    /// @dev A mapping of the hash of a blob ref to the timestamp when it was saved
+    mapping(bytes32 refHash => uint256 timestamp) private _registeredHashes;
+
+    /// @inheritdoc IBlobRefRegistry
+    function registerRef(uint256[] calldata blobIndices) external returns (bytes32 refHash, BlobRef memory ref) {
+        ref = _getRef(blobIndices);
+        refHash = _registerRefHash(ref);
+        emit Registered(refHash, ref);
+    }
+
+    /// @inheritdoc IBlobRefRegistry
+    function getRef(uint256[] calldata blobIndices) external view returns (BlobRef memory) {
+        return _getRef(blobIndices);
+    }
+
+    /// @inheritdoc IBlobRefRegistry
+    function isRefRegistered(bytes32 refHash) external view returns (bool) {
+        return _registeredHashes[refHash] != 0;
+    }
+
+    /// @dev Registers the hash of a blob ref to the registry
+    /// @param ref The blob ref whose hash to save
+    /// @return The hash of the blob source
+    function _registerRefHash(BlobRef memory ref) private returns (bytes32) {
+        bytes32 hash = keccak256(abi.encode(ref));
+        _registeredHashes[hash] = block.timestamp;
+        emit Registered(hash, ref);
+        return hash;
+    }
+
+    /// @dev Retrieves the blob ref for given blob indices
+    /// @param blobIndices The indices of the blobhashes to retrieve
+    /// @return The blob ref constructed from the block's number and the list of blob hashes
+    function _getRef(uint256[] calldata blobIndices) private view returns (BlobRef memory) {
+        uint256 nBlobs = blobIndices.length;
+        require(nBlobs != 0, "No blobs provided");
+
+        bytes32[] memory blobhashes = new bytes32[](nBlobs);
+        for (uint256 i; i < nBlobs; ++i) {
+            blobhashes[i] = blobhash(blobIndices[i]);
+            require(blobhashes[i] != 0, "Blob not found");
+        }
+
+        return BlobRef(block.number, blobhashes);
+    }
+}
---
+++ packages/protocol/contracts/layer1/blobs/IBlobRefRegistry.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+/// @title IBlobRefRegistry
+/// @notice Interface for accessing and registering blob hashes from transactions
+interface IBlobRefRegistry {
+    /// @dev Struct containing blob hashes from a specific block
+    /// @param blockNumber The block number where the blobs were included
+    /// @param blobs Array of blob hashes
+    struct BlobRef {
+        uint256 blockNumber;
+        bytes32[] blobhashes;
+    }
+
+    /// @notice Emitted when a blob ref hash is registered
+    /// @param refHash The keccak256 hash of the encoded blob ref
+    /// @param ref The blob ref
+    event Registered(bytes32 indexed refHash, BlobRef ref);
+
+    /// @notice Validates blobs at given indices, return a ref object, then register the ref hash for later usage
+    /// @param blobIndices Array of blob indices to retrieve
+    /// @return refHash The keccak256 hash of the encoded blob ref
+    /// @return ref The retrieved blob data including block number and blob hashes
+    /// @dev Should revert if any blob index is invalid or if no blobs are provided
+    function registerRef(uint256[] calldata blobIndices) external returns (bytes32 refHash, BlobRef memory ref);
+
+    /// @notice Validates blobs at given indices and return a ref object
+    /// @param blobIndices Array of blob indices to retrieve
+    /// @return The blob data including block number and blob hashes
+    /// @dev Should revert if any blob index is invalid or if no blobs are provided
+    function getRef(uint256[] calldata blobIndices) external view returns (BlobRef memory);
+
+    /// @notice Checks if a blob reference has been previously registered
+    /// @param refHash The keccak256 hash of the encoded blob ref
+    /// @return True if the blob ref hash exists in the registry, false otherwise
+    function isRefRegistered(bytes32 refHash) external view returns (bool);
+}
