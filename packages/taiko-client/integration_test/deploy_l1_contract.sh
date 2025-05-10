e324083891b6df91758a9a18a984c0063cd6e17f
David <david@taiko.xyz>
2025-05-10 12:19:52 +0800

    feat: update deployment scripts

--- packages/taiko-client/integration_test/deploy_l1_contract.sh
+++ packages/taiko-client/integration_test/deploy_l1_contract.sh
@@ -6,7 +6,7 @@ source scripts/common.sh
 source integration_test/l1_env.sh
 
 cd ${PACAYA_FORK_TAIKO_MONO}/packages/protocol &&
-  forge script script/layer1/DeployProtocolOnL1.s.sol:DeployProtocolOnL1 \
+  PRIVATE_KEY=$PRIVATE_KEY forge script script/layer1/based/DeployProtocolOnL1.s.sol:DeployProtocolOnL1 \
     --fork-url "$L1_HTTP" \
     --broadcast \
     --ffi \
