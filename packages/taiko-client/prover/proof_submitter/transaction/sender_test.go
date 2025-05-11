d1b0d2cbf9a993e811a86a405433581094df4752
David <david@taiko.xyz>
2025-05-11 23:12:09 +0800

    test: more sender tests

--- packages/taiko-client/prover/proof_submitter/transaction/sender_test.go
+++ packages/taiko-client/prover/proof_submitter/transaction/sender_test.go
@@ -1,6 +1,8 @@
 package transaction
 
 import (
+	"context"
+	"math/big"
 	"os"
 	"testing"
 	"time"
@@ -8,12 +10,16 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/txmgr"
 	"github.com/ethereum-optimism/optimism/op-service/txmgr/metrics"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/metadata"
+	pacayaBindings "github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/pacaya"
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/internal/testutils"
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/pkg/rpc"
+	producer "github.com/taikoxyz/taiko-mono/packages/taiko-client/prover/proof_producer"
 )
 
 type TransactionTestSuite struct {
@@ -56,6 +62,39 @@ func (s *TransactionTestSuite) SetupTest() {
 	s.Nil(err)
 
 	s.sender = NewSender(s.RPCClient, txmgr, txmgr, rpc.ZeroAddress, 0)
+}
+
+func (s *TransactionTestSuite) TestValidateProof() {
+	l1Head, err := s.RPCClient.L1.HeaderByNumber(context.Background(), nil)
+	s.Nil(err)
+
+	ts, err := s.RPCClient.GetLastVerifiedTransitionPacaya(context.Background())
+	s.Nil(err)
+
+	meta := metadata.NewTaikoDataBlockMetadataPacaya(
+		&pacayaBindings.TaikoInboxClientBatchProposed{
+			Meta: pacayaBindings.ITaikoInboxBatchMetadata{BatchId: 1},
+			Info: pacayaBindings.ITaikoInboxBatchInfo{LastBlockId: ts.BlockId + 1},
+			Raw: types.Log{
+				BlockNumber: l1Head.Number.Uint64(),
+				BlockHash:   l1Head.Hash(),
+			},
+		},
+	)
+
+	ok, err := s.sender.ValidateProof(
+		context.Background(),
+		&producer.ProofResponse{
+			BatchID:   common.Big1,
+			Meta:      meta,
+			Proof:     testutils.RandomBytes(100),
+			Opts:      &producer.ProofRequestOptionsPacaya{EventL1Hash: l1Head.Hash()},
+			ProofType: producer.ProofTypeOp,
+		},
+		new(big.Int).SetUint64(ts.BlockId),
+	)
+	s.Nil(err)
+	s.True(ok)
 }
 
 func TestTxSenderTestSuite(t *testing.T) {
