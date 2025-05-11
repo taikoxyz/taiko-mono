59a142729e71646763fb66a3f58b358df5f9561a
Daniel Wang <dan@taiko.xyz>
2025-05-11 14:00:45 +0800

    deploy

--- packages/protocol/script/layer1/mainnet/DeployTaikoDAOController.s.sol
+++ packages/protocol/script/layer1/mainnet/DeployTaikoDAOController.s.sol
@@ -5,12 +5,13 @@ import "src/layer1/governance/TaikoDAOController.sol";
 import "script/BaseScript.sol";
 
 contract DeployTaikoDAOController is BaseScript {
+    address private constant TAIKO_DAO = 0x9CDf589C941ee81D75F34d3755671d614f7cf261;
+
     function run() external broadcast {
-        address dao = vm.envAddress("TAIKO_DAO");
         deploy({
             name: "TaikoDAOController",
             impl: address(new TaikoDAOController()),
-            data: abi.encodeCall(TaikoDAOController.init, (dao))
+            data: abi.encodeCall(TaikoDAOController.init, (TAIKO_DAO))
         });
     }
 }
