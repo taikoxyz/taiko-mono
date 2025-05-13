61ab0c91a033932949d061573edeed8dc6fb4732
Daniel Wang <dan@taiko.xyz>
2025-05-13 09:18:13 +0800

    remove chain/bridge watch dogs from Bridge

--- packages/protocol/contracts/shared/libs/LibStrings.sol
+++ packages/protocol/contracts/shared/libs/LibStrings.sol
@@ -8,11 +8,9 @@ library LibStrings {
     bytes32 internal constant B_SGX_GETH_AUTOMATA = bytes32("sgx_geth_automata");
     bytes32 internal constant B_BOND_TOKEN = bytes32("bond_token");
     bytes32 internal constant B_BRIDGE = bytes32("bridge");
-    bytes32 internal constant B_BRIDGE_WATCHDOG = bytes32("bridge_watchdog");
     bytes32 internal constant B_BRIDGED_ERC1155 = bytes32("bridged_erc1155");
     bytes32 internal constant B_BRIDGED_ERC20 = bytes32("bridged_erc20");
     bytes32 internal constant B_BRIDGED_ERC721 = bytes32("bridged_erc721");
-    bytes32 internal constant B_CHAIN_WATCHDOG = bytes32("chain_watchdog");
     bytes32 internal constant B_ERC1155_VAULT = bytes32("erc1155_vault");
     bytes32 internal constant B_ERC20_VAULT = bytes32("erc20_vault");
     bytes32 internal constant B_ERC721_VAULT = bytes32("erc721_vault");
