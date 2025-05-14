23ccdf7ff8ccc0048658e546be1ba7ec1866f066
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-05-14 11:43:07 +0800

    chore(protocol): deploy mainnet ontake L1 with Pacaya height check

--- packages/protocol/contracts/layer1/based/TaikoData.sol
+++ packages/protocol/contracts/layer1/based/TaikoData.sol
@@ -7,7 +7,7 @@ import "src/shared/data/LibSharedData.sol";
 /// @notice This library defines various data structures used in the Taiko protocol.
 /// @custom:security-contact security@taiko.xyz
 library TaikoData {
-    uint256 public constant PACAYA_FORK_HEIGHT = 1_299_888;
+    uint256 public constant PACAYA_FORK_HEIGHT = 1_166_000;
         // / Mainnet Pacaya fork height is known
 
     /// @notice Struct holding Taiko configuration parameters. See {TaikoConfig}.
