64dc01d45e76cab50c904e65e97deb73465cb88b
Daniel Wang <dan@taikocha.in>
2025-05-16 18:40:31 +0800

    chore

--- packages/protocol/contracts/shared/tokenvault/ERC20VaultOriginal.sol
+++ packages/protocol/contracts/shared/tokenvault/ERC20VaultOriginal.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "../libs/LibNames.sol";
 import "../libs/LibAddress.sol";
 import "./IBridgedERC20.sol";
