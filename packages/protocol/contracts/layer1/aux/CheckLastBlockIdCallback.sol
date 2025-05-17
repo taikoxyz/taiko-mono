04a6c08bcf9c7b83aef87e81afc2ddacd972981d
Daniel Wang <dan@taikocha.in>
2025-05-17 09:45:23 +0800

    chore

--- packages/protocol/contracts/layer1/aux/CheckLastBlockIdCallback.sol
+++ packages/protocol/contracts/layer1/aux/CheckLastBlockIdCallback.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.24;
 import "contracts/layer1/based/IBatchProposedCallback.sol";
 
 /// @title CheckLastBlockIdCallback
+/// @notice This callback demonstrates the process of verifying the last block ID in a proposal. If
+/// there's a mismatch between the last block ID and the expected one, the callback will trigger a
+/// revert, subsequently causing the inbox to also revert.
+/// It's advisable for proposers to avoid transactions that may revert, hence this callback is more
+/// effective when used with revert protection.
 /// @custom:security-contact security@taiko.xyz
 contract CheckLastBlockIdCallback is IBatchProposedCallback {
     error LastBlockIdMismatch();
 
     /// @inheritdoc IBatchProposedCallback
-    /// @dev Allow proposers to specify the expected last block ID in a proposal.
     function onBatchProposed(
         ITaikoInbox.BatchInfo calldata _batchInfo,
         ITaikoInbox.BatchMetadata calldata,
