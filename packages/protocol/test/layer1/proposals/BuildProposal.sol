be51ba372dfaddecb62e57aac215b1687b96e61a
Daniel Wang <dan@taiko.xyz>
2025-05-21 17:54:38 +0800

    fix

--- packages/protocol/test/layer1/proposals/BuildProposal.sol
+++ packages/protocol/test/layer1/proposals/BuildProposal.sol
@@ -5,6 +5,7 @@ import "forge-std/src/Test.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "src/layer2/mainnet/DelegateController.sol";
+import "src/layer1/governance/TaikoDAOController.sol";
 import "test/shared/thirdparty/Multicall3.sol";
 import "src/shared/bridge/IBridge.sol";
 import { LibMainnetL1Addresses as L1 } from "src/layer1/mainnet/libs/LibMainnetL1Addresses.sol";
@@ -29,20 +30,19 @@ abstract contract BuildProposal is Test {
         });
     }
 
-    function buildProposal(uint64 executionId, uint32 gasLimit) internal pure {
+    function buildProposal(uint64 executionId, uint32 l2GasLimit) internal pure {
         Controller.Action[] memory l1Actions = buildL1Actions();
         Controller.Action[] memory allActions = new Controller.Action[](l1Actions.length + 1);
 
         for (uint256 i; i < l1Actions.length; ++i) {
             allActions[i] = l1Actions[i];
-            require(allActions[i].target == L1.DAO_CONTROLLER, "TARGET IS NOT_CONTROLLER");
         }
 
         Controller.Action[] memory l2Actions = buildL2Actions();
 
         IBridge.Message memory message;
         message.destChainId = 167_000;
-        message.gasLimit = gasLimit;
+        message.gasLimit = l2GasLimit;
         message.destOwner = L2.PERMISSIONLESS_EXECUTOR;
         message.data = abi.encodeCall(
             DelegateController.onMessageInvocation, (abi.encode(executionId, l2Actions))
@@ -54,16 +54,15 @@ abstract contract BuildProposal is Test {
             data: abi.encodeCall(IBridge.sendMessage, (message))
         });
 
-        for (uint256 i; i < allActions.length; ++i) {
-            console2.log("ACTION #", 1 + i, "==========================");
-            console2.log("target:", allActions[i].target);
-            if (allActions[i].value > 0) {
-                console2.log("value:", allActions[i].value);
-            }
-
-            console2.log("data:");
-            console2.logBytes(allActions[i].data);
-            console2.log("");
-        }
+        bytes memory callData = abi.encodeCall(
+            TaikoDAOController.execute, (allActions)
+        );
+    
+        console2.log("Num of L1 actions:", l1Actions.length);
+        console2.log("Num of L2 actions:", l2Actions.length);
+        console2.log("L2 gas limit:", l2GasLimit);
+        console2.log("Target:", L1.DAO_CONTROLLER);
+        console2.log("Data:");
+        console2.logBytes(callData);
     }
 }
