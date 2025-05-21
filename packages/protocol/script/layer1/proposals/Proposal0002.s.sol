f755ab17d0730dd73fa6df8720dfcb644dae2f84
Daniel Wang <dan@taiko.xyz>
2025-05-21 21:23:30 +0800

    Update Proposal0002.s.sol

--- packages/protocol/script/layer1/proposals/Proposal0002.s.sol
+++ packages/protocol/script/layer1/proposals/Proposal0002.s.sol
@@ -8,6 +8,7 @@ interface IBarContract {
     function withdraw(address _token, address _to, uint256 _amount) external;
 }
 
+// FOUNDRY_PROFILE=layer1 forge script script/layer1/proposals/Proposal0002.s.sol
 contract Proposal0002 is BuildProposal {
     // L1 contracts
     address public constant L1_DANIEL_WANG_ADDRESS = 0xf0A0d6Bd4aA94F53F3FB2c88488202a9E9eD2c55;
@@ -19,7 +20,6 @@ contract Proposal0002 is BuildProposal {
     address public constant L2_BAR_CONTRACT_NEW_IMPL = 0x8f752026dC3f53003C4772a81c7b38EA7430fECB;
     address public constant L2_DANIEL_WANG_ADDRESS = 0xf0A0d6Bd4aA94F53F3FB2c88488202a9E9eD2c55;
 
-    // FOUNDRY_PROFILE=layer1 forge script script/layer1/proposals/Proposal0002.s.sol
     function run() external pure {
         buildProposal({ executionId: 1, l2GasLimit: 25_000_000 });
     }
