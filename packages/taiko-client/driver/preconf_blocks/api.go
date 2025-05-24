e48535bdcc40704bd9a49d0edb40ab3934e0bd14
David <david@taiko.xyz>
2025-05-24 09:09:31 +0800

    feat: add same checks for `RemovePreconfBlocks`

--- packages/taiko-client/driver/preconf_blocks/api.go
+++ packages/taiko-client/driver/preconf_blocks/api.go
@@ -286,6 +286,22 @@ type RemovePreconfBlocksResponseBody struct {
 //		@Success		200	{object} RemovePreconfBlocksResponseBody
 //		@Router			/preconfBlocks [delete]
 func (s *PreconfBlockAPIServer) RemovePreconfBlocks(c echo.Context) error {
+	if s.rpc.PacayaClients.TaikoWrapper != nil {
+		// Check if the preconfirmation is enabled.
+		preconfRouter, err := s.rpc.GetPreconfRouterPacaya(&bind.CallOpts{Context: c.Request().Context()})
+		if err != nil {
+			return s.returnError(c, http.StatusInternalServerError, err)
+		}
+		if preconfRouter == rpc.ZeroAddress {
+			log.Warn("Preconfirmation is disabled via taikoWrapper", "preconfRouter", preconfRouter.Hex())
+			return s.returnError(
+				c,
+				http.StatusInternalServerError,
+				errors.New("preconfirmation is disabled via taikoWrapper"),
+			)
+		}
+	}
+
 	// Parse the request body.
 	reqBody := new(RemovePreconfBlocksRequestBody)
 	if err := c.Bind(reqBody); err != nil {
