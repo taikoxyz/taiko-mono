43981bc5973432bbdef76fcdeaab928adb6d89c7
Jeffery Walsh <cyberhorsey@gmail.com>
2025-05-23 21:44:09 -0700

    fix test

--- packages/taiko-client/driver/preconf_blocks/lookahead_test.go
+++ packages/taiko-client/driver/preconf_blocks/lookahead_test.go
@@ -46,7 +46,6 @@ func (s *PreconfBlockAPIServerTestSuite) TestLookheadSequencingWindowSplit() {
 	s.True(reflect.DeepEqual(currRanges,
 		[]SlotRange{
 			{Start: 0, End: 28},
-			{Start: 32, End: 60},
 		}), "currRanges = %v", currRanges)
 	s.True(reflect.DeepEqual(nextRanges, []SlotRange{{Start: 60, End: 64}}), "nextRanges = %v", nextRanges)
 }
