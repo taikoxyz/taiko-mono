a36bf9f1af98d29772277fbd27699c576c571a2e
Daniel Wang <dan@taiko.xyz>
2025-05-25 14:00:42 +0800

    revert

--- packages/protocol/script/layer1/mainnet/DeployTaikoDAOController.s.sol
+++ packages/protocol/script/layer1/mainnet/DeployTaikoDAOController.s.sol
@@ -2,20 +2,11 @@
 pragma solidity ^0.8.24;
 
 import "src/layer1/mainnet/TaikoDAOController.sol";
-import "src/layer1/mainnet/libs/LibL1Addrs.sol";
 import "script/BaseScript.sol";
 
 contract DeployTaikoDAOController is BaseScript {
     function run() external broadcast {
-        address taikoDaoControllerImpl3 = address(new TaikoDAOController());
-        console2.log("taikoDaoControllerImpl3:", taikoDaoControllerImpl3);
-        // 0x4347df63bdC82b8835fC9FF47bC5a71a12cC0f06
-
-        // 0x75Ba76403b13b26AD1beC70D6eE937314eeaCD0a
-        deploy({
-            name: "taiko_dao_controller2",
-            impl: taikoDaoControllerImpl3,
-            data: abi.encodeCall(TaikoDAOController.init, (LibL1Addrs.DAO))
-        });
+        address taikoDaoControllerImpl2 = address(new TaikoDAOController());
+        console2.log("taikoDaoControllerImpl2:", taikoDaoControllerImpl2);
     }
 }
--- packages/protocol/script/layer2/mainnet/DeployDelegateController.s.sol
+++ packages/protocol/script/layer2/mainnet/DeployDelegateController.s.sol
@@ -12,18 +12,7 @@ import { LibL2Addrs as L2 } from "src/layer2/mainnet/libs/LibL2Addrs.sol";
 //  forge script --rpc-url  https://rpc.mainnet.taiko.xyz script/DeployDelegateController.s.sol
 contract DeployDelegateController is BaseScript {
     function run() external broadcast {
-        // To verify the contract, run the following command:
-        // FOUNDRY_PROFILE=layer2 forge verify-contract \
-        // 0x6f4006D0f805B55D1106dFdDfb73C3D53d12c12D \
-        // contracts/layer2/mainnet/DelegateController.sol:DelegateController \
-        // --watch \
-        // --constructor-args
-        // 0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000001670000000000000000000000000000000000001000000000000000000000000fc3c4ca95a8c4e5a587373f1718cd91301d6b2d3
-        // \
-        // --verifier-url https://api.taikoscan.io/api \
-        // --etherscan-api-key (echo $ETHERSCAN_API_KEY)
-
-        address delegateControllerImpl4 = address(
+        address delegateControllerImpl1 = address(
             new DelegateController(
                 uint64(LibNetwork.ETHEREUM_MAINNET), L2.BRIDGE, L1.DAO_CONTROLLER
             )
@@ -31,7 +20,7 @@ contract DeployDelegateController is BaseScript {
 
         deploy({
             name: "delegate_controller",
-            impl: delegateControllerImpl4,
+            impl: delegateControllerImpl1,
             data: abi.encodeCall(DelegateController.init, ())
         });
     }
