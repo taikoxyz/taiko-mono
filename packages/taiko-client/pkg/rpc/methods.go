e76d61fa5241a7853d64ec083b2ac06ea4485d47
jeff <113397187+cyberhorsey@users.noreply.github.com>
2025-06-02 13:14:35 -0700

    fix(taiko-client): filter genesis block verified for hekla (#19576)
    Co-authored-by: Roger <50648015+RogerLamTd@users.noreply.github.com>

--- packages/taiko-client/pkg/rpc/methods.go
+++ packages/taiko-client/pkg/rpc/methods.go
@@ -20,6 +20,7 @@ import (
 	"golang.org/x/sync/errgroup"
 
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/encoding"
+	"github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/hekla"
 	ontakeBindings "github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/ontake"
 	pacayaBindings "github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/pacaya"
 	"github.com/taikoxyz/taiko-mono/packages/taiko-client/pkg/config"
@@ -69,6 +70,8 @@ func (c *Client) ensureGenesisMatched(ctx context.Context, taikoInbox common.Add
 		return err
 	}
 
+	log.Info("Genesis height", "height", genesisHeight, "nodeGenesisHash", nodeGenesis.Hash())
+
 	var (
 		l2GenesisHash common.Hash
 		filterOpts    = &bind.FilterOpts{Start: genesisHeight, End: &genesisHeight, Context: ctxWithTimeout}
@@ -79,26 +82,48 @@ func (c *Client) ensureGenesisMatched(ctx context.Context, taikoInbox common.Add
 		return err
 	}
 
-	// If chain actives ontake fork from genesis, we need to fetch the genesis block hash from `BlockVerifiedV2` event.
-	if protocolConfigs.ForkHeightsPacaya() == 0 {
-		// Fetch the genesis `BatchesVerified` event.
-		iter, err := c.PacayaClients.TaikoInbox.FilterBatchesVerified(filterOpts)
+	var chainIDHekla uint64 = 167009
+	// hekla has a specific block verified event that never made it to other chains.
+	// we need to check for it explicitly here.
+	if c.L2.ChainID.Uint64() == chainIDHekla {
+		event, err := hekla.FilterBlockVerifiedHekla(
+			ctx,
+			c.L1.EthClient(),
+			taikoInbox,
+			new(big.Int).SetUint64(filterOpts.Start),
+			new(big.Int).SetUint64(*filterOpts.End),
+		)
+
 		if err != nil {
 			return err
 		}
-		if iter.Next() {
-			l2GenesisHash = iter.Event.BlockHash
-		}
-		if iter.Error() != nil {
-			return iter.Error()
-		}
-	} else if protocolConfigs.ForkHeightsOntake() == 0 {
-		if l2GenesisHash, err = c.filterGenesisBlockVerifiedV2(ctx, filterOpts, taikoInbox); err != nil {
-			return err
-		}
+
+		l2GenesisHash = event[0].BlockHash
 	} else {
-		if l2GenesisHash, err = c.filterGenesisBlockVerified(ctx, filterOpts, taikoInbox); err != nil {
-			return err
+		// If chain actives ontake fork from genesis, we need to fetch the genesis block hash from `BlockVerifiedV2` event.
+		if protocolConfigs.ForkHeightsPacaya() == 0 {
+			// Fetch the genesis `BatchesVerified` event.
+			log.Info("Filtering batchesVerified events from TaikoInbox contract")
+			iter, err := c.PacayaClients.TaikoInbox.FilterBatchesVerified(filterOpts)
+			if err != nil {
+				return err
+			}
+			if iter.Next() {
+				l2GenesisHash = iter.Event.BlockHash
+			}
+			if iter.Error() != nil {
+				return iter.Error()
+			}
+		} else if protocolConfigs.ForkHeightsOntake() == 0 {
+			log.Info("Filtering blockVerifiedV2 events from TaikoInbox contract")
+			if l2GenesisHash, err = c.filterGenesisBlockVerifiedV2(ctx, filterOpts, taikoInbox); err != nil {
+				return err
+			}
+		} else {
+			log.Info("Filtering blockVerified events from TaikoInbox contract")
+			if l2GenesisHash, err = c.filterGenesisBlockVerified(ctx, filterOpts, taikoInbox); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -130,7 +155,7 @@ func (c *Client) filterGenesisBlockVerifiedV2(
 ) (common.Hash, error) {
 	client, err := ontakeBindings.NewTaikoL1Client(taikoInbox, c.L1)
 	if err != nil {
-		return common.Hash{}, fmt.Errorf("failed to create lagacy TaikoL1 client: %w", err)
+		return common.Hash{}, fmt.Errorf("failed to create legacy TaikoL1 client: %w", err)
 	}
 
 	// Fetch the genesis `BlockVerifiedV2` event.
@@ -145,7 +170,7 @@ func (c *Client) filterGenesisBlockVerifiedV2(
 		return common.Hash{}, iter.Error()
 	}
 
-	return common.Hash{}, fmt.Errorf("failed to find genesis block verified event")
+	return common.Hash{}, fmt.Errorf("failed to find genesis block verified V2 event")
 }
 
 // filterGenesisBlockVerified fetches the genesis block verified
