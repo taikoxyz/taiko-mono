dbb9ccaf1f1de14a8373c32f6e3bd7ec2a7457f4
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-06-06 14:58:17 +0800

    Revert "Merge branch 'main' into shasta-fork-client"
    This reverts commit 423a11381c8b73c360c69d109ce261e95616c579, reversing
    changes made to ead8af6d58bbf642ed49799cf463a10a3a89b1da.

--- packages/protocol/test/layer1/preconf/router/PreconfRouter.t.sol
+++ packages/protocol/test/layer1/preconf/router/PreconfRouter.t.sol
@@ -16,9 +16,8 @@ contract PreconfRouterTest is PreconfRouterTestBase {
         // Setup mock beacon for operator selection
         vm.chainId(1);
         uint256 epochOneStart = LibPreconfConstants.getGenesisTimestamp(block.chainid);
-        // Set current epoch to `RANDOMNESS_DELAY_EPOCHS` epochs after genesis
-        uint256 currentEpoch = epochOneStart
-            + LibPreconfConstants.RANDOMNESS_DELAY_EPOCHS * LibPreconfConstants.SECONDS_IN_EPOCH;
+        // Current epoch
+        uint256 epochTwoStart = epochOneStart + LibPreconfConstants.SECONDS_IN_EPOCH;
 
         _setupMockBeacon(epochOneStart, new MockBeaconBlockRoot());
 
@@ -45,8 +44,8 @@ contract PreconfRouterTest is PreconfRouterTestBase {
             proverAuth: ""
         });
 
-        // Warp to arbitrary slot in current epoch
-        vm.warp(currentEpoch + 2 * LibPreconfConstants.SECONDS_IN_SLOT);
+        // Warp to arbitrary slot in epoch 2
+        vm.warp(epochTwoStart + 2 * LibPreconfConstants.SECONDS_IN_SLOT);
 
         // Prank as Carol (selected operator) and propose blocks
         vm.prank(Carol);
@@ -67,12 +66,11 @@ contract PreconfRouterTest is PreconfRouterTestBase {
         vm.chainId(1);
         uint256 epochOneStart = LibPreconfConstants.getGenesisTimestamp(block.chainid);
         // Current epoch
-        uint256 currentEpoch = epochOneStart
-            + LibPreconfConstants.RANDOMNESS_DELAY_EPOCHS * LibPreconfConstants.SECONDS_IN_EPOCH;
+        uint256 epochTwoStart = epochOneStart + LibPreconfConstants.SECONDS_IN_EPOCH;
         _setupMockBeacon(epochOneStart, new MockBeaconBlockRoot());
 
-        // Warp to arbitrary slot in current epoch
-        vm.warp(currentEpoch + 2 * LibPreconfConstants.SECONDS_IN_SLOT);
+        // Warp to arbitrary slot in epoch 2
+        vm.warp(epochTwoStart + 2 * LibPreconfConstants.SECONDS_IN_SLOT);
 
         // Prank as David (not the selected operator) and propose blocks
         vm.prank(David);
@@ -91,8 +89,7 @@ contract PreconfRouterTest is PreconfRouterTestBase {
         vm.chainId(1);
         uint256 epochOneStart = LibPreconfConstants.getGenesisTimestamp(block.chainid);
         // Current epoch
-        uint256 currentEpoch = epochOneStart
-            + LibPreconfConstants.RANDOMNESS_DELAY_EPOCHS * LibPreconfConstants.SECONDS_IN_EPOCH;
+        uint256 epochTwoStart = epochOneStart + LibPreconfConstants.SECONDS_IN_EPOCH;
 
         _setupMockBeacon(epochOneStart, new MockBeaconBlockRoot());
 
@@ -119,8 +116,8 @@ contract PreconfRouterTest is PreconfRouterTestBase {
             proverAuth: ""
         });
 
-        // Warp to arbitrary slot in current epoch
-        vm.warp(currentEpoch + 2 * LibPreconfConstants.SECONDS_IN_SLOT);
+        // Warp to arbitrary slot in epoch 2
+        vm.warp(epochTwoStart + 2 * LibPreconfConstants.SECONDS_IN_SLOT);
 
         // Prank as Carol (selected operator) and propose blocks
         vm.prank(Carol);
