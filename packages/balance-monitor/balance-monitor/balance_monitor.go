116489c4326ae863aeb64c38fed4c17ce1d5801d
Jeffery Walsh <cyberhorsey@gmail.com>
2025-06-13 08:49:57 -0700

    unnecesary sleep, not passing a duration flag, not handling ticker channel correctly

--- packages/balance-monitor/balance-monitor/balance_monitor.go
+++ packages/balance-monitor/balance-monitor/balance_monitor.go
@@ -6,43 +6,25 @@ import (
 	"math"
 	"math/big"
 	"strings"
-	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/exp/slog"
 )
 
-type ethClient interface {
-	BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error)
-	CallContract(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error)
-	CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error)
-	PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error)
-	PendingNonceAt(ctx context.Context, account common.Address) (uint64, error)
-	EstimateGas(ctx context.Context, call ethereum.CallMsg) (uint64, error)
-	SendTransaction(ctx context.Context, tx *types.Transaction) error
-	FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error)
-	SubscribeFilterLogs(ctx context.Context, query ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error)
-	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
-	SuggestGasPrice(ctx context.Context) (*big.Int, error)
-	SuggestGasTipCap(ctx context.Context) (*big.Int, error)
-}
-
 type BalanceMonitor struct {
-	l1EthClient        ethClient
-	l2EthClient        ethClient
+	l1EthClient        *ethclient.Client
+	l2EthClient        *ethclient.Client
 	addresses          []common.Address
 	erc20Addresses     []common.Address
-	interval           int
-	wg                 *sync.WaitGroup
+	interval           time.Duration
 	erc20DecimalsCache map[common.Address]uint8
+	ctx                context.Context
 }
 
 // InitFromCli inits a new Indexer from command line or environment variables.
@@ -72,6 +54,7 @@ func InitFromConfig(ctx context.Context, b *BalanceMonitor, cfg *Config) (err er
 	b.erc20Addresses = cfg.ERC20Addresses
 	b.interval = cfg.Interval
 	b.erc20DecimalsCache = make(map[common.Address]uint8)
+	b.ctx = ctx
 
 	return nil
 }
@@ -81,38 +64,39 @@ func (b *BalanceMonitor) Name() string {
 }
 
 func (b *BalanceMonitor) Close(ctx context.Context) {
-	b.wg.Wait()
 }
 
 func (b *BalanceMonitor) Start() error {
 	slog.Info("hello from balance monitor")
 
-	ticker := time.NewTicker(time.Duration(b.interval) * time.Second)
+	ticker := time.NewTicker(b.interval)
 	defer ticker.Stop()
 
-	for range ticker.C {
-		for _, address := range b.addresses {
-			b.checkEthBalance(context.Background(), b.l1EthClient, l1EthBalanceGauge, "L1", address)
-			b.checkEthBalance(context.Background(), b.l2EthClient, l2EthBalanceGauge, "L2", address)
+	for {
+		select {
+		case <-b.ctx.Done():
+			slog.Info("Balance monitor context done, stopping...")
+			return nil
+		case <-ticker.C:
+			for _, address := range b.addresses {
+				b.checkEthBalance(context.Background(), b.l1EthClient, l1EthBalanceGauge, "L1", address)
+				b.checkEthBalance(context.Background(), b.l2EthClient, l2EthBalanceGauge, "L2", address)
 
-			// Check ERC-20 token balances
-			var balance float64 = 0
-			for _, tokenAddress := range b.erc20Addresses {
-				balance = balance + b.checkErc20Balance(context.Background(), b.l1EthClient, "L1", tokenAddress, address)
-				balance = balance + b.checkErc20Balance(context.Background(), b.l2EthClient, "L2", tokenAddress, address)
+				// Check ERC-20 token balances
+				var balance float64 = 0
+				for _, tokenAddress := range b.erc20Addresses {
+					balance = balance + b.checkErc20Balance(context.Background(), b.l1EthClient, "L1", tokenAddress, address)
+					balance = balance + b.checkErc20Balance(context.Background(), b.l2EthClient, "L2", tokenAddress, address)
 
+				}
+				l1Erc20BalanceGauge.WithLabelValues(address.Hex()).Set(balance)
+				slog.Info("ERC-20 Balance", "address", address.Hex(), "balance", balance)
 			}
-			l1Erc20BalanceGauge.WithLabelValues(address.Hex()).Set(balance)
-			slog.Info("ERC-20 Balance", "address", address.Hex(), "balance", balance)
-			// Add a 1 second sleep between address checks
-			time.Sleep(time.Second)
 		}
 	}
-
-	return nil
 }
 
-func (b *BalanceMonitor) checkEthBalance(ctx context.Context, client ethClient, gauge *prometheus.GaugeVec, clientLabel string, address common.Address) {
+func (b *BalanceMonitor) checkEthBalance(ctx context.Context, client *ethclient.Client, gauge *prometheus.GaugeVec, clientLabel string, address common.Address) {
 	balance, err := b.getEthBalance(ctx, client, address)
 	if err != nil {
 		slog.Warn(fmt.Sprintf("Failed to get %s ETH balance for address", clientLabel), "address", address.Hex(), "error", err)
@@ -123,7 +107,7 @@ func (b *BalanceMonitor) checkEthBalance(ctx context.Context, client ethClient, 
 	slog.Info(fmt.Sprintf("%s ETH Balance", clientLabel), "address", address.Hex(), "balance", balanceFloat)
 }
 
-func (b *BalanceMonitor) checkErc20Balance(ctx context.Context, client ethClient, clientLabel string, tokenAddress, holderAddress common.Address) float64 {
+func (b *BalanceMonitor) checkErc20Balance(ctx context.Context, client *ethclient.Client, clientLabel string, tokenAddress, holderAddress common.Address) float64 {
 	// Check the cache for the token decimals
 	tokenDecimals, ok := b.erc20DecimalsCache[tokenAddress]
 	if !ok {
@@ -167,7 +151,7 @@ type ERC20 interface {
 	BalanceOf(opts *bind.CallOpts, account common.Address) (*big.Int, error)
 }
 
-func (b *BalanceMonitor) getEthBalance(ctx context.Context, client ethClient, address common.Address) (*big.Int, error) {
+func (b *BalanceMonitor) getEthBalance(ctx context.Context, client *ethclient.Client, address common.Address) (*big.Int, error) {
 	balance, err := client.BalanceAt(ctx, address, nil)
 	if err != nil {
 		return nil, err
@@ -176,7 +160,7 @@ func (b *BalanceMonitor) getEthBalance(ctx context.Context, client ethClient, ad
 	return balance, nil
 }
 
-func (b *BalanceMonitor) getTokenBalance(ctx context.Context, client ethClient, tokenAddress, holderAddress common.Address, methodName string, abiDef string) (*big.Int, error) {
+func (b *BalanceMonitor) getTokenBalance(ctx context.Context, client *ethclient.Client, tokenAddress, holderAddress common.Address, methodName string, abiDef string) (*big.Int, error) {
 	parsedABI, err := abi.JSON(strings.NewReader(abiDef))
 	if err != nil {
 		return nil, err
@@ -205,15 +189,15 @@ func (b *BalanceMonitor) getTokenBalance(ctx context.Context, client ethClient, 
 	return balance, nil
 }
 
-func (b *BalanceMonitor) getErc20Balance(ctx context.Context, client ethClient, tokenAddress, holderAddress common.Address) (*big.Int, error) {
+func (b *BalanceMonitor) getErc20Balance(ctx context.Context, client *ethclient.Client, tokenAddress, holderAddress common.Address) (*big.Int, error) {
 	return b.getTokenBalance(ctx, client, tokenAddress, holderAddress, "balanceOf", erc20BalanceOfABI)
 }
 
-func (b *BalanceMonitor) getErc20BondBalance(ctx context.Context, client ethClient, tokenAddress, holderAddress common.Address) (*big.Int, error) {
+func (b *BalanceMonitor) getErc20BondBalance(ctx context.Context, client *ethclient.Client, tokenAddress, holderAddress common.Address) (*big.Int, error) {
 	return b.getTokenBalance(ctx, client, tokenAddress, holderAddress, "bondBalanceOf", erc20BondBalanceOfABI)
 }
 
-func (b *BalanceMonitor) getErc20Decimals(ctx context.Context, client ethClient, tokenAddress common.Address) (uint8, error) {
+func (b *BalanceMonitor) getErc20Decimals(ctx context.Context, client *ethclient.Client, tokenAddress common.Address) (uint8, error) {
 	parsedABI, err := abi.JSON(strings.NewReader(erc20BalanceOfABI))
 	if err != nil {
 		return 0, err
