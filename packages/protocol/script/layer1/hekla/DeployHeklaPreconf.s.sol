edf2041a593ccd754f01c45e6d948411b2faa1c4
Gavin Yu <gavin@taiko.xyz>
2025-06-17 23:17:24 +0800

    fix(protocol): re-use operator if consolidate was not called (#19627) (#19632)
    Co-authored-by: Daniel Wang <dan@taiko.xyz>
    Co-authored-by: dantaik <99078276+dantaik@users.noreply.github.com>
    Co-authored-by: RogerLamTd <bob.rogerbob1@gmail.com>
    Co-authored-by: YoGhurt111 <23009382+YoGhurt111@users.noreply.github.com>

--- packages/protocol/script/layer1/hekla/DeployHeklaPreconf.s.sol
+++ packages/protocol/script/layer1/hekla/DeployHeklaPreconf.s.sol
@@ -20,15 +20,15 @@ contract DeployHeklaPreconf is DeployCapability {
     }
 
     function run() external broadcast {
-        address  taikoWrapper = 0x8698690dEeDB923fA0A674D3f65896B0031BF7c9;
-        address  rollupResolver = 0x3C82907B5895DB9713A0BB874379eF8A37aA2A68;
-        address  taikoInbox = 0x79C9109b764609df928d16fC4a91e9081F7e87DB;
-        address  store = 0x54231533B8d8Ac2f4F9B05377B617EFA9be080Fd;
-        address  fallbackPreconfProposer = 0xD3f681bD6B49887A48cC9C9953720903967E9DC0;
-        address  proofVerifier = 0x9A919115127ed338C3bFBcdfBE72D4F167Fa9E1D;
-        address  taikoToken = 0x6490E12d480549D333499236fF2Ba6676C296011;
-        address  signalService = 0x6Fc2fe9D9dd0251ec5E0727e826Afbb0Db2CBe0D;
-        address  oldFork = 0x6e15d2049480C7E339C6B398774166e1ddbCd43e;
+        address taikoWrapper = 0x8698690dEeDB923fA0A674D3f65896B0031BF7c9;
+        address rollupResolver = 0x3C82907B5895DB9713A0BB874379eF8A37aA2A68;
+        address taikoInbox = 0x79C9109b764609df928d16fC4a91e9081F7e87DB;
+        address store = 0x54231533B8d8Ac2f4F9B05377B617EFA9be080Fd;
+        address fallbackPreconfProposer = 0xD3f681bD6B49887A48cC9C9953720903967E9DC0;
+        address proofVerifier = 0x9A919115127ed338C3bFBcdfBE72D4F167Fa9E1D;
+        address taikoToken = 0x6490E12d480549D333499236fF2Ba6676C296011;
+        address signalService = 0x6Fc2fe9D9dd0251ec5E0727e826Afbb0Db2CBe0D;
+        address oldFork = 0x6e15d2049480C7E339C6B398774166e1ddbCd43e;
 
         address whitelist = deployProxy({
             name: "preconf_whitelist",
@@ -45,7 +45,8 @@ contract DeployHeklaPreconf is DeployCapability {
         });
         address wrapper = address(new TaikoWrapper(taikoInbox, store, router));
         console2.log("taikoWrapper: ", wrapper);
-        address newFork = address(new HeklaInbox(taikoWrapper, proofVerifier, taikoToken, signalService));
+        address newFork =
+            address(new HeklaInbox(taikoWrapper, proofVerifier, taikoToken, signalService));
         address newRouter = address(new PacayaForkRouter(oldFork, newFork));
         console2.log("newRouterImpl: ", newRouter);
     }
