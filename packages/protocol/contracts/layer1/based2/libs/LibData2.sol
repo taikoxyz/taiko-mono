b2b1d402bf966de4ecaabd1a3e5760a08e52aca8
Daniel Wang <dan@taiko.xyz>
2025-06-28 18:54:49 +0800

    Update LibData2.sol

--- packages/protocol/contracts/layer1/based2/libs/LibData2.sol
+++ packages/protocol/contracts/layer1/based2/libs/LibData2.sol
@@ -5,17 +5,17 @@ import { ITaikoInbox2 as I } from "../ITaikoInbox2.sol";
 
 library LibData2 {
     function hashBatch(
-        uint256 batchId,
-        I.BatchMetadata memory meta
+        uint256 _batchId,
+        I.BatchMetadata memory _meta
     )
         internal
         pure
         returns (bytes32)
     {
-        bytes32 buildMetaHash = keccak256(abi.encode(meta.buildMeta));
-        bytes32 proposeMetaHash = keccak256(abi.encode(meta.proposeMeta));
-        bytes32 proveMetaHash = keccak256(abi.encode(meta.proveMeta));
-        bytes32 leftHash = keccak256(abi.encode(batchId, buildMetaHash));
+        bytes32 buildMetaHash = keccak256(abi.encode(_meta.buildMeta));
+        bytes32 proposeMetaHash = keccak256(abi.encode(_meta.proposeMeta));
+        bytes32 proveMetaHash = keccak256(abi.encode(_meta.proveMeta));
+        bytes32 leftHash = keccak256(abi.encode(_batchId, buildMetaHash));
         bytes32 rightHash = keccak256(abi.encode(proposeMetaHash, proveMetaHash));
         return keccak256(abi.encode(leftHash, rightHash));
     }
