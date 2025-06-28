1c935621eb4c25ec5597ca966ad8d20361f86f81
Daniel Wang <dan@taiko.xyz>
2025-06-28 23:34:32 +0800

    Update LibVerify2.sol

--- packages/protocol/contracts/layer1/based2/libs/LibVerify2.sol
+++ packages/protocol/contracts/layer1/based2/libs/LibVerify2.sol
@@ -89,25 +89,30 @@ library LibVerify2 {
     )
         private
         pure
-        returns (uint96 bondToReturn_)
+        returns (uint96)
     {
         unchecked {
             if (_tran.proofTiming == I.ProofTiming.InProvingWindow) {
                 // all liveness bond is returned to the prover, this is not a reward.
-                bondToReturn_ = _tran.livenessBond;
-                if (_isFirstTransition) bondToReturn_ += _tran.provabilityBond;
-            } else if (_tran.proofTiming == I.ProofTiming.InExtendedProvingWindow) {
+                return _isFirstTransition
+                    ? _tran.livenessBond + _tran.provabilityBond
+                    : _tran.livenessBond;
+            }
+
+            if (_tran.proofTiming == I.ProofTiming.InExtendedProvingWindow) {
                 // prover is rewarded with bondRewardPtcg% of the liveness bond.
-                bondToReturn_ = _tran.livenessBond * _conf.bondRewardPtcg / 100;
-                if (_isFirstTransition) bondToReturn_ += _tran.provabilityBond;
-            } else if (_tran.byAssignedProver) {
+                uint96 amount = _tran.livenessBond * _conf.bondRewardPtcg / 100;
+                return _isFirstTransition ? amount + _tran.provabilityBond : amount;
+            }
+
+            if (_tran.byAssignedProver) {
                 // The assigned prover gets back his liveness bond, and 100% provability
                 // bond. This allows him to user a higher gas price to submit his proof first.
-                bondToReturn_ = _tran.provabilityBond;
-            } else {
-                // Other prover get bondRewardPtcg% of the provability bond.
-                bondToReturn_ = _tran.provabilityBond * _conf.bondRewardPtcg / 100;
+                return _tran.provabilityBond;
             }
+
+            // Other prover get bondRewardPtcg% of the provability bond.
+            return _tran.provabilityBond * _conf.bondRewardPtcg / 100;
         }
     }
 
