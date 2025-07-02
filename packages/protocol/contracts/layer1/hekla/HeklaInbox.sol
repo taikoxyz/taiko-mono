ae6413ef70b9bdf2411d16b01379064388b5872c
Jeffery Walsh <cyberhorsey@gmail.com>
2025-07-02 13:48:20 -0700

    update sharing pctgs

--- packages/protocol/contracts/layer1/hekla/HeklaInbox.sol
+++ packages/protocol/contracts/layer1/hekla/HeklaInbox.sol
@@ -93,7 +93,7 @@ contract HeklaInbox is TaikoInbox {
             maxAnchorHeightOffset: 96,
             baseFeeConfig: LibSharedData.BaseFeeConfig({
                 adjustmentQuotient: 8,
-                sharingPctg: 50,
+                sharingPctg: 75,
                 gasIssuancePerSecond: 5_000_000,
                 minGasExcess: 1_344_899_430, // 0.01 gwei
                 maxGasIssuancePerBlock: 600_000_000 // two minutes
