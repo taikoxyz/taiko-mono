f22787ac3fdc89a52215f8ea49c18eaf29615eb3
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-08-07 13:01:27 +0800

    add chainBoundSequencer

--- packages/protocol/script/layer1/mainnet/DeployWLAndWrapper.s.sol
+++ packages/protocol/script/layer1/mainnet/DeployWLAndWrapper.s.sol
@@ -24,11 +24,12 @@ contract DeployWLAndWrapper is DeployCapability {
         address router = 0xD5AA0e20e8A6e9b04F080Cf8797410fafAa9688a;
         address rollupResolver = 0x5A982Fb1818c22744f5d7D36D0C4c9f61937b33a;
         address taikoToken = 0x10dea67478c5F8C5E2D90e5E9B26dBe60c54d800;
-        address sequencer = 0x5F62d006C10C009ff50C878Cd6157aC861C99990;
+        address taikoSequencer = 0x5F62d006C10C009ff50C878Cd6157aC861C99990;
         address ejector = 0x45D4403351Bc34283CE6450D91c099f40D06dA4e;
         address taikoWrapper = 0x9F9D2fC7abe74C79f86F0D1212107692430eef72;
         address whitelist = 0xFD019460881e6EeC632258222393d5821029b2ac;
         address fallbackProposer = 0x7A853a6480F4D7dB79AE91c16c960dBbB6710d25;
+        address chainBoundSequencer = 0x000cb000E880A92a8f383D69dA2142a969B93DE7;
 
 
     address whitelistImpl = address(new PreconfWhitelist());
@@ -40,8 +41,13 @@ contract DeployWLAndWrapper is DeployCapability {
         );
 
         console2.log(
-            "Add operator calldata: ",
-            abi.encodeCall(PreconfWhitelist.addOperator, (sequencer, sequencer))
+            "Add taiko operator calldata: ",
+            abi.encodeCall(PreconfWhitelist.addOperator, (taikoSequencer, taikoSequencer))
+        );
+
+        console2.log(
+            "Add chainbound operator calldata: ",
+            abi.encodeCall(PreconfWhitelist.addOperator, (chainBoundSequencer, chainBoundSequencer))
         );
 
         address router = address(new PreconfRouter(taikoWrapper, whitelist, fallbackProposer));
