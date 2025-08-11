d3521ecb172d1381f121d178310aea4e26367301
Jeffery Walsh <cyberhorsey@gmail.com>
2025-08-11 14:58:17 -0700

    re-use requests

--- packages/taiko-client/driver/preconf_blocks/server.go
+++ packages/taiko-client/driver/preconf_blocks/server.go
@@ -13,7 +13,6 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/p2p"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/beacon/engine"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -675,18 +674,8 @@ func (s *PreconfBlockAPIServer) ImportMissingAncientsFromCache(
 						s.blockRequestsCache.Add(currentPayload.Payload.ParentHash, struct{}{})
 					}
 				}
-				// Try get the highest block ID from the Pacaya protocol state variables.
-				stateVars, err := s.rpc.GetProtocolStateVariablesPacaya(&bind.CallOpts{Context: ctx})
-				if err != nil {
-					return err
-				}
 
-				batch, err := s.rpc.PacayaClients.TaikoInbox.GetBatch(&bind.CallOpts{Context: ctx}, stateVars.Stats2.NumBatches-1)
-				if err != nil {
-					return err
-				}
-
-				tip := batch.LastBlockId // last block ID proposed on chain
+				tip := progress.HighestOriginBlockID.Uint64()
 
 				if tip >= requestSyncMargin && parentNum <= tip-requestSyncMargin {
 					log.Debug("Skipping request for very old block",
