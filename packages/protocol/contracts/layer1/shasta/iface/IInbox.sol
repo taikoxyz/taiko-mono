ee9cc59ab568be225e76d5c0081e2ea667d61664
Gustavo Gonzalez <ggonzalezsomer@gmail.com>
2025-08-13 23:04:39 -0400

    fix(protocol): fix issue with prove function when aggregating claims (#19933)
    Co-authored-by: Gustavo Gonzalez <gustavo@Gustavos-MacBook-Pro.local>
    Co-authored-by: Daniel Wang <99078276+dantaik@users.noreply.github.com>
    Co-authored-by: Daniel Wang <dan@taiko.xyz>

--- packages/protocol/contracts/layer1/shasta/iface/IInbox.sol
+++ packages/protocol/contracts/layer1/shasta/iface/IInbox.sol
@@ -42,6 +42,8 @@ interface IInbox {
 
     /// @notice Represents a claim about the state transition of a proposal.
     struct Claim {
+        /// @notice The proposal's ID.
+        uint48 proposalId;
         /// @notice The proposal's hash.
         bytes32 proposalHash;
         /// @notice The parent claim's hash, this is used to link the claim to its parent claim to
@@ -93,10 +95,9 @@ interface IInbox {
     /// @param proposal The proposal that was proposed.
     event Proposed(Proposal proposal, CoreState coreState);
 
-    /// @notice Emitted when a proof is submitted for a proposal.
-    /// @param proposal The proposal that was proven.
+    /// @notice Emitted when a proof is submitted
     /// @param claimRecord The claim record containing the proof details.
-    event Proved(Proposal proposal, ClaimRecord claimRecord);
+    event Proved(ClaimRecord claimRecord);
 
     /// @notice Emitted when bond instructions are issued
     /// @param instructions The bond instructions that need to be performed.
