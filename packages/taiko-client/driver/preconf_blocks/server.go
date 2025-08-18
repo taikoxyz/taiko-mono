c3f600938ea73e3a5d6d2755ffd6b7f0ad5e8832
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-08-18 16:58:18 +0200

    feat(taiko-client): Add duration metrics to PreconfBlockApiServer methods
    Rationale: to help understanding whether the /preconfBlock endpoint is
    sometimes blocked by p2p methods

--- packages/taiko-client/driver/preconf_blocks/server.go
+++ packages/taiko-client/driver/preconf_blocks/server.go
@@ -218,6 +218,13 @@ func (s *PreconfBlockAPIServer) OnUnsafeL2Payload(
 ) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
+
+	start := time.Now()
+	defer func() {
+		elapsed := float64(time.Since(start).Milliseconds())
+		metrics.DriverPreconfOnUnsafeL2PayloadDuration.Observe(elapsed)
+	}()
+
 	// Ignore the message if it is from the current P2P node, when `from` is empty,
 	// it means the message is for importing the pending blocks from the cache after
 	// a new L2 EE chain has just finished a beacon-sync.
@@ -310,6 +317,12 @@ func (s *PreconfBlockAPIServer) OnUnsafeL2Response(
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
+	start := time.Now()
+	defer func() {
+		elapsed := float64(time.Since(start).Milliseconds())
+		metrics.DriverPreconfOnL2UnsafeResponseDuration.Observe(elapsed)
+	}()
+
 	// add responses seen to cache.
 	s.responseSeenCache.Add(msg.ExecutionPayload.BlockHash, time.Now().UTC())
 
@@ -397,6 +410,12 @@ func (s *PreconfBlockAPIServer) OnUnsafeL2Request(
 ) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
+
+	start := time.Now()
+	defer func() {
+		elapsed := float64(time.Since(start).Milliseconds())
+		metrics.DriverPreconfOnL2UnsafeRequestDuration.Observe(elapsed)
+	}()
 
 	// Ignore the message if it is from the current P2P node.
 	if from != "" && s.p2pNode.Host().ID() == from {
@@ -530,6 +549,12 @@ func (s *PreconfBlockAPIServer) OnUnsafeL2EndOfSequencingRequest(
 ) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
+
+	start := time.Now()
+	defer func() {
+		elapsed := float64(time.Since(start).Milliseconds())
+		metrics.DriverPreconfOnEndOfSequencingRequestDuration.Observe(elapsed)
+	}()
 
 	// Ignore the message if it is from the current P2P node.
 	if from != "" && s.p2pNode.Host().ID() == from {
