a509ba88e52ef5c2843e2d8b365dfcae32d7d627
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-08-19 15:42:28 +0800

    chore(taiko-client): add emoji to quickly identify logs

--- packages/taiko-client/driver/preconf_blocks/api.go
+++ packages/taiko-client/driver/preconf_blocks/api.go
@@ -140,7 +140,7 @@ func (s *PreconfBlockAPIServer) BuildPreconfBlock(c echo.Context) error {
 	}
 
 	log.Info(
-		"New preconfirmation block building request",
+		"🏗️New preconfirmation block building request",
 		"blockID", reqBody.ExecutableData.Number,
 		"coinbase", reqBody.ExecutableData.FeeRecipient.Hex(),
 		"timestamp", reqBody.ExecutableData.Timestamp,
