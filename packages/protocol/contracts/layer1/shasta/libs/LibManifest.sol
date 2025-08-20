2f8dbe2c1893e72eff4c73ee8e310ae9aa280535
Daniel Wang <dan@taiko.xyz>
2025-08-20 22:15:56 +0800

    Update LibManifest.sol

--- packages/protocol/contracts/layer1/shasta/libs/LibManifest.sol
+++ packages/protocol/contracts/layer1/shasta/libs/LibManifest.sol
@@ -18,10 +18,11 @@ library LibManifest {
     /// small as one second, 384 blocks will cover an Ethereum epoch.
     uint256 internal constant PROPOSAL_MAX_BLOCKS = 384;
 
-    /// @notice The maximum number of transactions allowed in a block in the manifest.
+    /// @notice Maximum number of transactions allowed in proposal's manifest data. This cap ensures
+    /// the cost of a worst-case prover attack is bounded.
     uint256 internal constant BLOCK_MAX_RAW_TRANSACTIONS = 4096 * 2;
 
-    /// @notice The maximum number of transactions allowed in the actual L2 block.
+    /// @notice Maximum number of transactions that can be included in an actual L2 block.
     uint256 internal constant BLOCK_MAX_TRANSACTIONS = 4096;
 
     /// @notice The maximum anchor block number offset from the proposal origin block number.
