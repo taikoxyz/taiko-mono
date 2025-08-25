bc84bd08b8475e17b23c356381d434d800fbeb49
Gustavo Gonzalez <gustavo@Gustavos-MacBook-Pro.local>
2025-08-25 15:29:30 -0700

    add encode functions to the base implementation

--- packages/protocol/contracts/layer1/shasta/impl/Inbox.sol
+++ packages/protocol/contracts/layer1/shasta/impl/Inbox.sol
@@ -285,6 +285,30 @@ abstract contract Inbox is EssentialContract, IInbox {
         return abi.encode(_payload);
     }
 
+    /// @dev Encodes the propose input data
+    /// @param _input The ProposeInput struct
+    /// @return The encoded data
+    function encodeProposeInput(ProposeInput memory _input)
+        external
+        pure
+        virtual
+        returns (bytes memory)
+    {
+        return abi.encode(_input);
+    }
+
+    /// @dev Encodes the prove input data
+    /// @param _input The ProveInput struct
+    /// @return The encoded data
+    function encodeProveInput(ProveInput memory _input)
+        external
+        pure
+        virtual
+        returns (bytes memory)
+    {
+        return abi.encode(_input);
+    }
+
     // ---------------------------------------------------------------
     // Internal Functions
     // ---------------------------------------------------------------
