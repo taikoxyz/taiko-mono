ab0c0183a4fa3658815edbb10596850d80b14d00
Gustavo Gonzalez <gustavo@Gustavos-MacBook-Pro.local>
2025-08-31 09:28:13 -0700

    polish initV2

--- packages/protocol/contracts/layer1/shasta/impl/Inbox.sol
+++ packages/protocol/contracts/layer1/shasta/impl/Inbox.sol
@@ -83,9 +83,13 @@ abstract contract Inbox is IInbox, IForcedInclusionStore, EssentialContract {
     constructor() EssentialContract() { }
 
     /// @notice Initializes the Inbox contract with genesis block
+    /// @dev This contract uses a reinitializer so that it works both on fresh deployments as well
+    /// as existing inbox proxies(i.e. mainnet)
+    /// @dev IMPORTANT: Make sure this function is called in the same tx as the deployment or
+    /// upgrade happens. On upgrades this is usually done calling `upgradeToAndCall`
     /// @param _owner The owner of this contract
     /// @param _genesisBlockHash The hash of the genesis block
-    function init(address _owner, bytes32 _genesisBlockHash) external reinitializer(3) {
+    function initV2(address _owner, bytes32 _genesisBlockHash) external reinitializer(2) {
         address owner = owner();
         require(owner == address(0) || owner == msg.sender, ACCESS_DENIED());
 
