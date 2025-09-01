72c1aa70ee29202b36ee8fc666f2a2b51abf01c3
Daniel Wang <dan@taiko.xyz>
2025-09-01 12:28:38 +0800

    Update MainnetShastaInbox.sol

--- packages/protocol/contracts/layer1/shasta/impl/MainnetShastaInbox.sol
+++ packages/protocol/contracts/layer1/shasta/impl/MainnetShastaInbox.sol
@@ -35,7 +35,6 @@ contract MainnetShastaInbox is InboxOptimized3 {
 
     /// @notice Gets the configuration for this Inbox contract
     /// @return _ The configuration struct with shasta-specific settings
-    // TODO: figure out these values
     function getConfig() public pure override returns (Config memory) {
         return Config({
             bondToken: address(0),
