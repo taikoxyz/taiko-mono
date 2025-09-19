c9260882152bebf266493bec62c743e3a6ae8fd5
Gustavo Gonzalez <gustavo@taiko.xyz>
2025-09-19 11:05:04 -0400

    initial verifiers

---
+++ packages/protocol/contracts/layer1/shasta/verifiers/compose/ShastaComposeVerifier.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "src/shared/common/EssentialContract.sol";
+import "src/shared/libs/LibNames.sol";
+import "../../iface/IProofVerifier.sol";
+
+/// @title ShastaComposeVerifier
+/// @notice This contract is an abstract verifier that composes multiple sub-verifiers to validate
+/// proofs.
+/// It ensures that a set of sub-proofs are verified by their respective verifiers before
+/// considering the overall proof as valid.
+/// @custom:security-contact security@taiko.xyz
+abstract contract ShastaComposeVerifier is EssentialContract, IProofVerifier {
+    uint256[50] private __gap;
+
+    struct SubProof {
+        address verifier;
+        bytes proof;
+    }
+
+    address public immutable taikoInbox;
+    /// The sgx/tdx-GethVerifier is the core verifier required in every proof.
+    /// All other proofs share its status root, despite different public inputs
+    /// due to different verification types.
+    /// proofs come from geth client
+    address public immutable sgxGethVerifier;
+    address public immutable tdxGethVerifier;
+    /// op for test purpose
+    address public immutable opVerifier;
+    /// proofs come from reth client
+    address public immutable sgxRethVerifier;
+    address public immutable risc0RethVerifier;
+    address public immutable sp1RethVerifier;
+
+    constructor(
+        address _sgxGethVerifier,
+        address _tdxGethVerifier,
+        address _opVerifier,
+        address _sgxRethVerifier,
+        address _risc0RethVerifier,
+        address _sp1RethVerifier
+    ) {
+        sgxGethVerifier = _sgxGethVerifier;
+        tdxGethVerifier = _tdxGethVerifier;
+        opVerifier = _opVerifier;
+        sgxRethVerifier = _sgxRethVerifier;
+        risc0RethVerifier = _risc0RethVerifier;
+        sp1RethVerifier = _sp1RethVerifier;
+    }
+
+    error CV_INVALID_SUB_VERIFIER();
+    error CV_INVALID_SUB_VERIFIER_ORDER();
+    error CV_VERIFIERS_INSUFFICIENT();
+
+    /// @notice Initializes the contract.
+    /// @param _owner The owner of this contract. msg.sender will be used if this value is zero.
+    function init(address _owner) external initializer {
+        __Essential_init(_owner);
+    }
+
+    /// @inheritdoc IProofVerifier
+    function verifyProof(
+        bytes32 _transitionsHash,
+        bytes calldata _proof
+    )
+        external view
+    {
+        SubProof[] memory subProofs = abi.decode(_proof, (SubProof[]));
+        uint256 size = subProofs.length;
+        address[] memory verifiers = new address[](size);
+
+        address verifier;
+
+        for (uint256 i; i < size; ++i) {
+            require(subProofs[i].verifier != address(0), CV_INVALID_SUB_VERIFIER());
+            require(subProofs[i].verifier > verifier, CV_INVALID_SUB_VERIFIER_ORDER());
+
+            verifier = subProofs[i].verifier;
+            IProofVerifier(verifier).verifyProof(_transitionsHash, subProofs[i].proof);
+
+            verifiers[i] = verifier;
+        }
+
+        require(areVerifiersSufficient(verifiers), CV_VERIFIERS_INSUFFICIENT());
+    }
+
+    function areVerifiersSufficient(address[] memory _verifiers)
+        internal
+        view
+        virtual
+        returns (bool);
+}
---
+++ packages/protocol/contracts/layer1/shasta/verifiers/compose/ShastaSgxAndZkVerifier.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./ShastaComposeVerifier.sol";
+
+/// @title SgxAndZkVerifier
+/// @notice SGX + (SP1 or Risc0) verifier
+/// @custom:security-contact security@taiko.xyz
+contract SgxAndZkVerifier is ShastaComposeVerifier {
+    uint256[50] private __gap;
+
+    constructor(
+        address _sgxRethVerifier,
+        address _risc0RethVerifier,
+        address _sp1RethVerifier
+    )
+        ShastaComposeVerifier(
+            address(0),
+            address(0),
+            address(0),
+            _sgxRethVerifier,
+            _risc0RethVerifier,
+            _sp1RethVerifier
+        )
+    { }
+
+    function areVerifiersSufficient(address[] memory _verifiers)
+        internal
+        view
+        override
+        returns (bool)
+    {
+        if (_verifiers.length != 2) return false;
+
+        if (_verifiers[0] == sgxRethVerifier) {
+            return _verifiers[1] == risc0RethVerifier || _verifiers[1] == sp1RethVerifier;
+        }
+
+        if (_verifiers[1] == sgxRethVerifier) {
+            return _verifiers[0] == risc0RethVerifier || _verifiers[0] == sp1RethVerifier;
+        }
+
+        return false;
+    }
+}
