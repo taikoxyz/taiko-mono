1ac027e4762b84899d8d003b528a2b90a5768f0d
Fibonacci747 <albertofibonacci12@gmail.com>
2025-09-20 10:54:46 +0200

    Update MainnetERC20Vault.sol

--- packages/protocol/contracts/layer1/mainnet/multirollup/MainnetERC20Vault.sol
+++ packages/protocol/contracts/layer1/mainnet/multirollup/MainnetERC20Vault.sol
@@ -8,7 +8,7 @@ import "../libs/LibFasterReentryLock.sol";
 /// @dev This contract shall be deployed to replace its parent contract on Ethereum for Taiko
 /// mainnet to reduce gas cost. In theory, the contract can also be deployed on Taiko L2 but this is
 /// not well testee nor necessary.
-/// @notice See the documentation in {ER20Vault}.
+/// @notice See the documentation in {ERC20Vault}.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetERC20Vault is ERC20Vault {
     constructor(address _resolver) ERC20Vault(_resolver) { }
