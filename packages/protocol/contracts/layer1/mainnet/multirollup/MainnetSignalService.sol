d6089a9918045ccc38bc9736876866681ca1ade5
Fibonacci747 <albertofibonacci12@gmail.com>
2025-09-20 10:53:54 +0200

    Update MainnetSignalService.sol

--- packages/protocol/contracts/layer1/mainnet/multirollup/MainnetSignalService.sol
+++ packages/protocol/contracts/layer1/mainnet/multirollup/MainnetSignalService.sol
@@ -7,7 +7,7 @@ import "../libs/LibFasterReentryLock.sol";
 /// @title MainnetSignalService
 /// @dev This contract shall be deployed to replace its parent contract on Ethereum for Taiko
 /// mainnet to reduce gas cost. In theory, the contract can also be deployed on Taiko L2 but this is
-/// not well testee nor necessary.
+/// not well tested nor necessary.
 /// @notice See the documentation in {SignalService}.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetSignalService is SignalService {
