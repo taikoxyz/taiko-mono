02ae4599881aba863d65f348c66b29e0557c185c
Fibonacci747 <albertofibonacci12@gmail.com>
2025-09-20 10:54:27 +0200

    Update MainnetERC721Vault.sol

--- packages/protocol/contracts/layer1/mainnet/multirollup/MainnetERC721Vault.sol
+++ packages/protocol/contracts/layer1/mainnet/multirollup/MainnetERC721Vault.sol
@@ -8,7 +8,7 @@ import "../libs/LibFasterReentryLock.sol";
 /// @dev This contract shall be deployed to replace its parent contract on Ethereum for Taiko
 /// mainnet to reduce gas cost. In theory, the contract can also be deployed on Taiko L2 but this is
 /// not well testee nor necessary.
-/// @notice See the documentation in {ER721Vault}.
+/// @notice See the documentation in {ERC721Vault}.
 /// @custom:security-contact security@taiko.xyz
 contract MainnetERC721Vault is ERC721Vault {
     constructor(address _resolver) ERC721Vault(_resolver) { }
