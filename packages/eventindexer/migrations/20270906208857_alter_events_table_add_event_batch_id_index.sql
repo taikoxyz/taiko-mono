a36ccea8bfbc80c32f83f794b077f820459bf018
Jeffery Walsh <cyberhorsey@gmail.com>
2025-09-22 08:11:34 -0700

    reuse container

--- packages/eventindexer/migrations/20270906208857_alter_events_table_add_event_batch_id_index.sql
+++ packages/eventindexer/migrations/20270906208857_alter_events_table_add_event_batch_id_index.sql
@@ -5,6 +5,6 @@ ALTER TABLE `events` ADD INDEX `event_event_batch_id_index` (`event`, `batch_id`
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE events 
-  DROP INDEX events_event_batch_id_index
+ALTER TABLE events
+  DROP INDEX event_event_batch_id_index
 -- +goose StatementEnd
