3339757bf8dfb12afa4ebf6c232934712d51a62d
Daniel Wang <dan@taiko.xyz>
2025-09-24 12:36:14 +0800

    works

---
+++ packages/protocol/test/layer1/shasta/libs/CodecSimpleFuzzTest.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { ICodec } from "src/layer1/shasta/iface/ICodec.sol";
+import { CodecSimple } from "src/layer1/shasta/impl/CodecSimple.sol";
+import { AbstractCodecFuzzTest } from "./AbstractCodecFuzzTest.t.sol";
+
+/// @title CodecSimpleFuzzTest
+/// @notice Fuzz test suite for CodecSimple (LibHashSimple) implementation
+contract CodecSimpleFuzzTest is AbstractCodecFuzzTest {
+    function _getCodec() internal override returns (ICodec) {
+        return ICodec(address(new CodecSimple()));
+    }
+}
