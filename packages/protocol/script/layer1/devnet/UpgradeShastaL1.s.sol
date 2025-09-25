f4281d77ab7bf71f0bb4cbaa4648616956290b47
Daniel Wang <dan@taiko.xyz>
2025-09-25 18:07:16 +0800

    Update UpgradeShastaL1.s.sol

--- packages/protocol/script/layer1/devnet/UpgradeShastaL1.s.sol
+++ packages/protocol/script/layer1/devnet/UpgradeShastaL1.s.sol
@@ -5,12 +5,11 @@ import "src/layer1/fork-router/ShastaForkRouter.sol";
 import "src/layer1/preconf/impl/PreconfWhitelist.sol";
 import "src/layer1/verifiers/compose/ComposeVerifier.sol";
 import "src/layer1/devnet/verifiers/DevnetVerifier.sol";
-import { Inbox } from "contracts/layer1/shasta/impl/Inbox.sol";
-import { DevnetShastaInbox } from "contracts/layer1/shasta/impl/DevnetShastaInbox.sol";
+import { ShastaDevnetInbox } from "contracts/layer1/shasta/impl/ShastaDevnetInbox.sol";
 import "test/shared/DeployCapability.sol";
 import "src/layer1/fork-router/PacayaForkRouter.sol";
 import "test/layer1/shasta/inbox/suite2/mocks/MockContracts.sol";
-import { InboxHelper } from "src/layer1/shasta/impl/InboxHelper.sol";
+import { CodecSimple } from "contracts/layer1/shasta/impl/CodecSimple.sol";
 
 contract UpgradeShastaL1 is DeployCapability {
     uint256 public privateKey = vm.envUint("PRIVATE_KEY");
@@ -46,16 +45,15 @@ contract UpgradeShastaL1 is DeployCapability {
         );
 
         address oldFork = PacayaForkRouter(inbox).newFork();
-        address helper = address(new InboxHelper());
+        address codec = address(new CodecSimple());
         address tempFork =
-            address(new DevnetShastaInbox(proofVerifier, whitelist, bondToken, helper));
+            address(new ShastaDevnetInbox(codec, proofVerifier, whitelist, bondToken));
 
         UUPSUpgradeable(inbox).upgradeTo({
             newImplementation: address(new ShastaForkRouter(oldFork, tempFork))
         });
 
-        address newFork =
-            address(new DevnetShastaInbox(proofVerifier, whitelist, bondToken, helper));
+        address newFork = address(new ShastaDevnetInbox(codec, proofVerifier, whitelist, bondToken));
 
         console2.log("  oldFork       :", oldFork);
         console2.log("  newFork       :", newFork);
