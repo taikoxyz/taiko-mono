1e07d46010d425c99615ce1d6f32a7bb099258a4
RogerLamTd <bob.rogerbob1@gmail.com>
2025-09-24 17:50:10 -0700

    link

--- packages/docs-site/src/content/config.ts
+++ packages/docs-site/src/content/config.ts
@@ -10,7 +10,7 @@ export const collections = {
           .max(160, { message: "Must be 160 characters or less." })
           .optional(),
         banner: z.object({ content: z.string() }).default({
-          content: 'Taiko Hekla will be sunsetting with the deprecation of the Holesky Testnet on September 30th, 2025. In it\'s place, we are deploying the Taiko Hoodi testnet with Ethereum Hoodi as L1. Please find network details <a href="{insert announcement link}">here</a>.',
+          content: 'Taiko Hekla will be sunsetting with the deprecation of the Holesky Testnet on September 30th, 2025. In it\'s place, we are deploying the Taiko Hoodi testnet with Ethereum Hoodi as L1. Please find network details <a href="{https://taiko.mirror.xyz/bmdcp5YfzInHvZonNp7WmVmiMqIJOCtVkmHdC8aANP0}">here</a>.',
           }),
       }),
     }),
