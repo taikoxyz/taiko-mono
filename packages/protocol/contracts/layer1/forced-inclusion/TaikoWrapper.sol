55facca1439f7242a99b91ccd6d81fe966011470
dantaik <99078276+dantaik@users.noreply.github.com>
2025-10-01 00:36:44 +0000

    Update protocol generated artifacts

--- packages/protocol/contracts/layer1/forced-inclusion/TaikoWrapper.sol
+++ packages/protocol/contracts/layer1/forced-inclusion/TaikoWrapper.sol
@@ -32,7 +32,6 @@ import "./ForcedInclusionStore.sol";
 ///
 /// @custom:security-contact security@taiko.xyz
 contract TaikoWrapper is EssentialContract, IProposeBatch {
-
     /// @dev Event emitted when a forced inclusion is processed.
     event ForcedInclusionProcessed(IForcedInclusionStore.ForcedInclusion);
 
