177cc6f9c6a936471be4292fdbc0e7d5d2152627
smtmfft <smtm@taiko.xyz>
2025-10-02 10:59:59 +0800

    update Proposal0004 to use abi.encodeCall for verifier actions

--- packages/protocol/script/layer1/proposals/Proposal0004.s.sol
+++ packages/protocol/script/layer1/proposals/Proposal0004.s.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.24;
 
 import "./BuildProposal.sol";
 import { LibL1Addrs as L1 } from "src/layer1/mainnet/libs/LibL1Addrs.sol";
+import "src/layer1/verifiers/TaikoSP1Verifier.sol";
+import "src/layer1/verifiers/TaikoRisc0Verifier.sol";
 
 // To print the proposal action data: `P=0004 pnpm proposal`
 // To dryrun the proposal on L1: `P=0004 pnpm proposal:dryrun:l1`
@@ -20,38 +22,57 @@ contract Proposal0004 is BuildProposal {
         actions[0] = Controller.Action({
             target: SP1_VERIFIER,
             value: 0,
-            data: hex"899e3b1a008f96447139673b3f2d29b30ad4b43fe6ccb3f31d40f6e61478ac5640201d9e0000000000000000000000000000000000000000000000000000000000000001"
+            data: abi.encodeCall(
+                TaikoSP1Verifier.setProgramTrusted,
+                (0x008f96447139673b3f2d29b30ad4b43fe6ccb3f31d40f6e61478ac5640201d9e, true)
+            )
         });
 
         actions[1] = Controller.Action({
             target: SP1_VERIFIER,
             value: 0,
-            data: hex"899e3b1a47cb22384e59cecf65a536612d4b43fe36659f987503db9828f158ac40201d9e0000000000000000000000000000000000000000000000000000000000000001"
+            data: abi.encodeCall(
+                TaikoSP1Verifier.setProgramTrusted,
+                (0x47cb22384e59cecf65a536612d4b43fe36659f987503db9828f158ac40201d9e, true)
+            )
         });
 
         actions[2] = Controller.Action({
             target: SP1_VERIFIER,
             value: 0,
-            data: hex"899e3b1a004775b86041915596830bbc5464584165b2641a277b6758e83723954946bee20000000000000000000000000000000000000000000000000000000000000001"
+            data: abi.encodeCall(
+                TaikoSP1Verifier.setProgramTrusted,
+                (0x004775b86041915596830bbc5464584165b2641a277b6758e83723954946bee2, true)
+            )
         });
 
         actions[3] = Controller.Action({
             target: SP1_VERIFIER,
             value: 0,
-            data: hex"899e3b1a23badc30106455655061778a464584162d9320d11ded9d63506e472a4946bee20000000000000000000000000000000000000000000000000000000000000001"
+            data: abi.encodeCall(
+                TaikoSP1Verifier.setProgramTrusted,
+                (0x23badc30106455655061778a464584162d9320d11ded9d63506e472a4946bee2, true)
+            )
         });
 
-        // Risc0 Verifier Actions (2 calls)
+        // Risc0 Verifier Actions (2 calls) - using raw hex because image IDs are 33 bytes (exceed
+        // bytes32 limit)
         actions[4] = Controller.Action({
             target: RISC0_VERIFIER,
             value: 0,
-            data: hex"ed7615813d933868e2ac698df98209b45e6c34c435df2d3c97754bb6739d541d5fd312e30000000000000000000000000000000000000000000000000000000000000001"
+            data: abi.encodeCall(
+                TaikoRisc0Verifier.setImageIdTrusted,
+                (0x3d933868e2ac698df98209b45e6c34c435df2d3c97754bb6739d541d5fd312e3, true)
+            )
         });
 
         actions[5] = Controller.Action({
             target: RISC0_VERIFIER,
             value: 0,
-            data: hex"ed761581326ce3b6f13708a0691ed4bc56e8c14d6ee4e1197c533c129b441e263350b87e0000000000000000000000000000000000000000000000000000000000000001"
+            data: abi.encodeCall(
+                TaikoRisc0Verifier.setImageIdTrusted,
+                (0x326ce3b6f13708a0691ed4bc56e8c14d6ee4e1197c533c129b441e263350b87e, true)
+            )
         });
 
         // Upgrade Risc0 Verifier to new implementation
