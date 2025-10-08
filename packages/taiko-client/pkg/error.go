5c2f0e8bddad3f661485029b60d35ff0bba55a3d
anim001k <140460766+anim001k@users.noreply.github.com>
2025-10-08 11:30:22 +0200

    Update error.go

--- packages/taiko-client/pkg/error.go
+++ packages/taiko-client/pkg/error.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	ErrBlobUsed           = errors.New("blob is used")
-	ErrBlobUnused         = errors.New("blob is not used")
+	ErrNoBlobHashes       = errors.New("no blob hashes provided")
 	ErrSidecarNotFound    = errors.New("sidecar not found")
 	ErrBeaconNotFound     = errors.New("beacon client not found")
 	ErrInvalidShastaBlobs = errors.New("invalid Shasta blobs")
