91b1e77ae53f1c4bbdb2af4cab160afbe58ee639
anim001k <140460766+anim001k@users.noreply.github.com>
2025-10-10 15:12:19 +0200

    Update getBlockTimestamp.ts

--- packages/bridge-ui/src/libs/util/getBlockTimestamp.ts
+++ packages/bridge-ui/src/libs/util/getBlockTimestamp.ts
@@ -3,7 +3,7 @@ import { getPublicClient } from '@wagmi/core';
 import { ClientError } from '$libs/error';
 import { config } from '$libs/wagmi';
 
-export const geBlockTimestamp = async (srcChainId: bigint, blockNumber: bigint) => {
+export const getBlockTimestamp = async (srcChainId: bigint, blockNumber: bigint) => {
   const client = getPublicClient(config, { chainId: Number(srcChainId) });
   if (!client) throw new ClientError('Client not found');
   const block = await client.getBlock({
