5306104f62579a37eacff326fa878fbc932d5152
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2025-10-24 09:11:18 +0800

    chore(protocol): add `MAX_BLOCK_GAS_LIMIT`

--- packages/protocol/contracts/layer1/core/libs/LibManifest.sol
+++ packages/protocol/contracts/layer1/core/libs/LibManifest.sol
@@ -28,6 +28,10 @@ library LibManifest {
     /// @dev This ensures block gas limit never drops below a critical threshold.
     uint256 internal constant MIN_BLOCK_GAS_LIMIT = 15_000_000;
 
+    /// @notice The maximum block gas limit.
+    /// @dev This ensures that the increased block gas limit never rises above the critical threshold.
+    uint256 internal constant MAX_BLOCK_GAS_LIMIT = 240_000_000;
+
     /// @notice The delay in processing bond instructions relative to the current proposal. A value
     /// of 1 signifies that the bond instructions of the immediate parent proposal will be
     /// processed.
