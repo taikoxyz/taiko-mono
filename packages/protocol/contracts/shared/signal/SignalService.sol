ca3e58eed1e723be0262963fcd99f7bb5c96f48c
dantaik <99078276+dantaik@users.noreply.github.com>
2025-10-28 13:55:40 +0000

    Update protocol generated artifacts

--- packages/protocol/contracts/shared/signal/SignalService.sol
+++ packages/protocol/contracts/shared/signal/SignalService.sol
@@ -316,3 +316,23 @@ error SS_SIGNAL_NOT_RECEIVED();
 //   _checkpoints                   | mapping(uint48 => struct SignalService.CheckpointRecord) | Slot: 254  | Offset: 0    | Bytes: 32
 //   __gap                          | uint256[44]                                        | Slot: 255  | Offset: 0    | Bytes: 1408
 // solhint-enable max-line-length
+
+// Storage Layout ---------------------------------------------------------------
+// solhint-disable max-line-length
+//
+//   _initialized                   | uint8                                              | Slot: 0    | Offset: 0    | Bytes: 1   
+//   _initializing                  | bool                                               | Slot: 0    | Offset: 1    | Bytes: 1   
+//   __gap                          | uint256[50]                                        | Slot: 1    | Offset: 0    | Bytes: 1600
+//   _owner                         | address                                            | Slot: 51   | Offset: 0    | Bytes: 20  
+//   __gap                          | uint256[49]                                        | Slot: 52   | Offset: 0    | Bytes: 1568
+//   _pendingOwner                  | address                                            | Slot: 101  | Offset: 0    | Bytes: 20  
+//   __gap                          | uint256[49]                                        | Slot: 102  | Offset: 0    | Bytes: 1568
+//   __gapFromOldAddressResolver    | uint256[50]                                        | Slot: 151  | Offset: 0    | Bytes: 1600
+//   __reentry                      | uint8                                              | Slot: 201  | Offset: 0    | Bytes: 1   
+//   __paused                       | uint8                                              | Slot: 201  | Offset: 1    | Bytes: 1   
+//   __gap                          | uint256[49]                                        | Slot: 202  | Offset: 0    | Bytes: 1568
+//   _slotsUsedByPacaya             | uint256[2]                                         | Slot: 251  | Offset: 0    | Bytes: 64  
+//   _receivedSignals               | mapping(bytes32 => bool)                           | Slot: 253  | Offset: 0    | Bytes: 32  
+//   _checkpoints                   | mapping(uint48 => struct SignalService.CheckpointRecord) | Slot: 254  | Offset: 0    | Bytes: 32  
+//   __gap                          | uint256[44]                                        | Slot: 255  | Offset: 0    | Bytes: 1408
+// solhint-enable max-line-length
