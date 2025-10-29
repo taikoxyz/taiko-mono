6f21798e285bcaaa2f145baae5ef6c097b6df91d
dantaik <99078276+dantaik@users.noreply.github.com>
2025-10-29 08:35:28 +0000

    Update protocol generated artifacts

--- packages/protocol/script/layer1/core/DeployTaikoToken.s.sol
+++ packages/protocol/script/layer1/core/DeployTaikoToken.s.sol
@@ -5,7 +5,6 @@ import "src/layer1/mainnet/TaikoToken.sol";
 import "test/shared/DeployCapability.sol";
 
 contract DeployTaikoToken is DeployCapability {
-
     modifier broadcast() {
         vm.startBroadcast();
         _;
@@ -13,12 +12,12 @@ contract DeployTaikoToken is DeployCapability {
     }
 
     function run() external broadcast {
-       address taikoToken = address(new TaikoToken());
-       console2.log("TaikoToken deployed:", taikoToken);
+        address taikoToken = address(new TaikoToken());
+        console2.log("TaikoToken deployed:", taikoToken);
     }
 }
 
 // FOUNDRY_PROFILE=layer1 forge script script/layer1/core/DeployTaikoToken.s.sol:DeployTaikoToken --chain-id 1 --rpc-url https://mainnet.infura.io/v3/29974e05282b45c89417014706857666 \
-    // --etherscan-api-key ZH85M18BZKJXSUT9RWFPB8JFIHYJ19E5ER --verifier etherscan  \
-    // --private-key 0x687d8a1aa66aef7f87c561f6c7c05260b9cbd39c34d7ba4d479f674810adc695 \
-    // --broadcast --verify
+// --etherscan-api-key ZH85M18BZKJXSUT9RWFPB8JFIHYJ19E5ER --verifier etherscan  \
+// --private-key 0x687d8a1aa66aef7f87c561f6c7c05260b9cbd39c34d7ba4d479f674810adc695 \
+// --broadcast --verify
