e62e2635d74adbec9dd28d5592ed18eaddd0f0e8
Daniel Wang <dan@taiko.xyz>
2025-10-31 11:48:03 +0800

    Update MainnetSignalService.sol

--- packages/protocol/contracts/layer1/mainnet/MainnetSignalService.sol
+++ packages/protocol/contracts/layer1/mainnet/MainnetSignalService.sol
@@ -11,27 +11,27 @@ import "./MainnetSignalService_Layout.sol"; // DO NOT DELETE
 /// mainnet to reduce gas cost.
 /// @notice See the documentation in {SignalService}.
 /// @custom:security-contact security@taiko.xyz
-contract MainnetSignalService is SignalService {
-    // ---------------------------------------------------------------
-    // Constructor
-    // ---------------------------------------------------------------
+contract MainnetSignalService {//is SignalService {
+    // // ---------------------------------------------------------------
+    // // Constructor
+    // // ---------------------------------------------------------------
 
-    constructor(
-        address authorizedSyncer,
-        address remoteSignalService
-    )
-        SignalService(authorizedSyncer, remoteSignalService)
-    { }
+    // constructor(
+    //     address authorizedSyncer,
+    //     address remoteSignalService
+    // )
+    //     SignalService(authorizedSyncer, remoteSignalService)
+    // { }
 
-    // ---------------------------------------------------------------
-    // Internal Functions
-    // ---------------------------------------------------------------
+    // // ---------------------------------------------------------------
+    // // Internal Functions
+    // // ---------------------------------------------------------------
 
-    function _storeReentryLock(uint8 _reentry) internal override {
-        LibFasterReentryLock.storeReentryLock(_reentry);
-    }
+    // function _storeReentryLock(uint8 _reentry) internal override {
+    //     LibFasterReentryLock.storeReentryLock(_reentry);
+    // }
 
-    function _loadReentryLock() internal view override returns (uint8) {
-        return LibFasterReentryLock.loadReentryLock();
-    }
+    // function _loadReentryLock() internal view override returns (uint8) {
+    //     return LibFasterReentryLock.loadReentryLock();
+    // }
 }
