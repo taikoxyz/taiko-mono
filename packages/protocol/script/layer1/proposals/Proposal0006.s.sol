3eb0c3d9adadb0db97d65c6d7af7c10ae2c4cf12
dantaik <99078276+dantaik@users.noreply.github.com>
2025-11-24 03:22:23 +0000

    Update protocol generated artifacts

--- packages/protocol/script/layer1/proposals/Proposal0006.s.sol
+++ packages/protocol/script/layer1/proposals/Proposal0006.s.sol
@@ -12,11 +12,11 @@ contract Proposal0006 is BuildProposal {
     // TODO: update these addresses after deployment.
     address public constant L1_SIGNAL_SERVICE_FORK_ROUTER =
         0x0000000000000000000000000000000000000000;
-    address public constant L1_PRECONF_WHITELIST_NEW_IMPL = 0x0000000000000000000000000000000000000000;
-    address public constant L2_SIGNAL_SERVICE_FORK_ROUTER=
+    address public constant L1_PRECONF_WHITELIST_NEW_IMPL =
         0x0000000000000000000000000000000000000000;
-    address public constant L2_ANCHOR_FORK_ROUTER=
+    address public constant L2_SIGNAL_SERVICE_FORK_ROUTER =
         0x0000000000000000000000000000000000000000;
+    address public constant L2_ANCHOR_FORK_ROUTER = 0x0000000000000000000000000000000000000000;
 
     function buildL1Actions() internal pure override returns (Controller.Action[] memory actions) {
         actions = new Controller.Action[](2);
@@ -37,8 +37,7 @@ contract Proposal0006 is BuildProposal {
         l2GasLimit = 1_500_000;
         actions = new Controller.Action[](2);
 
-        actions[0] =
-            buildUpgradeAction(L2.SIGNAL_SERVICE, L2_SIGNAL_SERVICE_FORK_ROUTER);
+        actions[0] = buildUpgradeAction(L2.SIGNAL_SERVICE, L2_SIGNAL_SERVICE_FORK_ROUTER);
         actions[1] = buildUpgradeAction(L2.ANCHOR, L2_ANCHOR_FORK_ROUTER);
     }
 }
