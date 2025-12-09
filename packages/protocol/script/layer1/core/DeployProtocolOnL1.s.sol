dfe6774a56dc73cd8e0822bf34c3b4e4f0485326
davidtaikocha <104078303+davidtaikocha@users.noreply.github.com>
2025-12-09 15:48:31 +0000

    Update protocol generated artifacts

--- packages/protocol/script/layer1/core/DeployProtocolOnL1.s.sol
+++ packages/protocol/script/layer1/core/DeployProtocolOnL1.s.sol
@@ -211,9 +211,8 @@ contract DeployProtocolOnL1 is DeployCapability {
         }
         console2.log("ShastaInbox deployed:", shastaInbox);
 
-        SignalService(signalService).upgradeTo(
-            address(new SignalService(shastaInbox, config.remoteSigSvc))
-        );
+        SignalService(signalService)
+            .upgradeTo(address(new SignalService(shastaInbox, config.remoteSigSvc)));
         console2.log("SignalService upgraded with Shasta inbox authorized syncer");
 
         if (config.contractOwner != msg.sender) {
@@ -284,9 +283,8 @@ contract DeployProtocolOnL1 is DeployCapability {
     }
 
     function _deployBridge(address sharedResolver, DeploymentConfig memory config) private {
-        address signalService = IResolver(sharedResolver).resolve(
-            uint64(block.chainid), LibNames.B_SIGNAL_SERVICE, false
-        );
+        address signalService = IResolver(sharedResolver)
+            .resolve(uint64(block.chainid), LibNames.B_SIGNAL_SERVICE, false);
 
         address bridge = deployProxy({
             name: "bridge",
@@ -351,7 +349,8 @@ contract DeployProtocolOnL1 is DeployCapability {
             name: "automata_dcap_attestation",
             impl: address(new AutomataDcapV3Attestation()),
             data: abi.encodeCall(
-                AutomataDcapV3Attestation.init, (owner, address(sigVerifyLib), address(pemCertChainLib))
+                AutomataDcapV3Attestation.init,
+                (owner, address(sigVerifyLib), address(pemCertChainLib))
             )
         });
         // Deploy sgx-geth automata attestation proxy
@@ -359,7 +358,8 @@ contract DeployProtocolOnL1 is DeployCapability {
             name: "sgx_geth_automata_dcap_attestation",
             impl: address(new AutomataDcapV3Attestation()),
             data: abi.encodeCall(
-                AutomataDcapV3Attestation.init, (owner, address(sigVerifyLib), address(pemCertChainLib))
+                AutomataDcapV3Attestation.init,
+                (owner, address(sigVerifyLib), address(pemCertChainLib))
             )
         });
     }
