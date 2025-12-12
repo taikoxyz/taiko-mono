adf39230e9a8a855e3a08348da2f8aecc9edac24
dantaik <99078276+dantaik@users.noreply.github.com>
2025-12-12 02:36:16 +0000

    Update protocol generated artifacts

--- packages/protocol/test/layer1/core/inbox/InboxWhitelistProver.t.sol
+++ packages/protocol/test/layer1/core/inbox/InboxWhitelistProver.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { InboxTestBase } from "./InboxTestBase.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { ICodec } from "src/layer1/core/iface/ICodec.sol";
 import { IInbox } from "src/layer1/core/iface/IInbox.sol";
 import { Codec } from "src/layer1/core/impl/Codec.sol";
