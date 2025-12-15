62b699ad9e8619a598c5f02c728a01cafcbf16ca
Irvin Gage <livapulishabeba@gmail.com>
2025-12-15 09:52:11 -0300

    chore(protocol): add missing constructor validation in SP1Verifier (#20834)
    Co-authored-by: Daniel Wang <99078276+dantaik@users.noreply.github.com>

--- packages/protocol/contracts/layer1/verifiers/SP1Verifier.sol
+++ packages/protocol/contracts/layer1/verifiers/SP1Verifier.sol
@@ -28,8 +28,12 @@ contract SP1Verifier is IProofVerifier, Ownable2Step {
     error SP1_INVALID_AGGREGATION_VKEY();
     error SP1_INVALID_PARAMS();
     error SP1_INVALID_PROOF();
+    error INVALID_CHAIN_ID();
+    error INVALID_SP1_REMOTE_VERIFIER();
 
     constructor(uint64 _taikoChainId, address _sp1RemoteVerifier, address _owner) {
+        require(_taikoChainId > 1, INVALID_CHAIN_ID());
+        require(_sp1RemoteVerifier != address(0), INVALID_SP1_REMOTE_VERIFIER());
         taikoChainId = _taikoChainId;
         sp1RemoteVerifier = _sp1RemoteVerifier;
 
