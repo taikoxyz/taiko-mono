db5aebcf1e337ae322f0ddcdd3dd65d9b9e78992
Daniel Wang <99078276+dantaik@users.noreply.github.com>
2026-01-06 20:17:35 +0800

    Update IProverAuction.sol

--- packages/protocol/contracts/layer1/core/iface/IProverAuction.sol
+++ packages/protocol/contracts/layer1/core/iface/IProverAuction.sol
@@ -14,6 +14,7 @@ interface IProverAuction {
         address indexed prover, uint128 slashed, address indexed recipient, uint128 rewarded
     );
 
+
     /// @notice Slash a prover for liveness failure.
     /// @param _proverAddr The prover to slash.
     /// @param _recipient The recipient of the slashed reward.
