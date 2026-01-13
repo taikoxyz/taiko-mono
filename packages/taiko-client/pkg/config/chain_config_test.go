78e1e25827cecbc55f7e281dd6f3e65a98cc9e7f
RogerLamTd <bob.rogerbob1@gmail.com>
2026-01-13 06:38:22 -0800

    taiko-client

---
+++ packages/taiko-client/pkg/config/chain_config_test.go
@@ -0,0 +1,19 @@
+package config
+
+import "testing"
+
+func TestNewChainConfig(t *testing.T) {
+
+}
+
+func TestIsOntake(t *testing.T) {
+
+}
+
+func TestIsPacaya(t *testing.T) {
+
+}
+
+func TestIsBlockForked(t *testing.T) {
+
+}
