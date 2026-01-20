e6161c45cdbb21b118682fd3815f16f938d96739
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2026-01-20 10:11:05 +0800

    upgrade l2

--- packages/protocol/script/layer1/core/UpgradeShastaL2Contracts.s.sol
+++ packages/protocol/script/layer1/core/UpgradeShastaL2Contracts.s.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {
-    SignalServiceForkRouter
-} from "../../../contracts/shared/signal/SignalServiceForkRouter.sol";
-import "src/layer1/preconf/impl/PreconfWhitelist.sol";
+import { DelegateController } from "../../../contracts/layer2/governance/DelegateController.sol";
 import "test/shared/DeployCapability.sol";
 
-contract UpgradeShastaContracts is DeployCapability {
+contract UpgradeShastaL2Contracts is DeployCapability {
     struct DeploymentConfig {
-        address preconfWhitelistProxy;
-        address preconfWhitelistImpl;
-        address proverWhitelistProxy;
+        address delegateController;
+        address l1Bridge;
+        address anchorProxy;
+        address anchorForkRouter;
         address signalServiceProxy;
-        address signalServiceForkRouterImpl;
+        address signalServiceForkRouter;
+        uint64 srcChainId;
+        uint64 destChainId;
     }
 
     modifier broadcast() {
@@ -26,26 +26,55 @@ contract UpgradeShastaContracts is DeployCapability {
 
     function run() external broadcast {
         DeploymentConfig memory config = _loadConfig();
-        PreconfWhitelist(config.preconfWhitelistProxy).upgradeTo(config.preconfWhitelistImpl);
-        Ownable2StepUpgradeable(config.proverWhitelistProxy).acceptOwnership();
-        SignalServiceForkRouter(config.signalServiceProxy)
-            .upgradeTo(config.signalServiceForkRouterImpl);
+        // TODO: We need to upgrade DelegateOwner to DelegateController on mainnet first.
+        Controller.Action[] memory dcall = new Controller.Action[](2);
+        dcall[0] = Controller.Action({
+            target: config.anchorProxy,
+            value: 0,
+            data: abi.encodeCall(Ownable2StepUpgradeable.upgradeTo, config.anchorForkRouter)
+        });
+        dcall[1] = Controller.Action({
+            target: config.signalServiceProxy,
+            value: 0,
+            data: abi.encodeCall(Ownable2StepUpgradeable.upgradeTo, config.signalServiceForkRouter)
+        });
+
+        IBridge.Message memory message = IBridge.Message({
+            id: 0,
+            fee: 0,
+            gasLimit: 5_000_000,
+            from: msg.sender,
+            srcChainId: config.srcChainId,
+            srcOwner: msg.sender,
+            destChainId: config.destChainId,
+            destOwner: config.delegateController,
+            to: config.delegateController,
+            value: 0,
+            data: abi.encodeCall(
+                DelegateController.onMessageInvocation,
+                (abi.encodePacked(uint64(0), abi.encode(dcall)))
+            )
+        });
+        IBridge(config.l1Bridge).sendMessage(message);
     }
 
     function _loadConfig() private view returns (DeploymentConfig memory config) {
-        config.preconfWhitelistProxy = vm.envAddress("PRECONF_WHITELIST_PROXY");
-        config.preconfWhitelistImpl = vm.envAddress("PRECONF_WHITELIST_IMPL");
-        config.proverWhitelistProxy = vm.envAddress("PROVER_WHITELIST_PROXY");
+        config.delegateController = vm.envAddress("DELEGATE_CONTROLLER");
+        config.l1Bridge = vm.envAddress("L1_BRIDGE");
+        config.anchorProxy = vm.envAddress("ANCHOR_PROXY");
+        config.anchorForkRouter = vm.envAddress("ANCHOR_FORK_ROUTER");
         config.signalServiceProxy = vm.envAddress("SIGNAL_SERVICE_PROXY");
-        config.signalServiceForkRouterImpl = vm.envAddress("SIGNAL_SERVICE_FORK_ROUTER_IMPL");
+        config.signalServiceForkRouter = vm.envAddress("SIGNAL_SERVICE_FORK_ROUTER");
+        config.srcChainId = uint64(vm.envUint("SRC_CHAIN_ID"));
+        config.destChainId = uint64(vm.envUint("DEST_CHAIN_ID"));
 
-        require(config.preconfWhitelistProxy != address(0), "PRECONF_WHITELIST_PROXY not set");
-        require(config.preconfWhitelistImpl != address(0), "PRECONF_WHITELIST_IMPL not set");
-        require(config.proverWhitelistProxy != address(0), "PROVER_WHITELIST_PROXY not set");
+        require(config.delegateController != address(0), "DELEGATE_CONTROLLER not set");
+        require(config.l1Bridge != address(0), "L1_BRIDGE not set");
+        require(config.anchorProxy != address(0), "ANCHOR_PROXY not set");
+        require(config.anchorForkRouter != address(0), "ANCHOR_FORK_ROUTER not set");
         require(config.signalServiceProxy != address(0), "SIGNAL_SERVICE_PROXY not set");
-        require(
-            config.signalServiceForkRouterImpl != address(0),
-            "SIGNAL_SERVICE_FORK_ROUTER_IMPL not set"
-        );
+        require(config.signalServiceForkRouter != address(0), "SIGNAL_SERVICE_FORK_ROUTER not set");
+        require(config.srcChainId != 0, "SRC_CHAIN_ID not set");
+        require(config.destChainId != 0, "DEST_CHAIN_ID not set");
     }
 }
