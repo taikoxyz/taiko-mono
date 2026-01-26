df150b768fd562a68040315433b7d312426fb5f3
Daniel Wang <dan@taiko.xyz>
2026-01-26 11:36:13 +0800

    Create Bridge2_ethMinter.t.sol

---
+++ packages/protocol/test/shared/bridge/Bridge2_ethMinter.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./TestBridge2Base.sol";
+import "src/shared/bridge/IEthMinter.sol";
+
+contract TestBridge2_ethMinter is TestBridge2Base {
+    function test_bridge2_setEthMinter_reverts_when_not_owner() public {
+        vm.prank(Alice);
+        vm.expectRevert("Ownable: caller is not the owner");
+        eBridge.setEthMinter(Alice, true);
+    }
+
+    function test_bridge2_setEthMinter_reverts_on_zero_address() public {
+        vm.expectRevert(EssentialContract.ZERO_ADDRESS.selector);
+        eBridge.setEthMinter(address(0), true);
+    }
+
+    function test_bridge2_setEthMinter_updates_state_and_emits() public {
+        assertEq(eBridge.isEthMinter(Alice), false);
+
+        vm.expectEmit(address(eBridge));
+        emit Bridge.EthMinterSet(Alice, true);
+        eBridge.setEthMinter(Alice, true);
+
+        assertEq(eBridge.isEthMinter(Alice), true);
+
+        vm.expectRevert(Bridge.B_INVALID_STATUS.selector);
+        eBridge.setEthMinter(Alice, true);
+
+        vm.expectEmit(address(eBridge));
+        emit Bridge.EthMinterSet(Alice, false);
+        eBridge.setEthMinter(Alice, false);
+
+        assertEq(eBridge.isEthMinter(Alice), false);
+
+        vm.expectRevert(Bridge.B_INVALID_STATUS.selector);
+        eBridge.setEthMinter(Alice, false);
+    }
+
+    function test_bridge2_mintEth_reverts_for_invalid_recipient() public {
+        eBridge.setEthMinter(Alice, true);
+
+        vm.startPrank(Alice);
+
+        vm.expectRevert(EssentialContract.ZERO_ADDRESS.selector);
+        eBridge.mintEth(address(0), 1 ether);
+
+        vm.expectRevert(Bridge.INVALID_MINT_RECIPIENT.selector);
+        eBridge.mintEth(address(eBridge), 1 ether);
+
+        vm.stopPrank();
+    }
+
+    function test_bridge2_mintEth_reverts_for_non_minter() public {
+        vm.prank(Bob);
+        vm.expectRevert(Bridge.B_INVALID_ETH_MINTER.selector);
+        eBridge.mintEth(Alice, 1 ether);
+    }
+
+    function test_bridge2_mintEth_transfers_and_emits() public assertSameTotalBalance {
+        uint256 amount = 2 ether;
+
+        eBridge.setEthMinter(Alice, true);
+
+        uint256 bridgeBalance = address(eBridge).balance;
+        uint256 bobBalance = Bob.balance;
+
+        vm.expectEmit(address(eBridge));
+        emit IEthMinter.EthMinted(Bob, amount);
+
+        vm.prank(Alice);
+        eBridge.mintEth(Bob, amount);
+
+        assertEq(address(eBridge).balance, bridgeBalance - amount);
+        assertEq(Bob.balance, bobBalance + amount);
+    }
+}
