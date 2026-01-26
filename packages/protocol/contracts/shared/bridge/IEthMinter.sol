9b6291b989e46812dab9df776f2ffa8f23a70175
Daniel Wang <dan@taiko.xyz>
2026-01-26 11:22:59 +0800

    add mintEth to Bridge

---
+++ packages/protocol/contracts/shared/bridge/IEthMinter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/// @title IEthMinter
+/// @custom:security-contact security@taiko.xyz
+interface IEthMinter {
+    /// @notice Emitted when ETH is minted.
+    /// @param recipient The address of the recipient.
+    /// @param amount The amount of ETH minted.
+    event EthMinted(address indexed recipient, uint256 amount);
+
+    /// @notice Mints ETH to the recipient.
+    /// @param _recipient The address of the recipient.
+    /// @param _amount The amount of ETH to mint.   
+    function mintEth(address _recipient, uint256 _amount) external;
+}
