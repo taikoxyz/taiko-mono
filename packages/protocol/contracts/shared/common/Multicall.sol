969b3de047f3ceb74f97f2fc7a149bea92a512e4
AnshuJalan <anshujalan206@gmail.com>
2026-01-30 15:08:22 +0530

    fix: nits

--- packages/protocol/contracts/shared/common/Multicall.sol
+++ packages/protocol/contracts/shared/common/Multicall.sol
@@ -8,12 +8,14 @@ contract Multicall {
         bytes data;
     }
 
+    error MULTICALL_CALL_FAILED(uint256 index, bytes returnData);
+
     function multicall(Call[] calldata calls) external payable returns (bytes[] memory results) {
         results = new bytes[](calls.length);
         for (uint256 i = 0; i < calls.length; i++) {
             (bool success, bytes memory result) =
                 calls[i].target.call{ value: calls[i].value }(calls[i].data);
-            require(success, "Multicall: call failed");
+            if (!success) revert MULTICALL_CALL_FAILED(i, result);
             results[i] = result;
         }
     }
