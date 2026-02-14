d6d67ecd2eee992648aaae8c5f296da700327d97
Maciej Skrzypkowski <mskr@gmx.com>
2026-02-14 02:11:44 +0100

    chore(taiko-client-rs): several improvments for preconfirmation-driver (#21330)

--- packages/taiko-client-rs/crates/protocol/src/preconfirmation/lookahead/mod.rs
+++ packages/taiko-client-rs/crates/protocol/src/preconfirmation/lookahead/mod.rs
@@ -37,6 +37,12 @@ pub enum LookaheadError {
     /// Failure when querying the LookaheadStore.
     #[error("failed to call lookahead store: {0}")]
     Lookahead(alloy_contract::Error),
+    /// Failed to fetch lookahead store config from the LookaheadStore.
+    #[error("failed to get lookahead store config: {0}")]
+    GetLookaheadStoreConfig(alloy_contract::Error),
+    /// Failed to fetch preconf whitelist address from the LookaheadStore.
+    #[error("failed to get preconf whitelist address: {0}")]
+    GetPreconfWhitelistAddress(alloy_contract::Error),
     /// Failure when querying the preconfirmation whitelist.
     #[error("failed to call preconf whitelist: {0}")]
     PreconfWhitelist(alloy_contract::Error),
