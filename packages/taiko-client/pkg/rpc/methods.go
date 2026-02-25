29e73dfcd3ec0db756f6824a50d503e91627c4cd
HenmaSab0 <henmasabry@gmail.com>
2026-02-25 09:19:21 +0800

    fix(taiko-client): close event filter iterators

--- packages/taiko-client/pkg/rpc/methods.go
+++ packages/taiko-client/pkg/rpc/methods.go
@@ -121,6 +121,7 @@ func (c *Client) ensureGenesisMatched(
 		if err != nil {
 			return err
 		}
+		defer iter.Close()
 		if iter.Next() {
 			l2GenesisHash = iter.Event.BlockHash
 		}
@@ -175,6 +176,7 @@ func (c *Client) filterGenesisBlockVerifiedV2(
 	if err != nil {
 		return common.Hash{}, err
 	}
+	defer iter.Close()
 	if iter.Next() {
 		return iter.Event.BlockHash, nil
 	}
@@ -202,6 +204,7 @@ func (c *Client) filterGenesisBlockVerified(
 	if err != nil {
 		return common.Hash{}, err
 	}
+	defer iter.Close()
 	if iter.Next() {
 		return iter.Event.BlockHash, nil
 	}
@@ -1554,6 +1557,7 @@ func (c *Client) GetProposalByIDShasta(
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to filter proposed events from Shasta Inbox: %w", err)
 	}
+	defer iter.Close()
 
 	var (
 		event *shastaBindings.ShastaInboxClientProposed
