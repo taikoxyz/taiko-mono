e5f0ccc7943781dc2bbd75f9251771ccb0ecdaf7
Karim <karim@taiko.xyz>
2026-03-02 13:49:11 +0100

    test(relayer): use port-based mysql readiness check

--- packages/relayer/pkg/repo/containers_test.go
+++ packages/relayer/pkg/repo/containers_test.go
@@ -30,7 +30,7 @@ func testMysql(t *testing.T) (db.DB, func(), error) {
 			"MYSQL_ROOT_PASSWORD": dbPassword,
 			"MYSQL_DATABASE":      dbName,
 		},
-		WaitingFor: wait.ForLog("port: 3306  MySQL Community Server - GPL").WithStartupTimeout(2 * time.Minute),
+		WaitingFor: wait.ForListeningPort("3306/tcp").WithStartupTimeout(2 * time.Minute),
 	}
 
 	ctx := context.Background()
