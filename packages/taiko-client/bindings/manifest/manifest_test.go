29acbd86b0d3c6c091385d1e67d026be3ddfb3c1
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2026-03-04 22:04:35 +0800

    adding test cases for nil/unknown

--- packages/taiko-client/bindings/manifest/manifest_test.go
+++ packages/taiko-client/bindings/manifest/manifest_test.go
@@ -1,6 +1,7 @@
 package manifest
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/params"
@@ -10,9 +11,13 @@ import (
 func TestAnchorMaxOffsetByChainID(t *testing.T) {
 	require.Equal(t, uint64(128), AnchorMaxOffsetByChainID(params.TaikoHoodiNetworkID))
 	require.Equal(t, uint64(512), AnchorMaxOffsetByChainID(params.TaikoMainnetNetworkID))
+	require.Equal(t, uint64(128), AnchorMaxOffsetByChainID(nil))
+	require.Equal(t, uint64(128), AnchorMaxOffsetByChainID(big.NewInt(12345))) // unknown chain
 }
 
 func TestTimestampMaxOffsetByChainID(t *testing.T) {
 	require.Equal(t, uint64(12*128), TimestampMaxOffsetByChainID(params.TaikoHoodiNetworkID))
 	require.Equal(t, uint64(12*512), TimestampMaxOffsetByChainID(params.TaikoMainnetNetworkID))
+	require.Equal(t, uint64(12*128), TimestampMaxOffsetByChainID(nil))
+	require.Equal(t, uint64(12*128), TimestampMaxOffsetByChainID(big.NewInt(12345))) // unknown chain
 }
