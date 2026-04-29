cda4e46b75c425c97aee15c44b85d0b934e27409
Maciej Skrzypkowski <mskr@gmx.com>
2026-04-29 10:17:47 +0200

    Updated Cargo.lock security related dependencies

--- packages/taiko-client-rs/crates/whitelist-preconfirmation-driver/src/network/discovery.rs
+++ packages/taiko-client-rs/crates/whitelist-preconfirmation-driver/src/network/discovery.rs
@@ -141,7 +141,7 @@ async fn spawn_discv5(
         .build(&key)
         .map_err(WhitelistPreconfirmationDriverError::p2p)?;
     let mut discv5 =
-        discv5::Discv5::<discv5::DefaultProtocolId>::new(local_enr, key, discv5_config)
+        discv5::Discv5::new(local_enr, key, discv5_config)
             .map_err(WhitelistPreconfirmationDriverError::p2p)?;
 
     for enr in &boot_enrs {
